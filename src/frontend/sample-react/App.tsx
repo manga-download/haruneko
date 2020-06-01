7c57f055ef49799d32ba2704811a843e92c861cf
ronny1982 <wegener.ronny@gmail.com>
2020-06-01 19:27:11 +0200

    completed react button sample

--- src/frontend/sample-react/App.tsx
+++ src/frontend/sample-react/App.tsx
@@ -1,12 +1,25 @@
 import * as React from 'react'
 
+const styles = {
+    container: {
+        padding: '2em',
+        textAlign: 'center' as const
+    },
+    button: {
+        padding: '0.5em'
+    }
+}
+
 export default function App() {
 
     const [ count, setCount ] = React.useState(0);
 
     return (
-        <div id="container">
+        <div style={styles.container}>
             <h3>Hello React Button</h3>
+            <button onClick={() => setCount(count + 1)} style={styles.button}>
+                Clicked: {count}
+            </button>
         </div>
     );
 }
