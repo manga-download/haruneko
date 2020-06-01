356418e0dcfdbd175483904fcf2c0633fa2a042a
ronny1982 <wegener.ronny@gmail.com>
2020-06-01 02:49:01 +0200

    moved testing code to button in UI

--- src/App.ts
+++ src/App.ts
@@ -15,23 +15,3 @@ nw.Window.get().showDevTools();
 
 window.HakuNeko = new HakuNeko();
 window.Frontend = new FrontendController();
-
-// +++ PLAYGROUND +++
-
-setTimeout(async () => {
-    let uri = new URL('https://postman-echo.com/get');
-    let request = new Request(uri.href, {
-        method: 'GET',
-        referrer: 'https://hakuneko.download',
-        headers: {
-            'user-agent': 'Mozilla/5.0 (HakuNeko; Intel Mac OS X 10.15.3)',
-            'referer': 'https://hakuneko.download',
-            'origin': 'hakuneko.download',
-            'cookie': 'adult=1',
-            //'X-Foo': 'bar',
-            'x-foo': 'bar'
-        }
-    });
-    let data = await window.HakuNeko.RequestProvider.FetchJSON(request);
-    console.log('FetchJSON:', data);
-}, 1000);
--- src/frontend/classic/App.ts
+++ src/frontend/classic/App.ts
@@ -1,24 +1,72 @@
-let buttons = window.Frontend.AvailableFrontends
-    .filter(info => info.ID !== 'classic')
-    .map(info => `<li><a href="#" onclick="Frontend.Load('${info.ID}')" title="${info.Description}">Load Frontend: ${info.Label}</a></li>`).join('');
-
-const htmlContent = `
-    <div style="padding: 2em;">
-        <p>
-            <strong>Platform :</strong> ${process.platform}<br>
-            <strong>NW.js :</strong> ${process.versions['node-webkit']}
-        </p>
-        <ul>
-            ${buttons}
-        </ul>
-    </div>
-`;
-
 export default class App {
 
     constructor(root: HTMLElement) {
         // artificial delay to make loading screen visible
         //await new Promise(resolve => setTimeout(resolve, 2500));
-        root.innerHTML = htmlContent;
+        let container = this.CreateContainer();
+        container.appendChild(this.CreateInfo());
+        container.appendChild(this.CreateElement('hr'));
+        container.appendChild(this.CreateFrontendButtonList());
+        container.appendChild(this.CreateElement('hr'));
+        container.appendChild(this.CreateRequestButtonList());
+        root.appendChild(container);
+    }
+
+    private CreateElement(tag: string, html?: string): HTMLElement {
+        let element = document.createElement(tag);
+        if(html) {
+            element.innerHTML = html;
+        }
+        return element;
+    }
+
+    private CreateContainer(): HTMLElement {
+        let container = this.CreateElement('div');
+        container.style.padding = '1em';
+        return container;
+    }
+
+    private CreateInfo(): HTMLElement {
+        return this.CreateElement('p', `
+            <strong>Platform :</strong> ${process.platform}<br>
+            <strong>NW.js :</strong> ${process.versions['node-webkit']}
+        `);
+    }
+
+    private CreateFrontendButtonList() {
+        let buttons = window.Frontend.AvailableFrontends
+            .filter(info => info.ID !== 'classic')
+            .map(info => `<li><a href="#" onclick="Frontend.Load('${info.ID}')" title="${info.Description}">Load Frontend: ${info.Label}</a></li>`).join('');
+        return this.CreateElement('ul', buttons);
+    }
+
+    private CreateRequestButtonList() {
+        let list = this.CreateElement('ul');
+
+        let buttonFetchJSON = this.CreateElement('a', `FetchJSON`) as HTMLAnchorElement;
+        buttonFetchJSON.href = '#';
+        buttonFetchJSON.onclick = async event => {
+            let uri = new URL('https://postman-echo.com/get');
+            let request = new Request(uri.href, {
+                method: 'GET',
+                referrer: 'https://hakuneko.download',
+                headers: {
+                    'user-agent': 'Mozilla/5.0 (HakuNeko; OS)',
+                    'referer': 'https://hakuneko.download',
+                    'origin': 'hakuneko.download',
+                    'cookie': 'adult=1',
+                    'X-Foo': 'bar'
+                }
+            });
+            let data = await window.HakuNeko.RequestProvider.FetchJSON(request);
+            console.log('FetchJSON:', data);
+        }
+        let itemFetchJSON = this.CreateElement('li');
+        itemFetchJSON.appendChild(buttonFetchJSON);
+        list.appendChild(itemFetchJSON);
+
+        // another button ...
+
+        return list;
     }
 }
