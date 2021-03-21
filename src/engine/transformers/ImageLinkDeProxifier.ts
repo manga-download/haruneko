3a99acfdbf976e74af32428dda17fcf2beafcf06
ronny1982 <wegener.ronny@gmail.com>
2021-03-21 16:25:25 +0100

    fix lint

--- src/engine/transformers/ImageLinkDeProxifier.ts
+++ src/engine/transformers/ImageLinkDeProxifier.ts
@@ -4,14 +4,14 @@ export class ImageLinkDeProxifier {
         const source = uri.searchParams.get('url');
         if(source) {
             return source;
-        };
+        }
         throw new Error(`Missing parameter <url> in '${uri.href}' for Google proxy!`);
     }
 
     private DeProxifyPhoton(uri: URL): string {
         let url = uri.searchParams.get('ssl') ? 'https://' : 'http://';
         url += uri.pathname.slice(1);
-        let search = uri.searchParams.get('q');
+        const search = uri.searchParams.get('q');
         if(search) {
             url += '?' + search;
         }
@@ -19,7 +19,7 @@ export class ImageLinkDeProxifier {
     }
 
     public Convert(url: string): string {
-        let uri = new URL(url);
+        const uri = new URL(url);
         if(/googleusercontent\.com$/.test(uri.hostname) && /\/proxy$/.test(uri.pathname)) {
             return this.DeProxifyGoogle(uri);
         }
