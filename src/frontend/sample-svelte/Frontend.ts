c10af59972c75305eb9fd35ff889990887e34f63
Sheepux <sheepux@live.com>
2021-04-03 04:08:16 +0200

    fixing windows rollup issue with svelte

--- src/frontend/sample-svelte/Frontend.ts
+++ src/frontend/sample-svelte/Frontend.ts
@@ -1,4 +1,4 @@
-import { IFrontendModule } from '../IFrontend'
+import { IFrontendModule } from '../IFrontend';
 import App from './App.svelte';
 
 class SampleSvelte implements IFrontendModule {
--- src/frontend/sample-svelte/rollup.config.ts
+++ src/frontend/sample-svelte/rollup.config.ts
@@ -24,7 +24,7 @@ export default {
     plugins: [
         svelte({
             //dev: !isProduction,
-            preprocess: preprocess()
+            preprocess: preprocess({ sourceMap: !isProduction })
             //css: css => css.write('css/sample-svelte.css')
         }),
         postcss({
