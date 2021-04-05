348e6cfc27541469e430539d8fd7535db79e3808
ronny1982 <wegener.ronny@gmail.com>
2021-04-05 16:11:41 +0200

    use window constant for main window

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,8 @@ import { FrontendController } from './frontend/FrontendController';
 
 //const gui = require('nw.gui');
 //gui.Window.get().
-nw.Window.get().showDevTools();
+const win = nw.Window.get();
+win.showDevTools();
 
 InitBlacklist();
 InitFetchProvider();
