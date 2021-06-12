472b7a3445d18f2ba8f292c5b15d67e98d72e360
ronny1982 <wegener.ronny@gmail.com>
2021-06-12 11:13:41 +0200

    added nw module to be bundled, cleanup

--- scripts/build.app.mjs
+++ scripts/build.app.mjs
@@ -37,3 +37,13 @@ async function getManifest(config) {
 const config = await fs.readJSON(pkg);
 const manifest = await getManifest(config);
 fs.writeJSON(target, manifest, { spaces: 4 });
+
+const nwVersion = config.devDependencies.nw.split('-').shift();
+const nwBuildType = config.devDependencies.nw.match(/(-sdk)?$/)[0];
+const nwPlatform = process.platform.replace('win32', 'win').replace('darwin', 'osx');
+const nwArchitecture = process.arch;
+const nwExtension = nwPlatform === 'linux' ? 'tar.gz' : 'zip';
+
+const url = `https://dl.nwjs.io/v${nwVersion}/nwjs${nwBuildType}-v${nwVersion}-${nwPlatform}-${nwArchitecture}.${nwExtension}`;
+
+console.log(url);
