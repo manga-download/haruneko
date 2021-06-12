f3402fb8c35ddcf19b6ba562dcbdbf5f1cfd0a99
ronny1982 <wegener.ronny@gmail.com>
2021-06-12 02:06:06 +0200

    cleanup globals

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,27 +1,10 @@
 import { IHakuNeko } from './engine/HakuNeko';
 import { IFrontendController } from './frontend/FrontendController';
 
-/*
-declare namespace NodeJS {
-    interface Process {
-        browser: boolean;
-    }
-}
-*/
-
 declare global {
-    const HakuNeko: IHakuNeko;
-    const Frontend: IFrontendController;
+    const nw: any;
     interface Window {
         HakuNeko: IHakuNeko;
         Frontend: IFrontendController;
     }
-    /*
-    interface NodeJS {
-        Process: {
-            'node-webkit': string;
-        }
-    }
-    */
-    const nw: any;
 }
