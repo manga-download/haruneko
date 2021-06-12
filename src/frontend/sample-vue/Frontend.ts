3af188a4a3a7a5f4e8863036c2c3a7089e385c3b
ronny1982 <wegener.ronny@gmail.com>
2021-06-12 02:10:42 +0200

    add ability for frontend to set window menu

--- src/frontend/sample-vue/Frontend.ts
+++ src/frontend/sample-vue/Frontend.ts
@@ -4,6 +4,10 @@ import App from './App.vue';
 
 class SampleVue implements IFrontendModule {
 
+    SetWindowMenu(): void {
+        // optionally change the menu of the NW.js window
+    }
+
     async Render(root: HTMLElement): Promise<void> {
         createApp(App).mount(root);
     }
