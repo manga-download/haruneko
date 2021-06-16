f82a99c0b517c8ab03a6da507d01aa9258a6d8ee
ronny1982 <wegener.ronny@gmail.com>
2021-06-16 12:48:01 +0200

    resize window based on splash / application content

--- src/index.ts
+++ src/index.ts
@@ -3,20 +3,33 @@ import { Initialize as InitFetchProvider } from './engine/FetchProvider';
 import { HakuNeko } from './engine/HakuNeko';
 import { FrontendController } from './frontend/FrontendController';
 
+const nwWindow = Object.keys(window).includes('nw') ? nw.Window.get() : undefined;
 const timerHideSplashScreen = setTimeout(HideSplashScreen, 7500);
 window.addEventListener('appready', HideSplashScreen);
 
-if(Object.keys(window).includes('nw')) {
-    const win = nw.Window.get();
-    win.show();
+if(nwWindow) {
+    AdjustWindowSize(720, 480);
+    nwWindow.show();
     InitBlacklist();
     InitFetchProvider();
     window.HakuNeko = new HakuNeko();
     window.Frontend = new FrontendController();
 }
 
+function AdjustWindowSize(width: number, height: number) {
+    if(nwWindow) {
+        nwWindow.width = Math.round(width);
+        nwWindow.height = Math.round(height);
+        const left = (screen.width - nwWindow.width) / 2;
+        const top = (screen.height - nwWindow.height) / 2;
+        nwWindow.moveTo(Math.round(left), Math.round(top));
+    }
+}
+
 function HideSplashScreen() {
     clearTimeout(timerHideSplashScreen);
     document.querySelector('#splash')?.remove();
     (document.querySelector('#app') as HTMLDivElement).style.display = 'initial';
+    // TODO: may set window based on last stored position and size
+    AdjustWindowSize(0.80 * screen.width, 0.80 * screen.height);
 }
