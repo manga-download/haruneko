f1e51ab5fe5d4e6adaf5105ccb5ba7d89d305537
monier <p.monier96@gmail.com>
2021-07-03 14:18:25 +0200

    fix(front/home): add better home page style

--- src/frontend/sample-svelte/components/Home.svelte
+++ src/frontend/sample-svelte/components/Home.svelte
@@ -1,6 +1,72 @@
+<script>
+    import Icon from "svelte-awesome";
+    import { search, plug, language, refresh } from "svelte-awesome/icons";
+
+    const iconsStyle = "margin-left: 5px; margin-right: 5px;";
+</script>
+
+<div id="Home">
+    <h2>Welcome to HakuNeko</h2>
+    <div>
+        <p class="border">
+            <img
+                alt="hakuneko logo"
+                class="logo"
+                src="./img/logo.png"
+                align="left"
+            />
+            <strong>HakuNeko</strong> was made to help users who download media
+            for circumstances that requires offline usage. <br />
+            The philosophy is <u>ad-hoc consumption</u>, get it when you want to
+            read/watch it.<br />
+            Read the "how to use" documentation at hakuneko.download
+        </p>
+    </div>
+    <div>
+        <h4>Find a site to get the content you want to view</h4>
+        <div class="border">
+            <p>
+                <strong>HakuNeko</strong> is not hosting anything. Find a website
+                on the internet that provides the content you want. Then :
+            </p>
+            <ol>
+                <li>
+                    <Icon data={plug} style={iconsStyle} /> Select your connector
+                    (the website). Use the filters
+                </li>
+                <li>
+                    <Icon data={refresh} style={iconsStyle} /> Click on the refresh
+                    button (can take minutes)
+                </li>
+                <li>
+                    <Icon data={search} style={iconsStyle} /> Find a manga
+                </li>
+                <li>
+                    <Icon data={language} style={iconsStyle} /> Filter chapters by
+                    language (optional)
+                </li>
+                <li>
+                    <Icon data={search} style={iconsStyle} /> Select a chapter
+                </li>
+            </ol>
+            <p>
+                The connector you are looking for is not in the list ?<br />
+                👉 Create a website request on the github (issue)<br />
+                Use the copy/paste feature if you already have the manga link<br
+                />
+                👉 See the documentation for details
+            </p>
+        </div>
+        <h4>Ready to go 🚀</h4>
+        <div class="border">
+            <p>Download, view, bookmark your favorite mangas 📚</p>
+        </div>
+    </div>
+</div>
+
 <style>
     #Home {
-        padding:0.5em;
+        padding: 0.5em;
     }
     .border {
         border: 2px solid var(--cds-ui-04);
@@ -12,35 +78,18 @@
         margin-right: 1em;
         border-radius: 10%;
     }
+
+    ol {
+        display: block;
+        list-style-type: decimal;
+        margin-block-start: 1em;
+        margin-block-end: 1em;
+        margin-inline-start: 0px;
+        margin-inline-end: 0px;
+        padding-inline-start: 40px;
+    }
+
+    li {
+        margin-top: 5px;
+    }
 </style>
-<div id="Home">
-    <h2>Welcome to HakuNeko</h2>
-    <div>
-        <p class="border">
-                <img alt="hakuneko logo" class="logo" src="./img/logo.png"  align="left"/>
-            <strong>HakuNeko</strong> was made to help users who download media for circumstances that requires offline usage. <br/>
-            The philosophy is <u>ad-hoc consumption</u>, get it when you want to read/watch it.<br/>
-            Read the "how to use" documentation at hakuneko.download
-        </p>
-    </div>
-    <div>
-        <h4>Find a site to get the content you want to view</h4>
-        <div class="border">
-            <p><strong>HakuNeko</strong> is not hosting anything. Find a website on the internet that provides the content you want. Then :
-            <ol>
-                <li><i class="fas fa-fw fa-plug fa-flip-horizontal" title="Select a website from which the manga list should be shown"></i> Select your connector (the website). Use the filters</li>
-                <li><i class="fas fa-fw fa-sync" title="Select a website from which the manga list should be shown"></i> Click on the refresh button (can take minutes)</li>
-                <li><i class="fas fa-fw fa-search" title="Filter the manga list by their titles"></i> Find a manga</li>
-                <li><i class="fas fa-fw fa-language" title="Select a language to filter the chapter list"></i> Filter chapters by language (optional)</li>
-                <li><i class="fas fa-fw fa-search" title="Filter the chapter list by their titles"></i> Select a chapter</li>
-            </ol >
-            <p>The connector you are looking for is not in the list ?<br/>
-            👉 Create a website request on the github (issue)<br/>
-            Use the copy/paste feature if you already have the manga link<br/>
-            👉 See the documentation for details
-            </p>
-        </div>
-        <h4>Ready to go 🚀</h4>
-        <div class="border"><p>Download, view, bookmark your favorite mangas 📚</p></div>
-    </div>
-</div>
