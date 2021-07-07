ac40daeb459905311b062cff1eed8342631f0975
ronny1982 <wegener.ronny@gmail.com>
2021-07-07 13:56:41 +0200

    add globals (for usage without window.)

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,8 @@ import { IFrontendController } from './frontend/FrontendController';
 
 declare global {
     const nw: any;
+    const HakuNeko: IHakuNeko;
+    const Frontend: IFrontendController;
     interface Window {
         HakuNeko: IHakuNeko;
         Frontend: IFrontendController;
