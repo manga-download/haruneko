93f43a9249fb450256d5c4488addad0a13b232ee
pierre <pmonier99@laposte.net>
2021-08-12 11:12:16 +0200

    fix(front): change type of MenuLeftPanelSelectItem

--- src/frontend/classic/components/MenuLeftPanelSelectItem.svelte
+++ src/frontend/classic/components/MenuLeftPanelSelectItem.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Select, SelectItem } from "carbon-components-svelte";
-    export let labelText: string | undefined;
-    export let helperText: string | undefined = undefined;
+    export let labelText: string;
+    export let helperText: string = "";
     export let selected: string | undefined;
     export let items: { value: string; text: string }[];
 </script>
