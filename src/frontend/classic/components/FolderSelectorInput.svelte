14d4d859987ca318e779ddda03f597b3ad4f14d9
pierre <pmonier99@laposte.net>
2021-08-17 16:42:10 +0200

    fix(front): add uuid to FolderSelectorInput in order to allow multiple FolderSelectorInput components

--- src/frontend/classic/components/FolderSelectorInput.svelte
+++ src/frontend/classic/components/FolderSelectorInput.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
     import { TextInput } from "carbon-components-svelte";
+    import { v4 as uuidv4 } from "uuid";
+
+    const uuid = uuidv4();
 
     export let labelText = "Select directory";
     let path: string = "";
@@ -7,8 +10,13 @@
 
 <div>
     <TextInput readonly value={path} />
-    <label for="folder-selector">{labelText}</label>
-    <input id="folder-selector" type="file" nwdirectory bind:value={path} />
+    <label for={`folder-selector-${uuid}`}>{labelText}</label>
+    <input
+        id={`folder-selector-${uuid}`}
+        type="file"
+        nwdirectory
+        bind:value={path}
+    />
 </div>
 
 <style>
