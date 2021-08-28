917db8323a8f47ad9f3ed80415214b252fff44f2
pierre <pmonier99@laposte.net>
2021-08-28 19:43:47 +0200

    feat(front/viewer): checking and adding image in cacheStorage before calling the preloadImage method

--- src/frontend/classic/utils/image.ts
+++ src/frontend/classic/utils/image.ts
@@ -1,9 +1,19 @@
+import { cacheStorageKey } from "./storage";
+
 export const preloadImage = async (src: string, throttlingDelay: number): Promise<void> => {
-    return new Promise<void>((res) => {
-        setTimeout(() => {
-            const img = new Image();
-            img.src = src;
-            res();
-        }, throttlingDelay);
-    });
+    const response = await caches.match(src)
+
+    if (!response) {
+        return new Promise<void>((res) => {
+
+            setTimeout(async () => {
+                const img = new Image();
+                img.src = src;
+
+                const cache = await caches.open(cacheStorageKey)
+                cache.add(src);
+                res();
+            }, throttlingDelay);
+        });
+    }
 }
---
+++ src/frontend/classic/utils/storage.ts
@@ -0,0 +1,1 @@
+export const cacheStorageKey = "hakuneko"
