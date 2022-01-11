e36417a52e06dda11784f30788511fc6215fa6e5
Ronny <wegener.ronny@gmail.com>
2022-01-11 23:21:28 +0100

    increase allowed bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     plugins: [ vue(), react(), svelte() ],
     publicDir: 'static',
     build: {
-        outDir: 'build.web'
+        outDir: 'build.web',
+        chunkSizeWarningLimit: 1024
     }
 });
