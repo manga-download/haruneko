381b8d5cb06bab25987fc3b7ffb4c1c2e2ade2a4
Ronny <wegener.ronny@gmail.com>
2022-01-13 01:17:55 +0100

    renamed e2e test files

--- test/Websites.e2e.ts
+++ test/Websites.e2e.ts
@@ -1,8 +1,8 @@
 import { Setup, Teardown } from './PuppeteerFixture';
-import Hiperdex from '../src/engine/websites/Hiperdex_e2e';
-import Leitor from '../src/engine/websites/Leitor_e2e';
-import ScansMangas from '../src/engine/websites/ScansMangas_e2e';
-import Toonily from '../src/engine/websites/Toonily_e2e';
+import Hiperdex from '../src/engine/websites/Hiperdex.e2e';
+import Leitor from '../src/engine/websites/Leitor.e2e';
+import ScansMangas from '../src/engine/websites/ScansMangas.e2e';
+import Toonily from '../src/engine/websites/Toonily.e2e';
 
 describe('End-to-End Test Suite for all Websites', () => {
 
