070cb627d23e8c621a419840e5d022fb72d7b149
Ronny <wegener.ronny@gmail.com>
2022-01-19 15:20:13 +0100

    change vue sample to use new setup script

--- src/frontend/sample-vue/App.vue
+++ src/frontend/sample-vue/App.vue
@@ -1,22 +1,10 @@
-<script lang="typescript">
-    // TODO: vue rollup plugin will add `import { * } from 'vue'` statements during compilation
-    //       => these will not work in NW.js and should be replaced by `require`
-    // TODO: Use require to load import from client/node_modules directory ...
+<script setup lang="ts">
     import { ref } from 'vue';
-    //const { ref } = require('vue');
 
-    export default {
-        setup() {
-            const count = ref(0);
-            const increment = () => {
-                count.value++;
-            };
-            return {
-                count,
-                increment
-            };
-        }
-    }
+    const count = ref(0);
+    const increment = () => {
+        count.value++;
+    };
 </script>
 
 <style scoped>
