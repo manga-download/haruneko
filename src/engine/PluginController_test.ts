57e4fe47948c004c03eb6f6dfd6c61cbb7747d6e
Ronny <wegener.ronny@gmail.com>
2022-02-04 13:52:21 +0100

    introduce tests for plugin controller

---
+++ src/engine/PluginController_test.ts
@@ -0,0 +1,67 @@
+import { Request as RequestMock } from 'node-fetch';
+global.Request = RequestMock;
+import { mock } from 'jest-mock-extended';
+import type { ISettings, SettingsManager } from './SettingsManager';
+import type { StorageController } from './StorageController';
+import { PluginController } from './PluginController';
+
+class TestFixture {
+
+    public readonly MockStorageController = mock<StorageController>();
+    public readonly MockSettingsManager = mock<SettingsManager>();
+
+    constructor() {
+        this.MockSettingsManager.OpenScope.mockReturnValue(mock<ISettings>());
+    }
+
+    public CreateTestee() {
+        return new PluginController(this.MockStorageController, this.MockSettingsManager);
+    }
+}
+
+describe('PluginController', () => {
+
+    describe('Constructor', () => {
+
+        it('Should initialize all settings', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const total = [
+                testee.WebsitePlugins,
+                testee.InfoTrackers
+            ].reduce((count, entries) => count + entries.length, 0);
+
+            expect(total).toBeGreaterThan(0);
+            expect(fixture.MockSettingsManager.OpenScope).toBeCalledTimes(total);
+        });
+    });
+
+    describe('WebsitePlugins', () => {
+
+        it('Should have unique website plugin identifiers', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const actual = testee.WebsitePlugins.map(plugin => plugin.Identifier);
+            const expected = [...new Set(actual).values()];
+
+            expect(actual.length).toBeGreaterThan(0);
+            expect(actual).toStrictEqual(expected);
+        });
+    });
+
+    describe('InfoTrackers', () => {
+
+        it('Should have unique info tracker identifiers', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const actual = testee.InfoTrackers.map(plugin => plugin.Identifier);
+            const expected = [...new Set(actual).values()];
+
+            expect(actual.length).toBeGreaterThan(0);
+            expect(actual).toStrictEqual(expected);
+        });
+    });
+});
