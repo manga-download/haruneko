f8359c944120afb2ce58bd49617e13e859d5ef12
Ronny <wegener.ronny@gmail.com>
2022-03-31 19:50:52 +0200

    Add structure representig rate limit

---
+++ src/engine/RequestRateLimit.ts
@@ -0,0 +1,16 @@
+export class RequestRateLimit {
+    /**
+     * 
+     * @param RequestCount Maximum number of requests within the given {@link TimeSpan}.
+     * @param TimeSpan Time range [sec] related to the given {@link RequestCount}.
+     */
+    constructor(public RequestCount: number, public TimeSpan: number = 1) {
+    }
+
+    /**
+     * The minimal calculated delay [ms] between requests to comply with the rate limit.
+     */
+    public get Throttle() {
+        return this.RequestCount > 0 && this.TimeSpan > 0 ? (this.TimeSpan * 1000 / this.RequestCount) : 0;
+    }
+}
---
+++ src/engine/RequestRateLimit_test.ts
@@ -0,0 +1,39 @@
+import { RequestRateLimit } from './RequestRateLimit';
+
+describe('RequestRateLimit', () => {
+
+    describe('Constructor', () => {
+
+        it('Should assign parameters', async () => {
+            const testee = new RequestRateLimit(7, 19);
+            expect(testee.RequestCount).toBe(7);
+            expect(testee.TimeSpan).toBe(19);
+        });
+    });
+
+    describe('Throttle', () => {
+
+        it.each([
+            [-7, -7, 0],
+            [-7, 0, 0],
+            [0, -7, 0],
+            [0, 0, 0],
+            [0, 7, 0],
+            [7, 0, 0],
+            [1, 7, 7000],
+            [7, 7, 1000],
+            [10, 7, 700]
+        ])('Should calculate expected delay', async (count, span, expected) => {
+            const testee = new RequestRateLimit(count, span);
+            expect(testee.Throttle).toBe(expected);
+        });
+
+        it('Should update when change properties', async () => {
+            const testee = new RequestRateLimit(0, 0);
+            expect(testee.Throttle).toBe(0);
+            testee.RequestCount = 10;
+            testee.TimeSpan = 7;
+            expect(testee.Throttle).toBe(700);
+        });
+    });
+});
