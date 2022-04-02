7b37053a1b3e3ad68bcdc8d9d4872ba84caec599
Ronny <wegener.ronny@gmail.com>
2022-04-02 15:08:12 +0200

    Introduce basic task pool

---
+++ src/engine/taskpool/TaskPool.ts
@@ -0,0 +1,105 @@
+import { DeferredTask, Priority } from './DeferredTask';
+import { Unlimited } from './RateLimit';
+
+export class TaskPool<T> {
+
+    private running = false;
+    private activeWorkersCount = 0;
+    private delay = Promise.resolve();
+    private queueTransactionLock: boolean = false;
+    private readonly queue: DeferredTask<T>[] = [];
+
+    /**
+     * Create a new task pool.
+     * @param Workers The number of workers for processing tasks (maximum number of tasks processed at the same time). Default: 4
+     * @param RateLimit The maximum throughput for processing tasks. Default: infinite
+     */
+    constructor(public Workers: number = 4, public RateLimit = Unlimited) {
+        //setInterval(this.Process.bind(this), 750);
+    }
+
+    /**
+     * Add a new (awaitable) {@link action} to this task pool and start processing the task pool (if not already started).
+     * @returns A promise that will be completed with the final result of the {@link action} after it was processed.
+     */
+    public Add(action: () => Promise<T>, priority: Priority): Promise<T> {
+        const task = new DeferredTask<T>(action, priority);
+        this.AddTask(task);
+        return task.Promise;
+    }
+
+    private async AddTask(task: DeferredTask<T>) {
+        await this.InvokeQueueTransaction(queue => queue.push(task));
+        this.Process();
+    }
+
+    /**
+     * Perform a thread/concurrency safe operation on {@link queue}
+     */
+    private async InvokeQueueTransaction<R>(transaction: (queue: typeof this.queue) => R): Promise<R> {
+        try {
+            // TODO: Use a better locking mechanism?
+            while(this.queueTransactionLock) {
+                await new Promise(resolve => setTimeout(resolve, 5));
+            }
+            this.queueTransactionLock = true;
+            return transaction(this.queue);
+        } finally {
+            this.queueTransactionLock = false;
+        }
+    }
+
+    /**
+     * Find and return the next task with highest priority in the {@link queue} and removes it.
+     * @returns The found task, or `undefined` in case the {@link queue} is empty or an internal error occured.
+     */
+    private async TakeNextTask(): Promise<DeferredTask<T>> {
+        return await this.InvokeQueueTransaction(queue => {
+            try {
+                let index = 0;
+                for(let i = 0; i < queue.length; i++) {
+                    if(queue[index].Priority < queue[i].Priority) {
+                        index = i;
+                    }
+                }
+                // NOTE: Will return undefined in case the queue is empty
+                return queue.splice(index, 1)[0];
+            } catch {
+                return undefined;
+            };
+        });
+    }
+
+    private async ConcurrencySlotAvailable() {
+        while(this.activeWorkersCount >= this.Workers) {
+            await new Promise(resolve => setTimeout(resolve, 50));
+        }
+    }
+
+    private async Process() {
+        if(this.running) {
+            return;
+        }
+        this.running = true;
+        try {
+            let task: DeferredTask<T>;
+            while(task = await this.TakeNextTask()) {
+                await this.delay;
+                this.delay = new Promise(resolve => setTimeout(resolve, this.RateLimit.Throttle));
+                await this.ConcurrencySlotAvailable();
+                this.Run(task);
+            }
+        } finally {
+            this.running = false;
+        }
+    }
+
+    private async Run(task: DeferredTask<T>) {
+        try {
+            this.activeWorkersCount++;
+            await task.Run();
+        } finally {
+            this.activeWorkersCount--;
+        }
+    }
+}
---
+++ src/engine/taskpool/TaskPool_test.ts
@@ -0,0 +1,154 @@
+import { inspect } from 'util';
+import { TaskPool } from './TaskPool';
+import { Priority } from './DeferredTask';
+import { RateLimit, Unlimited } from './RateLimit';
+
+class MockJob<T> {
+    constructor(public readonly Priority: Priority, public readonly Duration: number, public readonly Result: T) {
+    }
+}
+
+class Result<T> {
+    constructor(public readonly ResolveTime: number, public Status: string, public readonly Value: T) {
+    }
+}
+
+async function RunJobs<T>(testee: TaskPool<Result<T>>, ... jobs: MockJob<T>[]): Promise<Result<T>[]> {
+    const promises: Promise<Result<T>>[] = [];
+    for(const job of jobs) {
+        const task = testee.Add(() => new Promise<Result<T>>((resolve, reject) => {
+            setTimeout(() => resolve(new Result(Date.now(), '', job.Result)), job.Duration);
+        }), job.Priority);
+        promises.push(task);
+    }
+    const results = await Promise.allSettled(promises);
+    results.forEach(p => p['value'].Status = p.status);
+    return results.map(p => p['value']);
+}
+
+describe('TaskPool', () => {
+
+    describe('Constructor', () => {
+
+        it('Should initialize with parameters', async () => {
+            const workers = 7;
+            const limit = new RateLimit(5, 10);
+            const testee = new TaskPool<void>(workers, limit);
+            expect(testee.Workers).toBe(workers);
+            expect(testee.RateLimit).toBe(limit);
+        });
+    });
+
+    describe('Workers', () => {
+
+        it('Should correctly set value', async () => {
+            const expected = 7;
+            const testee = new TaskPool<void>(1, Unlimited);
+            testee.Workers = expected;
+            expect(testee.Workers).toBe(expected);
+        });
+    });
+
+    describe('RateLimit', () => {
+
+        it('Should correctly set value', async () => {
+            const expected = new RateLimit(5, 10);
+            const testee = new TaskPool<void>(1, Unlimited);
+            testee.RateLimit = expected;
+            expect(testee.RateLimit).toBe(expected);
+        });
+    });
+
+    describe('Add', () => {
+
+        it('Should automatically process added task', async () => {
+            const testee = new TaskPool<number>(1);
+            const promise = testee.Add(() => Promise.resolve(7), Priority.Normal);
+            expect(inspect(promise)).toContain('<pending>');
+            await new Promise(resolve => setTimeout(resolve, 10));
+            expect(await promise).toBe(7);
+        });
+
+        it('Should resolve queued tasks in order', async () => {
+            const testee = new TaskPool<Result<string>>(1, Unlimited);
+            const results = await RunJobs(testee,
+                new MockJob(Priority.Normal, 1, '①'),
+                new MockJob(Priority.Normal, 10, '②'),
+                new MockJob(Priority.Normal, 1, '③')
+            );
+            for(let i = 1; i < results.length; i++) {
+                expect(results[i-1].ResolveTime).toBeLessThan(results[i].ResolveTime);
+            }
+            expect(results.map(r => r.Value)).toEqual([ '①', '②', '③' ]);
+            expect(results.map(r => r.Status)).toEqual(new Array(results.length).fill('fulfilled'));
+        });
+
+        it('Should correctly prioritize tasks', async () => {
+            const testee = new TaskPool<Result<string>>(1, Unlimited);
+            const results = await RunJobs(testee,
+                new MockJob(Priority.Normal, 1, '①'),
+                new MockJob(Priority.Low, 1, '②'),
+                new MockJob(Priority.Low, 1, '③'),
+                new MockJob(Priority.Normal, 1, '④'),
+                new MockJob(Priority.Low, 1, '⑤'),
+                new MockJob(Priority.High, 1, '⑥'),
+                new MockJob(Priority.High, 1, '⑦'),
+                new MockJob(Priority.Normal, 1, '⑧'),
+                new MockJob(Priority.Low, 1, '⑨')
+            );
+            results.sort((a, b) => a.ResolveTime - b.ResolveTime);
+            expect(results.map(r => r.Value)).toEqual([ '⑥', '⑦', '①', '④', '⑧', '②', '③', '⑤', '⑨' ]);
+        });
+
+        it('Should process tasks added after break', async () => {
+            const testee = new TaskPool<Result<string>>(1, Unlimited);
+            let actual = await RunJobs(testee, new MockJob(Priority.Normal, 1, '①'));
+            expect(actual.map(r => r.Value)).toEqual([ '①' ]);
+            await new Promise(resolve => setTimeout(resolve, 25));
+            actual = await RunJobs(testee, new MockJob(Priority.Normal, 1, '②'));
+            expect(actual.map(r => r.Value)).toEqual([ '②' ]);
+            await new Promise(resolve => setTimeout(resolve, 25));
+            actual = await RunJobs(testee, new MockJob(Priority.Normal, 1, '③'));
+            expect(actual.map(r => r.Value)).toEqual([ '③' ]);
+        });
+
+        it('Should utilize workers for concurrent processing', async () => {
+            const testee = new TaskPool<Result<string>>(3, Unlimited);
+            const results = await RunJobs(testee,
+                new MockJob(Priority.Normal, 10, '①'),
+                new MockJob(Priority.Normal, 11, '②'),
+                new MockJob(Priority.Normal, 12, '③'),
+                new MockJob(Priority.Normal, 10, '④'),
+                new MockJob(Priority.Normal, 11, '⑤'),
+                new MockJob(Priority.Normal, 12, '⑥'),
+                new MockJob(Priority.Normal, 10, '⑦'),
+                new MockJob(Priority.Normal, 11, '⑧'),
+                new MockJob(Priority.Normal, 12, '⑨')
+            );
+            try {
+                results.sort((a, b) => a.ResolveTime - b.ResolveTime);
+                expect(results.map(r => r.Value)).toEqual([ '①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨' ]);
+                const timestamps = results.map(r => r.ResolveTime);
+                const batches = [ timestamps.slice(0, 3), timestamps.slice(3, 6), timestamps.slice(6, 9) ];
+                const averages = batches.map(batch => batch.reduce((accumulator, timestamp) => accumulator + timestamp, 0) / batch.length);
+
+                for(let i = 0; i < batches.length; i++) {
+                    for(let timestamp of batches[i]) {
+                        const difference = Math.abs(timestamp - averages[i]);
+                        expect(difference).toBeLessThan(5);
+                    }
+                }
+
+                // NOTE: Assumption that idle time for checking available workers is ~50 ms
+                //       See: TaskPool.ConcurrencySlotAvailable()
+                expect(averages[1] - averages[0]).toBeGreaterThan(50);
+                expect(averages[1] - averages[0]).toBeLessThan(65);
+                expect(averages[2] - averages[1]).toBeGreaterThan(50);
+                expect(averages[2] - averages[1]).toBeLessThan(65);
+            } catch(error) {
+                console.log(results);
+                throw error;
+            }
+        });
+    });
+});
