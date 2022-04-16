7b568b153ff3816b5c90a05e13ac1857bee9c77b
Ronny <wegener.ronny@gmail.com>
2022-04-17 01:05:15 +0200

    extend/improve download manager

--- src/engine/DownloadManager.ts
+++ src/engine/DownloadManager.ts
@@ -1,46 +1,98 @@
-import { DownloadTask, Status } from './DownloadTask';
-import type { IMediaContainer } from './providers/MediaPlugin';
+import { DownloadTask, IDownloadTask, Status } from './DownloadTask';
+import type { IMediaContainer, StoreableMediaContainer, IMediaItem } from './providers/MediaPlugin';
 import type { SettingsManager } from './SettingsManager';
 import type { StorageController } from './StorageController';
 import { Event } from './EventManager';
 
 export class DownloadManager {
 
-    private paused = false;
-    private readonly queue: DownloadTask[] = [];
-    public readonly EntryAdded: Event<typeof this, DownloadTask> = new Event<typeof this, DownloadTask>();
+    private processing = false;
+    private queue: DownloadTask[] = [];
+    private queueTransactionLock = false;
+    public readonly TasksAdded: Event<typeof this, IDownloadTask[]> = new Event<typeof this, IDownloadTask[]>();
+    public readonly TasksRemoved: Event<typeof this, IDownloadTask[]> = new Event<typeof this, IDownloadTask[]>();
 
     constructor(private readonly storageController: StorageController, private readonly settingsManager: SettingsManager) {
-        this.Process();
     }
 
-    public Enqueue(media: IMediaContainer): DownloadTask {
-        const task = new DownloadTask(media);
-        this.queue.push(task);
-        return task;
-    }
-
-    public Dequeue(... tasks: DownloadTask[]): void {
-        // find task(s) in queue and remove ...
-    }
-
-    private async Process() {
-        while(this) {
-            const task = this.queue.find(t => t.Status === Status.Queued);
-            if(task && !this.paused) {
-                await task.Run();
-                // TODO: keep task in queue after completed?
-            } else {
-                await new Promise(resolve => setTimeout(resolve, 750));
+    /**
+     * Perform a thread/concurrency safe operation on {@link queue}
+     */
+    private async InvokeQueueTransaction<R>(transaction: (queue: typeof this.queue) => R): Promise<R> {
+        try {
+            // TODO: Use a better locking mechanism?
+            while(this.queueTransactionLock) {
+                await new Promise(resolve => setTimeout(resolve, 5));
             }
+            this.queueTransactionLock = true;
+            return transaction(this.queue);
+        } finally {
+            this.queueTransactionLock = false;
         }
     }
 
-    public Pause() {
-        this.paused = true;
+    public GetTasks(): Promise<IDownloadTask[]> {
+        return this.InvokeQueueTransaction(queue => queue.map(task => task as IDownloadTask));
     }
 
-    public Resume() {
-        this.paused = false;
+    /**
+     * Add the given {@link containers} to the download queue.
+     * Only containers that are not present in the download queue will be added.
+     */
+    public async Enqueue(...containers: IMediaContainer[]): Promise<void> {
+        const added = await this.InvokeQueueTransaction(queue => {
+            const result = [];
+            for(const container of containers) {
+                if(!queue.some(task => container?.IsSameAs(task.Media))) {
+                    const task = new DownloadTask(container as StoreableMediaContainer<IMediaItem>, this.storageController);
+                    result.push(task);
+                    queue.push(task);
+                }
+            }
+            return result;
+        });
+        this.TasksAdded.Dispatch(this, added);
+        this.Process();
+    }
+
+    /**
+     * Remove the given {@link tasks} from the download queue.
+     * Only tasks that are present in the download queue will be removed.
+     */
+    public async Dequeue(...tasks: IDownloadTask[]): Promise<void> {
+        const removed = await this.InvokeQueueTransaction(queue => {
+            const result = [];
+            this.queue = queue.filter(task => {
+                if(tasks.includes(task)) {
+                    result.push(task);
+                    task.Abort();
+                    return false;
+                } else {
+                    return true;
+                }
+            });
+            return result;
+        });
+        this.TasksRemoved.Dispatch(this, removed);
+    }
+
+    private async Process() {
+        if(this.processing) {
+            return;
+        }
+        this.processing = true;
+
+        while(this) {
+            try {
+                const task = this.queue.find(t => t.Status === Status.Queued);
+                if(task) {
+                    await task.Run();
+                } else {
+                    await new Promise(resolve => setTimeout(resolve, 750));
+                }
+            } catch { /* IGNORE */ }
+        }
+
+        this.processing = false;
     }
 }
---
+++ src/engine/DownloadManager_test.ts
@@ -0,0 +1,236 @@
+import { mock, mockFn } from 'jest-mock-extended';
+import { Request as RequestMock } from 'node-fetch';
+global.Request = RequestMock;
+import { DownloadManager } from './DownloadManager';
+import type { IDownloadTask, DownloadTask } from './DownloadTask';
+import { type IMediaContainer, StoreableMediaContainer, type IMediaItem } from './providers/MediaPlugin';
+import type { SettingsManager } from './SettingsManager';
+import type { StorageController } from './StorageController';
+
+function MockContainer(identifier: string, parent: IMediaContainer = undefined) {
+    const base = {};
+    Object.defineProperty(base, 'Parent', { get: () => parent });
+    Object.defineProperty(base, 'Identifier', { get: () => identifier });
+    const container = mock<StoreableMediaContainer<IMediaItem>>(base);
+    container.IsSameAs.mockImplementation(StoreableMediaContainer.prototype.IsSameAs.bind(base));
+    return container;
+}
+
+class TestFixture {
+
+    public readonly StorageControllerMock = mock<StorageController>();
+    public readonly SettingsManagerMock = mock<SettingsManager>();
+
+    public CreateTestee() {
+        return new DownloadManager(this.StorageControllerMock, this.SettingsManagerMock);
+    }
+}
+
+describe('DownloadManager', () => {
+
+    describe('Constructor', () => {
+
+        it('Should correctly initialize', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            expect(testee).toBeInstanceOf(DownloadManager);
+        });
+    });
+
+    describe('GetTasks', () => {
+
+        it('Should be empty by default', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const tasks = await testee.GetTasks();
+
+            expect(tasks.length).toBe(0);
+        });
+
+        it('Should get all enqueued tasks', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            await testee.Enqueue(MockContainer('①'), MockContainer('②'));
+            await testee.Enqueue(MockContainer('③'));
+            const tasks = await testee.GetTasks();
+
+            expect(tasks.length).toBe(3);
+        });
+    });
+
+    describe('Enqueue', () => {
+
+        it('Should add distinct containers', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const expected = [ '①', '②', '③' ].map(id => MockContainer(id));
+            await testee.Enqueue(...expected);
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual(expected);
+        });
+
+        it('Should only add first unique containers', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const expected = [ '①', '②', '③' ].map(id => MockContainer(id));
+            await testee.Enqueue(...expected, MockContainer('②'), MockContainer('③'), MockContainer('①'));
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual(expected);
+        });
+
+        it('Should not add duplicate containers', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const expected = [ '①', '②', '③' ].map(id => MockContainer(id));
+            await testee.Enqueue(...expected);
+            await testee.Enqueue(MockContainer('②'), MockContainer('③'), MockContainer('①'));
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual(expected);
+        });
+
+        it('Should add containers with different root nodes', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const first = MockContainer('②', MockContainer('①', MockContainer('x')));
+            const second = MockContainer('②', MockContainer('①', MockContainer('o')));
+            await testee.Enqueue(first);
+            await testee.Enqueue(second);
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual([ first, second ]);
+        });
+
+        it('Should not add containers with subsequent parent equality', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const first = MockContainer('③', MockContainer('②'));
+            const second = MockContainer('③', MockContainer('②', MockContainer('①')));
+            await testee.Enqueue(first);
+            await testee.Enqueue(second);
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual([ first ]);
+        });
+
+        it('Should not add containers with subsequent parent equality', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const first = MockContainer('③', MockContainer('②', MockContainer('①')));
+            const second = MockContainer('③', MockContainer('②'));
+            await testee.Enqueue(first);
+            await testee.Enqueue(second);
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual([ first ]);
+        });
+
+        it('Should not add containers with equivalent parents', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const first = MockContainer('③', MockContainer('②', MockContainer('①')));
+            const second = MockContainer('③', MockContainer('②', MockContainer('①')));
+            await testee.Enqueue(first);
+            await testee.Enqueue(second);
+
+            const queued = (await testee.GetTasks()).map(task => task.Media);
+            expect(queued).toStrictEqual([ first ]);
+        });
+
+    });
+
+    describe('Dequeue', () => {
+
+        it('Should remove tasks from download queue', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            await testee.Enqueue(MockContainer('①'), MockContainer('②'), MockContainer('③'), MockContainer('④'));
+            const tasks = await testee.GetTasks();
+            await testee.Dequeue(tasks[1], tasks[2]);
+
+            const queued = await testee.GetTasks();
+            expect(queued).toStrictEqual([ tasks[0], tasks[3] ]);
+        });
+    });
+
+    describe('TasksAdded', () => {
+
+        it('Should invoke expected event with all added tasks', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const callback = mockFn<(sender: DownloadManager, args: IDownloadTask[]) => void>();
+            const containers = [ '①', '②', '③' ].map(id => MockContainer(id));
+            testee.TasksAdded.Subscribe(callback);
+            await testee.Enqueue(...containers);
+            const tasks = await testee.GetTasks();
+
+            expect(callback).toBeCalledTimes(1);
+            expect(callback).toBeCalledWith(testee, tasks);
+        });
+
+        it('Should invoke expected event with only new tasks added (in given order)', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const callback = mockFn<(sender: DownloadManager, args: IDownloadTask[]) => void>();
+            const containers = [ '①', '②', '③' ].map(id => MockContainer(id));
+            await testee.Enqueue(MockContainer('②'), MockContainer('x'), MockContainer('o'));
+            testee.TasksAdded.Subscribe(callback);
+            await testee.Enqueue(containers[2], containers[1], containers[0]);
+            const tasks = await testee.GetTasks();
+
+            expect(callback).toBeCalledTimes(1);
+            expect(callback).toBeCalledWith(testee, tasks.slice(-2));
+            expect(tasks.slice(-2).map(task => task.Media)).toStrictEqual([ containers[2], containers[0] ]);
+        });
+    });
+
+    describe('TasksRemoved', () => {
+
+        it('Should invoke expected event with all removed tasks', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const callback = mockFn<(sender: DownloadManager, args: IDownloadTask[]) => void>();
+            const containers = [ '①', '②', '③', '④' ].map(id => MockContainer(id));
+            testee.TasksRemoved.Subscribe(callback);
+            await testee.Enqueue(...containers);
+            const tasks = await testee.GetTasks();
+            await testee.Dequeue(tasks[1], tasks[2]);
+
+            expect(callback).toBeCalledTimes(1);
+            expect(callback).toBeCalledWith(testee, tasks.slice(1, 3));
+        });
+
+        it('Should invoke expected event with only existing tasks removed (in given order)', async () => {
+            const fixture = new TestFixture();
+            const testee = fixture.CreateTestee();
+
+            const callback = mockFn<(sender: DownloadManager, args: IDownloadTask[]) => void>();
+            const containers = [ '①', '②', '③', '④' ].map(id => MockContainer(id));
+            await testee.Enqueue(...containers);
+            const tasks = await testee.GetTasks();
+            await testee.Dequeue(tasks[3]);
+            testee.TasksRemoved.Subscribe(callback);
+            await testee.Dequeue(tasks[3], tasks[2], tasks[1]);
+
+            expect(callback).toBeCalledTimes(1);
+            expect(callback).toBeCalledWith(testee, [ tasks[1], tasks[2] ]);
+            expect(tasks.slice(1, 3).map(task => task.Media)).toStrictEqual([ containers[1], containers[2] ]);
+        });
+    });
+});
