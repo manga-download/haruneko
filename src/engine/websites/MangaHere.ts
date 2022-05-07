85ac7a639704494daec3cd1461b48a59e751c70e
Ronny <wegener.ronny@gmail.com>
2022-05-07 14:32:12 +0200

    [mangahere] added website support

---
+++ src/engine/websites/MangaHere.ts
@@ -0,0 +1,28 @@
+import { Tags } from '../Tags';
+import icon from './MangaHere.webp';
+import { FetchRequest, FetchWindowScript } from '../FetchProvider';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as DM5 from './decorators/DM5';
+
+const extractor = Common.AnchorInfoExtractor(true);
+@Common.MangaCSS(/^https?:\/\/www\.mangahere\.cc\/manga\//, 'div.detail-info span.detail-info-right-title-font')
+@Common.MangasSinglePageCSS('/mangalist/', 'div.browse-new-block-list div.browse-new-block p.browse-new-block-content a', extractor)
+@Common.ChaptersSinglePageCSS('div#chapterlist ul li a', extractor)
+@DM5.PagesSinglePageScript()
+@Common.ImageDirect()
+export default class extends DecoratableMangaScraper /* MangaFox */ {
+
+    public constructor() {
+        super('mangahere', 'MangaHere', 'https://www.mangahere.cc', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
+    }
+
+    public override async Initialize(): Promise<void> {
+        const request = new FetchRequest(this.URI.href);
+        return FetchWindowScript(request, `window.cookie.set('isAdult', '1')`);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
---
+++ src/engine/websites/MangaHere_e2e.ts
@@ -0,0 +1,25 @@
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'mangahere',
+        title: 'MangaHere'
+    },
+    container: {
+        url: 'https://www.mangahere.cc/manga/darwin_s_game/',
+        id: '/manga/darwin_s_game/',
+        title: `Darwin's Game`
+    },
+    child: {
+        id: '/manga/darwin_s_game/c001/1.html',
+        title: 'Ch.001'
+    },
+    entry: {
+        index: 0,
+        size: 186_010,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
