e3ac0c90f10dfb4ff460eb13afbeca80f6803a84
Sheepux <sheepux@live.com>
2022-05-07 22:56:07 +0200

    refactoring image preview svelte style

--- src/frontend/classic/components/viewer/ThumbnailViewerImage.svelte
+++ src/frontend/classic/components/viewer/ThumbnailViewerImage.svelte
@@ -1,63 +1,47 @@
 <script lang="ts">
-    import { fly } from 'svelte/transition';
+    import { InlineLoading, ImageLoader } from "carbon-components-svelte";
+    import { fly, fade } from 'svelte/transition';
     import type { IMediaItem } from '../../../../engine/providers/MediaPlugin';
     import { Priority } from '../../../../engine/taskpool/DeferredTask';
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
 
     export let page: IMediaItem;
-    export let handleClick: () => void;
     export let title: string;
 
-    const spinner = 'https://gifimage.net/wp-content/uploads/2017/08/spinner-gif-13.gif';
-    const error = 'https://cms-assets.tutsplus.com/uploads/users/30/posts/23176/image/final-grey-2.png';
-
-    function mountThumbnail(element: HTMLDivElement, page: IMediaItem) {
-        updateThumbnail(element, page);
-        return {
-            update: (page: IMediaItem) => updateThumbnail(element, page),
-            destroy: () => unmountThumbnail(element)
-        };
-    }
-
-    function unmountThumbnail(element: HTMLDivElement) {
-        const url = element.style.backgroundImage.replace(/(url\(['"]?|['"]?\))/g, '');
-        if(url && url.startsWith('blob:')) {
-            URL.revokeObjectURL(url);
-        }
-    }
-
-    async function updateThumbnail(element: HTMLDivElement, page: IMediaItem) {
-        try {
-            unmountThumbnail(element);
-            const data = await page.Fetch(Priority.High, new AbortController().signal);
-            element.style.backgroundImage = `url('${URL.createObjectURL(data)}')`;
-        } catch {
-            element.style.backgroundImage = `url('${error}')`;
-        }
-    }
+    let dataload : Promise<Blob>
+    $: dataload = page.Fetch(Priority.High, new AbortController().signal);
 </script>
 
-<div
-    class="thumbnail"
-    style="background-image: url('{spinner}');"
-    on:click={handleClick}
-    {title}
-    in:fly
-    use:mountThumbnail={page}
-/>
+<div class="thumbnail" transition:fly on:click={(e) => { dispatch("view", page); }}>
+    {#await dataload}
+        <InlineLoading />
+    {:then data } 
+        <ImageLoader class="imgload" alt={title} src={URL.createObjectURL(data)} fadeIn>
+            <svelte:fragment slot="loading"><InlineLoading /></svelte:fragment>
+            <svelte:fragment slot="error"><InlineLoading status="error"/></svelte:fragment>
+        </ImageLoader>
+    {:catch error}
+        <InlineLoading status="error"/>
+    {/await}
+
+</div> 
 
 <style>
     .thumbnail {
         display: inline-block;
         border: 2px solid var(--cds-ui-04);
         background-color: var(--cds-ui-01);
-        background-position: center;
-        background-size: contain;
-        background-repeat: no-repeat;
         border-radius: 1em;
         margin: 0.5em;
         width: 16em;
         height: 16em;
         cursor: pointer;
         box-shadow: 1em 1em 2em var(--cds-ui-01);
+    }
+    .thumbnail :global(.imgload) {
+        width:100%;
+        height:100%;
+        object-fit: contain;
     }
 </style>
