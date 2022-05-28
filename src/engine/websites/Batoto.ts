87cc6814d7c94d62e732bd3ad52dc2f053afe0a1
Ronny <wegener.ronny@gmail.com>
2022-05-28 23:26:53 +0200

    [batoto] added website support

---
+++ src/engine/websites/Batoto.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './Batoto.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as AnyACG from './decorators/AnyACG';
+import * as Common from './decorators/Common';
+
+@AnyACG.MangaCSS(/^https?:\/\/bato\.to\/series\/\d+\/[^/]+$/, 'div.mainer div.title-set', 'h3.item-title', 'em.item-flag')
+@AnyACG.MangasMultiPageCSS('/browse?page={page}', 'div.series-list div.item-text', 'a.item-title', 'em.item-flag')
+@Common.ChaptersSinglePageCSS('div.episode-list div.main a.chapt')
+@AnyACG.PagesSinglePageJS()
+@Common.ImageDirect()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('batoto', 'Batoto (by AnyACG)', 'https://bato.to', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Multilingual, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
