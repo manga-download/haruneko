fa80ca65d996097ad1d66ad879066eb7517093d1
Ronny <wegener.ronny@gmail.com>
2022-07-07 21:28:19 +0200

    create initial reporter

--- test/jest.config.js
+++ test/jest.config.js
@@ -5,7 +5,11 @@ export default {
     rootDir: '../src',
     testMatch: [ '**/*[_.](test|spec).[jt]s(x)?' ],
     testEnvironment: 'jest-environment-jsdom',
-    reporters: [ 'default', 'jest-junit' ],
+    reporters: [
+        'default',
+        'jest-junit',
+        '../test/github-test-reporter'
+    ],
     moduleNameMapper: {
         '\\.(png|jpg|webp|ico|gif)$': '<rootDir>/img/mock.ts'
     },
