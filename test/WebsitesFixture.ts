9ed77dc9297fb5e071a3852d2e2aa98550afb38c
Ronny <wegener.ronny@gmail.com>
2022-07-17 23:30:10 +0200

    add hints for failure of website end-to-end tests

--- test/WebsitesFixture.ts
+++ test/WebsitesFixture.ts
@@ -41,7 +41,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
     private async GetRemotePlugin(pluginID: string): Promise<JSHandle<TWebsitePlugin>> {
         return this.page.evaluateHandle(async (id: string) => {
             const plugin = window.HakuNeko.PluginController.WebsitePlugins.find(website => website.Identifier === id);
-            await (plugin as MediaPuginInstance).Initialize();
+            await (plugin as MediaPuginInstance)?.Initialize();
             return plugin as TWebsitePlugin;
         }, pluginID);
     }
@@ -49,7 +49,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
     private async GetRemoteContainer(remotePlugin: JSHandle<TWebsitePlugin>, containerURL: string): Promise<JSHandle<TContainer>> {
         return remotePlugin.evaluateHandle(async (plugin: TWebsitePlugin, url: string) => {
             const container = await plugin.TryGetEntry(url) as TContainer;
-            await container.Update();
+            await container?.Update();
             return container;
         }, containerURL);
     }
@@ -57,20 +57,20 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
     private async GetRemoteChild(remoteContainer: JSHandle<TContainer>, childID: string): Promise<JSHandle<TChild>> {
         return remoteContainer.evaluateHandle(async (container: TContainer, id: string) => {
             const child = (container.Entries as TChild[]).find(child => child.Identifier === id);
-            await child.Update();
+            await child?.Update();
             return child;
         }, childID);
     }
 
     private async GetRemoteEntry(remoteChild: JSHandle<TChild>, entryIndex?: number): Promise<JSHandle<TEntry>> {
         return remoteChild.evaluateHandle(async (child: TChild, index: number) => {
-            return child.Entries[index] as TEntry;
+            return child?.Entries[index] as TEntry;
         }, entryIndex);
     }
 
     private async GetRemoteData(remoteEntry: JSHandle<TEntry>): Promise<JSHandle<Blob>> {
         return remoteEntry.evaluateHandle(async (entry: TEntry, priority: number) => {
-            return await entry.Fetch(priority, null);
+            return await entry?.Fetch(priority, null);
         }, 0);
     }
 
@@ -89,7 +89,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
 
         (this.config.plugin ? it : it.skip)('Should get initialzed website plugin', async () => {
             remotePlugin = await this.GetRemotePlugin(this.config.plugin.id);
-            expect(await remotePlugin.evaluate(plugin => plugin.Identifier)).toEqual(this.config.plugin.id);
+            expect(await remotePlugin.evaluate(plugin => plugin?.Identifier || 'Website plugin not found!')).toEqual(this.config.plugin.id);
             expect(await remotePlugin.evaluate(plugin => plugin.Title)).toEqual(this.config.plugin.title);
         }, this.config.plugin.timeout ?? 25_000);
 
@@ -97,7 +97,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
 
         (this.config.container ? it : it.skip)('Should get specific manga', async () => {
             remoteContainer = await this.GetRemoteContainer(remotePlugin, this.config.container.url);
-            expect(await remoteContainer.evaluate(container => container.Identifier)).toEqual(this.config.container.id);
+            expect(await remoteContainer.evaluate(container => container?.Identifier || 'Manga not found!')).toEqual(this.config.container.id);
             expect(await remoteContainer.evaluate(container => container.Title)).toEqual(this.config.container.title);
         }, this.config.container?.timeout ?? 7500);
 
@@ -105,7 +105,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
 
         (this.config.child ? it : it.skip)('Should get specific chapter', async () => {
             remoteChild = await this.GetRemoteChild(remoteContainer, this.config.child.id);
-            expect(await remoteChild.evaluate(child => child.Identifier)).toEqual(this.config.child.id);
+            expect(await remoteChild.evaluate(child => child?.Identifier || 'Chapter not found!')).toEqual(this.config.child.id);
             expect(await remoteChild.evaluate(child => child.Title)).toEqual(this.config.child.title);
         }, this.config.child?.timeout ?? 7500);
 
@@ -113,7 +113,7 @@ export class TestFixture<TWebsitePlugin extends IMediaContainer, TContainer exte
 
         (this.config.entry ? it : it.skip)('Should get specific page', async () => {
             remoteEntry = await this.GetRemoteEntry(remoteChild, this.config.entry.index);
-            expect(await remoteEntry.evaluate(page => page.Parent.Identifier)).toEqual(this.config.child.id);
+            expect(await remoteEntry.evaluate(page => page?.Parent?.Identifier || 'Page not found!')).toEqual(this.config.child.id);
             expect(await remoteEntry.evaluate(page => page.Parent.Title)).toEqual(this.config.child.title);
         }, this.config.entry?.timeout ?? 7500);
 
