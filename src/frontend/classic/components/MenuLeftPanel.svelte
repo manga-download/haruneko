9bd214db8021482037346516eae438202b336788
Sheepux <sheepux@live.com>
2022-08-16 00:46:43 +0200

    scope svelte global styles (resolve warnings)

--- src/frontend/classic/components/MenuLeftPanel.svelte
+++ src/frontend/classic/components/MenuLeftPanel.svelte
@@ -57,131 +57,141 @@
     selectedTab={settingsSelectedTabs}
 />
 <SideNav bind:isOpen={isSideNavOpen} rail>
-    <SideNavItems>
-        <SideNavLink
-            text="[RES:Home]"
-            icon={Home}
-            on:click={() => dispatch('home')}
-        />
-        <SideNavLink
-            text="[RES:Plugins]"
-            icon={PlugFilled}
-            on:click={() => (isPluginModalOpen = true)}
-        />
-        <SideNavMenu text="[RES:Settings]" icon={Settings}>
+    <span class="menuleftpanel">
+        <SideNavItems>
             <SideNavLink
-                text="[RES:General]"
-                icon={SettingsAdjust}
-                on:click={() => {
-                    settingsPreselectedPlugin = undefined;
-                    settingsSelectedTabs = 0;
-                    isSettingsModalOpen = true;
-                }}
-            />
-            <SideNavLink
-                text="[RES:UI]"
-                icon={SettingsView}
-                on:click={() => {
-                    settingsPreselectedPlugin = undefined;
-                    settingsSelectedTabs = 1;
-                    isSettingsModalOpen = true;
-                }}
-            />
-            <SideNavLink
-                text="[RES:Trackers]"
-                icon={TaskSettings}
-                on:click={() => {
-                    settingsPreselectedPlugin = undefined;
-                    settingsSelectedTabs = 2;
-                    isSettingsModalOpen = true;
-                }}
-            />
-            <SideNavLink
-                text="[RES:Network]"
-                icon={NetworkOverlay}
-                on:click={() => {
-                    settingsPreselectedPlugin = undefined;
-                    settingsSelectedTabs = 3;
-                    isSettingsModalOpen = true;
-                }}
-            />
-        </SideNavMenu>
-        <SideNavMenu text="[RES:Help]" icon={Document}>
-            <SideNavLink
-                text="Documentation"
-                icon={Doc}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://hakuneko.download/docs/interface/')}
-            />
-            <SideNavLink
-                text="Discord"
-                icon={LogoDiscord}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://discordapp.com/invite/A5d3NDf')}
-            />
-            <SideNavLink
-                text="Open a ticket"
-                icon={Debug}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://hakuneko.download/docs/troubleshoot/')}
-            />
-            <SideNavLink
-                text="Home page"
+                text="[RES:Home]"
                 icon={Home}
-                class="clik-item"
-                on:click={() => window.open('https://hakuneko.download')}
+                on:click={() => dispatch('home')}
             />
             <SideNavLink
-                text="Show IP and localisation"
-                icon={Location}
-                class="clik-item"
-                on:click={() => window.open('https://ipinfo.io/json')}
+                text="[RES:Plugins]"
+                icon={PlugFilled}
+                on:click={() => (isPluginModalOpen = true)}
             />
-        </SideNavMenu>
-        <SideNavMenu text="[RES:About]" icon={Information}>
-            <SideNavLink
-                text="Code source"
-                icon={LogoGithub}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://hakuneko.download/docs/interface/')}
-            />
-            <SideNavLink
-                text="Using version X.X.X"
-                icon={App}
-                class="clik-item"
-                on:click={() => window.open('https://todo.com')}
-            />
-            <SideNavLink
-                text="Maintainers"
-                icon={Events}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://discordapp.com/invite/A5d3NDf')}
-            />
-            <SideNavLink
-                text="Contributors"
-                icon={EventsAlt}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://hakuneko.download/docs/troubleshoot/')}
-            />
-            <SideNavLink
-                text="Artwork"
-                icon={Image}
-                class="clik-item"
-                on:click={() =>
-                    window.open('https://www.deviantart.com/hakuneko3kune')}
-            />
-        </SideNavMenu>
-    </SideNavItems>
+            <SideNavMenu text="[RES:Settings]" icon={Settings}>
+                <SideNavLink
+                    text="[RES:General]"
+                    icon={SettingsAdjust}
+                    on:click={() => {
+                        settingsPreselectedPlugin = undefined;
+                        settingsSelectedTabs = 0;
+                        isSettingsModalOpen = true;
+                    }}
+                />
+                <SideNavLink
+                    text="[RES:UI]"
+                    icon={SettingsView}
+                    on:click={() => {
+                        settingsPreselectedPlugin = undefined;
+                        settingsSelectedTabs = 1;
+                        isSettingsModalOpen = true;
+                    }}
+                />
+                <SideNavLink
+                    text="[RES:Trackers]"
+                    icon={TaskSettings}
+                    on:click={() => {
+                        settingsPreselectedPlugin = undefined;
+                        settingsSelectedTabs = 2;
+                        isSettingsModalOpen = true;
+                    }}
+                />
+                <SideNavLink
+                    text="[RES:Network]"
+                    icon={NetworkOverlay}
+                    on:click={() => {
+                        settingsPreselectedPlugin = undefined;
+                        settingsSelectedTabs = 3;
+                        isSettingsModalOpen = true;
+                    }}
+                />
+            </SideNavMenu>
+            <SideNavMenu text="[RES:Help]" icon={Document}>
+                <SideNavLink
+                    text="Documentation"
+                    icon={Doc}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open(
+                            'https://hakuneko.download/docs/interface/'
+                        )}
+                />
+                <SideNavLink
+                    text="Discord"
+                    icon={LogoDiscord}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open('https://discordapp.com/invite/A5d3NDf')}
+                />
+                <SideNavLink
+                    text="Open a ticket"
+                    icon={Debug}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open(
+                            'https://hakuneko.download/docs/troubleshoot/'
+                        )}
+                />
+                <SideNavLink
+                    text="Home page"
+                    icon={Home}
+                    class="clik-item"
+                    on:click={() => window.open('https://hakuneko.download')}
+                />
+                <SideNavLink
+                    text="Show IP and localisation"
+                    icon={Location}
+                    class="clik-item"
+                    on:click={() => window.open('https://ipinfo.io/json')}
+                />
+            </SideNavMenu>
+            <SideNavMenu text="[RES:About]" icon={Information}>
+                <SideNavLink
+                    text="Code source"
+                    icon={LogoGithub}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open(
+                            'https://hakuneko.download/docs/interface/'
+                        )}
+                />
+                <SideNavLink
+                    text="Using version X.X.X"
+                    icon={App}
+                    class="clik-item"
+                    on:click={() => window.open('https://todo.com')}
+                />
+                <SideNavLink
+                    text="Maintainers"
+                    icon={Events}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open('https://discordapp.com/invite/A5d3NDf')}
+                />
+                <SideNavLink
+                    text="Contributors"
+                    icon={EventsAlt}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open(
+                            'https://hakuneko.download/docs/troubleshoot/'
+                        )}
+                />
+                <SideNavLink
+                    text="Artwork"
+                    icon={Image}
+                    class="clik-item"
+                    on:click={() =>
+                        window.open('https://www.deviantart.com/hakuneko3kune')}
+                />
+            </SideNavMenu>
+        </SideNavItems>
+    </span>
 </SideNav>
 
 <style>
-    :global(a.clik-item) {
+    .menuleftpanel :global(a.clik-item) {
         cursor: pointer;
     }
 </style>
