665814f73d719d4f9d5ef7b9488cd59488ff09ed
Sheepux <sheepux@live.com>
2022-09-23 00:37:43 +0200

    console cleanup

--- src/frontend/classic/stores/Events.ts
+++ src/frontend/classic/stores/Events.ts
@@ -5,7 +5,6 @@ export function EventWatcher<TSender, TArgs>(intitalvalue: TArgs, event: Event<T
     return readable<TArgs>(intitalvalue, set => {
         set(intitalvalue);
         const callback = (sender: TSender, value: TArgs) => {
-            console.log('should ignore?',scope,value);
             const ignore = scope ? value !== scope : false;
             if (!ignore) set(value);
         };
