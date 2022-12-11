89096674d32796cb83bf5de2a51b21f0d0650c96
Sheepux <sheepux@live.com>
2022-12-11 22:44:40 +0100

    Suggestions migrate from flex to grid

--- src/frontend/classic/components/Suggestions.svelte
+++ src/frontend/classic/components/Suggestions.svelte
@@ -47,13 +47,13 @@
 </script>
 
 {#if checkNewContent}
-    <div id="Suggestions" class="border">
-        <Tile id="Continue">
+    <Tile id="Suggestions" class="border">
+        <ClickableTile id="Continue" light>
             <h4 style="text-align:center;">
                 Continue
                 <BookmarkAdd size={24} />
             </h4>
-        </Tile>
+        </ClickableTile>
         {#each suggestions as bookmark (bookmark.Identifier)}
             <ClickableTile
                 class="suggesttile"
@@ -81,46 +81,34 @@
                 </div>
             </ClickableTile>
         {/each}
-    </div>
+    </Tile>
 {/if}
 
 <style>
-    #Suggestions {
-        border: 2px solid var(--cds-ui-04);
-        border-radius: 1em;
-        padding: 1em;
-        display: flex;
-        flex-flow: row wrap;
+    :global(#Suggestions) {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, 10em);
+        grid-template-rows: auto auto; /* 2 rows */
+        grid-auto-rows: 0px; /* next rows equal to 0 */
+        grid-gap: 0.5em;
+        overflow: hidden;
     }
-    #Suggestions :global(#Continue) {
-        position: relative;
-        width: 2em;
+    :global(#Suggestions #Continue) {
+        background-color: var(--cds-background);
     }
-    :global(.rotate) {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%) rotate(-90deg);
-    }
-    #Suggestions :global(.suggesttile) {
-        height: 7em;
-        width: 7em;
-        margin: 0.2em;
-        padding: 0;
-    }
-    #Suggestions :global(.suggesttile .bx--tag span) {
+    :global(#Suggestions .suggesttile .bx--tag span) {
         width: 100%;
         overflow: hidden;
         text-overflow: ellipsis;
     }
-    #Suggestions :global(.suggesttitle) {
+    :global(#Suggestions .suggesttitle) {
         width: 95%;
         white-space: nowrap;
         overflow: hidden;
         text-overflow: ellipsis;
     }
 
-    #Suggestions :global(.suggestcount) {
+    :global(#Suggestions .suggestcount) {
         position: absolute;
         bottom: 0;
         right: 0.2em;
