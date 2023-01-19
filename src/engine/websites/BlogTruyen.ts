372746d4cf62d36b999b79ed7e2a97f9a9ef0c0b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-19 19:33:38 +0000

    BlogTruyen : filter bloat images

--- src/engine/websites/BlogTruyen.ts
+++ src/engine/websites/BlogTruyen.ts
@@ -2,17 +2,18 @@ import { Tags } from '../Tags';
 import icon from './BlogTruyen.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
+import * as MangaStream from './decorators/WordPressMangaStream';
 
 @Common.MangaCSS(/^https?:\/\/blogtruyen\.vn\/[^/]+\//, 'section.manga-detail h1.entry-title')
 @Common.MangasMultiPageCSS('/ajax/Search/AjaxLoadListManga?key=tatca&orderBy=1&p={page}', 'div.list p span.tiptip a', 1, 1, 200)
 @Common.ChaptersSinglePageCSS('div#list-chapters span.title a')
-@Common.PagesSinglePageCSS('article#content img')
+@MangaStream.PagesSinglePageCSS([/donate/], 'article#content img:not([marginheight])')
 @Common.ImageDirect()
 
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('blogtruyen', `BlogTruyen`, 'https://blogtruyen.vn', Tags.Language.Vietnamese, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
+        super('blogtruyen', `BlogTruyen`, 'https://blogtruyen.vn', Tags.Language.Vietnamese, Tags.Media.Manhua, Tags.Media.Manhua, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
