bf6e9ef84c0db7c89decc46d7a3b7e3465e98e09
Ronny <wegener.ronny@gmail.com>
2023-01-22 02:23:57 +0100

    fix test config

---
+++ app/nw/test/jest.config.js
@@ -0,0 +1,19 @@
+export default {
+    preset: 'ts-jest/presets/js-with-ts-esm',
+    rootDir: '../src',
+    testMatch: [ '**/*[_.](test|spec).[jt]s(x)?' ],
+    testEnvironment: 'node',
+    reporters: [
+        'default',
+        'jest-junit',
+        '../../../test/github-test-reporter'
+    ],
+    globals: {
+        'ts-jest': {
+            tsconfig: {
+                allowJs: true,
+                esModuleInterop: true
+            }
+        }
+    }
+};
