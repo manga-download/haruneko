e318d700cdbeac5c1495dde94a407f4f8d23cec7
Ronny <wegener.ronny@gmail.com>
2023-01-22 17:21:13 +0100

    initial rpc implementation

---
+++ app/nw/src/rpc/Contract.ts
@@ -0,0 +1,19 @@
+import type { RemoteContract } from 'websocket-rpc';
+
+// See => chrome.cookies.Cookie
+type TypeFromInterface<T> = {
+    [key in keyof T]: T[key];
+};
+
+export class Contract implements RemoteContract<Contract> {
+
+    async SetCloudFlareBypass(userAgent: string, cookies: TypeFromInterface<chrome.cookies.Cookie>[]): Promise<void> {
+        alert('SetCloudFlareBypass\n\n' + userAgent);
+        console.log('SetCloudFlareBypass', '=>', userAgent, cookies);
+    }
+
+    async LoadMediaContainerFromURL(url: string): Promise<void> {
+        alert('LoadMediaContainerFromURL\n\n' + url);
+        console.log('LoadMediaContainerFromURL', '=>', url);
+    }
+}
---
+++ app/nw/src/rpc/Server.ts
@@ -0,0 +1,71 @@
+import { createServer, type Server, type IncomingMessage } from 'node:http';
+import { createHash } from 'node:crypto';
+import { CreateServer, type WebSocketServer } from 'websocket-rpc/server';
+import { Contract } from './Contract';
+
+class AccessDeniedError extends Error {
+    constructor(message?: string) {
+        super(message);
+        this.name = AccessDeniedError.name;
+    }
+}
+
+export class RPCServer {
+
+    private readonly httpd: Server;
+    private readonly websocket: Promise<WebSocketServer>;
+    private allowedOrigins: RegExp[] = [];
+    private token?: string = undefined;
+
+    constructor(endpoint: string) {
+        this.httpd = createServer()
+            .on('connect', this.DestroyConnection.bind(this))
+            .on('request', this.DestroyConnection.bind(this))
+            .on('upgrade', this.Authorize.bind(this));
+        this.websocket = CreateServer(new Contract(), {
+            server: this.httpd,
+            path: endpoint
+        });
+    }
+
+    public async Stop(): Promise<void> {
+        // TODO: Implement locking to prevent concurrent calls ...
+        return new Promise<void>(resolve => this.httpd.close(() => resolve()));
+        //return new Promise<void>(resolve => this.websocket.close(() => resolve()));
+    }
+
+    public async Listen(port: number, secret: string, allowedOrigins: RegExp[]) {
+        // TODO: Implement locking to prevent concurrent calls ...
+        this.token = createHash('SHA256').update(secret).digest('hex').toUpperCase();
+        this.allowedOrigins = allowedOrigins;
+        await this.Stop();
+        this.httpd.listen(port);
+    }
+
+    private DestroyConnection(request: IncomingMessage) {
+        request?.socket?.destroy();
+    }
+
+    private Authorize(request: IncomingMessage) {
+        try {
+            if(!this.allowedOrigins.some(pattern => pattern.test(request.headers.origin ?? ''))) {
+                throw new AccessDeniedError(`Requests with origin '${request.headers.origin}' are not allowed!`);
+            }
+            if(!/upgrade/i.test(request.headers.connection ?? '')) {
+                throw new AccessDeniedError(`Requests with connection '${request.headers.connection}' are not allowed!`);
+            }
+            if(!/websocket/i.test(request.headers.upgrade ?? '')) {
+                throw new AccessDeniedError(`Request with upgrade '${request.headers.upgrade}' are not allowed!`);
+            }
+            if(new URL(request.url ?? '', 'http://' + request.headers.host).searchParams.get('token') !== this.token) {
+                // TODO: Show forbidden response with helpful error message?
+                //request.end(403);
+                throw new AccessDeniedError('Requests with missing or invalid token are not allowed!');
+            }
+            console.log(`RPC Accepted <${request.method}>:`, request.url, request.headers);
+        } catch(error) {
+            console.warn(`RPC Rejected <${request.method}>:`, request.url, error);
+            this.DestroyConnection(request);
+        }
+    }
+}
