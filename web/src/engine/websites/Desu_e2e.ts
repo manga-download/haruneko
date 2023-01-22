76091c173b4c2ed79883d8c6bba5aee5203c49a3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-22 10:36:49 +0000

    Legacy to Haru #2 (#75)
    * Porting 4 websites to Haru
    
    * CyComi
    * DeathTollScans
    * DemoneCeleste
    * Desu

---
+++ web/src/engine/websites/Desu_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'desu',
+        title: 'Desu'
+    },
+    container: {
+        url: 'https://desu.me/manga/wondance.3628/',
+        id: '/manga/wondance.3628/',
+        title: 'WonDance'
+    },
+    child: {
+        id: '/manga/wondance.3628/vol1/ch1/rus',
+        title: 'Том 1. Глава 1 - Танец Ванды-сан'
+    },
+    entry: {
+        index: 5,
+        size: 146_711,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
