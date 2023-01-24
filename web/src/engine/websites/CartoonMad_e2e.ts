0ba8b846de7db354093eb834589585dfbe69ae58
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-24 21:31:55 +0000

    Legacy to haru : #3 (#80)
    * Legady to haru #3
    
    * CartoonMad
    * DigitalTeam
    * Doujins
    * DynastyScans

---
+++ web/src/engine/websites/CartoonMad_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'cartoonmad',
+        title: 'CarToonMad'
+    },
+    container: {
+        url: 'https://www.cartoonmad.com/comic/1152.html', //One Piece
+        id: '/comic/1152.html',
+        title: '海賊王'
+    },
+    child: {
+        id: '/comic/115200011104001.html',
+        title: '第 001 卷'
+    },
+    entry: {
+        index: 0,
+        size: 116_736,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
