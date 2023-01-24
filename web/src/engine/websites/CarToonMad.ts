b5c15a6e97a1f7d02b1e496bcb31cc2357350fd7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-24 08:50:39 +0000

    Update CarToonMad.ts

--- web/src/engine/websites/CarToonMad.ts
+++ web/src/engine/websites/CarToonMad.ts
@@ -20,15 +20,15 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const referer = new URL(chapter.Identifier, this.URI);
+        const referer = new URL(chapter.Identifier, this.URI).href;
         const request = new FetchRequest(new URL(chapter.Identifier, this.URI.href.replace('.com', '.cc')).href, {
-            headers : { 'referer' : referer.href}
+            headers : { 'referer' : referer}
         });
         const data = await FetchCSS(request, 'body');
         const maxpage = parseInt(data[0].querySelector('a:nth-last-of-type(2).pages').textContent);
         const pageone = data[0].querySelector <HTMLImageElement>('a > img[oncontextmenu]').src;
         return [...new Array(maxpage).keys()].map(index => {
-            return new Page(this, chapter, new URL(pageone.replace(/(\d+)$/, String(index + 1).padStart(3, '0')), this.URI), { 'Referer': referer.href });
+            return new Page(this, chapter, new URL(pageone.replace(/(\d+)$/, String(index + 1).padStart(3, '0')), this.URI), { 'Referer': referer });
         });
     }
 }
