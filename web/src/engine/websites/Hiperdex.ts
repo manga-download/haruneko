d232818cf524aed07fba93e64f31c40ace011515
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-27 19:27:44 +0000

    Hiperdex url fix (#83)
    * Hiperdex url fix
    
    * Update Hiperdex_e2e.ts

--- web/src/engine/websites/Hiperdex.ts
+++ web/src/engine/websites/Hiperdex.ts
@@ -3,7 +3,7 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^https?:\/\/1sthiperdex.com\/manga\/[^/]+\/$/, 'meta[property="og:title"]:not([content*="Hiperdex"])')
+@Madara.MangaCSS(/^https?:\/\/hiperdex.com\/manga\/[^/]+\/$/, 'meta[property="og:title"]:not([content*="Hiperdex"])')
 @Madara.MangasMultiPageCSS(undefined, 1000, '/manga-list/page/{page}/')
 @Madara.ChaptersSinglePageAJAXv2()
 @Madara.PagesSinglePageCSS()
@@ -11,6 +11,6 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('hiperdex', 'Hiperdex', 'https://1sthiperdex.com', Tags.Media.Manhwa, Tags.Source.Aggregator, Tags.Rating.Erotica, Tags.Language.English);
+        super('hiperdex', 'Hiperdex', 'https://hiperdex.com', Tags.Media.Manhwa, Tags.Source.Aggregator, Tags.Rating.Erotica, Tags.Language.English);
     }
 }
