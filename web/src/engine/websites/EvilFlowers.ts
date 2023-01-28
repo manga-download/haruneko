0c72be1fdb996cdf3a27f6fa25f22d9082c257c5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-01-28 12:03:59 +0000

    Legacy to Haru #4
    * EarlyMangas
    * EvilFlowers
    * FuryoSquad
    * Goldenmangas

---
+++ web/src/engine/websites/EvilFlowers.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './EvilFlowers.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as FoolSlide from './decorators/FoolSlide';
+
+@FoolSlide.MangaCSS(/https?:\/\/reader\.evilflowers\.com\/series\//)
+@FoolSlide.MangasMultiPageCSS()
+@FoolSlide.ChaptersSinglePageCSS()
+@FoolSlide.PagesSinglePageREGEX()
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('evilflowers', `EvilFlowers`, 'http://reader.evilflowers.com', Tags.Language.English, Tags.Source.Scanlator, Tags.Media.Manga);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
