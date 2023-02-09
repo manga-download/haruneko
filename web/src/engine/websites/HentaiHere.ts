dd9288a604e3c8bcd6c2e0ae7c05fcf4d6bbb1ab
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-02-09 16:26:50 +0000

    Legacy to Haru 6
    * HentaiHand
    * HentaiHere
    * HiperCool
    * IMHentai

---
+++ web/src/engine/websites/HentaiHere.ts
@@ -0,0 +1,31 @@
+import { Tags } from '../Tags';
+import icon from './HentaiHere.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+const script = `
+            new Promise((resolve, reject) => {
+                    try {
+                        resolve(rff_imageList.map(page => 'https://hentaicdn.com/hentai'+ page));
+                    } catch(error) {
+                        reject(error);
+                    }
+            });
+        `;
+
+@Common.MangaCSS(/^https?:\/\/hentaihere\.com/, 'div.bg-black h4 a', Common.ElementLabelExtractor('span'))
+@Common.MangasMultiPageCSS('/directory?page={page}', 'div.item a.text-ellipsis')
+@Common.ChaptersSinglePageCSS('li.sub-chp a#chapterBlock', Common.AnchorInfoExtractor(false, 'i'))
+@Common.PagesSinglePageJS(script, 500)
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('hentaihere', `HentaiHere`, 'https://hentaihere.com', Tags.Language.English, Tags.Media.Manga, Tags.Rating.Erotica, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
---
+++ web/src/engine/websites/HiperCool_e2e.ts
@@ -0,0 +1,25 @@
+import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'hipercool',
+        title: 'Hiper Cool'
+    },
+    container: {
+        url: 'https://hipercool.xyz/manga/nama-hone-josou/',
+        id: '{"post":"19381","slug":"/manga/nama-hone-josou/"}',
+        title: 'Nama-Hone Josou'
+    },
+    child: {
+        id: '/manga/nama-hone-josou/vol-1/',
+        title: 'Vol. 1'
+    },
+    entry: {
+        index: 0,
+        size: 325_942,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
