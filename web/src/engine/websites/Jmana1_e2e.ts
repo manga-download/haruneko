780c083819a91c68fc883e72bc88405f65846201
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-02-14 22:03:35 +0000

    Update Jmana1_e2e.ts

--- web/src/engine/websites/Jmana1_e2e.ts
+++ web/src/engine/websites/Jmana1_e2e.ts
@@ -7,7 +7,7 @@ const config: Config = {
     },
     container: {
         url: 'https://kr14.jmana.one/comic_list_title?bookname=' + encodeURIComponent('저, 용사가 아니니까요.'),
-        id: '/comic_list_title?bookname=%EC%A0%80%2C+%EC%9A%A9%EC%82%AC%EA%B0%80+%EC%95%84%EB%8B%88%EB%8B%88%EA%B9%8C%EC%9A%94.',
+        id: '/comic_list_title?bookname=' + encodeURIComponent('저, 용사가 아니니까요.'),
         title: '저, 용사가 아니니까요.'
     },
     child: {
