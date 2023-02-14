d97d92a6f087b9bb52d1b8b2c56ea2ad808d92ea
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-02-14 22:02:33 +0000

    Update Jmana1_e2e.ts

--- web/src/engine/websites/Jmana1_e2e.ts
+++ web/src/engine/websites/Jmana1_e2e.ts
@@ -6,7 +6,7 @@ const config: Config = {
         title: '제이마나 (Jaymana)'
     },
     container: {
-        url: `'https://kr14.jmana.one/comic_list_title?bookname=' + encodeURIComponent('저, 용사가 아니니까요.')`
+        url: 'https://kr14.jmana.one/comic_list_title?bookname=' + encodeURIComponent('저, 용사가 아니니까요.'),
         id: '/comic_list_title?bookname=%EC%A0%80%2C+%EC%9A%A9%EC%82%AC%EA%B0%80+%EC%95%84%EB%8B%88%EB%8B%88%EA%B9%8C%EC%9A%94.',
         title: '저, 용사가 아니니까요.'
     },
