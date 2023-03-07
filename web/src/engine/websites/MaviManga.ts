8c79fc77fdfc2603a5fc5cd85e2299cef5c4d9f5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-07 13:27:12 +0100

    Legacy To Haru 10
    * MangaWorld
    * MangaYeh
    * MaviManga
    * MenudoFansub

---
+++ web/src/engine/websites/MaviManga.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './MaviManga.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+@Common.MangaCSS(/^https?:\/\/mavimanga\.com\/manga\//, 'span.mangasc-title')
+@Common.MangasMultiPageCSS('/manga-listesi/sayfa/{page}/', 'ul.manga-list li a')
+@Common.ChaptersSinglePageCSS('div.mangaep-list tbody tr td:first-of-type a')
+@Common.PagesSinglePageCSS('div.viewer-cnt div#all img.img-responsive')
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('mavimanga', `Mavi Manga`, 'https://mavimanga.com', Tags.Language.Turkish, Tags.Media.Manga, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
