7c1c3559136f4eceae5bf065bf652bc29ff7cd5a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-09 09:34:07 +0000

    Legacy To Haru 10 (#103)
    * MangaWorld
    * MangaYeh
    * MaviManga
    * MenudoFansub

---
+++ web/src/engine/websites/MangaWorld_e2e.ts
@@ -0,0 +1,25 @@
+import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'mangaworld',
+        title: 'Manga World'
+    },
+    container: {
+        url: 'https://www.mangaworld.so/manga/2682/subzero/',
+        id: '/manga/2682/subzero/',
+        title: 'SubZero'
+    },
+    child: {
+        id: '/manga/2682/subzero/read/6406d54f3382854b52f4311f?style=list',
+        title: 'Capitolo 47'
+    },
+    entry: {
+        index: 0,
+        size: 1_908_032,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
