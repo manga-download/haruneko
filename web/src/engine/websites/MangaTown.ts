40610615e15982ec91cfc7c056c0a8b81c485e54
Ronny <wegener.ronny@gmail.com>
2023-03-09 07:47:51 +0100

    [mangatown] improve performance by using html page links instead of image links

--- web/src/engine/websites/MangaTown.ts
+++ web/src/engine/websites/MangaTown.ts
@@ -1,14 +1,14 @@
 import { Tags } from '../Tags';
 import icon from './MangaTown.webp';
-import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS, FetchRequest } from '../FetchProvider';
+import { Fetch, FetchCSS, FetchRequest } from '../FetchProvider';
+import type { Priority } from '../taskpool/DeferredTask';
 
 @Common.MangaCSS(/^https?:\/\/www\.mangatown\.com\/manga\//, 'div.article_content h1.title-top')
 @Common.MangasMultiPageCSS('/directory/0-0-0-0-0-0/{page}.htm', 'ul.manga_pic_list li p.title a', 1, 1, 0, Common.AnchorInfoExtractor(true))
 @Common.ChaptersSinglePageCSS('ul.chapter_list li a')
-@Common.ImageDirect()
-
+@Common.PagesSinglePageCSS('div.manga_read_footer div.page_select select option', (option: HTMLOptionElement) => option.value)
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -19,19 +19,19 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const pageList = [];
-        const url = new URL(chapter.Identifier, this.URI);
-        let request = new FetchRequest(url.href);
-        const data = await FetchCSS<HTMLOptionElement>(request, 'div.manga_read_footer div.page_select select option');
+    public override async FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
+        const request = new FetchRequest(page.Link.href);
+        const images = await FetchCSS<HTMLImageElement>(request, 'img#image');
 
-        for (const page of data) {
-            request = new FetchRequest(new URL(page.value, this.URI).href);
-            const pages = await FetchCSS<HTMLImageElement>(request, 'img#image');
-            pages.forEach(element => pageList.push(new Page(this, chapter, new URL(element.src), { Referer : 'mangahere.com' })));
-        }
-
-        return pageList;
+        return this.imageTaskPool.Add(async () => {
+            const request = new FetchRequest(new URL(images.pop().src, this.URI).href, {
+                signal: signal,
+                headers: {
+                    Referer: 'mangahere.com'
+                }
+            });
+            const response = await Fetch(request);
+            return response.blob();
+        }, priority, signal);
     }
-
 }
