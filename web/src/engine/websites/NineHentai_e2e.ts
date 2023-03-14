7b9f039f2f57d9f8eaa620e429e4de6cf27969f0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-14 09:57:07 +0000

    Legacy to Haru 12 (#108)
    * Legacy to Haru 12
    
    * MundoMangaKun
    * Nightow
    * 9Hentai
    * PatyScans

---
+++ web/src/engine/websites/NineHentai_e2e.ts
@@ -0,0 +1,27 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: '9hentai',
+        title: '9hentai'
+    },
+    container: {
+        url: 'https://9hentai.to/g/61506/',
+        id: '/g/61506/',
+        title: '(@RoBosquat) Aphrodisiacs and Giyushino (Demon Slayer)',
+    },
+    child: {
+        id: '/g/61506/',
+        title: '(@RoBosquat) Aphrodisiacs and Giyushino (Demon Slayer)',
+    },
+    entry: {
+        index: 0,
+        size: 344_537,
+        type: 'image/jpeg',
+        timeout : 30000 //tried to find manga that download faster. First was 45s.
+
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
---
+++ web/src/engine/websites/PatyScans.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './PatyScans.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as FoolSlide from './decorators/FoolSlide';
+
+@FoolSlide.MangaCSS(/^https?:\/\/lector\.patyscans\.com/)
+@FoolSlide.MangasMultiPageCSS()
+@FoolSlide.ChaptersSinglePageCSS()
+@FoolSlide.PagesSinglePageREGEX(/"pages":(\[[\S\W]+\]),/)
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('patyscans', `PatyScans`, 'http://lector.patyscans.com', Tags.Language.Spanish, Tags.Media.Manga, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
