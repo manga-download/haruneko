632652b00e33ce55a509a8b5605ff40a01e7e4ba
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-16 15:00:02 +0100

    Legacy to Haru 13
    * PCNet
    * PornComix
    * PowerManga
    * Pururin

---
+++ web/src/engine/websites/PCNet.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './PCNet.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as FoolSlide from './decorators/FoolSlide';
+
+@FoolSlide.MangaCSS(/^http?:\/\/pcnet\.patyscans\.com/)
+@FoolSlide.MangasMultiPageCSS()
+@FoolSlide.ChaptersSinglePageCSS()
+@FoolSlide.PagesSinglePageREGEX()
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('pcnet', `PCNet`, 'http://pcnet.patyscans.com', Tags.Language.Spanish, Tags.Media.Manga, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
