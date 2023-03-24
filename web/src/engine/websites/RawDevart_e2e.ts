80eb386b19e23e7383b91a747d91e1d09c66f32e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-24 10:37:50 +0000

    Legacy To Haru 14 (#116)
    * Legaty To Haru 14
    
    * RawDevart
    * RawSenManga
    * ReadM
    * ReadManga

---
+++ web/src/engine/websites/RawDevart_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'rawdevart',
+        title: 'RawDevart'
+    },
+    container: {
+        url: 'https://endevart.com/comic/ca460be0-15c4-426c-a35f-4b6db35b7d49',
+        id: 'ca460be0-15c4-426c-a35f-4b6db35b7d49',
+        title: 'Kono Gomi o Nanto Yobu'
+    },
+    child: {
+        id: 'cf62cc17-e5c5-4aaa-a469-d32189b9134a',
+        title: 'Chapter 21'
+    },
+    entry: {
+        index: 0,
+        size: 1_179_102,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
---
+++ web/src/engine/websites/ReadManga.ts
@@ -0,0 +1,29 @@
+import { Tags } from '../Tags';
+import icon from './ReadManga.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+const pagesScript = `
+    rm_h.pics.map(picture => {
+        const url = new URL(picture.url)
+        url.search = '';
+        return url.href;
+    });
+`;
+
+@Common.MangaCSS(/^https?:\/\/readmanga\.live/, 'h1.names span.name')
+@Common.MangasMultiPageCSS('/list?offset={page}', 'div.tile div.desc h3 a', 0, 70, 0)
+@Common.ChaptersSinglePageCSS('tr.item-row a.chapter-link')
+@Common.PagesSinglePageJS(pagesScript, 500)
+@Common.ImageDirect()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('readmanga', `ReadManga`, 'https://readmanga.live', Tags.Language.Russian, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
