865837a9f1c46829bb5be458ec87a27d99c9129c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-26 16:57:19 +0200

    tuning tets

--- web/src/engine/websites/ReaperScansBR_e2e.ts
+++ web/src/engine/websites/ReaperScansBR_e2e.ts
@@ -4,7 +4,7 @@ const config: Config = {
     plugin: {
         id: 'reaperscansbr',
         title: 'Reaper Scans (Portuguese)',
-        //timeout : 25000
+        timeout : 35000,//website takes around this time to load in real browser o_o
     },
     container: {
         url: 'https://reaperscans.net/series/max-level-player-1679526000038',
@@ -14,7 +14,7 @@ const config: Config = {
     child: {
         id: '/series/max-level-player-1679526000038/capitulo-10',
         title: 'Capítulo 10',
-        //timeout : 20000
+        timeout: 20000
     },
     entry: {
         index: 1,
