244cc7a345e3cb4473413ce38f257cf3a547abd9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-03-26 14:39:50 +0200

    Legacy To Haru 15
    * ReaperScansBR
    * ReaperScansFR
    * ReaperScansTR
    * SadScans

---
+++ web/src/engine/websites/ReaperScansBR_e2e.ts
@@ -0,0 +1,27 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'reaperscansbr',
+        title: 'Reaper Scans (Portuguese)',
+        //timeout : 25000
+    },
+    container: {
+        url: 'https://reaperscans.net/series/max-level-player-1679526000038',
+        id: '/series/max-level-player-1679526000038',
+        title: 'Max Level Player'
+    },
+    child: {
+        id: '/series/max-level-player-1679526000038/capitulo-10',
+        title: 'Capítulo 10',
+        //timeout : 20000
+    },
+    entry: {
+        index: 1,
+        size: 1_536_165,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
---
+++ web/src/engine/websites/ReaperScansTR.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './ReaperScansTR.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as Madara from './decorators/WordPressMadara';
+
+@Madara.MangaCSS(/^https?:\/\/reaperscanstr\.com\/seri\//)
+@Madara.MangasMultiPageAJAX()
+@Madara.ChaptersSinglePageCSS()
+@Madara.PagesSinglePageCSS()
+@Common.ImageDirect()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('reaperscanstr', `Reaper Scans (Turkish)`, 'https://reaperscanstr.com', Tags.Language.Turkish, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
