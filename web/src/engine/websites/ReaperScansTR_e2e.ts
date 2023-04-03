395dcc1579900f5b560f29be42afb75e463edaef
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-03 14:50:38 +0200

    Legacy to Haru 15
    * ReaperScansBR
    * ReaperScansFR
    * ReaperScansTR
    * Sadscans
    * ScantradUnion
    * SenManga
    * SeriManga
    * Shqaa

---
+++ web/src/engine/websites/ReaperScansTR_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'reaperscanstr',
+        title: 'Reaper Scans (Turkish)',
+    },
+    container: {
+        url: 'https://reaperscanstr.com/seri/hyper-luck/',
+        id: '{"post":"1161","slug":"/seri/hyper-luck/"}',
+        title: 'Hyper Luck'
+    },
+    child: {
+        id: '/seri/hyper-luck/bolum-39/',
+        title: 'Bölüm 39'
+    },
+    entry: {
+        index: 1,
+        size: 3_006_190,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
---
+++ web/src/engine/websites/SeriManga_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'serimanga',
+        title: 'Seri Manga'
+    },
+    container: {
+        url: 'https://serimanga.com/manga/kara-yonca',
+        id: '/manga/kara-yonca' ,
+        title: 'Black Clover'
+    },
+    child: {
+        id: '/manga/kara-yonca/277',
+        title: 'Bölüm 277: Yaban Güllerinin Kraliçesi'
+    },
+    entry: {
+        index: 0,
+        size: 301_207,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
