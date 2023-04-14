c92d9098d13e0455aaeac126a8d8548f2865563d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-14 16:55:08 +0000

    Legacy To Haru 17 (#130)
    * TopToon
    * TRWebtoon
    * TuMangaOnlineHentai
    * TvYManga

---
+++ web/src/engine/websites/TRWebtoon.ts
@@ -0,0 +1,29 @@
+﻿import { Tags } from '../Tags';
+import icon from './TRWebtoon.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+function ChapterExtractor(anchor: HTMLAnchorElement) {
+    const id = anchor.pathname;
+    const title = anchor.text.split('.Bölüm')[0].trim();
+    return { id, title };
+}
+
+@Common.MangaCSS(/^https?:\/\/trwebtoon\.com\/webtoon\//, 'div#movie-card h2.movie__title')
+@Common.MangasMultiPageCSS('/webtoon-listesi?page={page}', 'div.page-content div.card div.card-body div.table-responsive a.text-hover-primary')
+@Common.ChaptersSinglePageCSS('table#chapters tbody tr td:first-of-type a', ChapterExtractor)
+@Common.PagesSinglePageCSS('div#images img')
+@Common.ImageAjax()
+
+//TODO : CODE LOGIN : 'https://trwebtoon.com/panel/giris'
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('trwebtoon', `TR Webtoon`, 'https://trwebtoon.com', Tags.Language.Turkish, Tags.Media.Manhwa, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
---
+++ web/src/engine/websites/TRWebtoon_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'trwebtoon',
+        title: 'TR Webtoon'
+    },
+    container: {
+        url: 'https://trwebtoon.com/webtoon/oscar-zahnin-garip-seruvenleri',
+        id: '/webtoon/oscar-zahnin-garip-seruvenleri' ,
+        title: 'Oscar Zahn’ın Garip Serüvenleri'
+    },
+    child: {
+        id: '/webtoon/oscar-zahnin-garip-seruvenleri/12',
+        title: '#12'
+    },
+    entry: {
+        index: 0,
+        size: 1_280_801,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
