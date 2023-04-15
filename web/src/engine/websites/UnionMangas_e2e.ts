36b51984a133b7f2bba24f9e38d16adeb5ae4180
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-15 12:29:21 +0200

    Legacy to Haru 18
    * UnionMangas
    * WantedTeam
    * WebComicApp
    * WebtoonTR
    * WeiboManhua

---
+++ web/src/engine/websites/UnionMangas_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'unionmangas',
+        title: 'UnionMangas',
+        //timeout : 25000 //homepage load is damn slow in real browser
+    },
+    container: {
+        url: 'https://unionleitor.top/pagina-manga/one-piece',
+        id: '/pagina-manga/one-piece',
+        title: 'One Piece'
+    },
+    child: {
+        id: '/leitor/One_Piece/1080',
+        title: 'Cap. 1080'
+    },
+    entry: {
+        index: 0,
+        size: 426_983,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
