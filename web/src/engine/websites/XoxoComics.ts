2025fb8fc2c4b4be2661f3876ad3780afaa20dc7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-17 19:15:13 +0200

    Legacy to Haru 19
    * Wnacg
    * XlecX
    * XoxoComics
    * YaoiHeavenReborn
    * YawarakaSpirits

---
+++ web/src/engine/websites/XoxoComics.ts
@@ -0,0 +1,45 @@
+import { Tags } from '../Tags';
+import icon from './XoxoComics.webp';
+import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import { FetchCSS, FetchRequest } from '../FetchProvider';
+
+function LabelExtractor(head: HTMLHeadingElement) {
+    return head.textContent.replace(/ Comic/i, '').trim();
+}
+
+function ImageExtractor(img: HTMLImageElement) {
+    return img.dataset.original;
+}
+
+@Common.MangaCSS(/^https?:\/\/xoxocomics\.com\/comic\/[\da-z-]+$/, 'article#item-detail > h1.title-detail', LabelExtractor)
+@Common.MangasMultiPageCSS('/comic-list/alphabet?c=&page={page}', 'div.chapter > a')
+@Common.PagesSinglePageCSS('div.page-chapter img', ImageExtractor)
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('xoxocomics', `XoxoComics`, 'https://xoxocomics.com', Tags.Language.English, Tags.Media.Comic, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const chapterList = [];
+        for (let page = 1, run = true; run; page++) {
+            const chapters = await this.getChaptersFromPage(manga, page);
+            chapters.length > 0 ? chapterList.push(...chapters) : run = false;
+        }
+        return chapterList;
+    }
+
+    async getChaptersFromPage(manga: Manga, page: number): Promise<Chapter[]> {
+        const uri = new URL(manga.Identifier + '?page=' + page, this.URI);
+        const request = new FetchRequest(uri.href);
+        const data = await FetchCSS<HTMLAnchorElement>(request, 'div.chapter > a');
+        return data.map(element => new Chapter(this, manga, element.pathname + '/all', element.text.replace(manga.Title, '').trim()));
+    }
+}
---
+++ web/src/engine/websites/YawarakaSpirits.ts
@@ -0,0 +1,46 @@
+import { Tags } from '../Tags';
+import icon from './YawarakaSpirits.webp';
+import { DecoratableMangaScraper, type Manga, type MangaPlugin, type MangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+function ChapterExtractor(anchor: HTMLAnchorElement) {
+    const id = anchor.pathname;
+    const title = anchor.querySelector('dl dt').textContent.trim();
+    return { id, title };
+}
+
+function MangaExtractor(anchor: HTMLAnchorElement) {
+    const id = anchor.pathname;
+    const title = anchor.querySelector('dl dt strong').textContent.trim();
+    return { id, title };
+}
+
+@Common.MangaCSS(/^https?:\/\/yawaspi\.com\/[\S]+\/index.html$/, 'div.page__header h2')
+@Common.ChaptersSinglePageCSS('section.page__read div.page__read__inner ul.inner__content li a', ChapterExtractor)
+@Common.PagesSinglePageCSS('div.page__detail__inner div.page__detail__vertical div.vertical__inner ul li img')
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('yawarakaspirits', `やわらかスピリッツ (Yawaraka Spirits)`, 'https://yawaspi.com', Tags.Language.Japanese, Tags.Media.Manga);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+
+    public override async FetchMangas(this: MangaScraper, provider: MangaPlugin): Promise<Manga[]> {
+        const mangalist = [];
+        const path = ['/series/', '/completion/'];
+        for (const p of path) {
+            const mangas = await Common.FetchMangasSinglePageCSS.call(this, provider, p, 'article.work section.work__inner ul li a, article.oldwork section.oldwork__inner ul li a ', MangaExtractor);
+            if (mangas.length > 0) {
+                mangalist.push(...mangas);
+            }
+        }
+        return mangalist;
+
+    }
+
+}
