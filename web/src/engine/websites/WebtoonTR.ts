98e085d54f697e788b0b04701b3e4f9a755daa2e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-04-17 10:48:21 +0000

    Legacy to Haru 18 (#131)
    * Legacy to Haru 18
    
    * UnionMangas
    * WantedTeam
    * WebComicApp
    * WebtoonTR
    * WeiboManhua
    
    * Update UnionMangas.ts
    
    Aggregator not Scanlator

---
+++ web/src/engine/websites/WebtoonTR.ts
@@ -0,0 +1,27 @@
+import { Tags } from '../Tags';
+import icon from './WebtoonTR.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+
+function ChapterExtractor(anchor: HTMLAnchorElement) {
+    const id = anchor.pathname;
+    const title = anchor.querySelector('b').textContent.split(' - ')[1].trim();
+    return { id, title };
+}
+
+@Common.MangaCSS(/^https?:\/\/webtoontr\.com\/_\//, 'div.tanitim li.list-group-item blockquote.h3')
+@Common.MangasSinglePageCSS('/webtoon-listesi', 'ul.list-inline li a')
+@Common.ChaptersSinglePageCSS('table.table tbody tr td a', ChapterExtractor)
+@Common.PagesSinglePageCSS('img.cImg')
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('webtoontr', `WebtoonTR`, 'https://webtoontr.com', Tags.Language.Turkish, Tags.Media.Manga);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
