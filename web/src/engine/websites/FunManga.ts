2e7fddcfee637f9bb8e262740e664a7ae2928121
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-05-10 15:00:07 +0200

    Porting FunManga
    using MangaInn deocrator

---
+++ web/src/engine/websites/FunManga.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './FunManga.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as MangaInn from './decorators/MangaInn';
+
+@Common.MangaCSS(/^https?:\/\/www\.funmanga\.com/, MangaInn.queryMangaTitle)
+@MangaInn.MangasMultiPageCSS()
+@Common.ChaptersSinglePageCSS(MangaInn.queryChapters, MangaInn.ChapterInfoExtractor)
+@Common.PagesSinglePageCSS(MangaInn.queryPages)
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('funmanga', `FunManga`, 'http://www.funmanga.com', Tags.Language.English, Tags.Media.Manga, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
