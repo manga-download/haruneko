3caa7499eabc3159655e121e9a7d7692cbf1e989
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-22 18:56:57 +0200

    Update SelfManga.ts

--- web/src/engine/websites/legacy/SelfManga.ts
+++ web/src/engine/websites/legacy/SelfManga.ts
@@ -4,8 +4,6 @@ import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
 import * as Common from '../decorators/Common';
 import * as ReadM from '../decorators/ReadMangaLive';
 
-//TODO http://selfmanga.ru is not HTTP therefore all fetch fails. Suggestion to replace it with https://selfmanga.live
-
 @Common.MangaCSS(/^https?:\/\/selfmanga\.ru\/[^/]+$/, ReadM.queryMangaTitle)
 @Common.MangasMultiPageCSS(ReadM.pathMangas, ReadM.queryMangas, 0, ReadM.pageMangaOffset, 0, Common.AnchorInfoExtractor(true))
 @Common.ChaptersSinglePageCSS(ReadM.queryChapters)
@@ -13,7 +11,7 @@ import * as ReadM from '../decorators/ReadMangaLive';
 @ReadM.ImageAjax()
 export default class extends DecoratableMangaScraper {
     public constructor() {
-        super('selfmanga', `SelfManga`, 'http://selfmanga.ru', Tags.Language.Russian, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
+        super('selfmanga', `SelfManga`, 'https://selfmanga.live', Tags.Language.Russian, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
     }
     public override get Icon() {
         return icon;
