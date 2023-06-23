c094c6c18deeabd813bfd791ad95ac01957c77fe
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-06-23 18:01:18 +0000

    Porting some graphql websites (#147)
    * Porting some graphql websites
    
    * Argos Scans
    * HqNow
    * Luscious
    
    * add Little Garden
    
    * requested changes on ArgosScans
    
    * Revert "requested changes on ArgosScans"
    
    This reverts commit 4d69485772fc11a7b9c42fbdce0832d4d1b066c3.
    
    * requested changes on ArgoScans
    
    * second batch of requested changes

---
+++ web/src/engine/websites/ArgosScan.ts
@@ -0,0 +1,173 @@
+import { Tags } from '../Tags';
+import icon from './ArgosScan.webp';
+import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import { FetchGraphQL, FetchRequest } from '../FetchProvider';
+
+type ApiResult<T> = {
+    [id: string]: {
+        [id: string]: T
+    }
+}
+
+type ApiSingleResult<T> = {
+    [id: string]: T
+}
+
+type APIManga = {
+    name: string,
+    id: number,
+    getChapters: APIChapter[]
+}
+
+type APIChapter = {
+    id: number,
+    number: number,
+    title: string
+    images: string[]
+}
+
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('argosscan', `Argos Scan`, 'https://argosscan.com', Tags.Language.Portuguese, Tags.Source.Scanlator, Tags.Media.Manhwa, Tags.Media.Manga);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+
+    public override ValidateMangaURL(url: string): boolean {
+        return /https?:\/\/argosscan\.com\/obras\/([\d]+)/.test(url);
+    }
+
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        const id = parseInt(new URL(url).pathname.match(/obras\/(\d+)\//i)[1]);
+        const gql = `
+            query project($id: Int!) {
+                project(id: $id) {
+                    name
+                }
+            }
+        `;
+        const vars = { id: id };
+        const request = new FetchRequest(new URL('/graphql', this.URI).href);
+        const operationName = 'project';
+        const data = await FetchGraphQL<ApiSingleResult<APIManga>>(request, operationName, gql, JSON.stringify(vars));
+        const title = data[operationName].name;
+        return new Manga(this, provider, String(id), title.trim());
+    }
+
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page, provider);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+
+    async _getMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
+        const gql = `
+            query latestProjects($filters: FiltersExpression!, $orders: OrdersExpression!, $pagination: PaginationInput) {
+                getProjects(orders: $orders, filters: $filters, pagination: $pagination) {
+                    projects {
+                        id, name
+                    } 
+                }
+            }
+        `;
+        const vars = {
+            filters: {
+                operator: "AND",
+                childExpressions: [{
+                    operator: "AND",
+                    filters: [{
+                        op: "GE",
+                        field: "Project.id",
+                        values: ["1"]
+                    }]
+                }]
+            },
+            orders: {
+                orders: [{
+                    or: "ASC",
+                    field: "Project.name"
+                }]
+            },
+            pagination: {
+                limit: 100,
+                page: page
+            }
+        };
+
+        const request = new FetchRequest(new URL('/graphql', this.URI).href);
+        const data = await FetchGraphQL<ApiResult<APIManga[]>>(request, 'latestProjects', gql, JSON.stringify(vars));
+        return data['getProjects']['projects'].map(manga => new Manga(this, provider, String(manga.id), manga.name));
+
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const gql = `
+            query project($id: Int!) {
+                project(id: $id) {
+                    getChapters(order: {number: DESC}) {
+                        id, number, title
+                    }
+                }
+            }
+        `;
+        const vars = { id: parseInt(manga.Identifier) };
+        const request = new FetchRequest(new URL('/graphql', this.URI).href);
+        const operationName = 'project';
+        const data = await FetchGraphQL<ApiSingleResult<APIManga>>(request, operationName, gql, JSON.stringify(vars));
+        return data[operationName].getChapters.map(chapter => {
+            const title = `Ch. ${chapter.number} - ${chapter.title}`;
+            return new Chapter(this, manga, String(chapter.id), title.trim());
+        });
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const gql = `
+            query getChapter($filters: FiltersExpression!, $orders: OrdersExpression!) {
+                getChapters(orders: $orders, filters: $filters) {
+                    chapters {
+                        images
+                    }
+                }
+            }
+        `;
+        const vars = {
+            filters: {
+                operator: "AND",
+                filters: [{
+                    op: "EQ",
+                    field: "Chapter.id",
+                    values: [String(chapter.Identifier)]
+                }],
+                childExpressions: {
+                    operator: "AND",
+                    filters: {
+                        op: "GE",
+                        field: "Project.id",
+                        relationField: "Chapter.project",
+                        values: ["1"]
+                    }
+                }
+            },
+            orders: {
+                orders: {
+                    or: "ASC",
+                    field: "Chapter.id"
+                }
+            }
+        };
+
+        const request = new FetchRequest(new URL('/graphql', this.URI).href);
+        const data = await FetchGraphQL<ApiResult<APIChapter[]>>(request, 'getChapter', gql, JSON.stringify(vars));
+        return data['getChapters']['chapters'][0].images.map(image => new Page(this, chapter, new URL(`images/${chapter.Parent.Identifier}/${image}`, this.URI)));
+
+    }
+}
---
+++ web/src/engine/websites/ArgosScan_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'argosscan',
+        title: 'Argos Scan'
+    },
+    container: {
+        url: 'https://argosscan.com/obras/159/battle-frenzy-webtoon-',
+        id: '159' ,
+        title: 'Battle Frenzy (Webtoon)'
+    },
+    child: {
+        id: 'a204ff55-72b2-4ebe-b8db-ee9cfe138f48',
+        title: 'Ch. 222 - Capítulo 222'
+    },
+    entry: {
+        index: 0,
+        size: 1_191_872,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
