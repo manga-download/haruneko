bd3a912bd464402e2656066f218a62cd4757407f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-06 13:55:11 +0000

    Change swatmanga url (#173)
    * Fix SwatManga
    
    change domain
    
    * Update MangaSwat_e2e.ts

--- web/src/engine/websites/MangaSwat.ts
+++ web/src/engine/websites/MangaSwat.ts
@@ -14,7 +14,7 @@ const pageScripts = `
     });
 `;
 
-@MangaStream.MangaCSS(/^https?:\/\/swatmanga\.net\/manga\/[^/]+\/$/)
+@MangaStream.MangaCSS(/^https?:\/\/swatop\.club\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS('div#content div.soralist ul li a.series', '/manga/list-mode/')
 @MangaStream.ChaptersSinglePageCSS('div.bxcl ul li span.lchx a')
 @MangaStream.PagesSinglePageJS([], pageScripts)
@@ -22,7 +22,7 @@ const pageScripts = `
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('mangaswat', 'SWAT Manga', 'https://swatmanga.net', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Arabic);
+        super('mangaswat', 'SWAT Manga', 'https://swatop.club', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Arabic);
     }
 
     public override get Icon() {
