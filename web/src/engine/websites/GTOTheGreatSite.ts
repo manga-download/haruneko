7b834b54d1c70a1621cd624bdc7c0698012f0ff1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-06 17:09:55 +0200

    ADD PizzaReader and websites
    ez JSON

---
+++ web/src/engine/websites/GTOTheGreatSite.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './GTOTheGreatSite.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as PizzaReader from './decorators/PizzaReader';
+import * as Common from './decorators/Common';
+
+@PizzaReader.MangaAJAX(/^https?:\/\/reader\.gtothegreatsite\.net\/comics\/\S+$/)
+@PizzaReader.MangasSinglePageAJAX()
+@PizzaReader.ChaptersSinglePageAJAX()
+@PizzaReader.PagesSinglePageAJAX()
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('gtotgs', `GTO The Great Site`, 'https://reader.gtothegreatsite.net', Tags.Language.Italian, Tags.Media.Manga, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
---
+++ web/src/engine/websites/decorators/PizzaReader.ts
@@ -0,0 +1,146 @@
+import { FetchRequest, FetchJSON } from '../../FetchProvider';
+import { type MangaScraper, Manga, Chapter, Page, type MangaPlugin } from '../../providers/MangaPlugin';
+import type * as Common from './Common';
+
+type APISingleManga = {
+    comic: APIManga
+};
+
+type APIMangas = {
+    comics: APIManga[]
+};
+
+type APIPages = {
+    chapter : APIChapter
+}
+
+type APIManga = {
+    title: string,
+    slug: string,
+    url: string,
+    chapters: APIChapter[]
+}
+
+type APIChapter = {
+    full_title: string,
+    title: string,
+    url: string,
+    language: string,
+    pages : string[]
+}
+
+/***********************************************
+ ******** Manga Extraction Methods ********
+ ***********************************************/
+
+/**
+ * A class decorator that adds the ability to extract a manga from any {@link url}
+ * The last part of the url will be used as an id to call the api and get the title
+ */
+async function FetchMangaAJAX(this: MangaScraper, provider: MangaPlugin, url: string): Promise<Manga> {
+    const slug = new URL(url).pathname.match(/([^/]*)\/*$/)[1];
+    const uri = new URL('/api/comics/' + slug, this.URI);
+    const request = new FetchRequest(uri.href);
+    const data = await FetchJSON<APISingleManga>(request);
+    return new Manga(this, provider, slug, data.comic.title);
+}
+
+/**
+ * A class decorator that adds the ability to extract a manga from any url that matches the given {@link pattern}.
+ * The last part of the url will be used as an id to call the api and get the title
+ * @param pattern - An expression to check if a manga can be extracted from an url or not
+ */
+export function MangaAJAX(pattern: RegExp) {
+    return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
+        if (context && context.kind !== 'class') {
+            throw new Error(context.name);
+        }
+        return class extends ctor {
+            public ValidateMangaURL(this: MangaScraper, url: string): boolean {
+                return pattern.test(url);
+            }
+            public async FetchManga(this: MangaScraper, provider: MangaPlugin, url: string): Promise<Manga> {
+                return FetchMangaAJAX.call(this, provider, url);
+            }
+        };
+    };
+}
+/***********************************************
+******** Manga List Extraction Methods ********
+***********************************************/
+async function FetchMangasSinglePageAJAX(this: MangaScraper, provider: MangaPlugin): Promise<Manga[]> {
+    const uri = new URL('/api/comics', this.URI);
+    const request = new FetchRequest(uri.href);
+    const data = await FetchJSON<APIMangas>(request);
+    return data.comics.map(manga => new Manga(this, provider, manga.slug, manga.title));
+}
+
+/**
+ * A class decorator that adds the ability to extract a manga list using the website api.
+ */
+export function MangasSinglePageAJAX() {
+    return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
+        if (context && context.kind !== 'class') {
+            throw new Error(context.name);
+        }
+        return class extends ctor {
+            public async FetchMangas(this: MangaScraper, provider: MangaPlugin): Promise<Manga[]> {
+                return FetchMangasSinglePageAJAX.call(this, provider);
+            }
+        };
+    };
+}
+
+/*************************************************
+ ******** Chapter List Extraction Methods ********
+ *************************************************/
+
+async function FetchChapterSinglePageAJAX(this: MangaScraper, manga: Manga): Promise<Chapter[]> {
+    const uri = new URL('/api/comics/' + manga.Identifier, this.URI);
+    const request = new FetchRequest(uri.href);
+    const data = await FetchJSON<APISingleManga>(request);
+    return data.comic.chapters.map(chapter => new Chapter(this, manga, chapter.url, chapter.full_title));
+}
+
+/**
+ * A class decorator that adds the ability to extract chapter list from a manga using the website api.
+ */
+export function ChaptersSinglePageAJAX() {
+    return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
+        if (context && context.kind !== 'class') {
+            throw new Error(context.name);
+        }
+        return class extends ctor {
+            public async FetchChapters(this: MangaScraper, manga: Manga): Promise<Chapter[]> {
+                return FetchChapterSinglePageAJAX.call(this, manga);
+            }
+        };
+    };
+}
+
+/*************************************************
+ ******** Pages Extraction Methods ********
+ *************************************************/
+
+/**
+ * A class decorator that adds the ability to extract pages list from a chapter using the website api.
+ */
+export function PagesSinglePageAJAX() {
+    return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
+        if (context && context.kind !== 'class') {
+            throw new Error(context.name);
+        }
+        return class extends ctor {
+            public async FetchPages(this: MangaScraper, chapter: Chapter): Promise<Page[]> {
+                return FetchPagesSinglePageAJAX.call(this, chapter);
+            }
+        };
+    };
+}
+
+async function FetchPagesSinglePageAJAX(this: MangaScraper, chapter: Chapter): Promise<Page[]> {
+    const uri = new URL('/api' + chapter.Identifier, this.URI);
+    const request = new FetchRequest(uri.href);
+    const data = await FetchJSON<APIPages>(request);
+    return data.chapter.pages.map(page => new Page(this, chapter, new URL(page)));
+}
