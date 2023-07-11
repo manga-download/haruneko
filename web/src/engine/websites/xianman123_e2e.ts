c33a88de9a7520b711766b2771d22f1905b0183e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-11 19:29:04 +0200

    add tests

---
+++ web/src/engine/websites/xianman123_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'xianman123',
+        title: 'XianMan123'
+    },
+    container: {
+        url: 'https://www.xianmanwang.com/bailianchengshen/',
+        id: '/bailianchengshen/',
+        title: '百炼成神',
+    },
+    child: {
+        id: '/bailianchengshen/1859.html',
+        title: '特别篇 神秘剑客',
+    },
+    entry: {
+        index: 1,
+        size: 526_910,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
