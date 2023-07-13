5823b91249146d92930dc4c21dea52ca88d0c5f8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-13 15:22:23 +0200

    Add SinMH decorator and "websites"
    Most SinMH websites are :
    
    * dead
    * Not SSL/TLS (cant add them)
    * Change url VERY often (cant add them for now)
    
    Also, dmzj is no SINMH anymore, but added it for reference

--- web/src/engine/websites/legacy/Dmzj.ts
+++ web/src/engine/websites/legacy/Dmzj.ts
@@ -1,44 +1,95 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './Dmzj.webp';
-import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../../providers/MangaPlugin';
+import * as Common from '../decorators/Common';
+import { FetchJSON, FetchRequest } from '../../FetchProvider';
 
+type APIResult<T> = {
+    errno: number,
+    errmsg: string,
+    data: T
+}
+
+type APIComicList = {
+    comicList: APIManga[]
+}
+
+type APIComicSingle = {
+    comicInfo: APIManga
+}
+
+type APIChapterInfo = {
+    chapterInfo: APIChapter
+}
+
+type APIManga = {
+    id: number,
+    name: string,
+    title: string
+    comicPy: string,
+    comic_py: string, //yes api is dumb like that
+    chapterList: APIChapter[]
+}
+
+type APIChapter = {
+    title: string,
+    data: {
+        chapter_id: number,
+        chapter_title: string
+    }[]
+    page_url_hd: string[],
+    page_url: string[]
+}
+
+type ChapterID = {
+    comic: string,
+    chapter: string
+}
+
+@Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
-
     public constructor() {
-        super('dmzj', `动漫之家(DMZJ)`, 'https://www.dmzj.com' /*, Tags.Language.English, Tags ... */);
+        super('dmzj', `动漫之家(DMZJ)`, 'https://www.dmzj.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
     }
-
     public override get Icon() {
         return icon;
     }
-}
 
-// Original Source
-/*
-class Dmzj extends SinMH {
+    public override ValidateMangaURL(url: string) {
+        return /^https?:\/\/www\.dmzj\.com\/info\/\S+\.html$/.test(url);
+    }
 
-    constructor() {
-        super();
-        super.id = 'dmzj';
-        super.label = '动漫之家(DMZJ)';
-        this.tags = [ 'manga', 'chinese', 'webtoon' ];
-        this.url = 'https://www.dmzj.com';
-        this.requestOptions.headers.set('x-referer', this.url);
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        const comicPy = url.match(/\/info\/(\S+)\.html$/)[1];
+        const request = new FetchRequest(new URL(`/api/v1/comic1/comic/detail?comic_py=${comicPy}`, this.URI).href);
+        const data = await FetchJSON<APIResult<APIComicSingle>>(request);
+        return new Manga(this, provider, data.data.comicInfo.comicPy, data.data.comicInfo.title.trim());
+    }
 
-        this.queryChapters = 'div.tab-content-selected ul li a';
-        this.queryManga = 'div.comic_deCon > h1';
-        this.path = '/category/0-0-0-0-0-0-%PAGE%.html';
-        this.queryMangasPageCount = 'a.pg_last';
-        this.queryMangas = 'h3 > a';
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this._getMangasFromPage(page, provider);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+        }
+        return mangaList;
+    }
+    private async _getMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
+        const request = new FetchRequest(new URL(`/api/v1/comic1/filter?page=${page}&size=100`, this.URI).href);
+        const data = await FetchJSON<APIResult<APIComicList>>(request);
+        return data.errno != 0 || !data.data.comicList ? [] : data.data.comicList.map(manga => new Manga(this, provider, manga.comic_py, manga.name.trim()));
+    }
 
-        this.queryPagesScript =`
-            new Promise(resolve => {
-                resolve(picArry.map(el => new URL(el,img_prefix).href));
-            });
-        `;
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const request = new FetchRequest(new URL(`/api/v1/comic1/comic/detail?comic_py=${manga.Identifier}`, this.URI).href);
+        const data = await FetchJSON<APIResult<APIComicSingle>>(request);
+        return data.errno != 0 ? [] : data.data.comicInfo.chapterList[0].data.map(chapter => new Chapter(this, manga, JSON.stringify({ comic: data.data.comicInfo.id, chapter: chapter.chapter_id }), chapter.chapter_title.trim()));
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const chapterId: ChapterID = JSON.parse(chapter.Identifier);
+        const request = new FetchRequest(new URL(`/api/v1/comic1/chapter/detail?comic_id=${chapterId.comic}&chapter_id=${chapterId.chapter}`, this.URI).href);
+        const data = await FetchJSON<APIResult<APIChapterInfo>>(request);
+        return data.errno != 0 ? [] : (data.data.chapterInfo.page_url_hd || data.data.chapterInfo.page_url).map(page => new Page(this, chapter, new URL(page)));
     }
 }
-*/
--- web/src/engine/websites/legacy/ManHuaGui.ts
+++ web/src/engine/websites/legacy/ManHuaGui.ts
@@ -1,50 +1,25 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './ManHuaGui.webp';
 import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import * as Common from '../decorators/Common';
+import * as SinMH from '../decorators/SinMH';
 
-export default class extends DecoratableMangaScraper {
-
-    public constructor() {
-        super('manhuagui', `看漫画 (ManHuaGui)`, 'https://www.manhuagui.com' /*, Tags.Language.English, Tags ... */);
-    }
-
-    public override get Icon() {
-        return icon;
-    }
-}
-
-// Original Source
 /*
-class ManHuaGui extends SinMH {
+this.config = {
+    throttle: {
+        label: 'Page Throttle Requests [ms]',
+        description: 'Enter the timespan in [ms] to delay consecuitive HTTP requests while downloading Pages.\nThe website may ban your IP for to many consecuitive requests.',
+        input: 'numeric',
+        min: 500,
+        max: 10000,
+        value: 2500
+    }
+};
+*/
 
-    constructor() {
-        super();
-        super.id = 'manhuagui';
-        super.label = '看漫画 (ManHuaGui)';
-        this.tags = [ 'webtoon', 'chinese' ];
-        this.url = 'https://www.manhuagui.com';
-        this.requestOptions.headers.set('x-referer', this.url);
-
-        this.api = 'SMH';
-        //this.path = '/list/index_p%PAGE%.html';
-        //this.queryMangasPageCount = 'div.pager a:last-of-type';
-        this.queryChapters = 'div.chapter-list ul li a';
-        this.config = {
-            throttle: {
-                label: 'Page Throttle Requests [ms]',
-                description: 'Enter the timespan in [ms] to delay consecuitive HTTP requests while downloading Pages.\nThe website may ban your IP for to many consecuitive requests.',
-                input: 'numeric',
-                min: 500,
-                max: 10000,
-                value: 2500
-            }
-        };
-        this.queryPagesScript =`
+const queryPagesScript = `
             new Promise(resolve => {
-                ${this.api}.imgData = function(data) {
+                SMH.imgData = function(data) {
                     let origin = 'https://' + servs[pVars.curServ].hosts[pVars.curHost].h + '.hamreus.com';
                     let pageLinks = data.files.map(file => origin + data.path + file + '?cid=' + data.cid + '&md5=' + data.sl.md5);
                     return {
@@ -54,16 +29,21 @@ class ManHuaGui extends SinMH {
                 let script = [...document.querySelectorAll('script:not([src])')].find(script => script.text.trim().startsWith('window[')).text;
                 eval(script);
             } );
-        `;
+`;
+
+@Common.MangaCSS(/^https?:\/\/(?:www\.)?(mhgui|manhuagui).com\/comic\/\d+\/$/, SinMH.queryManga)
+@Common.MangasNotSupported()
+@SinMH.ChaptersSinglePageJS(SinMH.queryChaptersScript, 'div.chapter-list ul li a')
+@Common.PagesSinglePageJS(queryPagesScript)
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('manhuagui', `看漫画 (ManHuaGui)`, 'https://www.manhuagui.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
     }
 
-    canHandleURI(uri) {
-        return /https?:\/\/(?:www\.)?(mhgui|manhuagui).com/.test(uri.origin);
-    }
-
-    async _getMangas() {
-        let msg = 'This function was disabled to prevent of being IP banned by the website owner, please copy and paste the URL containing the chapters directly from your browser into HakuNeko.';
-        throw new Error(msg);
+    public override get Icon() {
+        return icon;
     }
 }
-*/
