82f239e0c23d3717fb5a9e020bc4698ce5ea8fb1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-14 13:21:15 +0200

    Add SixParkBBS  decorator

--- web/src/engine/websites/legacy/SixParkbbsClub.ts
+++ web/src/engine/websites/legacy/SixParkbbsClub.ts
@@ -1,39 +1,21 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './SixParkbbsClub.webp';
 import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import * as Common from '../decorators/Common';
+import * as SixParkbbs from '../decorators/SixParkbbs';
 
+@Common.MangaCSS(/^https?:\/\/club\.6parkbbs\.com\/enter6\/index\.php\?app=forum&act=threadview&tid=\d+/, 'td.show_content font b', SixParkbbs.MangaLabelExtractor, true)
+@SixParkbbs.MangasMultiPageCSS('/index.php?app=forum&act=list&pre=55764&nowpage={page}&start=55764', 'div#d_list ul li a:nth-child(1)', /(【(连载|英肉|短篇|生肉|韩肉)】.*)|(\[连载\].*)/, '/enter6')
+@Common.ChaptersUniqueFromManga()
+@Common.PagesSinglePageCSS('td.show_content pre img', SixParkbbs.PagesExtractor)
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('sixparkbbsclub', `6parkbbs (卡通漫画)`, 'https://club.6parkbbs.com' /*, Tags.Language.English, Tags ... */);
+        super('sixparkbbsclub', `6parkbbs (卡通漫画)`, 'https://club.6parkbbs.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
         return icon;
     }
 }
-
-// Original Source
-/*
-class SixParkbbsClub extends SixParkbbs {
-
-    constructor() {
-        super();
-        super.id = 'sixparkbbsclub';
-        super.label = '6parkbbs (卡通漫画)';
-        this.tags = [ 'manga', 'webtoon', 'chinese', 'english', 'korean', 'raw' ];
-        this.url = 'https://club.6parkbbs.com';
-        this.sub = '/enter6';
-
-        this.path = '/index.php?app=forum&act=list&pre=55764&nowpage=%PAGE%&start=55764';
-        this.pathMatch = /nowpage=(\d+)&start/;
-        this.queryMangaTitle = 'td.show_content font b';
-        this.queryMangas = 'div#d_list ul li a:nth-child(1)';
-        this.queryMangasMatch = /(【(连载|英肉|短篇|生肉|韩肉)】.*)|(\[连载\].*)/;
-        this.queryPage = 'td.show_content pre source';
-    }
-}
-*/
--- web/src/engine/websites/legacy/SixParkbbsWeb.ts
+++ web/src/engine/websites/legacy/SixParkbbsWeb.ts
@@ -1,38 +1,21 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './SixParkbbsWeb.webp';
 import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import * as Common from '../decorators/Common';
+import * as SixParkbbs from '../decorators/SixParkbbs';
 
+@Common.MangaCSS(/^https?:\/\/web\.6parkbbs\.com\/index\.php\?app=forum&act=view&tid=\d+/, 'div.c-box p.c-box-h b', SixParkbbs.MangaLabelExtractor, true)
+@SixParkbbs.MangasMultiPageCSS('/index.php?app=forum&act=bbs&bbsid=2032&p={page}', 'div#d_list div.repl-list-one a:nth-child(1)', /.*\[漫画\]/)
+@Common.ChaptersUniqueFromManga()
+@Common.PagesSinglePageCSS('div.cen-main div.c-box-m center img', SixParkbbs.PagesExtractor)
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('sixparkbbsweb', `6parkbbs (新❀华漫)`, 'https://web.6parkbbs.com' /*, Tags.Language.English, Tags ... */);
+        super('sixparkbbsweb', `6parkbbs (新❀华漫)`, 'https://web.6parkbbs.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
         return icon;
     }
 }
-
-// Original Source
-/*
-class SixParkbbsWeb extends SixParkbbs {
-
-    constructor() {
-        super();
-        super.id = 'sixparkbbsweb';
-        super.label = '6parkbbs (新❀华漫)';
-        this.tags = [ 'manga', 'webtoon', 'chinese' ];
-        this.url = 'https://web.6parkbbs.com';
-
-        this.path = '/index.php?app=forum&act=bbs&bbsid=2032&p=%PAGE%';
-        this.pathMatch = /p=(\d+)/;
-        this.queryMangaTitle = 'div.c-box p.c-box-h b';
-        this.queryMangas = 'div#d_list div.repl-list-one a:nth-child(1)';
-        this.queryMangasMatch = /.*\[漫画\]/;
-        this.queryPage = 'div.cen-main div.c-box-m center source';
-    }
-}
-*/
