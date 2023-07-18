5697c7a0265dc81cc3cf454a615f9d895a0208b1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-18 14:46:49 +0200

    Update KissAway.ts

--- web/src/engine/websites/legacy/KissAway.ts
+++ web/src/engine/websites/legacy/KissAway.ts
@@ -5,17 +5,49 @@ import * as Common from '../decorators/Common';
 import * as FlatManga from '../decorators/FlatManga';
 import { FetchRequest, FetchWindowScript } from '../../FetchProvider';
 
+const chapterScript = `
+    new Promise(async resolve => {
+        const uri = new URL('app/manga/controllers/cont.listChapter.php', window.location.origin);
+        uri.searchParams.set('slug', dataL);
+        const response = await fetch(uri);
+        data = await response.text();
+        const dom = new DOMParser().parseFromString(data, "text/html");
+        const nodes = [...dom.querySelectorAll('a.chapter[title]')];
+        const chapters= nodes.map(chapter => {
+            return {
+                id : chapter.pathname,
+                title : chapter.title
+            };
+        });
+        const uniqueChapters = chapters.filter((obj, index) =>chapters.findIndex((item) => item.id === obj.id) === index );
+        resolve(uniqueChapters);
+    });
+`;
+
+const pageScript = `
+    new Promise(async resolve => {
+        const chapId = document.querySelector('input#chapter').value;
+        const uri = new URL('app/manga/controllers/cont.listImg.php', window.location.origin);
+        uri.searchParams.set('cid', chapId);
+        const response = await fetch(uri);
+        const data = await response.text();
+        const dom = new DOMParser().parseFromString(data, "text/html");
+        const nodes = [...dom.querySelectorAll('img.chapter-img')];
+        resolve(nodes.map(picture => picture.src));
+    });
+`;
+
 //TODO Implement at request level a BYPASS for this DDOSS protection
 //its not triggered on main page but on manga page
 
-let ddosProtectionPassed = false;
-
 @Common.MangaCSS(/^https?:\/\/klz9\.com\/\S+\.html$/, FlatManga.queryMangaTitle)
 @Common.MangasSinglePageCSS(FlatManga.pathSinglePageManga, FlatManga.queryMangas)
-@FlatManga.PagesSinglePageCSS()
+@Common.PagesSinglePageJS(pageScript, 1000)
 @Common.ImageAjax()
 
 export default class extends DecoratableMangaScraper {
+
+    private ddosProtectionPassed = false;
 
     public constructor() {
         super('kissaway', `KLManga`, 'https://klz9.com', Tags.Language.Japanese, Tags.Media.Manga, Tags.Source.Aggregator);
@@ -26,16 +58,11 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        if (!ddosProtectionPassed) {
+        if (!this.ddosProtectionPassed) {
             await FetchWindowScript(new FetchRequest(new URL(manga.Identifier, this.URI).href), '', 5000);
-            ddosProtectionPassed = true;
+            this.ddosProtectionPassed = true;
         }
-        const chapters = await FlatManga.FetchChaptersSinglePageCSS.call(this, manga, FlatManga.queryChapters);
-        //dupe example : https://klz9.com/ybed-29-years-old-bachelor-was-brought-to-a-different-world-to-live-freely-raw.html
-        const uniqueChapters = chapters.filter(
-            (obj, index) =>
-                chapters.findIndex((item) => item.Identifier === obj.Identifier) === index
-        );
-        return uniqueChapters;
+        return Common.FetchChaptersSinglePageJS.call(this, manga, chapterScript, 1000);
     }
+
 }
