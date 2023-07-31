a8f461b368dba51a3820b80a9925e21bf2bdeb4e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-07-31 10:10:36 +0000

    fixing TempestFansub (#189)
    * change domain
    * fix getting mangas
    * fix getting pictures
    * add proper e2e test

--- web/src/engine/websites/Tempestfansub.ts
+++ web/src/engine/websites/Tempestfansub.ts
@@ -1,38 +1,25 @@
-// Auto-Generated export from HakuNeko Legacy
 import { Tags } from '../Tags';
 import icon from './Tempestfansub.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/manga\.tempestfansub\.com\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS()
+function MangaExtractor(anchor: HTMLAnchorElement) {
+    return { id: anchor.pathname, title: anchor.title };
+}
+
+@MangaStream.MangaCSS(/^https?:\/\/tempestfansub\.com\/manga\/[^/]+\/$/)
+@Common.MangasMultiPageCSS('/manga/?page={page}', 'div.bs div.bsx > a',1,1,0,MangaExtractor )
 @MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('tempestfansub', 'Tempestfansub', 'https://manga.tempestfansub.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish);
+        super('tempestfansub', 'Tempestfansub', 'https://tempestfansub.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish);
     }
 
     public override get Icon() {
         return icon;
     }
 }
-
-// Original Source
-/*
-class Tempestfansub extends WordPressMangastream {
-
-    constructor() {
-        super();
-        super.id = 'tempestfansub';
-        super.label = 'Tempestfansub';
-        this.tags = [ 'webtoon', 'manga', 'turkish' ];
-        this.url = 'https://manga.tempestfansub.com';
-
-        this.path = '/manga/list-mode/';
-    }
-}
-*/
