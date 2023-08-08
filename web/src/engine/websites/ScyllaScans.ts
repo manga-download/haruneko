e5e2ff8f2e1c44070d5acf91ca9ff72ef01e558a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-08 07:41:12 +0000

    Add ReaderFront decorator & websites (#146)
    * New Decorator : ReaderFront
    
    Used by ScyllaScans and RavensScans
    
    * fix lint in readerfront
    
    * Update ReaderFront.ts
    
    * Add Readerfront websites
    
    * Merge Ravenscans into one
    
    * Update _index.ts
    
    * create tests and move files
    
    * requested changes on readerfront
    
    * splitted ravens scans again

---
+++ web/src/engine/websites/ScyllaScans.ts
@@ -0,0 +1,25 @@
+import { Tags } from '../Tags';
+import icon from './ScyllaScans.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as ReaderFront from './decorators/ReaderFront';
+
+const apiUrl = 'https://api.scyllascans.org';
+const cdnUrl = apiUrl;
+
+@ReaderFront.MangaAJAX(/^https?:\/\/scyllascans\.org\/work\/[a-z]{2}\/[^/]+/, apiUrl)
+@ReaderFront.MangasSinglePageAJAX(apiUrl)
+@ReaderFront.ChaptersSinglePageAJAX(apiUrl)
+@ReaderFront.PagesSinglePageAJAX(apiUrl, cdnUrl)
+@Common.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('scyllascans', `Scylla Scans`, 'https://scyllascans.org', Tags.Language.English, Tags.Media.Manga, Tags.Media.Manhwa, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
