1774ce4b0dd8895d0b7758d9c11b1a597dd669ad
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-08 11:44:43 +0200

    Update ManHuaGui_e2e.ts

--- web/src/engine/websites/ManHuaGui_e2e.ts
+++ web/src/engine/websites/ManHuaGui_e2e.ts
@@ -3,19 +3,16 @@
 const config = {
     plugin: {
         id: 'manhuagui',
-        title: '看漫画 (ManHuaGui)',
+        title: '看漫画 (ManHuaGui)'
     },
     container: {
         url: 'https://www.manhuagui.com/comic/7580/',
         id: '/comic/7580/',
-        title: '一拳超人',
-        timeout : 30000
+        title: '一拳超人'
     },
     child: {
         id: '/comic/7580/705774.html',
-        title: '第234话',
-        timeout: 30000
-
+        title: '第234话'
     },
     entry: {
         index: 0,
