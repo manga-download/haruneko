f7c52ab45f8f1e2baa18bf248f7a17ece9c64199
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-15 13:25:23 +0200

    websites fixes
    * some Madara websites use ajaxv2 instead of v1
    
    * they also uses WPmangaProtector for images
    
    * FirstKissManga rebranded to LikeManga (and changed a bit)

--- web/src/engine/websites/CeriseScans_e2e.ts
+++ web/src/engine/websites/CeriseScans_e2e.ts
@@ -6,13 +6,13 @@ const config = {
         title: 'Cerise Scans'
     },
     container: {
-        url: 'https://cerisescans.com/inicio/manga/adele02/',
-        id: JSON.stringify({ post: '87', slug: '/inicio/manga/adele02/' }),
+        url: 'https://cerisescans.com/home/manga/adele02/',
+        id: JSON.stringify({ post: '45', slug: '/home/manga/adele02/' }),
         title: 'Just Leave Me Be'
     },
     child: {
-        id: '/inicio/manga/adele02/cap-01/',
-        title: 'Cap. 01'
+        id: '/home/manga/adele02/cap-01/',
+        title: 'Cap. 01',
     },
     entry: {
         index: 0,
