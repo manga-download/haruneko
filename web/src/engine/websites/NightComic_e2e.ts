a43f93e0d343b830641c5ba6df4eff54cd77e5f4
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-08-30 11:39:05 +0200

    madara fixes - letter N

--- web/src/engine/websites/NightComic_e2e.ts
+++ web/src/engine/websites/NightComic_e2e.ts
@@ -4,21 +4,21 @@ const config = {
     plugin: {
         id: 'nightcomic',
         title: 'NIGHT COMIC'
-    }/*,
+    },
     container: {
-        url: 'https://www.nightcomic.com/manga/.../',
-        id: JSON.stringify({ post: '0', slug: '/manga/.../' }),
-        title: 'Manga ?'
+        url: 'https://www.nightcomic.com/manga/the-devil-butler/',
+        id: JSON.stringify({ post: '2032', slug: '/manga/the-devil-butler/' }),
+        title: 'The Devil Butler'
     },
     child: {
-        id: '/manga/.../.../',
-        title: 'Chapter ?'
+        id: '/manga/the-devil-butler/chapter-1/',
+        title: 'Chapter 1'
     },
     entry: {
         index: 0,
-        size: -1,
+        size: 132_954,
         type: 'image/jpeg'
-    }*/
+    }
 };
 
 const fixture = new TestFixture(config);
