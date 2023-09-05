e18bffe714a2eed7f293d29efbef9e921a517498
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-05 17:14:14 +0000

    Website Maintenance : Madara to Mangastream
    * IrisScanlator
    * Mangacim
    *Diskusscan

--- web/src/engine/websites/IrisScanlator.ts
+++ web/src/engine/websites/IrisScanlator.ts
@@ -1,13 +1,13 @@
 import { Tags } from '../Tags';
 import icon from './IrisScanlator.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Madara from './decorators/WordPressMadara';
+import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^https?:\/\/irisscanlator\.com\.br\/manga\/[^/]+\/$/)
-@Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv2()
-@Madara.PagesSinglePageCSS()
+@MangaStream.MangaCSS(/^https?:\/\/irisscanlator\.com\.br\/manga\/[^/]+\/$/)
+@MangaStream.MangasSinglePageCSS()
+@MangaStream.ChaptersSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
