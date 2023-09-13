f2e7dbcc3bc45ae3b5fd5bef6b52133940d64c02
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-13 08:32:23 +0000

    asurascans : domain change (#211)

--- web/src/engine/websites/AsuraScans.ts
+++ web/src/engine/websites/AsuraScans.ts
@@ -4,14 +4,14 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/asura\.nacm\.xyz\/manga\/[^/]+\/$/)
+@MangaStream.MangaCSS(/^https?:\/\/asuracomics\.com\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS()
 @MangaStream.ChaptersSinglePageCSS()
 @MangaStream.PagesSinglePageCSS([ /ENDING-PAGE/i ], 'div#readerarea p img')
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
     public constructor() {
-        super('asurascans', 'Asura Scans', 'https://asura.nacm.xyz', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English);
+        super('asurascans', 'Asura Scans', 'https://asuracomics.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English);
     }
     public override get Icon() {
         return icon;
