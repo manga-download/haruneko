4e6bc4423d824684e6b60db36778e412cb3142be
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-19 20:14:56 +0200

    mangashiro change domain & fix

--- web/src/engine/websites/MangaShiro.ts
+++ web/src/engine/websites/MangaShiro.ts
@@ -1,46 +1,21 @@
-// Auto-Generated export from HakuNeko Legacy
 import { Tags } from '../Tags';
 import icon from './MangaShiro.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/mangashiro\.co\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS('div#content div.soralist ul li a.series', '/manga/?list')
-@MangaStream.ChaptersSinglePageCSS('div.bxcl ul li span.lchx a')
-@MangaStream.PagesSinglePageCSS([], 'div#readerarea > :not(.kln) img[src]:not([src=""])')
+@MangaStream.MangaCSS(/^https?:\/\/mangashiro\.me\/manga\/[^/]+\/$/)
+@MangaStream.MangasSinglePageCSS()
+@MangaStream.ChaptersSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('mangashiro', 'MangaShiro', 'https://mangashiro.co', Tags.Media.Manga, Tags.Language.Indonesian);
+        super('mangashiro', 'MangaShiro', 'https://mangashiro.me', Tags.Media.Manga, Tags.Language.Indonesian);
     }
 
     public override get Icon() {
         return icon;
     }
 }
-
-// Original Source
-/*
-class MangaShiro extends WordPressMangastream {
-
-    constructor() {
-        super();
-        super.id = 'mangashiro';
-        super.label = 'MangaShiro';
-        this.tags = [ 'manga', 'indonesian' ];
-        this.url = 'https://mangashiro.co';
-        this.path = '/manga/?list';
-
-        this.queryChapters = 'div.bxcl ul li span.lchx a';
-        this.queryChaptersTitle = undefined;
-        this.queryPages = 'div#readerarea > :not(.kln) img[src]:not([src=""])';
-    }
-
-    async _getPages(chapter) {
-        let pageList = await super._getPages(chapter);
-        return pageList.filter(page => !page.endsWith('Last%2Bcover%2Bshironime.png'));
-    }
-}
-*/
