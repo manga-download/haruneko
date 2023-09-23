40fb29a2ade7bdfdd390cb053e5b4b8cc80573b0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-23 19:49:47 +0200

    mangastream fixes : L (2)

--- web/src/engine/websites/LELManga.ts
+++ web/src/engine/websites/LELManga.ts
@@ -1,14 +1,13 @@
-// Auto-Generated export from HakuNeko Legacy
 import { Tags } from '../Tags';
 import icon from './LELManga.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/www\.lelmanga\.com\/manga\/[^/]+\/$/)
+@MangaStream.MangaCSS(/^https?:\/\/www\.lelmanga\.com\/manga\/[^/]+$/)
 @MangaStream.MangasSinglePageCSS()
 @MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -20,18 +19,3 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 }
-
-// Original Source
-/*
-class LELManga extends WordPressMangastream {
-
-    constructor() {
-        super();
-        super.id = 'lelmanga';
-        super.label = 'LELManga';
-        this.tags = [ 'manga', 'webtoon', 'french' ];
-        this.url = 'https://www.lelmanga.com';
-        this.path = '/manga/list-mode/';
-    }
-}
-*/
