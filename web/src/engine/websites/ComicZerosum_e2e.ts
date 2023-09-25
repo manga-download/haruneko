54e0a12fe1616d1df60ab365b7b331d3db185e14
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-25 16:47:36 +0200

    sample website : ComicZeroSum

---
+++ web/src/engine/websites/ComicZerosum_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'comiczerosum',
+        title: 'Comic ゼロサム (Comic ZEROSUM)'
+    },
+    container: {
+        url: 'https://zerosumonline.com/detail/imoutoni',
+        id: 'imoutoni',
+        title: '妹に婚約者を取られたら、獣な王子に求婚されました～またたびとして溺愛されてます～'
+    },
+    child: {
+        id: '284',
+        title: '第1章'
+    },
+    entry: {
+        index: 0,
+        size: 661_783,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
--- web/src/engine/websites/legacy/ComicZerosum.ts
+++ web/src/engine/websites/legacy/ComicZerosum.ts
@@ -1,56 +1,63 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './ComicZerosum.webp';
-import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../../providers/MangaPlugin';
+import * as Common from '../decorators/Common';
+import { FetchProto, FetchRequest } from '../../FetchProvider';
 
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-
+    private api_url = 'https://api.zerosumonline.com/api/v1/';
+    private protoTypes = '/src/engine/websites/legacy/ComicZerosum.proto';
     public constructor() {
-        super('comiczerosum', `Comic ゼロサム (Comic ZEROSUM)`, 'https://online.ichijinsha.co.jp/zerosum' /*, Tags.Language.English, Tags ... */);
+        super('comiczerosum', `Comic ゼロサム (Comic ZEROSUM)`, 'https://zerosumonline.com', Tags.Language.Japanese, Tags.Media.Manga, Tags.Source.Official);
     }
-
     public override get Icon() {
         return icon;
     }
-}
 
-// Original Source
-/*
-class ComicZerosum extends SpeedBinb {
-
-    constructor() {
-        super();
-        super.id = 'comiczerosum';
-        super.label = 'Comic ゼロサム (Comic ZEROSUM)';
-        this.tags = ['manga', 'japanese'];
-        this.url = 'https://online.ichijinsha.co.jp/zerosum';
-        this.data_url = 'https://online.ichijinsha.co.jp/json/zerosum';
+    public override ValidateMangaURL(url: string): boolean {
+        return /https?:\/\/zerosumonline\.com\/detail\//.test(url);
     }
 
-    async _getMangas() {
-        const request = new Request(`${this.data_url}/list/name.json`, this.requestOptions);
-        const data = await this.fetchJSON(request);
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        //'https://api.zerosumonline.com/api/v1/title?tag=
+        const mangaid = url.match(/\/detail\/([\w]+)/)[1];
+        const requri = new URL(`title?tag=${mangaid}`, this.api_url);
+        const responseType = 'ComicZerosum.TitleView';
+        const request = new FetchRequest(requri.href);
+        const data = await FetchProto(request, this.protoTypes, responseType);
+        return new Manga(this, provider, data['title'].tag, data['title'].name);
+    }
 
-        return data.Stories.map(story => {
-            return {
-                id: story.Work.Tag,
-                title: story.Work.Name
-            };
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const uri = new URL('list?category=series&sort=date', this.api_url);
+        const responseType = 'ComicZerosum.Listview';
+        const request = new FetchRequest(uri.href);
+        const data = await FetchProto(request, this.protoTypes, responseType);
+        return data['titles'].map(element => {
+            return new Manga(this, provider, element.tag, element.name.trim());
         });
     }
 
-    async _getChapters(manga) {
-        const request = new Request(`${this.data_url}/works/${manga.id}.json`, this.requestOptions);
-        const data = await this.fetchJSON(request);
-
-        return data.Work.Stories.map(story => {
-            return {
-                id: story.Url,
-                title: story.Name
-            };
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        //'https://api.zerosumonline.com/api/v1/title?tag=
+        const uri = new URL(`title?tag=${manga.Identifier}`, this.api_url);
+        const responseType = 'ComicZerosum.TitleView';
+        const request = new FetchRequest(uri.href);
+        const data = await FetchProto(request, this.protoTypes, responseType);
+        return data['chapters'].map(chapter => {
+            return new Chapter(this, manga, chapter.id, chapter.name.trim());
         });
     }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        //https://api.zerosumonline.com/api/v1/viewer?chapter_id=
+        const uri = new URL(`viewer?chapter_id=${chapter.Identifier}`, this.api_url);
+        const responseType = 'ComicZerosum.MangaViewerView';
+        const request = new FetchRequest(uri.href, {
+            method: 'POST',
+        });
+        const data = await FetchProto(request, this.protoTypes, responseType);
+        return data['pages'].filter(page => page.imageUrl).map(page => new Page(this, chapter, new URL(page.imageUrl)));
+    }
 }
-*/
