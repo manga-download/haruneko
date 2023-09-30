e0a35497514ee9ce686e551e5b4e8a3200f5072d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-09-30 12:02:44 +0200

    legacy-scans : using their API
    * no more Mangastream
    * EZ api

--- web/src/engine/websites/LegacyScans.ts
+++ web/src/engine/websites/LegacyScans.ts
@@ -1,16 +1,29 @@
-// Auto-Generated export from HakuNeko Legacy
 import { Tags } from '../Tags';
 import icon from './LegacyScans.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as MangaStream from './decorators/WordPressMangaStream';
+import { DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
+import { FetchJSON, FetchRequest } from '../FetchProvider';
 
-@MangaStream.MangaCSS(/^https?:\/\/legacy-scans\.com\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageCSS()
+type APIMangas = {
+    comics: {
+        title: string,
+        slug : string
+    }[]
+}
+function ChapterExtractor(anchor: HTMLAnchorElement) {
+    return {
+        id: anchor.pathname,
+        title: anchor.querySelector<HTMLSpanElement>('span').textContent.trim()
+    };
+}
+
+@Common.MangaCSS(/^https?:\/\/legacy-scans\.com\/comics\/[^/]+$/, 'div.serieTitle h1')
+@Common.ChaptersSinglePageCSS('div.chapterList a', ChapterExtractor)
+@Common.PagesSinglePageCSS('div.readerMainContainer img[data-nuxt-img]')
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
+    private readonly apiUrl = 'https://api.legacy-scans.com';
+    private readonly mangasPerPages = 100;
 
     public constructor() {
         super('legacyscans', 'Legacy-Scans', 'https://legacy-scans.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.French);
@@ -19,19 +32,25 @@ export default class extends DecoratableMangaScraper {
     public override get Icon() {
         return icon;
     }
-}
 
-// Original Source
-/*
-class LegacyScans extends WordPressMangastream {
-
-    constructor() {
-        super();
-        super.id = 'legacyscans';
-        super.label = 'Legacy-Scans';
-        this.tags = ['webtoon', 'french'];
-        this.url = 'https://legacy-scans.com';
-        this.path = '/manga/list-mode/';
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const mangaList = [];
+        let run = true;
+        let start = 1;
+        let end = start + this.mangasPerPages;
+        while (run) {
+            const mangas = await this.getMangasFromRange(provider, start, end);
+            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
+            start = end + 1;
+            end = start + this.mangasPerPages;
+        }
+        return mangaList;
     }
+    async getMangasFromRange(provider: MangaPlugin, start: number, end : number): Promise<Manga[]>{
+        const url = new URL(`/misc/comic/search/query?status=&order=&genreNames=&type=&start=${start}&end=${end}`, this.apiUrl);
+        const request = new FetchRequest(url.href);
+        const mangas = await FetchJSON<APIMangas>(request);
+        return mangas.comics.map(manga => new Manga(this, provider, `/comics/${manga.slug}`, manga.title.trim()));
+    }
+
 }
-*/
