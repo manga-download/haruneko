98ad94e29398511679114db99099f65003dc1dfe
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-01 20:57:44 +0200

    use proto file as module import

--- web/src/engine/websites/ComicZerosum.ts
+++ web/src/engine/websites/ComicZerosum.ts
@@ -3,6 +3,7 @@ import icon from './ComicZerosum.webp';
 import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchProto, FetchRequest } from './../FetchProvider';
+import { protoTypes } from './ComicZerosum.proto';
 
 type APISingleTitle = {
     title: APITitle
@@ -38,125 +39,6 @@ type APIPages = {
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
     private api_url = 'https://api.zerosumonline.com/api/v1/';
-    private protoTypes = `
-        package ComicZerosum;
-        syntax = "proto3";
-
-        //Common
-        message Header {
-          optional uint32 lastUpdateTime = 1;
-          optional uint32 nextUpdateTime = 2;
-        }
-
-        //Mangas
-        message Listview {
-          optional Header header = 1;
-          repeated Title titles = 3;
-        }
-
-        message Title {
-          optional uint32 id = 1;
-          optional string tag = 2;
-          optional string name = 3;
-          optional string nameKana = 4;
-          optional string author = 5;
-          optional string authorKana = 6;
-          optional string description = 7;
-          optional string imgUrl = 8;
-          optional uint32 startTime = 9;
-          optional uint32 endTime = 10;
-          optional bool fanLetterEnable = 11;
-          optional bool r18Flag = 12;
-          optional Genres genres = 13;
-          optional uint32 lastUpdateTime = 14;
-          optional uint32 latestChapterId = 15;
-        }
-
-        message Genres {
-          repeated Genre genre = 1;
-        }
-
-        message Genre {
-          optional uint32 id = 1;
-          optional string name = 2;
-        }
-
-        message IndependentBook {
-          optional uint32 id = 1;
-          optional uint32 titleId = 2;
-          optional string name = 3;
-          optional string imageUrl = 4;
-          optional uint32 releaseTime = 5;
-          optional uint32 startTime = 6;
-          optional uint32 endTime = 7;
-          optional StoreUrls storeUrls = 8;
-        }
-
-        message StoreUrls {
-          repeated StoreUrl storeUrl = 1;
-        }
-
-        message StoreUrl {
-          optional int32 store = 1;
-          optional string url = 2;
-        }
-
-        //Chapter
-        message TitleView {
-          optional Header header = 1;
-          optional Title title = 2;
-          repeated Chapter chapters = 3;
-          repeated IndependentBook independentBooks =  4;
-          repeated TitleRelatedBanner titleRelatedBanners = 5;
-          optional Sns sns = 6;
-        }
-
-
-
-        message Chapter {
-          optional uint32 id = 1;
-          optional string name = 2;
-          optional string imageUrl = 3;
-          optional uint32 startTime = 4;
-          optional uint32 endTime = 5;
-          optional string nextUpdateTime = 6;
-          optional bool fanLetterEnabled = 7;
-        }
-
-
-        message TitleRelatedBanners {
-          repeated TitleRelatedBanner titleRelatedBanners = 1;
-        }
-
-        message TitleRelatedBanner {
-          optional string imageUrl = 1;
-          optional string url = 2;
-        }
-
-        message Sns {
-          optional string facebook = 1;
-          optional string twitter = 2;
-        }
-
-        //Pages
-
-        message MangaViewerView {
-          optional int32 status = 1;
-          optional uint32 titleId = 2;
-          optional string titleTag = 3;
-          optional string viewerTitle = 4;
-          repeated MangaPage pages = 5;
-        }
-
-        message MangaPage {
-          optional string imageUrl = 1;
-          optional LastPage lastPage = 2;
-        }
-
-        message LastPage {
-          optional uint32 t = 1;
-        }
-`;
     public constructor() {
         super('comiczerosum', `Comic ゼロサム (Comic ZEROSUM)`, 'https://zerosumonline.com', Tags.Language.Japanese, Tags.Media.Manga, Tags.Source.Official);
     }
@@ -172,14 +54,14 @@ export default class extends DecoratableMangaScraper {
         const mangaid = url.match(/\/detail\/([\w]+)/)[1];
         const requri = new URL(`title?tag=${mangaid}`, this.api_url);
         const request = new FetchRequest(requri.href);
-        const data = await FetchProto<APISingleTitle>(request, this.protoTypes, 'ComicZerosum.TitleView');
+        const data = await FetchProto<APISingleTitle>(request, protoTypes, 'ComicZerosum.TitleView');
         return new Manga(this, provider, data.title.tag, data.title.name);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         const uri = new URL('list?category=series&sort=date', this.api_url);
         const request = new FetchRequest(uri.href);
-        const data = await FetchProto<APITitles>(request, this.protoTypes, 'ComicZerosum.Listview');
+        const data = await FetchProto<APITitles>(request, protoTypes, 'ComicZerosum.Listview');
         return data.titles.map(element => {
             return new Manga(this, provider, element.tag, element.name.trim());
         });
@@ -188,7 +70,7 @@ export default class extends DecoratableMangaScraper {
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
         const uri = new URL(`title?tag=${manga.Identifier}`, this.api_url);
         const request = new FetchRequest(uri.href);
-        const data = await FetchProto<TitleView>(request, this.protoTypes, 'ComicZerosum.TitleView');
+        const data = await FetchProto<TitleView>(request, protoTypes, 'ComicZerosum.TitleView');
         return data.chapters.map(chapter => {
             return new Chapter(this, manga, String(chapter.id), chapter.name.trim());
         });
@@ -199,7 +81,7 @@ export default class extends DecoratableMangaScraper {
         const request = new FetchRequest(uri.href, {
             method: 'POST',
         });
-        const data = await FetchProto<APIPages>(request, this.protoTypes, 'ComicZerosum.MangaViewerView');
+        const data = await FetchProto<APIPages>(request, protoTypes, 'ComicZerosum.MangaViewerView');
         return data.pages.filter(page => page.imageUrl).map(page => new Page(this, chapter, new URL(page.imageUrl)));
     }
 }
