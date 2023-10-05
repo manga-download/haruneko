b8ac584a1ee2d1e7a18295f24707271b9884e37a
Ronny <wegener.ronny@gmail.com>
2023-10-06 01:29:19 +0200

    introduce interactive file reading/saving

---
+++ web/src/engine/InteractiveFileContentProvider.ts
@@ -0,0 +1,15 @@
+export class InteractiveFileContentProvider {
+
+    public async LoadFile(options?: OpenFilePickerOptions): Promise<Blob | undefined> {
+        const files = await window.showOpenFilePicker(options);
+        console.log('Files:', files);
+        return files[0].getFile();
+    }
+
+    public async SaveFile(data: Blob, options?: SaveFilePickerOptions): Promise<void> {
+        const file = await window.showSaveFilePicker(options);
+        const stream = await file.createWritable();
+        await stream.write(data);
+        await stream.close();
+    }
+}
