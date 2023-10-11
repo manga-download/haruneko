d7fdb2fe47011f71cd19a4cb9f4c69d0c91e4836
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-11 08:02:03 +0000

    Move comico from legacy (#239)

--- web/src/engine/websites/Comico.ts
+++ web/src/engine/websites/Comico.ts
@@ -1,8 +1,8 @@
-import { Tags } from '../../Tags';
+import { Tags } from '../Tags';
 import icon from './Comico.webp';
-import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../../providers/MangaPlugin';
-import { Fetch, FetchRequest } from '../../FetchProvider';
-import * as Common from '../decorators/Common';
+import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
+import { Fetch, FetchRequest } from '../FetchProvider';
+import * as Common from './decorators/Common';
 
 type APIResult<T> = {
     result: {
@@ -77,11 +77,9 @@ export default class extends DecoratableMangaScraper {
         return new RegExp(/^https?:\/\/comico.jp\/\S+\/\d+$/).test(url);
     }
 
-    //good manga to test : https://comico.jp/comic/9331
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const id = new URL(url).pathname;
-        const response = await this._fetchPOST(id);
-        const data: APIResult<ApiChapters> = JSON.parse(response);
+        const data = await this.fetchPOST<APIResult<ApiChapters>>(id);
         const title = data.data.volume.content != null ? data.data.volume.content.name : data.data.episode.content.name;
         return new Manga(this, provider, id, title);
     }
@@ -89,21 +87,19 @@ export default class extends DecoratableMangaScraper {
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         const mangaList = [];
         for (let page = 1, run = true; run; page++) {
-            const mangas = await this._getMangasFromPage(page, provider);
+            const mangas = await this.getMangasFromPage(page, provider);
             mangas.length > 0 ? mangaList.push(...mangas) : run = false;
         }
         return mangaList;
     }
 
-    private async _getMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
-        const response = await this._fetchPOST('/all_comic/new_release?pageNo=' + page + '&pageSize=50');
-        const data: APIResult<ApiMangas> = JSON.parse(response);
-        return data.result.code != 200 ? [] : data.data.contents.map(manga => new Manga(this, provider, '/' + manga.type + '/' + manga.id, manga.name));
+    private async getMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
+        const data = await this.fetchPOST<APIResult<ApiMangas>>(`/all_comic/new_release?pageNo=${page}&pageSize=50`);
+        return data.result.code != 200 ? [] : data.data.contents.map(manga => new Manga(this, provider, `/${manga.type}/${manga.id}`, manga.name));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const response = await this._fetchPOST(manga.Identifier);
-        const data: APIResult<ApiChapters> = JSON.parse(response);
+        const data = await this.fetchPOST<APIResult<ApiChapters>>(manga.Identifier);
         //episode or volume?
         const element = data.data.episode.content != null ? data.data.episode.content : data.data.volume.content;
         return element.chapters
@@ -112,19 +108,18 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const response = await this._fetchPOST(chapter.Parent.Identifier + '/chapter/' + chapter.Identifier + '/product');
-        const data: APIResult<ApiPage> = JSON.parse(response);
+        const data = await this.fetchPOST<APIResult<ApiPage>>(`${chapter.Parent.Identifier}/chapter/${chapter.Identifier}/product`);
         if (data.data.content.chapterFileFormat == 'epub') {
             throw Error('This chapter is an Epub :/');
         }
         const pages = [];
         for (const page of data.data.chapter.images) {
-            pages.push(await this.cookPictureUrl(page));
+            pages.push(await this.decryptPictureUrl(page));
         }
         return pages.map(image => new Page(this, chapter, new URL(image)));
     }
 
-    async cookPictureUrl(page: ApiImage): Promise<string> {
+    async decryptPictureUrl(page: ApiImage): Promise<string> {
         const AESKey = 'a7fc9dc89f2c873d79397f8a0028a4cd';
         const iv = new Uint8Array(16).buffer;//empty IV, just a 16 bytes buffer
         const passPhrase = Buffer.from(AESKey, 'utf-8');
@@ -144,7 +139,7 @@ export default class extends DecoratableMangaScraper {
         return new TextDecoder('utf-8').decode(decrypted) + '?' + page.parameter;
     }
 
-    async _fetchPOST(path: string): Promise<string> {
+    async fetchPOST<T>(path: string): Promise<T> {
         const webkey = '9241d2f090d01716feac20ae08ba791a';
         const ip = '0.0.0.0';
         const tm = Math.round(new Date().getTime() / 1000);
@@ -170,6 +165,7 @@ export default class extends DecoratableMangaScraper {
             }
         });
         const response = await Fetch(request);
-        return await response.text();
+        const data = await response.text();
+        return JSON.parse(data) as T;
     }
 }
