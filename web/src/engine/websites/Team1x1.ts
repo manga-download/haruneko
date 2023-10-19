483046ba0cba71d664f0182a41fbb676e654baf6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-19 18:16:43 +0200

    move team1x1 from legacy

---
+++ web/src/engine/websites/Team1x1.ts
@@ -0,0 +1,47 @@
+import { Tags } from '../Tags';
+import icon from './Team1x1.webp';
+import { Chapter, DecoratableMangaScraper, type Manga, type MangaPlugin } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import { FetchCSS, FetchRequest } from '../FetchProvider';
+
+//TODO: Implement Url setting
+function MangaExtractor(anchor: HTMLAnchorElement) {
+    const id = anchor.pathname;
+    const title = anchor.querySelector('.tt').textContent.trim();
+    return {id, title };
+}
+
+@Common.MangasMultiPageCSS('/series?page={page}', 'div.bs div.bsx a', 1, 1, 0, MangaExtractor)
+@Common.PagesSinglePageCSS('.page-break img')
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+    public constructor() {
+        super('team1x1', `Team X`, 'https://team11x11.com', Tags.Language.Arabic, Tags.Media.Manhua, Tags.Media.Manhwa);
+    }
+    public override get Icon() {
+        return icon;
+    }
+
+    public override ValidateMangaURL(url: string): boolean {
+        return new RegExp(`^${this.URI.origin}/series/[^/]+$`).test(url);
+    }
+
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        return await Common.FetchMangaCSS.call(this, provider, url, 'div.author-info-title h1');
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const chapterList = [];
+        for (let page = 1, run = true; run; page++) {
+            const chapters = await this._getChaptersFromPage(manga, page);
+            chapters.length > 0 ? chapterList.push(...chapters) : run = false;
+        }
+        return chapterList;
+    }
+    private async _getChaptersFromPage(manga: Manga, page: number): Promise<Chapter[]>{
+        const uri = new URL(`${manga.Identifier}?page=${page}`, this.URI);
+        const request = new FetchRequest(uri.href);
+        const data = await FetchCSS<HTMLAnchorElement>(request, 'div.eplister ul li a:not([data-bs-toggle])');
+        return data.map(element => new Chapter(this, manga, element.pathname, element.querySelector('.epl-num:nth-of-type(2)').textContent.trim()));
+    }
+}
