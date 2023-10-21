57fcafb923399cb18dcc9b8f2ea309b092545086
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-21 09:31:40 +0000

    add ImageAjaxFromHTML Decorator (#264)
    * add ImageAjaxFromHTML Decorator
    
    and fix Assortedscans & Mexat accordingly
    
    * Update Mexat.ts
    
    * add deproxifying argument to Image methods
    
    and modify Mangatown.
    
    Websites using ImageElement, new decorator and FetchImageAjax were tested successfully.
    
    * use imageLink.origin as referer

--- web/src/engine/websites/Mexat.ts
+++ web/src/engine/websites/Mexat.ts
@@ -3,11 +3,10 @@ import icon from './Mexat.webp';
 import { Chapter, DecoratableMangaScraper, Page, type Manga } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchCSS, FetchRequest } from '../FetchProvider';
-import type { Priority } from '../taskpool/TaskPool';
-import DeProxify from '../transformers/ImageLinkDeProxifier';
 
 @Common.MangaCSS(/^https?:\/\/manga\.mexat\.com\/category\/[^/]+\/$/, 'div.page-head h1.page-title')
 @Common.MangasSinglePageCSS('/قائمة-المانجا/', 'div.content ul.MangaList li div.SeriesName a')
+@Common.ImageAjaxFromHTML('div.content div.post-inner div.pic a img')
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -40,15 +39,4 @@ export default class extends DecoratableMangaScraper {
         return data.map(page => new Page(this, chapter, new URL(`${request.url}?pid=${page.value}`, this.URI)));
     }
 
-    public override async FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
-        const image = await this.imageTaskPool.Add(async () => {
-            const request = new FetchRequest(page.Link.href, {
-                signal: signal
-            });
-            const realimage = new URL((await FetchCSS<HTMLImageElement>(request, 'div.content div.post-inner div.pic a img'))[0].src).pathname;
-            return new Page(this, page.Parent as Chapter, new URL(DeProxify(new URL(realimage, page.Link.origin)).href));
-        }, priority, signal);
-
-        return await Common.FetchImageAjax.call(this, image, priority, signal, false);
-    }
 }
