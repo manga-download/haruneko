2f92fbde0c3c8ad704bcc6c30ac39909459c2e8a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-21 19:27:59 +0200

    Update LELScan.ts

--- web/src/engine/websites/LELScan.ts
+++ web/src/engine/websites/LELScan.ts
@@ -3,8 +3,6 @@ import icon from './LELScan.webp';
 import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchCSS, FetchRequest } from '../FetchProvider';
-import type { Priority } from '../taskpool/TaskPool';
-import DeProxify from '../transformers/ImageLinkDeProxifier';
 
 function ChapterExtractor(option: HTMLOptionElement) {
     const id = new URL(option.value).pathname;
@@ -21,6 +19,7 @@ function MangaExtractor(anchor: HTMLAnchorElement) {
 @Common.MangaCSS(/^https?:\/\/lelscans\.net\/lecture[^/]+$/, 'meta[name="lelscan"]')
 @Common.MangasSinglePageCSS('', 'div.outil_lecture ul li a', MangaExtractor)
 @Common.ChaptersSinglePageCSS('div#header-image form select:first-of-type option', ChapterExtractor)
+@Common.ImageAjaxFromHTML('div#image img')
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -37,17 +36,4 @@ export default class extends DecoratableMangaScraper {
         data = data.filter(element => parseInt(element.text.trim()));
         return data.map(page => new Page(this, chapter, new URL(page.pathname, this.URI)));
     }
-
-    public override async FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
-        const image = await this.imageTaskPool.Add(async () => {
-            const request = new FetchRequest(page.Link.href, {
-                signal: signal
-            });
-            const realimage = new URL((await FetchCSS<HTMLImageElement>(request, 'div#image img'))[0].src).pathname;
-            return new Page(this, page.Parent as Chapter, new URL(DeProxify(new URL(realimage, page.Link.origin)).href));
-        }, priority, signal);
-
-        return await Common.FetchImageAjax.call(this, image, priority, signal, false);
-    }
-
 }
