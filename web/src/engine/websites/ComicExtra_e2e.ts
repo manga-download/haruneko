852825d79d63f722931a0c85b34780a2517e998e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-21 10:27:07 +0200

    comicextra changed url

---
+++ web/src/engine/websites/ComicExtra_e2e.ts
@@ -0,0 +1,25 @@
+import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'comicextra',
+        title: 'ComicExtra'
+    },
+    container: {
+        url: 'https://comicextra.me/comic/grimm-fairy-tales-2016',
+        id: '/comic/grimm-fairy-tales-2016',
+        title: 'Grimm Fairy Tales (2016)'
+    },
+    child: {
+        id: '/comic/grimm-fairy-tales-2016/issue-2023-swimsuit-special/full',
+        title: '#2023 Swimsuit Special'
+    },
+    entry: {
+        index: 0,
+        size: 349_415,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
