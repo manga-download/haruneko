c9782861e272dcb0a95dce4524835a8242f89e89
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-21 13:21:51 +0200

    move webace from legacy

--- web/src/engine/websites/WebAce.ts
+++ web/src/engine/websites/WebAce.ts
@@ -1,11 +1,12 @@
-import { Tags } from '../../Tags';
+import { Tags } from '../Tags';
 import icon from './WebAce.webp';
-import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../../providers/MangaPlugin';
-import * as Common from '../decorators/Common';
-import { FetchCSS, FetchJSON, FetchRequest } from '../../FetchProvider';
+import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import { FetchCSS, FetchJSON, FetchRequest } from '../FetchProvider';
+
 function MangaExtractor(element: HTMLDivElement) {
     const anchor = element.querySelector<HTMLAnchorElement>('a');
-    const id = anchor.pathname.split('comics/')[0];
+    const id = anchor.pathname.match(/(\/\S+\/contents\/\d+\/)/)[1];
     const title = element.querySelector <HTMLHeadingElement>('h3').textContent.trim();
     return { id, title };
 }
@@ -21,15 +22,18 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExp(/^https?:\/\/web-ace\.jp\/\S+\/contents\/\d+\/(?!episode)/).test(url);
+        // NOTE: only mangas with ID >= 1000000 have chapters for online reading
+        return new RegExp(/^https?:\/\/web-ace\.jp\/\S+\/contents\/\d{7}\//).test(url);
+
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        //good url should be https://web-ace.jp/youngaceup/contents/1000183/
-        //website manga list gives links like this https://web-ace.jp/youngaceup/contents/1000183/comics/3524/
-        //we have to remove the garbage (its easier to accept both for the user)
+        //website manga list gives links like this https://web-ace.jp/sheepmagazine/contents/123456/comics/12345688/
+        //we have to remove the garbage so its in the form https://web-ace.jp/sheepmagazine/contents/
+        //We cant ask the user to get a matching url since the website doesnt provide them within the good format
+        //we also enforce / a the end of the identifier
         const uri = new URL(url);
-        uri.pathname = uri.pathname.split('comics/')[0];
+        uri.pathname = uri.pathname.match(/(\/\S+\/contents\/\d{7}\/)/)[1];
         const request = new FetchRequest(uri.href);
         const data = await FetchCSS<HTMLHeadingElement>(request, 'div#sakuhin-info div.credit h1');
         return new Manga(this, provider, uri.pathname, data[0].textContent.trim());
@@ -42,7 +46,7 @@ export default class extends DecoratableMangaScraper {
             mangas.length > 0 ? mangaList.push(...mangas) : run = false;
         }
         // NOTE: only mangas with ID >= 1000000 have chapters for online reading
-        mangaList = mangaList.filter(manga => /\d{7}\/?$/.test(manga.Identifier));
+        mangaList = mangaList.filter(manga => /\d{7}\/$/.test(manga.Identifier));
         //filter duplicates (because we removed the /comics/ part we have dupes)
         return mangaList.filter((obj, index, arr) => {
             return arr.map(mapObj => mapObj.Identifier).indexOf(obj.Identifier) === index;
@@ -51,14 +55,14 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const url = new URL(manga.Identifier + 'episode/', this.URI).href;
+        const url = new URL(`${manga.Identifier}episode/`, this.URI).href;
         const request = new FetchRequest(url);
         const data = await FetchCSS<HTMLAnchorElement>(request, 'div#read ul.table-view li.media:not(.yudo) a.navigate-right');
         return data.map(element => new Chapter(this, manga, element.pathname, element.querySelector('div.media-body p.text-bold').textContent.trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const url = new URL(chapter.Identifier + 'json/', this.URI).href;
+        const url = new URL(`${chapter.Identifier}json/`, this.URI).href;
         const request = new FetchRequest(url);
         const data = await FetchJSON<string[]>(request);
         return data.map(image => new Page(this, chapter, new URL(image, request.url)));
