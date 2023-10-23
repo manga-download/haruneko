7d4ff1600807ede066f92363ea7727eeee6af9e0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-23 11:56:37 +0200

    change manga for testing

--- web/src/engine/websites/Shakai_e2e.ts
+++ web/src/engine/websites/Shakai_e2e.ts
@@ -6,18 +6,17 @@ const config: Config = {
         title: 'Shakai'
     },
     container: {
-        url: 'https://shakai.ru/manga/350',
-        id: '350',
-        title: 'One Piece / Ван Пис',
-        timeout: 10000 //api fetch all chapter data, and well, its one piece after all
+        url: 'https://shakai.ru/manga/2489',
+        id: '2489',
+        title: 'Oyasumi Punpun / Спокойной ночи, Пунпун',
     },
     child: {
-        id: '0_e',
-        title: '0_e'
+        id: '13_147',
+        title: '13_147'
     },
     entry: {
         index: 0,
-        size: 839_916,
+        size: 513_938,
         type: 'image/jpeg'
     }
 };
