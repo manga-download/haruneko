e81b635af5abe0d863ddde3b32a94beda5bcd754
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-10-23 10:30:55 +0000

    move UraSunday from legacy (#274)

---
+++ web/src/engine/websites/UraSunday_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'urasunday',
+        title: '裏サンデー (Ura Sunday)'
+    },
+    container: {
+        url: 'https://urasunday.com/title/1749',
+        id: '/title/1749',
+        title: '最強女師匠たちが育成方針を巡って修羅場'
+    },
+    child: {
+        id: '/title/1749/225480',
+        title: '第39話(前編) - 制御不能',
+    },
+    entry: {
+        index: 0,
+        size: 113_326,
+        type: 'image/webp'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
