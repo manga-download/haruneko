377c8b696310e6b460912c09b44e4e0375c5b2bc
Ronny <wegener.ronny@gmail.com>
2023-10-23 18:10:13 +0200

    use more expressive name for callback

--- web/src/engine/transformers/ImageDescrambler.ts
+++ web/src/engine/transformers/ImageDescrambler.ts
@@ -3,10 +3,10 @@ import { Key as GlobalKey } from '../SettingsGlobal';
 
 type DrawingCallback = (source: ImageBitmap, target: OffscreenCanvasRenderingContext2D) => PromiseLike<void>;
 
-export default async function DeScramble(scrambled: ImageBitmapSource, render: DrawingCallback): Promise<Blob | null> {
+export default async function DeScramble(scrambled: ImageBitmapSource, draw: DrawingCallback): Promise<Blob | null> {
     const bitmap = await createImageBitmap(scrambled);
     const canvas = new OffscreenCanvas(bitmap.width, bitmap.height);
-    await render(bitmap, canvas.getContext('2d'));
+    await draw(bitmap, canvas.getContext('2d'));
 
     const settings = HakuNeko.SettingsManager.OpenScope();
     const options = {
