1ff35ffc37f8d5e3b9434e420cdc238964218067
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-07 20:51:38 +0100

    add rate limit for images

--- web/src/engine/websites/MangAs.ts
+++ web/src/engine/websites/MangAs.ts
@@ -3,7 +3,10 @@ import icon from './MangAs.webp';
 import { DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as MangaReader from './decorators/MangaReaderCMS';
-import type { Priority } from '../taskpool/DeferredTask';
+import { RateLimit } from '../taskpool/RateLimit';
+import { Numeric } from '../SettingsManager';
+import { WebsiteResourceKey as R } from '../../i18n/ILocale';
+import type { Priority } from '../taskpool/TaskPool';
 
 const chapterScript = `
     new Promise((resolve, reject) => {
@@ -32,6 +35,10 @@ export default class extends DecoratableMangaScraper {
 
     public constructor() {
         super('mangas', `MangAs`, 'https://mangas.in', Tags.Language.Spanish, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
+        this.Settings.throttle = new Numeric('throttle.images', R.Plugin_Settings_ThrottlingInteraction, R.Plugin_Settings_ThrottlingInteractionInfo, 4, 1, 60);
+        this.Settings.throttle.ValueChanged.Subscribe((_, value: number) => this.imageTaskPool.RateLimit = new RateLimit(value, 5));
+        this.imageTaskPool.RateLimit = new RateLimit(this.Settings.throttle.value as number, 5);
+        console.log(this.imageTaskPool.RateLimit);
     }
 
     public override get Icon() {
@@ -41,12 +48,9 @@ export default class extends DecoratableMangaScraper {
     public override async FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const server1 = 's1.mangas.in/uploads/manga';
         const server2 = 's2.mangas.in/uploads/manga';
-
-        const blob = await Common.FetchImageElement.call(this, page, priority, signal);
+        const blob = await Common.FetchImageElement.call(this, page, priority, signal, true, true);
         if (blob.type.startsWith('image/')) return blob;
-
         page.Link.href = page.Link.href.includes(server1) ? page.Link.href.replace(server1, server2) : page.Link.href.replace(server2, server1);
-        return Common.FetchImageElement.call(this, page, priority, signal);
+        return Common.FetchImageElement.call(this, page, priority, signal, true, true);
     }
-
 }
