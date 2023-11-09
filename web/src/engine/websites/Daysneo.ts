e91678f63d0102ef170b466ff1386458dd402374
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-09 12:15:50 +0100

    add Daysneo

---
+++ web/src/engine/websites/Daysneo.ts
@@ -0,0 +1,26 @@
+import { Tags } from '../Tags';
+import icon from './Daysneo.webp';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import { FetchRegex, FetchRequest } from '../FetchProvider';
+
+@Common.MangaCSS(/^https?:\/\/daysneo\.com\/works\/[^/]+\.html$/, 'p.f150.b')
+@Common.MangasMultiPageCSS('/search/?page_num={page}', 'ul.tile li strong.f130 a')
+@Common.ChaptersSinglePageCSS('ul.ul01 li strong.f130 a')
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('daysneo', `Daysneo`, 'https://daysneo.com', Tags.Language.Japanese, Tags.Media.Manga);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const request = new FetchRequest(new URL(chapter.Identifier, this.URI).href);
+        const data = await FetchRegex(request, /src="([^"]*)">'\);/g);
+        return data.map(image => new Page(this, chapter, new URL(image, this.URI)));
+    }
+}
