ec81394ceae376f2d187a5bbf1f96a4a225d682a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-10 15:09:29 +0000

    port SInMh websites and SinMH fixes (#341)

---
+++ web/src/engine/websites/ZuiMH_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'zuimh',
+        title: '最漫画 (ZuiMH)'
+    },
+    container: {
+        url: 'https://www.zuimh.com/manhua/chenshuiwanguchushihengtuizhutian/',
+        id: '/manhua/chenshuiwanguchushihengtuizhutian/',
+        title: '沉睡万古：出世横推诸天',
+        timeout: 20000
+    },
+    child: {
+        id: '/manhua/chenshuiwanguchushihengtuizhutian/2108800.html',
+        title: '1话 姜家祖地，任行觉醒！'
+    },
+    entry: {
+        index: 0,
+        size: 26_037,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
