3b398d358b96da5f88424aa0b3c0b7b76500bffd
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-12 16:43:37 +0100

    use origin placeholder

--- web/src/engine/websites/ReadComicOnlineLI.ts
+++ web/src/engine/websites/ReadComicOnlineLI.ts
@@ -22,7 +22,7 @@ function MangInfoExtractor(element: HTMLDivElement) { //needed because titles ar
     };
 }
 
-@Common.MangaCSS(/^https?:\/\/readcomiconline.li\/Comic\/[^/]+$/, 'div.barContent a.bigChar')
+@Common.MangaCSS(/^{origin}\/Comic\/[^/]+$/, 'div.barContent a.bigChar')
 @Common.MangasMultiPageCSS('/ComicList?page={page}', 'div.list-comic div.item, div.item-list div.group div.col.info', 1, 1, 0, MangInfoExtractor)
 @Common.ChaptersSinglePageCSS('div.episodeList table.listing tr td:first-of-type a, div.section ul.list li a', ChapterExtractor)
 @Common.PagesSinglePageJS('lstImages', 2500) //may trigger a captcha request
