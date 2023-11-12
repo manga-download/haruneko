dd903574f0614e068aea89d4d62d5d7ce71044b7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-12 15:39:28 +0100

    Update ElevenToon.ts

--- web/src/engine/websites/ElevenToon.ts
+++ web/src/engine/websites/ElevenToon.ts
@@ -11,7 +11,7 @@ function MangaExtractor(anchor: HTMLAnchorElement) {
     };
 }
 
-@Common.MangaCSS(/^https?:\/\/www\.11toon\d*\.com\/bbs\/board\.php\?bo_table=toons&stx=[^/]+/, '#cover-info h2', Common.ElementLabelExtractor(), true )
+@Common.MangaCSS(/^{origin}\/bbs\/board\.php\?bo_table=toons&stx=[^/]+/, '#cover-info h2', Common.ElementLabelExtractor(), true )
 @Common.MangasMultiPageCSS('/bbs/board.php?bo_table=toon_c&type=upd&page={page}', 'ul.homelist li[data-id] a', 1, 1, 0, MangaExtractor)
 @Common.PagesSinglePageJS('img_list', 500)
 @Common.ImageAjax()
