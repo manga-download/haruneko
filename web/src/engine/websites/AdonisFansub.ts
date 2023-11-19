69f7484e017984f03629e33319af8a5685e098d2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-19 13:10:01 +0100

    add scanlator tags

--- web/src/engine/websites/AdonisFansub.ts
+++ web/src/engine/websites/AdonisFansub.ts
@@ -12,7 +12,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('adonisfansub', 'Adonis Fansub', 'https://manga.adonisfansub.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish);
+        super('adonisfansub', 'Adonis Fansub', 'https://manga.adonisfansub.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
