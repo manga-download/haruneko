7c3c05ae6963242152134aabd1ab1bc192028e53
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-20 14:13:27 +0100

    add bestmanhua

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -34,6 +34,7 @@ export { default as BacaManga } from './BacaManga';
 export { default as BakaMan } from './BakaMan';
 export { default as Batoto } from './Batoto';
 export { default as BestManga } from './BestManga';
+export { default as BestManhua } from './BestManhua';
 export { default as BibiManga } from './BibiManga';
 export { default as BlogTruyen } from './BlogTruyen';
 export { default as Boosei } from './Boosei';
