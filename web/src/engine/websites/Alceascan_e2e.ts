8708e7c8f098ffb4483ccc27f737d302d3ee3ab9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-20 11:59:35 +0000

    Add missing manga websites from Hakuneko to Haruneko - Letter A (#344)
    * add adonisfansub
    
    * add ainzscans
    
    * add Alceascan
    
    * add "Animated Glitched Scans"
    
    * add AnzManga
    
    * add AquaManga
    
    * add ArcaneScans
    
    * add AstralManga
    
    * Update Alceascan.ts
    
    * fix adonis test
    
    * add scanlator tags

---
+++ web/src/engine/websites/Alceascan_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'alceascan',
+        title: 'Alceascan'
+    },
+    container: {
+        url: 'https://alceascan.my.id/manga/18-year-old-spy/',
+        id: '/manga/18-year-old-spy/',
+        title: '18 Year Old Spy'
+    },
+    child: {
+        id: '/18-year-old-spy-chapter-56-indonesia-terbaru/',
+        title: 'Chapter 56'
+    },
+    entry: {
+        index: 1,
+        size: 299_001,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
