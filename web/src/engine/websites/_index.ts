4a21f22d19d99eaf35d889496648464fab508df1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-21 20:10:36 +0100

    add Cocorip

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -43,6 +43,7 @@ export { default as CeriseScans } from './CeriseScans';
 export { default as ChibiManga } from './ChibiManga';
 export { default as CizgiRomanArsivi } from './CizgiRomanArsivi';
 export { default as CloverManga } from './CloverManga';
+export { default as Cocorip } from './Cocorip';
 export { default as ColoredManga } from './ColoredManga';
 export { default as ComicAction } from './ComicAction';
 export { default as ComicBorder } from './ComicBorder';
