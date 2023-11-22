11abebcab24c4af2fee8a54362969a2204a4c901
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-22 13:50:38 +0100

    add CosmicScansID

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -65,6 +65,7 @@ export { default as ComicZerosum } from './ComicZerosum';
 export { default as ComikeyArchive } from './ComikeyArchive';
 export { default as CopyPasteScan } from './CopyPasteScan';
 export { default as CosmicScans } from './CosmicScans';
+export { default as CosmicScansID } from './CosmicScansID';
 export { default as CrunchyScan } from './CrunchyScan';
 export { default as CyComi } from './CyComi';
 export { default as DankeFursLesen } from './DankeFursLesen';
