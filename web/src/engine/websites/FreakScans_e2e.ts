8b1bc032c6be11c13866a663025a242108b30774
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-23 19:21:09 +0000

    Missing websites from Hakuneko - Letter F (#352)
    * add FbSquads
    
    * FirstKissManga > LikeManga
    
    * and fix getting ALL pages (not just first  10)
    
    * add FoxWhiteScan
    
    * add FreakScans
    
    * add Futari
    
    * Update _index.ts

---
+++ web/src/engine/websites/FreakScans_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'freakscans',
+        title: 'Freak Scans'
+    },
+    container: {
+        url: 'https://freakscans.com/manga/dawn-of-the-predec1essor/',
+        id: '/manga/dawn-of-the-predec1essor/',
+        title: 'Dawn of the Predecessor'
+    },
+    child: {
+        id: '/dawn-of-the-predecessor-ch-01/',
+        title: 'Chapter 1'
+    },
+    entry: {
+        index: 0,
+        size: 673_007,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
