eee48cf40a4b33392159717de8168db35c67ac51
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-23 15:07:20 +0100

    remove duniakomik
    The team disbanded website is empty

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -92,7 +92,6 @@ export { default as DragonTea } from './DragonTea';
 export { default as DragonTranslation } from './DragonTranslation';
 export { default as DrakeScans } from './DrakeScans';
 export { default as DropeScan } from './DropeScan';
-export { default as DuniaKomik } from './DuniaKomik';
 export { default as DynastyScans } from './DynastyScans';
 export { default as EarlyManga } from './EarlyManga';
 export { default as EightMuses } from './EightMuses';
