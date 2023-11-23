14b181b5b500a65381709ab02bd43c8d62fe8980
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-23 16:41:01 +0000

    Missing websites from Hakuneko - Letter D (#351)
    * add Viya Fansub (ex-Dalsei)
    
    * add DIamondFansub
    
    * fix DoujinDesu
    
    * add DrakeScans
    
    * remove duniakomik
    
    The team disbanded website is empty

--- web/src/engine/transformers/BookmarkConverter_test.ts
+++ web/src/engine/transformers/BookmarkConverter_test.ts
@@ -62,6 +62,7 @@ describe('BookmarkConverter', () => {
             { sourceID: 'bananascan', targetID: 'harmonyscan' },
             { sourceID: 'bacamangaorg', targetID: 'bacamanga' },
             { sourceID: 'crazyscans', targetID: 'mangacultivator' },
+            { sourceID: 'dalsei', targetID: 'viyafansub' },
             { sourceID: 'flamescans-org', targetID: 'flamecomics' },
             { sourceID: 'kisscomic', targetID: 'readcomiconline' },
             { sourceID: 'heavenmanga', targetID: 'beetoon' }, // (future zbulu PR)
