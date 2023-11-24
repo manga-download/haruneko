db8544d021998c54fad70c420121be1593460821
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-24 19:50:40 +0100

    Add MH decorator & websites

---
+++ web/src/engine/websites/kuimh_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'kuimh',
+        title: '酷爱漫画 (Kuimh)'
+    },
+    container: {
+        url: 'https://www.kuimh.com/book/mh11568',
+        id: '/book/mh11568',
+        title: '被大公家领养的圣女',
+    },
+    child: {
+        id: '/chapter/393045-3747909',
+        title: '序章',
+    },
+    entry: {
+        index: 0,
+        size: 34_815,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
