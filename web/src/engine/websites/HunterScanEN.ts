af38bd5b435aa438347aaf2e9fc91b5e3359cc87
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-25 13:11:56 +0000

    Missing websites from Hakuneko - Letter H (#357)
    * [hadesnofansub] change domain
    
    * add HastaTeamDDT (from legacy)
    
    * [hayalistic] change domain & fix clipboard
    
    * [HentaiFox] uses ChaptersUniqueFromManga
    
    * add Hinapyon
    
    * Update HManhwa.ts
    
    * add HunterScanEN
    
    * Update _index.ts

--- web/src/engine/websites/HunterScanEN.ts
+++ web/src/engine/websites/HunterScanEN.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
-import icon from './HManhwa.webp';
+import icon from './HunterScanEN.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^{origin}\/manhwa\/[^/]+\/$/, 'div.post-title h1')
+@Madara.MangaCSS(/^{origin}\/series\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
 @Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv1()
+@Madara.ChaptersSinglePageAJAXv2()
 @Madara.PagesSinglePageCSS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('hmanhwa', 'HManhwa', 'https://hmanhwa.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Language.Korean, Tags.Language.Thai, Tags.Rating.Pornographic);
+        super('hunterscan-en', 'Hunters Scan (English)', 'https://en.huntersscan.xyz', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English);
     }
 
     public override get Icon() {
