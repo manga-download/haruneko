7f993d28169eb975e3e6a4363826718ccca7d793
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-11-26 13:33:23 +0100

    add InfraFandub
    and fixes some icons

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -165,6 +165,7 @@ export { default as IceKr } from './IceKr';
 export { default as IMHentai } from './IMHentai';
 export { default as ImmortalUpdates } from './ImmortalUpdates';
 export { default as ImperioScans } from './ImperioScans';
+export { default as InfraFandub } from './InfraFandub';
 export { default as InManga } from './InManga';
 export { default as InmortalScan } from './InmortalScan';
 export { default as InstaManhwa } from './InstaManhwa';
