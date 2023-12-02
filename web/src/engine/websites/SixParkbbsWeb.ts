5dcaaa1c4ede78e591c4a23ad09231434e57a39f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-02 18:48:42 +0100

    remove decorator, extend classes

--- web/src/engine/websites/SixParkbbsWeb.ts
+++ web/src/engine/websites/SixParkbbsWeb.ts
@@ -1,18 +1,27 @@
 import { Tags } from '../Tags';
 import icon from './SixParkbbsWeb.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import * as SixParkbbs from './decorators/SixParkbbs';
+import SixParkbbsClub from './SixParkbbsClub';
 
-@Common.MangaCSS(/^{origin}\/index\.php\?app=forum&act=view&tid=\d+/, 'div.c-box p.c-box-h b', SixParkbbs.MangaLabelExtractor, true)
-@SixParkbbs.MangasMultiPageCSS('/index.php?app=forum&act=bbs&bbsid=2032&p={page}', 'div#d_list div.repl-list-one a:nth-child(1)', /.*\[漫画\]/)
 @Common.ChaptersUniqueFromManga()
-@Common.PagesSinglePageCSS('div.cen-main div.c-box-m center img', SixParkbbs.PagesExtractor)
 @Common.ImageAjax()
-export default class extends DecoratableMangaScraper {
+export default class extends SixParkbbsClub {
+
+    //FetchManga
+    protected mangaRegex = /^{origin}\/index\.php\?app=forum&act=view&tid=\d+/;
+    protected queryMangaTitle = 'div.c-box p.c-box-h b';
+
+    //FetchMangas
+    protected sub = '';
+    protected path = '/index.php?app=forum&act=bbs&bbsid=2032&p={page}';
+    protected queryManga = 'div#d_list div.repl-list-one a:nth-child(1)';
+    protected queryMatch = /.*\[漫画\]/;
+
+    //Fetchpages
+    protected queryPages = 'div.cen-main div.c-box-m center img';
 
     public constructor() {
-        super('sixparkbbsweb', `6parkbbs Web(新❀华漫)`, 'https://web.6parkbbs.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Source.Aggregator);
+        super('sixparkbbsweb', `6parkbbs Web(新❀华漫)`, 'https://web.6parkbbs.com', [Tags.Language.Chinese, Tags.Media.Manga, Tags.Source.Aggregator]);
     }
 
     public override get Icon() {
