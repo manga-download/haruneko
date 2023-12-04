3470d52eba09b82446b9eb14e9a9f6637a58961f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-04 15:14:45 +0100

    Add Mangalink
    Little captcha to pass

---
+++ web/src/engine/websites/MangaLink.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './MangaLink.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Madara from './decorators/WordPressMadara';
+import * as Common from './decorators/Common';
+
+@Madara.MangaCSS(/^{origin}\/readcomics\/[^/]+\/$/)
+@Madara.MangasMultiPageAJAX()
+@Madara.ChaptersSinglePageAJAXv1()
+@Madara.PagesSinglePageCSS()
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('mangalink', 'MangaLink', 'https://manga-link.org', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhua, Tags.Language.Arabic, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
