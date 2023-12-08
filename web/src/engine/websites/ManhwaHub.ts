0b0d1e8e53be11cd0f3034f323b6e645d67a9470
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-08 12:47:52 +0000

    Missing websites from Hakuneko - Letter M (part2) (#375)
    * remove mangakeyfi
    
    redirect to WebToonTR
    
    * remove mangakid
    
    * mangakik : update domain
    
    * mangakita update domain
    
    * mangakyo update domain
    
    * add MangaLike
    
    * Update MangaLike.ts
    
    * add MangaOni (MangaMX)
    
    * MangaNel >Manganato
    
    * fixes tests
    * fix website regex
    
    * add Manhwa68
    
    * ManhwaClub > ManhwaHentai.to
    
    * add ManhwaFreak
    
    * add Manhwahub
    
    * Update _index.ts

--- web/src/engine/websites/ManhwaHub.ts
+++ web/src/engine/websites/ManhwaHub.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
-import icon from './MangaLike.webp';
+import icon from './ManhwaHub.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^{origin}\/manhwa\/[^/]+\/$/)
-@Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv1()
+@Common.MangaCSS(/^{origin}\/webtoon\/[^/]+$/, 'ol.breadcrumb li:last-of-type a')
+@Common.MangasMultiPageCSS('?page={page}', 'div.post-title a')
+@Common.ChaptersSinglePageCSS('ul li.wp-manga-chapter > a')
 @Madara.PagesSinglePageCSS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('mangalike', 'MangaLike', 'https://manga-like.org', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhua, Tags.Language.Arabic, Tags.Source.Aggregator);
+        super('manhwahub', 'ManhwaHub', 'https://manhwahub.net', Tags.Media.Manhwa, Tags.Language.English, Tags.Language.Korean, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
