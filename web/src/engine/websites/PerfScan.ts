e91776a57fa269721cbc4193a52e7771b2aaef50
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-11 18:42:01 +0100

    add PerfScan

--- web/src/engine/websites/PerfScan.ts
+++ web/src/engine/websites/PerfScan.ts
@@ -1,19 +1,19 @@
 import { Tags } from '../Tags';
-import icon from './YugenMangasES.webp';
+import icon from './PerfScan.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as HeamCMS from './decorators/HeanCMS';
 
-const apiUrl = 'https://api.yugenmangas.net';
+const apiUrl = 'https://api.perf-scan.fr';
 
 @HeamCMS.MangaCSS(/^{origin}\/series\/[^/]+$/, apiUrl)
 @HeamCMS.MangasMultiPageAJAX(apiUrl)
 @HeamCMS.ChaptersSinglePageAJAX(apiUrl)
 @HeamCMS.PagesSinglePageAJAX(apiUrl)
-@HeamCMS.ImageAjax(true)
+@HeamCMS.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('yugenmangas-es', 'YugenMangas (ES)', 'https://yugenmangas.lat', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Novel, Tags.Language.Spanish, Tags.Source.Aggregator);
+        super('perfscan', 'Perf Scan', 'https://perf-scan.fr', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Novel, Tags.Language.French, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
---
+++ web/src/engine/websites/PerfScan_e2e.ts
@@ -0,0 +1,49 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const ComicConfig = {
+    plugin: {
+        id: 'perfscan',
+        title: 'Perf Scan'
+    },
+    container: {
+        url: 'https://perf-scan.fr/series/martial-peak-1702249200756',
+        id: 'martial-peak-1702249200756',
+        title: 'Martial Peak'
+    },
+    child: {
+        id: 'chapitre-1298',
+        title: 'S6 Chapitre 1298'
+    },
+    entry: {
+        index: 2,
+        size: 215_840,
+        type: 'image/webp'
+    }
+};
+
+const ComicFixture = new TestFixture(ComicConfig);
+describe(ComicFixture.Name, () => ComicFixture.AssertWebsite());
+
+const NovelConfig = {
+    plugin: {
+        id: 'perfscan',
+        title: 'Perf Scan'
+    },
+    container: {
+        url: 'https://perf-scan.fr/series/demonic-emperor-novel-1702249200676',
+        id: 'demonic-emperor-novel-1702249200676',
+        title: 'Demonic emperor - Novel'
+    },
+    child: {
+        id: 'chapitre-492',
+        title: 'S4 Chapitre 492'
+    },
+    entry: {
+        index: 0,
+        size: 789_130,
+        type: 'image/png'
+    }
+};
+
+const NovelFixture = new TestFixture(NovelConfig);
+describe(NovelFixture.Name, () => NovelFixture.AssertWebsite());
