1750644b15465f8bb23346e8a2e42a764a4cd676
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-15 17:10:39 +0100

    add RidiBooks

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -470,6 +470,7 @@ export { default as ReaperScansFR } from './ReaperScansFR';
 export { default as ReaperScansTR } from './ReaperScansTR';
 export { default as ResetScans } from './ResetScans';
 export { default as Retsu } from './Retsu';
+export { default as RidiBooks } from './RidiBooks';
 export { default as RizzComics } from './RizzComics';
 export { default as RomantikManga } from './RomantikManga';
 export { default as S2Manga } from './S2Manga';
