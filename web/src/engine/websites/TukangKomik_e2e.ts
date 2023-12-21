12e81000a3ed2fe203e27896ba4ea9787acffd94
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-21 16:27:46 +0000

    Missing websites from Hakuneko - Letter T (2) (#429)
    * Add tukangkomik
    
    * Add TuMangaNet
    
    * remove TvYManga (dead)

---
+++ web/src/engine/websites/TukangKomik_e2e.ts
@@ -0,0 +1,26 @@
+import { TestFixture, type Config } from '../../../test/WebsitesFixture';
+
+const config: Config = {
+    plugin: {
+        id: 'tukangkomik',
+        title: 'TukangKomik'
+    },
+    container: {
+        url: 'https://tukangkomik.id/manga/nano-m/',
+        id: '/manga/nano-m/',
+        title: 'Nano Machine',
+        timeout: 10000
+    },
+    child: {
+        id: '/nano-m-chapter-1/',
+        title: 'Chapter 1'
+    },
+    entry: {
+        index: 1,
+        size: 316_840,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
