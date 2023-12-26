20dcb76b4f2a14cc6c3f9a5ca8c37728dd0f3391
MikeZeDev <MikeZeDev@users.noreply.github.com>
2023-12-26 14:08:47 +0100

    Add Imgur

---
+++ web/src/engine/websites/Imgur_e2e.ts
@@ -0,0 +1,25 @@
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'imgur',
+        title: 'Imgur'
+    },
+    container: {
+        url: 'https://imgur.com/gallery/sNDK0',
+        id: '/gallery/sNDK0',
+        title: 'My Walpapers'
+    },
+    child: {
+        id: '/gallery/sNDK0',
+        title: 'My Walpapers'
+    },
+    entry: {
+        index: 0,
+        size: 226_151,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
