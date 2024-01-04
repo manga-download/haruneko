afdb62191bca9b691bcc627bc6926f4856ea9036
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-04 18:24:30 +0100

    fix ToonGod

--- web/src/engine/websites/ToonGod.ts
+++ web/src/engine/websites/ToonGod.ts
@@ -1,36 +1,21 @@
-// Auto-Generated export from HakuNeko Legacy
-//import { Tags } from '../Tags';
+import { Tags } from '../Tags';
 import icon from './ToonGod.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
-@Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv1()
+@Madara.MangaCSS(/^{origin}\/webtoon\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
+@Madara.MangasMultiPageCSS(undefined, 0, '/home/page/{page}/')
+@Madara.ChaptersSinglePageAJAXv2()
 @Madara.PagesSinglePageCSS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('toongod', 'ToonGod', 'https://www.toongod.com'/*, Tags.Media., Tags.Language.*/);
+        super('toongod', 'ToonGod', 'https://www.toongod.org', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Language.English, Tags.Source.Aggregator, Tags.Rating.Pornographic);
     }
 
     public override get Icon() {
         return icon;
     }
 }
-
-// Original Source
-/*
-class ToonGod extends WordPressMadara {
-
-    constructor() {
-        super();
-        super.id = 'toongod';
-        super.label = 'ToonGod';
-        this.tags = [ 'webtoon', 'hentai', 'english' ];
-        this.url = 'https://www.toongod.com';
-    }
-}
-*/
