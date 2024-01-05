fa262c3edc5f5cbee7803c8f3e23f12b455f2bd3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-05 18:19:22 +0100

    Disable url setting for now

--- web/src/engine/websites/AllHentai.ts
+++ web/src/engine/websites/AllHentai.ts
@@ -3,8 +3,8 @@ import icon from './AllHentai.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as ReadM from './decorators/ReadMangaLive';
-import { Text } from '../SettingsManager';
-import { WebsiteResourceKey as R } from '../../i18n/ILocale';
+//import { Text } from '../SettingsManager';
+//import { WebsiteResourceKey as R } from '../../i18n/ILocale';
 
 //To see pictures we need to be logged
 //TODO: Add a login button? Well login works for now anyway, using the button in fluent core UI
@@ -18,9 +18,9 @@ export default class extends DecoratableMangaScraper {
 
     public constructor() {
         super('allhentai', `AllHentai`, 'https://20.allhen.online', Tags.Language.Russian, Tags.Media.Manga, Tags.Rating.Pornographic, Tags.Source.Aggregator);
-        this.Settings.url = new Text('UrlOverride', R.Plugin_Settings_UrlOverride, R.Plugin_Settings_UrlOverrideInfo, this.URI.href);
-        this.Settings.url.ValueChanged.Subscribe((_, value: string) => this.URI.href = value);
-        this.URI.href = this.Settings.url.value as string;
+        //this.Settings.url = new Text('UrlOverride', R.Plugin_Settings_UrlOverride, R.Plugin_Settings_UrlOverrideInfo, this.URI.href);
+        //this.Settings.url.ValueChanged.Subscribe((_, value: string) => this.URI.href = value);
+        //this.URI.href = this.Settings.url.Value as string;
     }
 
     public override get Icon() {
