483807db2f52b2b76505dbbea061aded385a2d12
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-06 18:21:31 +0100

    MangaSect use Liliana decorator

--- web/src/engine/websites/MangaSect.ts
+++ web/src/engine/websites/MangaSect.ts
@@ -1,18 +1,14 @@
 import { Tags } from '../Tags';
 import icon from './MangaSect.webp';
-import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchJSON, FetchRequest } from '../FetchProvider';
-import DeProxify from '../transformers/ImageLinkDeProxifier';
-
-type AJAXResponse = {
-    html: string;
-}
+import * as Liliana from './decorators/Liliana';
 
 @Common.MangaCSS(/^{origin}\/manga\/[^/]+\/\d+$/, 'article header h1')
-@Common.MangasMultiPageCSS('/all-manga/{page}/', 'div.grid div.text-center > a')
-@Common.ChaptersSinglePageCSS('li.chapter > a')
-@Common.ImageAjax()
+@Common.MangasMultiPageCSS(Liliana.mangaPath, 'div.grid div.text-center > a')
+@Common.ChaptersSinglePageCSS(Liliana.queryChapters)
+@Common.PagesSinglePageJS(Liliana.queryPagesScript, 500)
+@Common.ImageAjax(true, true)
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -22,25 +18,4 @@ export default class extends DecoratableMangaScraper {
     public override get Icon() {
         return icon;
     }
-
-    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const chapterid = chapter.Identifier.match(/\/([\d]+)$/)[1];
-        const request = new FetchRequest(new URL(`/ajax/image/list/chap/${chapterid}`, this.URI).href, {
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                Referer: new URL(chapter.Identifier, this.URI).href
-            }
-        });
-
-        const response = await FetchJSON<AJAXResponse>(request);
-        const dom = new DOMParser().parseFromString(response.html, 'text/html');
-        const data = [...dom.querySelectorAll<HTMLImageElement>('a.readimg img.lazy.loaded')].sort(function (a, b) {
-            const za = parseInt(a.parentElement.parentElement.dataset.index);
-            const zb = parseInt(b.parentElement.parentElement.dataset.index);
-            return za - zb;
-        });
-        return data.map(image => new Page(this, chapter, DeProxify(new URL(image.getAttribute('src')))));
-
-    }
-
 }
