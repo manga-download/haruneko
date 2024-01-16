9c2cdb329f3baf6c8327560b6d7dc1fca3cbabda
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-16 14:02:51 +0100

    improve platform abstraction

--- web/src/engine/websites/SixParkbbsClub.ts
+++ web/src/engine/websites/SixParkbbsClub.ts
@@ -3,7 +3,7 @@ import icon from './SixParkbbsClub.webp';
 import type { Chapter, Page} from '../providers/MangaPlugin';
 import { DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchRequest, FetchWindowCSS } from '../FetchProvider';
+import { FetchWindowCSS } from '../platform/FetchProvider';
 
 @Common.ChaptersUniqueFromManga()
 @Common.ImageAjax()
@@ -46,13 +46,13 @@ export default class extends DecoratableMangaScraper {
 
         //fetch pages count (because we must continue despites empty pages)
         const url = new URL(this.sub + this.path.replace('{page}', '1000'), this.URI).href;//great page number to make sure we get last
-        const data = await FetchWindowCSS<HTMLAnchorElement>(new FetchRequest(url), 'div#d_list_page a:nth-last-child(2)');
+        const data = await FetchWindowCSS<HTMLAnchorElement>(new Request(url), 'div#d_list_page a:nth-last-child(2)');
         const pageMaxUrl = new URL(data[0].href);
         const pageMax = parseInt(pageMaxUrl.searchParams.get('p') || pageMaxUrl.searchParams.get('nowpage'));
 
         for (let page = 0; page < pageMax; page += 1) {
             const url = new URL(this.sub + this.path.replace('{page}', `${page}`), this.URI).href;
-            const data = await FetchWindowCSS<HTMLAnchorElement>(new FetchRequest(url), this.queryManga);//If not using FetchWindowCSS we always fetch the same page
+            const data = await FetchWindowCSS<HTMLAnchorElement>(new Request(url), this.queryManga);//If not using FetchWindowCSS we always fetch the same page
             if (!data || data.length == 0) continue;
             const mangas = data
                 .filter(element => this.queryMatch.test(element.text))
