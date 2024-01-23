98b41d0cf6555a1b82ee66eba5ebc5349d233a36
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-23 21:15:07 +0100

    Add Hentai20

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -152,6 +152,7 @@ export { default as HeavenManga2 } from './HeavenManga2';
 export { default as HenChan } from './HenChan';
 export { default as Hentai2Read } from './Hentai2Read';
 export { default as Hentai3z } from './Hentai3z';
+export { default as Hentai20 } from './Hentai20';
 export { default as Hentaidexy } from './Hentaidexy';
 export { default as HentaiFox } from './HentaiFox';
 export { default as HentaiHand } from './HentaiHand';
