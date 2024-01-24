0e70fae3aab82999ea7919fbd2e7777b530f03a9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-24 11:16:27 +0000

    Add MagKan (#440)
    * Add MagKan
    
    * improve platform abstraction
    
    * MagKan chance selector in FetchChapters

---
+++ web/src/engine/websites/MagKan_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'magkan',
+        title: 'MagKan'
+    },
+    container: {
+        url: 'https://kansai.mag-garden.co.jp/oshihen/',
+        id: '/oshihen/',
+        title: '推し変しやがれ!!'
+    },
+    child: {
+        id: '/assets/files/oshihen1',
+        title: '第零話'
+    },
+    entry: {
+        index: 1,
+        size: 317_098,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
