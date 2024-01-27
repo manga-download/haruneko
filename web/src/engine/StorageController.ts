afc901fa297108bd1d062fefa32d88d2a0313d46
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-27 11:34:12 +0100

    StorageController : display warning when removing control chars in fiilepath

--- web/src/engine/StorageController.ts
+++ web/src/engine/StorageController.ts
@@ -33,8 +33,20 @@ export function SanitizeFileName(name: string): string {
         '?': '？', // https://unicode-table.com/en/FF1F/, https://unicode-table.com/en/FE56/
         '*': '＊', // https://unicode-table.com/en/FF0A/
     };
-    //https://en.wikipedia.org/wiki/C0_and_C1_control_codes
-    return name.replace(/./g, c => c.charCodeAt(0) < 32 ? '' : c.charCodeAt(0) > 0x7E && c.charCodeAt(0) < 0xA0 ? '' : lookup[c] ?? c).replace(/[\s.]+$/, '').trim() || 'untitled';
+
+    // eslint-disable-next-line no-control-regex
+    const patternControlCharsUTF8 = /[\u0000-\u001F\u007F-\u009F]/gu; //https://en.wikipedia.org/wiki/C0_and_C1_control_codes
+
+    if (patternControlCharsUTF8.test(name)) {
+        const sequenceCharsUTF8 = name.split('').map(c => c.charCodeAt(0).toString(16).toUpperCase().padStart(4, '0'));
+        console.warn(`The filename '${name}' contains one or more invalid control characters which are going to be removed:`, sequenceCharsUTF8, '=>', name.match(patternControlCharsUTF8));
+    }
+
+    return name
+        .replace(patternControlCharsUTF8, '')
+        .replace(/./g, c => lookup[c] ?? c)
+        .replace(/[\s.]+$/, '')
+        .trim() || 'untitled';
 }
 
 /*
