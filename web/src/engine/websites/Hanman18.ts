2fedf1354afc7c80834f2721479207dc52bf6da8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-01-27 14:11:51 +0100

    Add Hanman18
    similar to Manhwa18Club

--- web/src/engine/websites/Hanman18.ts
+++ web/src/engine/websites/Hanman18.ts
@@ -1,24 +1,19 @@
 import { Tags } from '../Tags';
-import icon from './MangaEighteenClub.webp';
+import icon from './Hanman18.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as MangaReader from './decorators/MangaReaderCMS';
-
-export const pageScript = `
-    new Promise(resolve => {
-        resolve(window.slides_p_path.map(element => window.atob(element)));
-    });
-`;
+import { pageScript } from './MangaEighteenClub';
 
 @Common.MangaCSS(/^{origin}\/manhwa\/[^/]+$/, 'div.detail_infomation div.detail_name')
-@Common.MangasMultiPageCSS('/list-manga/{page}', 'div.story_item div.mg_info div.mg_name > a' )
+@Common.MangasMultiPageCSS('/list-manga/{page}', 'div.story_item div.mg_info div.mg_name > a')
 @Common.ChaptersSinglePageCSS(MangaReader.queryChapters, MangaReader.ChapterInfoExtractor)
 @Common.PagesSinglePageJS(pageScript, 500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('manga18-club', `Manga18.club`, 'https://manga18.club', Tags.Language.English, Tags.Media.Manhwa, Tags.Rating.Erotica, Tags.Source.Aggregator);
+        super('hanman18', 'Hanman18', 'https://hanman18.com', Tags.Media.Manhwa, Tags.Language.Chinese, Tags.Rating.Pornographic, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
