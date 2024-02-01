76e58e8b84a0f8ecb996c42fd27d72064c3bc8c8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-01 14:00:39 +0100

    Add OlimpoScan
    https://github.com/manga-download/hakuneko/issues/6286

---
+++ web/src/engine/websites/OlimpoScans.ts
@@ -0,0 +1,31 @@
+import { Tags } from '../Tags';
+import icon from './OlimpoScans.webp';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as FlatManga from './decorators/FlatManga';
+
+@Common.MangaCSS(/^{origin}\/[^/]+\.html$/, FlatManga.queryMangaTitle, FlatManga.MangaLabelExtractor)
+@Common.MangasMultiPageCSS('/manga-list.html?page={page}', FlatManga.queryMangas, 1, 1, 0, FlatManga.MangaExtractor)
+@FlatManga.ChaptersSinglePageCSS()
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('olimposcans', `OlimpoScans`, 'https://olimposcans.com', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Language.Spanish, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const pages = await FlatManga.FetchPagesSinglePageCSS.call(this, chapter);
+        return pages.map(page => new Page(this, chapter, this.stripSearch(page.Link)));
+    }
+
+    stripSearch(Link: URL): URL {
+        Link.pathname = Link.pathname.replace(/&.*/g, '');
+        return Link;
+
+    }
+}
---
+++ web/src/engine/websites/OlimpoScans_e2e.ts
@@ -0,0 +1,25 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'olimposcans',
+        title: 'OlimpoScans'
+    },
+    container: {
+        url: 'https://olimposcans.com/comic-bjorn-el-barbaro.html',
+        id: '/comic-bjorn-el-barbaro.html',
+        title: 'BJORN EL BARBARO'
+    },
+    child: {
+        id: '/leer-bjorn-el-barbaro-capitulo-50.html',
+        title: 'Capítulo 50',
+    },
+    entry: {
+        index: 1,
+        size: 273_422,
+        type: 'image/webp'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, () => fixture.AssertWebsite());
