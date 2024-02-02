9436140196893583f6c84b00dcdd8546084a3ff4
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-02 13:15:21 +0100

    Add UmeTruyen
    https://github.com/manga-download/hakuneko/issues/6415

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -613,6 +613,7 @@ export { default as TuMangaOnlineHentai } from './TuMangaOnlineHentai';
 export { default as TuManhwas } from './TuManhwas';
 export { default as Turktoon } from './Turktoon';
 export { default as Tuttoanimemanga } from './Tuttoanimemanga';
+export { default as Umetruyen } from './Umetruyen';
 export { default as UraSunday } from './UraSunday';
 export { default as UzayManga } from './UzayManga';
 export { default as VerManhwa } from './VerManhwa';
