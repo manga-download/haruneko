961a1bf1a821cdff0a158acfab0a7b8ced9ee9f8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-04 15:05:53 +0100

    Add MrYaoi
    https://github.com/manga-download/hakuneko/issues/6717

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -444,6 +444,7 @@ export { default as MomoNoHanaScan } from './MomoNoHanaScan';
 export { default as MonochromeScans } from './MonochromeScans';
 export { default as MonoManga } from './MonoManga';
 export { default as MoonWitchInLove } from './MoonWitchInLove';
+export { default as MrYaoi } from './MrYaoi';
 export { default as MundoManhwa } from './MundoManhwa';
 export { default as NatsuID } from './NatsuID';
 export { default as Nekomik } from './Nekomik';
