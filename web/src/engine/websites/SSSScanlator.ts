176f7b35f779939a87e351d1da88c4e8b99b977b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-13 15:13:28 +0100

    Add SSS Scanlator
    https://github.com/manga-download/hakuneko/issues/6778

---
+++ web/src/engine/websites/SSSScanlator.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './SSSScanlator.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as MangaStream from './decorators/WordPressMangaStream';
+import * as Common from './decorators/Common';
+
+@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
+@MangaStream.MangasSinglePageCSS()
+@MangaStream.ChaptersSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('sssscanlator', 'SSS Scanlator', 'https://sssscanlator.com.br', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Portuguese, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
