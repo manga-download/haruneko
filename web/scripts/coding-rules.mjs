05f2516cefb46715e919e0133d0483dfabe51655
Ronny <wegener.ronny@gmail.com>
2024-02-16 23:52:48 +0100

    improve code rule verifier

--- web/scripts/coding-rules.mjs
+++ web/scripts/coding-rules.mjs
@@ -1,16 +1,16 @@
 import { exec } from 'node:child_process';
 
 await (async function check() {
-    const bannedSourceFiles = (await new Promise((resolve, reject) => exec(`git diff --name-only master`, (error, stdout, _stderr) => error ? reject(error) : resolve(stdout?.split('\n')))))
+    const restrictedSourceFilesWithChanges = (await new Promise((resolve, reject) => exec(`git diff --name-only master`, (error, stdout, _stderr) => error ? reject(error) : resolve(stdout?.split('\n')))))
         .filter(line => /locales\/[a-z]{2,3}_[A-Z]{2,3}\.ts/.test(line))
         .filter(line => !line.includes('en_US.ts'))
         .map(line => `- ${line}`);
-    if(bannedSourceFiles.some(line => line)) {
+    if(restrictedSourceFilesWithChanges.length > 0) {
         console.error([
             '',
             'It is not allowed to modify translation files which are handled by a continuous-localization service (e.g., Crowdin):',
             '',
-            ...bannedSourceFiles,
+            ...restrictedSourceFilesWithChanges,
             ''
         ].join('\n'));
         process.exit(1);
