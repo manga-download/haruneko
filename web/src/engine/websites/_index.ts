e9ec104fd77b8924a4791316e9cc1529ad84f34c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-18 14:34:44 +0100

    Add ArcuraFansub
    https://github.com/manga-download/hakuneko/issues/6798

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -20,6 +20,7 @@ export { default as AquaManga } from './AquaManga';
 export { default as ArabToons } from './ArabToons';
 export { default as ArcaneScans } from './ArcaneScans';
 export { default as ArcRelight } from './ArcRelight';
+export { default as ArcuraFansub } from './ArcuraFansub';
 export { default as AresManga } from './AresManga';
 export { default as ArgosScan } from './ArgosScan';
 export { default as ArthurScan } from './ArthurScan';
