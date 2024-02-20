8f2c3b046bca194538750a7ab8e761244a867784
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-20 17:22:37 +0100

    Add ComiciViewer decorator & websites
    tests will come later

---
+++ web/src/engine/websites/COMICMeDu.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './COMICMeDu.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as Comici from './decorators/ComiciViewer';
+
+@Common.MangaCSS(/^{origin}\/series\/[^/]/, Comici.queryMangaTitleURI)
+@Common.MangasMultiPageCSS(Comici.mangaListPath, Comici.queryManga, 0, 1, 0, Comici.MangaExtractor)
+@Comici.ChaptersSinglePageCSS()
+@Comici.PagesSinglePageAJAX()
+@Comici.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('comicmedu', `COMIC MeDu (こみっくめづ)`, 'https://comic-medu.com', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
---
+++ web/src/engine/websites/ComicRide.ts
@@ -0,0 +1,22 @@
+import { Tags } from '../Tags';
+import icon from './ComicRide.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as Comici from './decorators/ComiciViewer';
+
+@Common.MangaCSS(/^{origin}\/series\/[^/]/, Comici.queryMangaTitleURI)
+@Common.MangasMultiPageCSS(Comici.mangaListPath, Comici.queryManga, 0, 1, 0, Comici.MangaExtractor)
+@Comici.ChaptersSinglePageCSS()
+@Comici.PagesSinglePageAJAX()
+@Comici.ImageAjax()
+
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('comicride', `Comic Ride`, 'https://comicride.jp', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
