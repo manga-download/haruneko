bc030dcdf0897af63e95468624f7e587caba6bb0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-02-20 14:55:52 +0100

    properly handle promise errors

--- web/src/engine/websites/MangaShip.ts
+++ web/src/engine/websites/MangaShip.ts
@@ -4,27 +4,30 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 
 const pageScript = `
-    new Promise(async (resolve) => {
+    new Promise(async (resolve, reject) => {
+
         async function fetchImageUrl(obj) {
             return await $.post("/PartialView/SifreCoz", obj);
         }
+        try {
+            const matches = document.body.innerHTML.matchAll(/{\\s*"id":\\s*'([^,]+)',\\s*"token"/g);
+            const token = getTokenValue();
+            const parsedMatches = [];
+            for (const match of matches) {
+                const obj = {
+                    id: match[1],
+                    token
+                };
+                parsedMatches.push(obj);
+            }
+            const images = await Promise.all(parsedMatches.map(async parameter => {
+                return fetchImageUrl(parameter);
+            }));
 
-        const matches = document.body.innerHTML.matchAll(/{\\s*"id":\\s*'([^,]+)',\\s*"token"/g);
-        const token = getTokenValue();
-        const parsedMatches = [];
-        for (const match of matches) {
-            const obj = {
-                id: match[1],
-                token
-            };
-            parsedMatches.push(obj);
+            resolve(images.map(image => new URL(image, window.location.origin).href));
+        } catch (error) {
+            reject(error)
         }
-        const images = await Promise.all(parsedMatches.map(async parameter => {
-            return fetchImageUrl(parameter);
-        }));
-
-        resolve(images.map(image => new URL(image, window.location.origin).href));
-
     });
 
 `;
