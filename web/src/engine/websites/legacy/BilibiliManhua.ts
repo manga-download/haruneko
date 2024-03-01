77b56dcc1f406a8d2de7442c57e206adcee6ff65
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-03-01 15:57:07 +0100

    Add Bilibilimanhua

--- web/src/engine/websites/legacy/BilibiliManhua.ts
+++ web/src/engine/websites/legacy/BilibiliManhua.ts
@@ -1,81 +1,121 @@
-// Auto-Generated export from HakuNeko Legacy
-// See: https://gist.github.com/ronny1982/0c8d5d4f0bd9c1f1b21dbf9a2ffbfec9
-
-//import { Tags } from '../../Tags';
+import { Tags } from '../../Tags';
 import icon from './BilibiliManhua.webp';
-import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../../providers/MangaPlugin';
+import { Choice } from '../../SettingsManager';
+import { EngineResourceKey as E, WebsiteResourceKey as W } from '../../../i18n/ILocale';
+import { FetchJSON, FetchWindowScript } from '../../platform/FetchProvider';
+import * as Common from '../decorators/Common';
+import type { JSONObject } from '../../../../../node_modules/websocket-rpc/dist/types';
 
+type APIResult<T> = {
+    code: number,
+    msg: string,
+    data: T
+}
+
+type APIManga = {
+    season_id: number,
+    title: string
+}
+
+type APIChapters = {
+    id: number,
+    title: string,
+    ep_list: {
+        id: number,
+        is_locked: boolean,
+        is_in_free: boolean,
+        short_title: string,
+        title: string
+    }[]
+}
+
+type APIPages = {
+    images: {
+        x: number,
+        y: number,
+        path: string
+    }[]
+}
+
+type APIImageToken = {
+    url: string,
+    token: string
+}
+
+type APICredential = {
+    credential: string
+}
+
+type APIGetAccessToken = {
+    access_token: string,
+    refresh_token: string
+}
+
+type AccessCookie = {
+    area: number,
+    accessToken: string,
+    refreshToken: string
+}
+
+const auhTokenScript = `
+    new Promise(resolve => {
+        window.cookieStore.get('access_token')
+            .then(cookie => !cookie ? resolve(cookie) : resolve(decodeURIComponent(cookie.value))) ;
+    });
+`;
+
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
+    private readonly areacode = { 1: 'us-user', 2: 'sg-user' };
+    private readonly credentialsServer = 'https://%AREA%.bilibilicomics.com';
+    private auth = {
+        accessToken: '',
+        refreshToken: '',
+        area: 1,
+    };
+    private token_expires_at: number = -1;
+
     public constructor() {
-        super('neteasecomic', `哔哩哔哩 漫画 (Bilibili Manhua)`, 'https://manga.bilibili.com' /*, Tags.Language.English, Tags ... */);
+        super('neteasecomic', `哔哩哔哩 漫画 (Bilibili Manhua)`, 'https://manga.bilibili.com', Tags.Language.Chinese, Tags.Media.Manga, Tags.Source.Official);
+
+        this.Settings.format = new Choice('image.format',
+            W.Plugin_Settings_ImageFormat,
+            W.Plugin_Settings_ImageFormatInfo,
+            '.jpeg',
+            { key: '.jpeg', label: E.Settings_Global_Format_JPEG },
+            { key: '.webp', label: E.Settings_Global_Format_WEBP },
+            { key: '.png', label: E.Settings_Global_Format_PNG },
+        );
     }
 
     public override get Icon() {
         return icon;
     }
-}
 
-// Original Source
-/*
-class BilibiliManhua extends Connector {
-
-    constructor() {
-        super();
-        super.id = 'neteasecomic';
-        super.label = '哔哩哔哩 漫画 (Bilibili Manhua)';
-        this.tags = [ 'manga', 'webtoon', 'chinese' ];
-        this.url = 'https://manga.bilibili.com';
-
-        this.config = {
-            quality:  {
-                label: 'Preferred format',
-                description: 'format of images\nwebp (low)\njpg (medium)\npng (high))',
-                input: 'select',
-                options: [
-                    { value: 'webp', name: 'webp' },
-                    { value: 'jpg', name: 'jpg' },
-                    { value: 'png', name: 'png' },
-                ],
-                value: 'png'
-            }
-        };
+    public override ValidateMangaURL(url: string): boolean {
+        return new RegExp(`^${this.URI.origin}/detail/mc([\\d]+)`).test(url);
     }
 
-    async _fetchTwirp(path, body) {
-        const uri = new URL('/twirp/comic.v1.Comic' + path, this.url);
-        uri.searchParams.set('device', 'pc');
-        uri.searchParams.set('platform', 'web');
-        const request = new Request(uri, {
-            method: 'POST',
-            body: JSON.stringify(body),
-            headers: {
-                'x-origin': this.url,
-                'Content-Type': 'application/json;charset=UTF-8'
-            }
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        const { data } = await this.fetchTwirp<APIResult<APIChapters>>('ComicDetail', {
+            comic_id: parseInt(url.match(/\/mc(\d+)/)[1])
         });
-        const data = await this.fetchJSON(request);
-        return data.data;
+        return new Manga(this, provider, data.id.toString(), data.title);
     }
 
-    async _getMangaFromURI(uri) {
-        const data = await this._fetchTwirp('/ComicDetail', {
-            comic_id: parseInt(uri.pathname.match(/\/mc(\d+)/)[1])
-        });
-        return new Manga(this, data.id, data.title);
-    }
-
-    async _getMangas() {
-        let mangaList = [];
-        for(let page = 1, run = true; run; page++) {
-            let mangas = await this._getMangasFromPage(page);
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const mangaList = [];
+        for (let page = 1, run = true; run; page++) {
+            const mangas = await this.getMangasFromPage(page, provider);
             mangas.length > 0 ? mangaList.push(...mangas) : run = false;
         }
         return mangaList;
     }
 
-    async _getMangasFromPage(page) {
-        const data = await this._fetchTwirp('/ClassPage', {
+    async getMangasFromPage(page, provider): Promise<Manga[]> {
+        const { data } = await this.fetchTwirp<APIResult<APIManga[]>>('ClassPage', {
             style_id: -1,
             area_id: -1,
             is_free: -1,
@@ -84,36 +124,191 @@ class BilibiliManhua extends Connector {
             page_size: 18,
             page_num: page
         });
-        return data.map(entry => {
-            return {
-                id: entry.season_id,
-                title: entry.title.trim()
-            };
-        });
+        return data.map(entry => new Manga(this, provider, entry.season_id.toString(), entry.title.trim()));
     }
 
-    async _getChapters(manga) {
-        const data = await this._fetchTwirp('/ComicDetail', {
-            comic_id: manga.id
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        await this.getToken();
+        const { data } = await this.fetchTwirp<APIResult<APIChapters>>('ComicDetail', {
+            comic_id: manga.Identifier
         });
-        return data.ep_list.filter(entry => entry.is_in_free || !entry.is_locked).map(entry => {
-            return {
-                id: entry.id,
-                title: entry.short_title + ' - ' + entry.title,
-                language: ''
-            };
-        });
+        return data.ep_list
+            .filter(entry => entry.is_in_free || !entry.is_locked)
+            .map(entry => new Chapter(this, manga, entry.id.toString(), (entry.short_title + ' - ' + entry.title).trim()));
     }
 
-    async _getPages(chapter) {
-        const data = await this._fetchTwirp('/GetImageIndex', {
-            ep_id: chapter.id
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        await this.getToken();
+        let credentials: APIResult<APICredential> = null;
+        let data: APIResult<APIPages> = null;
+
+        //Using access_token from cookies, try to get token for unlocked chapters
+        if (this.auth.accessToken) {
+            credentials = await this.fetchWithAccessToken<APIResult<APICredential>>('GetCredential', {
+                ep_id: chapter.Identifier,
+                comic_id: chapter.Parent.Identifier,
+                type: 1
+            });
+        }
+
+        //if we got chapter credentials, fetch full chapter using them
+        if (credentials && credentials.data && credentials.data.credential) {
+            data = await this.fetchTwirp<APIResult<APIPages>>('GetImageIndex', {
+                ep_id: chapter.Identifier,
+                credential: credentials.data.credential
+            });
+        } else {
+            //get only 2 picture for locked chapter or full pictures if free chapter
+            data = await this.fetchTwirp<APIResult<APIPages>>('GetImageIndex', {
+                ep_id: chapter.Identifier,
+            });
+        }
+
+        const images = data.data.images.map(image => {
+            const quality = this.getImageSizeByQuality(image.x);
+            const suffix = quality + 'w';
+            //suffix += qualdata.quality ? '_' + qualdata.quality + 'q' : '';
+            return image.path + '@' + suffix + (this.Settings.format.Value as string);
         });
-        let images = data.images.map(image => image.path + '@' + image.x + 'w.' + this.config.quality.value);
-        images = await this._fetchTwirp('/ImageToken', {
+
+        const finalImages = await this.fetchTwirp<APIResult<APIImageToken[]>>('ImageToken', {
             urls: JSON.stringify(images)
         });
-        return images.map(image => image.url + '?token=' + image.token);
+        return finalImages.data.map(image => new Page(this, chapter, new URL(image.url + '?token=' + image.token)));
+
+    }
+
+    getImageSizeByQuality(imgWidth: number): number {
+        const ratioArray = { "verypoor": 0.4, "poor": 0.5, "normal": 0.7, "good": 0.85, "veryhigh": 1 };
+        const widthArray = { verypoor: 350, poor: 450, normal: 800, good: 1100, veryhigh: 1600 };
+        /*
+        const o = {
+            imgWidth: width,
+            quality: undefined
+        };*/
+
+        //sometimes pictures size from JSON are 0 in this case Bibili forces a 0.85 ratio ,"Good"quality.
+        if (imgWidth < 1) {
+            /*    o.imgWidth = widthArray.good;
+                  return o;*/
+            return widthArray.good;
+        }
+
+        //const choosedQuality = ratioArray[this.Settings.picquality.value];
+        const choosedQuality = ratioArray['veryhigh'];
+        const calcWidth = Math.floor(imgWidth * choosedQuality); //0.85
+        switch (choosedQuality) {
+            case ratioArray.verypoor:
+                calcWidth > widthArray.verypoor && (imgWidth = widthArray.verypoor);
+                break;
+            case ratioArray.poor:
+                calcWidth > widthArray.poor && (imgWidth = widthArray.poor);
+                break;
+            case ratioArray.normal:
+                calcWidth > widthArray.normal && (imgWidth = widthArray.normal);
+                break;
+            case ratioArray.good:
+                calcWidth > widthArray.good && (imgWidth = widthArray.good);
+                break;
+            case ratioArray.veryhigh:
+                calcWidth > widthArray.veryhigh && (imgWidth = widthArray.veryhigh);
+                break;
+        }
+
+        //if (this.config.forcepicturesize.value) o.imgWidth = Math.max(o.imgWidth, width);
+        return imgWidth;
+    }
+
+    async fetchTwirp<T>(path: string, body: JSONObject): Promise<T> {
+        const uri = new URL(`/twirp/comic.v1.Comic/${path}`, this.URI);
+        uri.search = new URLSearchParams({
+            device: 'pc',
+            platform: 'web',
+            lang: 'cn',
+            sys_lang: 'cn'
+        }).toString();
+
+        const request = new Request(uri, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                Origin: this.URI.origin,
+                'Content-Type': 'application/json;charset=UTF-8',
+                Referer: uri.href
+            }
+        });
+        if (this.auth.accessToken) request.headers.set('Authorization', ' Bearer ' + this.auth.accessToken);
+        return FetchJSON<T>(request);
+    }
+
+    async fetchWithAccessToken<T>(path: string, body: JSONObject): Promise<T> {
+        const server = this.credentialsServer.replace('%AREA%', this.areacode[this.auth.area]);
+        const uri = new URL('/twirp/global.v1.User' + path, server);
+        uri.search = new URLSearchParams({
+            device: 'pc',
+            platform: 'web',
+            lang: 'cn',
+            sys_lang: 'cn'
+        }).toString();
+
+        const request = new Request(uri, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                Origin: this.URI.origin,
+                'Content-Type': 'application/json;charset=utf-8',
+                Referer: uri.href,
+                Authorization: ' Bearer ' + this.auth.accessToken
+            }
+        });
+        return FetchJSON<T>(request);
+    }
+
+    async getToken() {
+        try {
+            const now = Math.floor(Date.now() / 1000);
+            const result = await FetchWindowScript<string>(new Request(this.URI), auhTokenScript, 500);
+
+            //if there is no cookie user is disconnected, force cleanup
+            if (!result) throw new Error('User is not connected.');
+
+            const authJson: AccessCookie = JSON.parse(decodeURIComponent(result));
+
+            //if token is not defined, get it from cookies
+            if (!this.auth.accessToken) {
+                this.auth.area = authJson.area;
+                this.auth.accessToken = authJson.accessToken;
+                this.auth.refreshToken = authJson.refreshToken;
+                this.token_expires_at = now + 60 * 10; //expires in 10 minutes
+                return;
+            }
+
+            //if token exists check if expired and refresh it if needed
+            if (this.auth.accessToken && this.token_expires_at < now) {
+                await this.refreshToken();
+                return;
+            }
+
+        } catch (error) {
+            this.auth.accessToken = '';
+            this.auth.refreshToken = '';
+            this.auth.area = 1;
+            this.token_expires_at = -1;
+
+        }
+
+    }
+
+    async refreshToken() {
+        try {
+            const now = Math.floor(Date.now() / 1000);
+            const { data } = await this.fetchWithAccessToken<APIResult<APIGetAccessToken>>('RefreshToken', { refresh_token: this.auth.refreshToken });
+            this.auth.accessToken = data.access_token;
+            this.auth.refreshToken = data.refresh_token;
+            this.token_expires_at = now + 60 * 10;//expires in 10 minutes
+        } catch (error) {
+            //
+        }
+
     }
 }
-*/
