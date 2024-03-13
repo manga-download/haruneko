71b3109d79eed76eeadb58b40fdb32b0625a3d34
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-03-13 13:44:35 +0100

    Sirenkomik : fix getting chapters

--- web/src/engine/websites/SirenKomik.ts
+++ web/src/engine/websites/SirenKomik.ts
@@ -4,15 +4,15 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/sirenkomik\.my\.id\/manga\/[^/]+\/$/)
+@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
+@MangaStream.ChaptersSinglePageCSS('div#chaptermanga ul li div.kotak-chapter div.episode a', 'span.nomer-chapter')
 @MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('sirenkomik', 'SirenKomik', 'https://sirenkomik.my.id', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian);
+        super('sirenkomik', 'SirenKomik', 'https://sirenkomik.my.id', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
