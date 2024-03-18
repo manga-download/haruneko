368ed14986c97ad0e4ba690fc6b86fc6e96d922a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-03-18 16:17:54 +0100

    Add ReadAllComics
    https://github.com/manga-download/hakuneko/issues/6048

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -518,6 +518,7 @@ export { default as RawDevart } from './RawDevart';
 export { default as Rawkuma } from './Rawkuma';
 export { default as RawSenManga } from './RawSenManga';
 export { default as RawXZ } from './RawXZ';
+export { default as ReadAllComics } from './ReadAllComics';
 export { default as ReadComicOnline } from './ReadComicOnline';
 export { default as ReadComicsOnline } from './ReadComicsOnline';
 export { default as ReadFreeComics } from './ReadFreeComics';
