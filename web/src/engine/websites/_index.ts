5994ac343c3dea47b0a2801bbb95079b075d7685
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-03-31 09:09:35 +0000

    Add VortexScans (#548)
    * Add VortexScans
    
    https://github.com/manga-download/hakuneko/issues/6499
    
    * requested changes

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -651,6 +651,7 @@ export { default as ViewComics } from './ViewComics';
 export { default as ViyaFansub } from './ViyaFansub';
 export { default as VNSharing } from './VNSharing';
 export { default as VoidScans } from './VoidScans';
+export { default as VortexScans } from './VortexScans';
 export { default as VyManga } from './VyManga';
 export { default as WarungKomik } from './WarungKomik';
 export { default as WebAce } from './WebAce';
