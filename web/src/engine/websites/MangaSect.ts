5a6d449cbb1b2ee78244ede380f48b1033c343eb
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-04 20:04:07 +0200

    revert changes on mangasect
    Its now using mojo template, check mojo branch

--- web/src/engine/websites/MangaSect.ts
+++ web/src/engine/websites/MangaSect.ts
@@ -1,14 +1,18 @@
 import { Tags } from '../Tags';
 import icon from './MangaSect.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import * as Liliana from './decorators/Liliana';
+import { FetchJSON } from '../platform/FetchProvider';
+import DeProxify from '../transformers/ImageLinkDeProxifier';
+
+type AJAXResponse = {
+    html: string;
+}
 
 @Common.MangaCSS(/^{origin}\/manga\/[^/]+\/\d+$/, 'article header h1')
-@Common.MangasMultiPageCSS(Liliana.mangaPath, 'div.grid div.text-center > a')
-@Common.ChaptersSinglePageCSS(Liliana.queryChapters)
-@Common.PagesSinglePageJS(Liliana.queryPagesScript, 500)
-@Common.ImageAjax(true, true)
+@Common.MangasMultiPageCSS('/all-manga/{page}/', 'div.grid div.text-center > a')
+@Common.ChaptersSinglePageCSS('li.chapter > a')
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -18,4 +22,25 @@ export default class extends DecoratableMangaScraper {
     public override get Icon() {
         return icon;
     }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const chapterid = chapter.Identifier.match(/\/([\d]+)$/)[1];
+        const request = new Request(new URL(`/ajax/image/list/chap/${chapterid}`, this.URI).href, {
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                Referer: new URL(chapter.Identifier, this.URI).href
+            }
+        });
+
+        const response = await FetchJSON<AJAXResponse>(request);
+        const dom = new DOMParser().parseFromString(response.html, 'text/html');
+        const data = [...dom.querySelectorAll<HTMLImageElement>('a.readimg img.lazy.loaded')].sort(function (a, b) {
+            const za = parseInt(a.parentElement.parentElement.dataset.index);
+            const zb = parseInt(b.parentElement.parentElement.dataset.index);
+            return za - zb;
+        });
+        return data.map(image => new Page(this, chapter, DeProxify(new URL(image.getAttribute('src')))));
+
+    }
+
 }
