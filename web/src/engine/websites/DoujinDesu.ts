3f1f63ace327998381b08fb60c97cd60f56fd5c6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-16 10:33:01 +0200

    Update DoujinDesu.ts

--- web/src/engine/websites/DoujinDesu.ts
+++ web/src/engine/websites/DoujinDesu.ts
@@ -1,29 +1,12 @@
 import { Tags } from '../Tags';
 import icon from './DoujinDesu.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-
-const pageScript = `
-    new Promise(resolve => {
-        const chapterId = document.querySelector('main#reader').dataset.id;
-        $.ajax({
-            url: '/themes/ajax/ch.php',
-            method: 'POST',
-            data: {
-                id: chapterId
-            },
-            success: function(data) {
-                const dom = new DOMParser().parseFromString(data, 'text/html');
-                resolve([...dom.querySelectorAll('img')].map(image => image.getAttribute('src')))
-            }
-        })
-    });
-`;
+import { FetchCSS } from '../platform/FetchProvider';
 
 @Common.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'section.metadata h1.title', Common.ElementLabelExtractor('span.alter'))
 @Common.MangasMultiPageCSS('/manga/page/{page}/', 'article.entry a', 1, 1, 0, Common.AnchorInfoExtractor(true))
 @Common.ChaptersSinglePageCSS('div#chapter_list div.epsleft span.lchx a', Common.AnchorInfoExtractor(true))
-@Common.PagesSinglePageJS(pageScript, 2500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -33,5 +16,25 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const chapterurl = new URL(chapter.Identifier, this.URI);
+        let request = new Request(chapterurl.href);
+        let data = await FetchCSS(request, "main#reader");
+        const chapterid = data[0].dataset['id'];
+
+        const url = new URL('/themes/ajax/ch.php', this.URI);
+        request = new Request(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Referer': chapterurl.href
+            },
+            body: new URLSearchParams({ id: chapterid })
+        });
+        data = await FetchCSS<HTMLImageElement>(request, 'img');
+        return data.map(element => new Page(this, chapter, new URL(element.getAttribute('src'))));
     }
 }
