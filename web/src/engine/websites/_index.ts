d83381d095ef58d32de8ef36086e66602869081d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-19 16:42:54 +0200

    Add MyHentaiGallery
    https://github.com/manga-download/hakuneko/issues/6315

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -459,6 +459,7 @@ export { default as MonzeeKomik } from './MonzeeKomik';
 export { default as MoonWitchInLove } from './MoonWitchInLove';
 export { default as MrYaoi } from './MrYaoi';
 export { default as MundoManhwa } from './MundoManhwa';
+export { default as MyHentaiGallery } from './MyHentaiGallery';
 export { default as NatsuID } from './NatsuID';
 export { default as Nekomik } from './Nekomik';
 export { default as NeoxScan } from './NeoxScan';
