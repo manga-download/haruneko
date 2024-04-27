97dd36274eb6fa05b39aa7e39ef8290acdeedd17
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-04-27 21:59:27 +0200

    Change websites domains
    KnightNoFansub; RizzComic, TopManhua

--- web/src/engine/websites/RizzComics_e2e.ts
+++ web/src/engine/websites/RizzComics_e2e.ts
@@ -6,12 +6,12 @@ const config: Config = {
         title: 'Rizz Comics'
     },
     container: {
-        url: 'https://rizzcomic.com/series/r3513102-demonic-master-of-mount-kunlun',
-        id: '/series/r3513102-demonic-master-of-mount-kunlun',
+        url: 'https://rizzfables.com/series/r2311170-demonic-master-of-mount-kunlun',
+        id: '/series/r2311170-demonic-master-of-mount-kunlun',
         title: 'Demonic Master of Mount Kunlun'
     },
     child: {
-        id: '/chapter/r3513102-demonic-master-of-mount-kunlun-chapter-49',
+        id: '/chapter/r2311170-demonic-master-of-mount-kunlun-chapter-49',
         title: 'Chapter 49'
     },
     entry: {
