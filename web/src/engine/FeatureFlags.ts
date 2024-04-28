ddbd75c4adc97d1e74cba9e20861a99500f6b52d
Ronny <wegener.ronny@gmail.com>
2024-04-28 13:49:28 +0200

    update feature flags

--- web/src/engine/FeatureFlags.ts
+++ web/src/engine/FeatureFlags.ts
@@ -1,9 +1,12 @@
 import { Observable } from './Observable';
 
 const category = 'hakuneko:flags';
-const keyHideSplashScreen = `${category}:no-splash`;
-const keyVerboseBackgroundFetchWindow = `${category}:verbose-fetchwindow`;
-//const keyCrowdinTranslationMode = `${category}:crowdin-translation`;
+
+enum Key {
+    HideSplashScreen = `${category}:no-splash`,
+    VerboseFetchWindow = `${category}:verbose-fetchwindow`,
+    CrowdinTranslationMode = `${category}:crowdin-translation`,
+}
 
 /**
  * Provide flags for switching functionality that is not meant to be controlled by the end-user.
@@ -12,19 +15,17 @@ export class FeatureFlags {
 
     public readonly HideSplashScreen = new Observable(false);
     public readonly VerboseFetchWindow = new Observable(false);
-    //public readonly CrowdinTranslationMode = new Observable(false);
+    public readonly CrowdinTranslationMode = new Observable(false);
 
     public Initialize(): FeatureFlags {
-
-        this.HideSplashScreen.Value = window.localStorage.getItem(keyHideSplashScreen) === 'true';
-        this.HideSplashScreen.Subscribe(value => window.localStorage.setItem(keyHideSplashScreen, value ? 'true' : 'false'));
-
-        this.VerboseFetchWindow.Value = window.localStorage.getItem(keyVerboseBackgroundFetchWindow) === 'true';
-        this.VerboseFetchWindow.Subscribe(value => window.localStorage.setItem(keyVerboseBackgroundFetchWindow, value ? 'true' : 'false'));
-
-        //this.CrowdinTranslationMode.Value = window.localStorage.getItem(keyCrowdinTranslationMode) === 'true';
-        //this.CrowdinTranslationMode.Subscribe(value => window.localStorage.setItem(keyCrowdinTranslationMode, value ? 'true' : 'false'));
-
+        this.Register(this.HideSplashScreen, Key.HideSplashScreen);
+        this.Register(this.VerboseFetchWindow, Key.VerboseFetchWindow);
+        this.Register(this.CrowdinTranslationMode, Key.CrowdinTranslationMode);
         return this;
+    }
+
+    private Register(observable: Observable<boolean, null>, key: Key) {
+        observable.Value = window.localStorage.getItem(key) === 'true';
+        observable.Subscribe(value => window.localStorage.setItem(key, value ? 'true' : 'false'));
     }
 }
