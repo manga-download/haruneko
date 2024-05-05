718a6004fb222ceac75046c46617805ffd3a9e85
Ronny <wegener.ronny@gmail.com>
2024-05-05 22:32:14 +0200

    update end-to-end tests

--- test/PuppeteerFixture.ts
+++ test/PuppeteerFixture.ts
@@ -3,14 +3,25 @@ import { AppURL } from './PuppeteerGlobal';
 
 export class PuppeteerFixture {
 
+    #browser: puppeteer.Browser;
+    #page: puppeteer.Page;
+
     constructor() {}
 
-    public async Connect() {
-        const browser = await puppeteer.connect({ browserWSEndpoint: process.env.browserWS });
-        const pages = await browser.pages();
-        const page = pages.find(page => page.url() === AppURL);
-        await page.reload();
-        await page.waitForSelector('body div#app main#hakunekoapp', { timeout: 7500 });
-        return page;
+    protected get Browser() {
+        return this.#browser;
+    }
+
+    protected get Page() {
+        return this.#page;
+    }
+
+    public async Connect<T extends this>(): Promise<T> {
+        this.#browser = await puppeteer.connect({ browserWSEndpoint: process.env.browserWS });
+        const pages = await this.#browser.pages();
+        this.#page = pages.find(page => page.url() === AppURL);
+        await this.#page.reload();
+        await this.#page.waitForSelector('body div#app main#hakunekoapp', { timeout: 7500 });
+        return this as T;
     }
 }
