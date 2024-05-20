03930ff6140617c879298754124bef56632cfa67
Ronny <wegener.ronny@gmail.com>
2024-05-20 20:41:59 +0200

    cleanup

--- app/nw/src/ipc/InterProcessCommunication.ts
+++ app/nw/src/ipc/InterProcessCommunication.ts
@@ -24,6 +24,14 @@ export class IPC<TChannelsOut extends string, TChannelsIn extends string> {
         }
     }
 
+    public Listen(channel: TChannelsIn, callback: Callback) {
+        if(!this.subscriptions.has(channel)) {
+            this.subscriptions.set(channel, []);
+        }
+        // TODO: Prevent duplicate callbacks
+        this.subscriptions.get(channel).push(callback);
+    }
+
     public async Send(channel: TChannelsOut, ...parameters: JSONArray): Promise<void> {
         // TODO: improve query filter e.g., windowID or tabID
         const tabs = await new Promise<chrome.tabs.Tab[]>(resolve => chrome.tabs.query({ active: true }, resolve));
@@ -35,13 +43,5 @@ export class IPC<TChannelsOut extends string, TChannelsIn extends string> {
                 throw new Error(); // TODO: Message
             }
         });
-    }
-
-    public Listen(channel: TChannelsIn, callback: Callback) {
-        if(!this.subscriptions.has(channel)) {
-            this.subscriptions.set(channel, []);
-        }
-        // TODO: Prevent duplicate callbacks
-        this.subscriptions.get(channel).push(callback);
     }
 }
