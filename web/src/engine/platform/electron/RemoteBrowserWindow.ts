569b3374e53d64ee19010810b5d6c96876a98673
Ronny <wegener.ronny@gmail.com>
2024-05-21 14:27:52 +0200

    introduce rmeote window proxy

---
+++ web/src/engine/platform/electron/RemoteBrowserWindow.ts
@@ -0,0 +1,83 @@
+import type { BrowserWindowConstructorOptions, LoadURLOptions } from 'electron';
+import { Observable } from '../../Observable';
+import type { IPC } from '../InterProcessCommunication';
+import type { ScriptInjection } from '../FetchProviderCommon';
+import { RemoteBrowserWindowController as Channels } from '../../../../../app/src/ipc/Channels';
+
+export default class RemoteBrowserWindow {
+
+    private windowID = Number.NaN;
+    public DOMReady = new Observable<Document, RemoteBrowserWindow>(null, this);
+    public BeforeNavigate = new Observable<URL, RemoteBrowserWindow>(null, this);
+
+    constructor(private readonly ipc: IPC<Channels.App, Channels.Web>) {
+        this.ipc.Listen(Channels.Web.OnDomReady, this.OnDomReady.bind(this));
+        this.ipc.Listen(Channels.Web.OnBeforeNavigate, this.OnBeforeNavigate.bind(this));
+    }
+
+    private async OnDomReady(windowID: number): Promise<void> {
+        if(windowID === this.windowID) {
+            console.log('OnDomReady()', windowID);
+            const html = await this.ExecuteScript<string>(`document.querySelector('html').innerHTML`);
+            this.DOMReady.Value = new DOMParser().parseFromString(html, 'text/html');
+        }
+    }
+
+    private async OnBeforeNavigate(windowID: number): Promise<void> {
+        if(windowID === this.windowID) {
+            console.log('OnBeforeNavigate()', windowID);
+            // TODO: Get new url ...
+            this.BeforeNavigate.Value = new URL('about:blank');
+        }
+    }
+
+    public async Open(request: Request, show: boolean = false, preload: ScriptInjection<void> = '') {
+        const openOptions: BrowserWindowConstructorOptions = {
+            show: show,
+            width: 1280,
+            height: 720,
+            webPreferences: {
+                preload: preload instanceof Function ? `(${preload})()` : preload,
+                webSecurity: true,
+                contextIsolation: true,
+                nodeIntegration: false,
+                nodeIntegrationInWorker: false,
+                nodeIntegrationInSubFrames: false,
+                disableBlinkFeatures: 'AutomationControlled',
+            }
+        };
+
+        this.windowID = await this.ipc.Send(Channels.App.OpenWindow, JSON.stringify(openOptions)) as number;
+
+        /*
+        if(isNaN(this.windowID)) {
+            throw new Error('[I18N] - Failed to open Browser Window!');
+        }
+        */
+
+        const loadOptions: LoadURLOptions = {
+            userAgent: request.headers.get('User-Agent') ?? navigator.userAgent,
+            httpReferrer: request.headers.get('Referer') ?? request.referrer ?? request.url,
+        };
+
+        await this.ipc.Send(Channels.App.LoadURL, this.windowID, request.url, JSON.stringify(loadOptions));
+    }
+
+    public async Close(): Promise<void> {
+        if(!isNaN(this.windowID)) {
+            return this.ipc.Send(Channels.App.CloseWindow, this.windowID);
+        }
+    }
+
+    public async Show(): Promise<void> {
+        return this.ipc.Send(Channels.App.SetVisibility, this.windowID, true);
+    }
+
+    public async Hide(): Promise<void> {
+        return this.ipc.Send(Channels.App.SetVisibility, this.windowID, false);
+    }
+
+    public async ExecuteScript<T extends void | JSONElement>(script: ScriptInjection<T> = ''): Promise<T> {
+        return this.ipc.Send(Channels.App.ExecuteScript, this.windowID, script instanceof Function ? `(${script})()` : script);
+    }
+}
