518a853addca68d2f8a458e6ec52645609163a83
Ronny <wegener.ronny@gmail.com>
2024-06-05 22:07:34 +0200

    forward ipc to electron

--- app/electron/src/ipc/BloatGuard.ts
+++ app/electron/src/ipc/BloatGuard.ts
@@ -1,20 +1,13 @@
-import path from 'node:path';
-import { app, BrowserWindow, type BrowserWindowConstructorOptions } from 'electron';
-import { argvPreloadScript } from './RemoteBrowserWindowPreload';
 import type { IPC } from './InterProcessCommunication';
 import { BloadGuard as Channels } from '../../../src/ipc/Channels';
 
 export class BloadGuard {
 
     constructor (private readonly ipc: IPC<Channels.Web, Channels.App>) {
-        this.ipc.Listen(Channels.App.Initialize, this.OpenWindow.bind(this));
+        this.ipc.Listen(Channels.App.Initialize, this.Initialize.bind(this));
     }
 
-    private Throw<T>(message: string): T {
-        throw new Error(message);
-    }
-
-    private async (windowID: number): Promise<void> {
-        //this.FindWindow(windowID).destroy();
+    private async Initialize(patterns: string[]): Promise<void> {
+        console.log('BloadGuard::Initialize', '=>', patterns);
     }
 }
--- web/src/engine/platform/electron/BloatGuard.ts
+++ web/src/engine/platform/electron/BloatGuard.ts
@@ -1,14 +1,12 @@
 import type { IBloatGuard } from '../BloatGuard';
 import type { IPC } from '../InterProcessCommunication';
-import { ApplicationWindow as Channels } from '../../../../../app/src/ipc/Channels';
+import { BloadGuard as Channels } from '../../../../../app/src/ipc/Channels';
 
 export default class implements IBloatGuard {
 
     constructor(private readonly ipc: IPC<Channels.App, Channels.Web>, private readonly patterns: Array<string>) {}
 
     async Initialize(): Promise<void> {
-        console.log('Platform::Electron::BloatGuard::Initialize()');
-        this.ipc.Send(Channels.App., this.patterns);
-        //throw new Error('Method not implemented.');
+        return this.ipc.Send(Channels.App.Initialize, this.patterns);
     }
 }
