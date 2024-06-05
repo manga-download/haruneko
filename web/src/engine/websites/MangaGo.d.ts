b217f409f38e0374d8d3221afcf19d23775a07bb
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-05 17:24:20 +0200

    Port MangaGo
    Seems to work without problems

---
+++ web/src/engine/websites/MangaGo.d.ts
@@ -0,0 +1,8 @@
+import { DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
+import type { Priority } from '../taskpool/DeferredTask';
+export default class extends DecoratableMangaScraper {
+    private readonly scrambleKeys;
+    constructor();
+    get Icon(): any;
+    FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob>;
+}
