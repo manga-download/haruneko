1ec4f3be1f6d3b784ef8a00e66dd5a0e9b97022b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-06 10:07:43 +0200

    Scanvforg : use head requests to gather page list

--- web/src/engine/websites/ScanVFOrg.ts
+++ web/src/engine/websites/ScanVFOrg.ts
@@ -2,7 +2,7 @@ import { Tags } from '../Tags';
 import icon from './ScanVFOrg.webp';
 import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
+import { Fetch } from '../platform/FetchProvider';
 
 function MangaLabelExtractor(element: HTMLElement): string {
     return element.textContent.trim().replace(/Scan VF$/i, '').trim();
@@ -11,7 +11,7 @@ function MangaLabelExtractor(element: HTMLElement): string {
 @Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, 'div.card div.row h1', MangaLabelExtractor)
 @Common.MangasMultiPageCSS('/manga?page={page}', 'div.series a.link-series')
 @Common.ChaptersSinglePageCSS('div.chapters-list a', Common.AnchorInfoExtractor(false, 'div.text-muted, div.rate-value'))
-@Common.ImageAjax()
+@Common.ImageAjaxFromHTML('div.book-page img.img-fluid')
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -23,12 +23,12 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const pagesList : Page[] = [];
+        const urlList = [];
         for (let page = 1, run = true; run; page++) {
             const url = new URL(`${chapter.Identifier}/${page}`, this.URI);
-            const [ data ] = await FetchCSS<HTMLImageElement>(new Request(url), 'div.book-page img.img-fluid');
-            data ? pagesList.push(new Page(this, chapter, new URL(data.getAttribute('src'), this.URI))) : run = false;
+            const response = await Fetch(new Request(url, { method: 'HEAD' }));
+            response.redirected && response.url.indexOf('bypass=1') != -1 ? run = false : urlList.push(url);
         }
-        return pagesList;
+        return urlList.map(url => new Page(this, chapter, url));
     }
 }
