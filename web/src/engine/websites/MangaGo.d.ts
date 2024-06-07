f970f18425ad93fc1501a58ea7ee83162bcfb702
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-07 11:11:32 +0200

    [mangago] extract tile index table

--- web/src/engine/websites/MangaGo.d.ts
+++ web/src/engine/websites/MangaGo.d.ts
@@ -1,7 +1,6 @@
 import { DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
 import type { Priority } from '../taskpool/DeferredTask';
 export default class extends DecoratableMangaScraper {
-    private readonly scrambleKeys;
     constructor();
     get Icon(): any;
     FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob>;
