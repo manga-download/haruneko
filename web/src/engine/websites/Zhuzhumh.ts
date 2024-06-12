6b1ca9320983a15825aec0dbb4b63b4a91a6928f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-12 18:46:50 +0200

    use api to get chapters

--- web/src/engine/websites/Zhuzhumh.ts
+++ web/src/engine/websites/Zhuzhumh.ts
@@ -1,27 +1,16 @@
 import { Tags } from '../Tags';
 import icon from './Zhuzhumh.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
+import { FetchJSON } from '../platform/FetchProvider';
 
-const queryChaptersScript = `
-    new Promise((resolve, reject) => {
-        const interval = setInterval(() => {
-            let morebtn = document.querySelector('a#zhankai:not([style*="display"])');
-            if (morebtn) morebtn.click()
-            else {
-                clearInterval(interval);
-                const chapters = [...document.querySelectorAll('ul#mh-chapter-list-ol-0 li a')];
-                resolve( chapters.map(chapter => {
-                    return { id : chapter.pathname, title : chapter.textContent.trim()}
-                }));
-            }
-        }, 500);
-    });
-`;
+type APIChapter = {
+    chaptername: string,
+    chapterurl: string
+}
 
 @Common.MangaCSS(/^{origin}\/book\/[^/]+\.html$/, 'div.cy_title h1')
 @Common.MangasMultiPageCSS('/sort/1-{page}.html', 'div.cy_list_mh ul li.title a')
-@Common.ChaptersSinglePageJS(queryChaptersScript)
 @Common.PagesSinglePageJS('newImgs', 1500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
@@ -33,4 +22,13 @@ export default class extends DecoratableMangaScraper {
     public override get Icon() {
         return icon;
     }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const chapterslist = await Common.FetchChaptersSinglePageCSS.call(this, manga, 'ul#mh-chapter-list-ol-0 li a');
+        const mangaid = manga.Identifier.match(/\/book\/([^.]+)\.html$/)[1];
+        const data = await FetchJSON<APIChapter[]>(new Request(new URL(`/api/bookchapter?id=${mangaid}&id2=1`, this.URI)));
+        chapterslist.push(...data.map(chapter => new Chapter(this, manga, new URL(chapter.chapterurl).pathname, chapter.chaptername.trim())));
+        return chapterslist;
+    }
+
 }
