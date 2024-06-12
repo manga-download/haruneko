5a98de6c0cdb984e5f4e2d440f16d23d54368091
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-12 12:16:55 +0000

    add requested websites (#645)
    * Add KingOfShojo
    
    https://github.com/manga-download/hakuneko/issues/7132
    
    * update king of manga
    
    icon and language
    
    * Add HentaiTeca
    
    https://github.com/manga-download/hakuneko/issues/7142
    
    * Remove Manga1001 & Manga1001top
    
    Dead websites. Other "Manga1001" found are different websites

--- web/src/engine/websites/KingOfShojo.ts
+++ web/src/engine/websites/KingOfShojo.ts
@@ -1,5 +1,5 @@
 import { Tags } from '../Tags';
-import icon from './KingOfManga.webp';
+import icon from './KingOfShojo.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
@@ -7,12 +7,12 @@ import * as Common from './decorators/Common';
 @MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS()
 @MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageJS()
+@Common.PagesSinglePageJS('ts_reader.params.sources.shift().images', 500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('kingofmanga', 'KingOfManga', 'https://kingofmanga.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English);
+        super('kingofshojo', 'KingOfShojo', 'https://kingofshojo.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English);
     }
 
     public override get Icon() {
