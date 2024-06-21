7859b7a24abd7b1632eb9d7a6a2ead0866e86d1c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-21 10:11:41 +0000

    Port ScanManga (#648)
    * Port ScanManga
    
    * use async promise

---
+++ web/src/engine/websites/ScanManga_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'scanmanga',
+        title: 'ScanManga'
+    },
+    container: {
+        url: 'https://www.scan-manga.com/12522/Desire-Realization-App.html',
+        id: '/12522/Desire-Realization-App.html',
+        title: 'Desire Realization App'
+    },
+    child: {
+        id: '/lecture-en-ligne/Desire-Realization-App-Chapitre-34-FR_409765.html',
+        title: 'Chapitre 34'
+    },
+    entry: {
+        index: 0,
+        size: 658_994,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
