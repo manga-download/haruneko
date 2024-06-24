bb0d78cea9f9b34ba87f14d44071a6a6c3ccb6e6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-24 13:29:10 +0200

    chapter title fix

--- web/src/engine/websites/ColoredManga.ts
+++ web/src/engine/websites/ColoredManga.ts
@@ -1,12 +1,11 @@
 import { Tags } from '../Tags';
-import icon from './AdonisFansub.webp';
+import icon from './ColoredManga.webp';
 import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchJSON } from '../platform/FetchProvider';
 import type { Priority } from '../taskpool/DeferredTask';
 
 type APIManga = {
-    id: string,
     name: string
     chapters: APIChapter[]
 }
@@ -14,7 +13,7 @@ type APIManga = {
 type APIChapter = {
     id: string,
     title: string,
-    number: string, //yes, its not a typo
+    number: string,
     totalImage: number
 }
 
@@ -22,7 +21,7 @@ type APIImage = {
     image : string
 }
 export default class extends DecoratableMangaScraper {
-    private readonly apiUrl = this.URI.origin + '/api/';
+    private readonly apiUrl = `${this.URI.origin}/api/`;
     private readonly mangaRegexp = new RegExp(`^${this.URI.origin}/manga/([^/]+)$`);
 
     public constructor() {
@@ -38,9 +37,9 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const mangaId = url.match(this.mangaRegexp)[1];
-        const mangaData = await this.GetMangaData(mangaId);
-        return new Manga(this, provider, mangaId, mangaData.name);
+        const mangaSlug = url.match(this.mangaRegexp)[1];
+        const mangaData = await this.GetMangaData(mangaSlug);
+        return new Manga(this, provider, mangaSlug, mangaData.name);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
@@ -50,7 +49,7 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
         const { chapters } = await this.GetMangaData(manga.Identifier);
-        return chapters.map(chapter => new Chapter(this, manga, chapter.id, [chapter.number, chapter.title].join(' ').trim()));
+        return chapters.map(chapter => new Chapter(this, manga, chapter.id, [chapter.number, chapter.title].join(' - ').trim().replace(/-$/, '')));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
@@ -75,13 +74,15 @@ export default class extends DecoratableMangaScraper {
             const request = this.CreateRequest('dynamicImages', formdata);
             const { image } = await FetchJSON<APIImage>(request);
 
-            return await (await fetch(image)).blob();
+            const response = await fetch(image);//turn base64image into response
+            return Common.GetTypedData(await response.arrayBuffer());
+
         }, priority, signal);
     }
 
-    private async GetMangaData(identifier: string): Promise<APIManga> {
+    private async GetMangaData(mangaSlug: string): Promise<APIManga> {
         const formdata = new FormData();
-        formdata.append('id', identifier);
+        formdata.append('id', mangaSlug);
         return await FetchJSON<APIManga>(this.CreateRequest('selectedManga', formdata));
     }
 
