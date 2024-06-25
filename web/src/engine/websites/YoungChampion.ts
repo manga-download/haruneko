ef767c17bc9ebb85e6cd3c92d0bf1e429e0fcb20
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-25 11:20:47 +0200

    Youngchampion: fix mangalist

--- web/src/engine/websites/YoungChampion.ts
+++ web/src/engine/websites/YoungChampion.ts
@@ -5,7 +5,7 @@ import * as Common from './decorators/Common';
 import * as Comici from './decorators/ComiciViewer';
 
 @Common.MangaCSS(/^{origin}\/series\/[^/]+(\/)?$/, Comici.queryMangaTitleURI)
-@Common.MangasMultiPageCSS(Comici.mangaListPath, Comici.queryManga, 0, 1, 0, Comici.MangaExtractor)
+@Common.MangasMultiPageCSS(Comici.mangaListPath, 'div.series-box-vertical div.article-text > a', 0, 1, 0, Comici.MangaExtractor)
 @Comici.ChaptersSinglePageCSS()
 @Comici.PagesSinglePageAJAX()
 @Comici.ImageAjax()
