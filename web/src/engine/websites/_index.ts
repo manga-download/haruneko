69d09c0aaa1637a14f4d2d53261a0c30c906f080
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-29 22:34:01 +0200

    Add OurFeel
    https://github.com/manga-download/hakuneko/issues/7147

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -528,6 +528,7 @@ export { default as Opiatoon } from './Opiatoon';
 export { default as Oremanga } from './Oremanga';
 export { default as OrigamiOrpheans } from './OrigamiOrpheans';
 export { default as Otsugami } from './Otsugami';
+export { default as OurFeel } from './OurFeel';
 export { default as PairOfTwo } from './PairOfTwo';
 export { default as PatyScans } from './PatyScans';
 export { default as PCNet } from './PCNet';
