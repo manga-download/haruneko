bf5d8fdfa6ef94b5eeaf90079a6f4e493d73aba3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-06-30 11:23:48 +0200

    Jmanga: fix getting pages

--- web/src/engine/websites/JManga.ts
+++ web/src/engine/websites/JManga.ts
@@ -3,7 +3,25 @@ import icon from './JManga.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as Liliana from './decorators/Liliana';
-import { pageScript } from './RawOtaku';
+
+const pageScript = `
+    new Promise(resolve => {
+        const elementid = '#' + manga.lang + '-chapters li[data-number="'+ manga.name +'"]';
+        const chapterid = document.querySelector(elementid).dataset.id;
+        $.get(
+            "/json/chapter", {
+                id: chapterid
+            },
+            function(j) {
+                if (j.status == 1) {
+                    const dom = new DOMParser().parseFromString(j.html, 'text/html');
+                    resolve([...dom.querySelectorAll('div.iv-card img')].map(image => image.dataset.src));
+                } else reject();
+            },
+            "json"
+        );
+    })
+`;
 
 @Common.MangaCSS(/^{origin}\/read\/[^/]+-raw\/$/, Liliana.queryMangaTitleFromURI)
 @Common.MangasMultiPageCSS('/raw-manga/?p={page}', Liliana.queryMangas, 1, 1, 0, Common.AnchorInfoExtractor(true))
--- web/src/engine/websites/JManga_e2e.ts
+++ web/src/engine/websites/JManga_e2e.ts
@@ -7,12 +7,12 @@ const config = {
         title: 'JManga'
     },
     container: {
-        url: 'https://jmanga.org/read/%E3%81%8B%E3%82%89%E3%81%8B%E3%81%84%E4%B8%8A%E6%89%8B-%EF%BC%9F-%E3%81%AE%E8%A5%BF%E7%89%87%E3%81%95%E3%82%93.1-raw/',
-        id: '/read/%E3%81%8B%E3%82%89%E3%81%8B%E3%81%84%E4%B8%8A%E6%89%8B-%EF%BC%9F-%E3%81%AE%E8%A5%BF%E7%89%87%E3%81%95%E3%82%93.1-raw/',
+        url: encodeURI('https://jmanga.org/read/からかい上手-？-の西片さん.1-raw/'),
+        id: encodeURI('/read/からかい上手-？-の西片さん.1-raw/'),
         title: 'からかい上手(？)の西片さん'
     },
     child: {
-        id: '/read/%E3%81%8B%E3%82%89%E3%81%8B%E3%81%84%E4%B8%8A%E6%89%8B-%EF%BC%9F-%E3%81%AE%E8%A5%BF%E7%89%87%E3%81%95%E3%82%93/ja/chapter-7-raw/',
+        id: encodeURI('/read/からかい上手-？-の西片さん/ja/chapter-7-raw/'),
         title: '章 7: 第7話'
     },
     entry: {
