331280e7d38b2b0312a4bb21cb2707f55b1349e1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-18 09:14:30 +0200

    requested changes

--- web/src/engine/websites/NicoNicoSeiga.ts
+++ web/src/engine/websites/NicoNicoSeiga.ts
@@ -10,19 +10,19 @@ type APIManga = {
     title: string
 }
 
-type APIChapter = {
+type APIChapters = APIResult<{
     id: number,
     meta: {
         title: string
     }
-}
+}[]>;
 
-type APIPage = {
+type APIPages = APIResult<{
     meta: {
         source_url: string,
         drm_hash: string
     }
-}
+}[]>
 
 type APIResult<T> = {
     meta: {
@@ -51,7 +51,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const title = (await FetchCSS<HTMLHeadingElement>(new Request(url), 'section.comic_header h2.title')).pop().textContent.trim();
+        const title = (await FetchCSS<HTMLHeadingElement>(new Request(url), 'section.comic_header h2.title')).shift().textContent.trim();
         return new Manga(this, provider, url.match(this.mangaRegexp)[1], title);
     }
 
@@ -65,32 +65,33 @@ export default class extends DecoratableMangaScraper {
     }
 
     private async GetMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
-        const data = await FetchJSON<APIManga[]>(new Request(new URL(`/manga/ajax/ranking?span=hourly&category=all&page=${page}`, this.URI)));
-        return Array.isArray(data) ? data.map(manga => new Manga(this, provider, manga.id.toString(), manga.title.trim())) : [];
+        const data = await FetchJSON<APIManga[] | null>(new Request(new URL(`/manga/ajax/ranking?span=hourly&category=all&page=${page}`, this.URI)));
+        return (Array.isArray(data) ? data : []).map(manga => new Manga(this, provider, manga.id.toString(), manga.title.trim()));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { meta, data: { result } } = await FetchJSON<APIResult<APIChapter[]>>(new Request(new URL(`contents/${manga.Identifier}/episodes`, this.apiUrl)));
+        const { meta, data: { result } } = await FetchJSON<APIChapters>(new Request(new URL(`contents/${manga.Identifier}/episodes`, this.apiUrl)));
         return meta.status === 200 ? result.map(chapter => new Chapter(this, manga, chapter.id.toString(), chapter.meta.title.trim())) : [];
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const { meta, data: { result } } = await FetchJSON<APIResult<APIPage[]>>(new Request(new URL(`episodes/${chapter.Identifier}/frames`, this.apiUrl)));
+        const { meta, data: { result } } = await FetchJSON<APIPages>(new Request(new URL(`episodes/${chapter.Identifier}/frames`, this.apiUrl)));
         return meta.status === 200 ? result.map(page => new Page(this, chapter, new URL(page.meta.source_url), { drm_hash: page.meta.drm_hash })) : [];
     }
 
     public override async FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const data = await Common.FetchImageAjax.call(this, page, priority, signal);
-        const encrypted = await new Response(data).arrayBuffer();
+        const encrypted = await data.arrayBuffer();
         const key = page.Parameters['drm_hash'] as string;
         const decrypted = this.Decrypt(new Uint8Array(encrypted), key);
-        return Common.GetTypedData(Buffer.from(decrypted));
+        return Common.GetTypedData(decrypted);
     }
 
     private Decrypt(buffer: Uint8Array, key: string): Uint8Array {
         const xorkey = new Uint8Array(key.slice(0, 16).match(/.{1,2}/g).map(e => parseInt(e, 16)));
+        const result = new Uint8Array(buffer);
         for (let n = 0; n < buffer.length; n++)
-            buffer[n] = buffer[n] ^ xorkey[n % 8];
-        return buffer;
+            result[n] = result[n] ^ xorkey[n % 8];
+        return result;
     }
 }
