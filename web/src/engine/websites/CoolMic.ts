fe870b41ff9b16f6393b2ab3e035afafe6c3dfd0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-19 15:17:14 +0200

    add manga listing

--- web/src/engine/websites/CoolMic.ts
+++ web/src/engine/websites/CoolMic.ts
@@ -1,11 +1,23 @@
 import { Tags } from '../Tags';
 import icon from './CoolMic.webp';
-import { Chapter, DecoratableMangaScraper, Page, type Manga } from '../providers/MangaPlugin';
+import type { MangaPlugin } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, Page, Manga } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { Fetch, FetchCSS, FetchJSON } from '../platform/FetchProvider';
 import type { Priority } from '../taskpool/DeferredTask';
 import { Exception } from '../Error';
 import { WebsiteResourceKey as R } from '../../i18n/ILocale';
+
+type APIMangas = {
+    hits: {
+        hit: {
+            id: number,
+            fields: {
+                title_name : string
+            }
+        }[]
+    }
+}
 
 type JsonChapters = {
     episodes: {
@@ -28,7 +40,6 @@ type CookieSigner = {
 }
 
 @Common.MangaCSS(/^{origin}\/titles\/\d+$/, 'meta[property="og:title"]')
-@Common.MangasNotSupported()
 
 export default class extends DecoratableMangaScraper {
     private readonly apiUrl = `${this.URI.origin}/api/v1/`;
@@ -41,8 +52,42 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const token = (await FetchCSS<HTMLMetaElement>(new Request(this.URI), 'meta[name="csrf-token"]')).shift().content;
+        const promises = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map(character => {
+            const url = new URL('https://en-search.coolmic.me/search');
+            const params = new URLSearchParams({
+                q: `(${character}|*${character})`,
+                size: '10000',
+                start: '0',
+                'q.parser': 'simple',
+                return: '_all_fields',
+                sort: '_score desc, like_vote_count desc',
+                fq: ''
+            });
+
+            url.search = params.toString();
+            const request = new Request(url, {
+                headers: {
+                    'x-csrf-token': token,
+                    'x-requested-with': 'XMLHttpRequest'
+                }
+            });
+
+            return FetchJSON<APIMangas>(request);
+        });
+
+        const results = (await Promise.all(promises)).reduce((accumulator: Manga[], element) => {
+            const mangas = element.hits.hit.map(manga => new Manga(this, provider, `/titles/${manga.id}`, manga.fields.title_name.trim()));
+            accumulator.push(...mangas);
+            return accumulator;
+        }, []);
+
+        return results.distinct();
+    }
+
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const [jsonNode] = await FetchCSS(new Request(new URL(manga.Identifier, this.URI)), 'title-page');
+        const [jsonNode] = await FetchCSS(new Request(new URL(manga.Identifier, this.URI)), '[\\:page-objects]');
         const { episodes } = JSON.parse(jsonNode.getAttribute(':page-objects')) as JsonChapters;
         return episodes.map(episode => new Chapter(this, manga, episode.id.toString(), episode.number.trim()));
     }
@@ -50,7 +95,7 @@ export default class extends DecoratableMangaScraper {
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
         const request = new Request(new URL(`/api/v1/viewer/episodes/${chapter.Identifier}`, this.apiUrl));
         const { image_data, signed_cookie } = await FetchJSON<APIPages>(request);
-        if (!image_data || !signed_cookie) {
+        if (!signed_cookie) {
             throw new Exception(R.Plugin_Common_Chapter_UnavailableError);
         }
         return image_data.map(page => new Page(this, chapter, new URL(page.path), { ...signed_cookie }));
@@ -60,16 +105,12 @@ export default class extends DecoratableMangaScraper {
         return this.imageTaskPool.Add(async () => {
 
             const cookiesData = page.Parameters as CookieSigner;
-            const cookies: string[] = [];
-            Object.keys(cookiesData).forEach(name => {
-                const value = cookiesData[name];
-                cookies.push(`${name}=${value}`);
-            });
+            const cookies = Object.keys(cookiesData).map(name => `${name}=${cookiesData[name]}`).join(';');
 
             const request = new Request(page.Link, {
                 signal: signal,
                 headers: {
-                    Cookie: cookies.join(';'),
+                    Cookie: cookies,
                     Referer: page.Link.origin,
                 }
             });
