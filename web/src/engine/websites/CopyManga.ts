920f038d30ce60d787a2616f36604d4fb571ce4a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-19 11:43:51 +0000

    Copymanga : fix getting chapters and pages (#678)
    * Copymanga : fix getting chapters and pages
    
    * requested changes

--- web/src/engine/websites/CopyManga.ts
+++ web/src/engine/websites/CopyManga.ts
@@ -2,9 +2,7 @@ import { Tags } from '../Tags';
 import icon from './CopyManga.webp';
 import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { Check, Choice } from '../SettingsManager';
-import { EngineResourceKey as E, WebsiteResourceKey as W } from '../../i18n/ILocale';
-import { FetchJSON } from '../platform/FetchProvider';
+import { FetchCSS, FetchJSON } from '../platform/FetchProvider';
 
 type APIResponse<T> = {
     code: number,
@@ -16,56 +14,41 @@ type APISingleComic = {
     comic: APIComic
 }
 
-type APIResultList<T> = {
-    list: T[]
-}
-
-type APIPages = {
-    chapter: APIChapter
-}
-
 type APIComic = {
-    uuid: string,
     path_word: string,
     name: string
 }
 
-type APIChapter = {
-    uuid: string,
-    name: string,
-    contents: {
-        url: string
-    }[];
-    words: number[]
+type APIResultList<T> = {
+    list: T[]
+}
+
+type APIChapters = {
+    groups: {
+        default: {
+            chapters: {
+                name: string,
+                id: string
+            }[]
+        }
+    }
+}
+
+type APIPage = {
+    url :string
 }
 
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-    private get Apidate() { return new Date().toISOString().replace(/-/g, ".").split('T').shift(); }
 
-    private readonly apiurl = 'https://api.copymanga.org';
+    private readonly defaultKey = 'xxxmanga.woo.key';
 
     public constructor() {
-        super('copymanga', 'CopyManga', 'https://www.copymanga.site', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Chinese, Tags.Source.Aggregator);
+        super('copymanga', 'CopyManga', 'https://copymanga.site', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Chinese, Tags.Source.Aggregator);
 
         //this.Settings.url = new Text('urloverride', W.Plugin_Settings_UrlOverride, W.Plugin_Settings_UrlOverrideInfo, this.URI.href);
         //(this.Settings.url as Text).Subscribe(value => this.URI.href = value);
         //this.URI.href = this.Settings.url.Value as string;
-
-        this.Settings.format = new Choice('image.format',
-            W.Plugin_Settings_ImageFormat,
-            W.Plugin_Settings_ImageFormatInfo,
-            'jpeg',
-            { key: 'jpeg', label: E.Settings_Global_Format_JPEG },
-            { key: 'webp', label: E.Settings_Global_Format_WEBP },
-        );
-
-        this.Settings.useGlobalCDN = new Check(
-            'check-useGlobalCDN',
-            W.Plugin_CopyManga_Settings_GlobalCDN,
-            W.Plugin_CopyManga_Settings_GlobalCDNInfo,
-            true
-        );
 
     }
 
@@ -79,9 +62,9 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const id = new URL(url).pathname.split('/').pop();
-        const request = this.CreateApiRequest(`comic2/${id}`);
-        const data = await FetchJSON<APIResponse<APISingleComic>>(request);
-        return new Manga(this, provider, data.results.comic.path_word, data.results.comic.name.trim());
+        const request = this.CreateApiRequest(`/api/v3/comic2/${id}`);
+        const { results: { comic } } = await FetchJSON<APIResponse<APISingleComic>>(request);
+        return new Manga(this, provider, comic.path_word, comic.name.trim());
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
@@ -95,7 +78,7 @@ export default class extends DecoratableMangaScraper {
 
     private async GetMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
         try {
-            const request = this.CreateApiRequest(`comics?ordering=-datetime_updated&limit=50&offset=${page * 50}`);
+            const request = this.CreateApiRequest(`/api/v3/comics?ordering=-datetime_updated&limit=50&offset=${page * 50}`);
             const data = await FetchJSON<APIResponse<APIResultList<APIComic>>>(request);
             return data.results.list.map(item => new Manga(this, provider, item.path_word, item.name.trim()));
         } catch (error) {
@@ -104,47 +87,45 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapterList = [];
-        for (let page = 0, run = true; run; page++) {
-            const chapters = await this.GetChaptersFromPage(manga, page);
-            chapters.length > 0 ? chapterList.push(...chapters) : run = false;
-        }
-        return chapterList;
-    }
 
-    private async GetChaptersFromPage(manga: Manga, page: number): Promise<Chapter[]> {
-        try {
-            const request = this.CreateApiRequest(`comic/${manga.Identifier}/group/default/chapters?limit=500&offset=${page * 500}`);
-            const data = await FetchJSON<APIResponse<APIResultList<APIChapter>>>(request);
-            return data.results.list.map(item => new Chapter(this, manga, item.uuid, item.name.trim()));
-        } catch (error) {
-            return [];
-        }
+        const request = this.CreateApiRequest(`/comicdetail/${manga.Identifier}/chapters`);
+        const { results } = await FetchJSON<APIResponse<string>>(request);
+        const { groups: { default: { chapters } } } = await this.Decrypt<APIChapters>(results);
+        return chapters.map(chapter => new Chapter(this, manga, chapter.id, chapter.name.trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const request = this.CreateApiRequest(`comic/${chapter.Parent.Identifier}/chapter2/${chapter.Identifier}?platform=3`);
-        const data = await FetchJSON<APIResponse<APIPages>>(request);
-        const imageUrls = data.results.chapter.contents.map(item => item.url);
-        const imageOrder = data.results.chapter.words;
-        const orderedPages : string[] = [];
-        imageOrder.forEach((order, index) => {
-            orderedPages[order] = imageUrls[index];
-        });
-        return orderedPages.map(page => new Page(this, chapter, new URL(page)));
+
+        const [dataElement] = await FetchCSS<HTMLDivElement>(new Request(new URL(`/comic/${chapter.Parent.Identifier}/chapter/${chapter.Identifier}`, this.URI)), '.imageData');
+        const imageData = dataElement.getAttribute('contentKey');
+        const images = await this.Decrypt<APIPage[]>(imageData);
+        return images.map(image => new Page(this, chapter, new URL(image.url)));
+
+    }
+
+    private async Decrypt<T>(encryptedData: string): Promise<T> {
+
+        const key = Buffer.from(this.defaultKey, 'utf-8');
+        const iv = Buffer.from(encryptedData.substring(0, 16), 'utf-8');
+        const cipher = Buffer.from(encryptedData.substring(16, encryptedData.length), 'hex');
+        const secretKey = await crypto.subtle.importKey('raw', key, {
+            name: 'AES-CBC',
+            length: 128
+        }, true, ['decrypt']);
+
+        const data = await crypto.subtle.decrypt({
+            name: 'AES-CBC',
+            iv: iv
+        }, secretKey, cipher);
+
+        return JSON.parse(Buffer.from(data).toString('utf-8')) as T;
     }
 
     private CreateApiRequest(pathname: string): Request {
-        const request = new Request(new URL(`/api/v3/${pathname}`, this.apiurl).href, {
+        return new Request(new URL(pathname, this.URI), {
             headers: {
-                Version: this.Apidate,
-                Platform: '1',
-                Referer: this.URI.href,
-                Region: this.Settings.useGlobalCDN.Value ? '0' : '1',
-                WebP: this.Settings.format.Value == 'webp' ? '1' : '0',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
             }
         });
-
-        return request;
     }
 }
