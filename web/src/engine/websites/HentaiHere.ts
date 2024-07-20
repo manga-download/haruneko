8b7d16077c19eacac0526485bdd2134ac9777ce1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-20 11:20:58 +0200

    HentaiHere : minor changes
    * better manga regexp
    * adjust tags
    * fix pagescript promise
    * remove chapters ending dash

--- web/src/engine/websites/HentaiHere.ts
+++ web/src/engine/websites/HentaiHere.ts
@@ -1,30 +1,30 @@
 import { Tags } from '../Tags';
 import icon from './HentaiHere.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 
 const script = `
-            new Promise((resolve, reject) => {
-                    try {
-                        resolve(rff_imageList.map(page => 'https://hentaicdn.com/hentai'+ page));
-                    } catch(error) {
-                        reject(error);
-                    }
-            });
-        `;
+    new Promise( resolve => {
+        resolve(rff_imageList.map(page => 'https://hentaicdn.com/hentai'+ page));
+    });
+`;
 
-@Common.MangaCSS(/^{origin}/, 'div.bg-black h4 a', Common.ElementLabelExtractor('span'))
+@Common.MangaCSS(/^{origin}\/m\/[^/]+$/, 'div.bg-black h4 a', Common.ElementLabelExtractor('span'))
 @Common.MangasMultiPageCSS('/directory?page={page}', 'div.item a.text-ellipsis')
-@Common.ChaptersSinglePageCSS('li.sub-chp a#chapterBlock', Common.AnchorInfoExtractor(false, 'i'))
 @Common.PagesSinglePageJS(script, 500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('hentaihere', `HentaiHere`, 'https://hentaihere.com', Tags.Language.English, Tags.Media.Manga, Tags.Rating.Erotica, Tags.Source.Aggregator);
+        super('hentaihere', `HentaiHere`, 'https://hentaihere.com', Tags.Language.English, Tags.Media.Manga, Tags.Rating.Pornographic, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const chapters = await Common.FetchChaptersSinglePageCSS.call(this, manga, 'li.sub-chp a#chapterBlock', Common.AnchorInfoExtractor(false, 'i'));
+        return chapters.map(chapter => new Chapter(this, manga, chapter.Identifier, chapter.Title.replace(/-$/, '').trim()));
     }
 }
