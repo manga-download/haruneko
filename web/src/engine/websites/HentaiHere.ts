91cb6d0da0f4fb9954227adbffe6cb281743b1cd
Ronny <wegener.ronny@gmail.com>
2024-07-21 16:45:00 +0200

    [hentaihere] use decorator for getting chapters

--- web/src/engine/websites/HentaiHere.ts
+++ web/src/engine/websites/HentaiHere.ts
@@ -1,16 +1,21 @@
 import { Tags } from '../Tags';
 import icon from './HentaiHere.webp';
-import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 
-const script = `
-    new Promise( resolve => {
-        resolve(rff_imageList.map(page => 'https://hentaicdn.com/hentai'+ page));
-    });
-`;
+function ChapterInfoExtractor(element: HTMLAnchorElement) {
+    element.querySelectorAll('i').forEach(bloat => bloat.parentElement?.removeChild(bloat));
+    return {
+        id: element.pathname,
+        title: element.innerText.split('-').shift().trim(),
+    };
+}
+
+const script = `new Promise(resolve => resolve(rff_imageList.map(page => 'https://hentaicdn.com/hentai'+ page)));`;
 
 @Common.MangaCSS(/^{origin}\/m\/[^/]+$/, 'div.bg-black h4 a', Common.ElementLabelExtractor('span'))
 @Common.MangasMultiPageCSS('/directory?page={page}', 'div.item a.text-ellipsis')
+@Common.ChaptersSinglePageCSS('li.sub-chp a#chapterBlock', ChapterInfoExtractor)
 @Common.PagesSinglePageJS(script, 500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
@@ -21,10 +26,5 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
-    }
-
-    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters = await Common.FetchChaptersSinglePageCSS.call(this, manga, 'li.sub-chp a#chapterBlock', Common.AnchorInfoExtractor(false, 'i'));
-        return chapters.map(chapter => new Chapter(this, manga, chapter.Identifier, chapter.Title.replace(/-$/, '').trim()));
     }
 }
