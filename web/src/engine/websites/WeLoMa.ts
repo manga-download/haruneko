5327e2e13f23f0a5ccf3df8cad0d4791ee5d7ecb
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-22 18:46:56 +0200

    Weloma: handle AntiDDoss

--- web/src/engine/websites/WeLoMa.ts
+++ web/src/engine/websites/WeLoMa.ts
@@ -3,6 +3,13 @@ import icon from './WeLoMa.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as FlatManga from './decorators/FlatManga';
+import { FetchWindowScript } from '../platform/FetchProvider';
+import { AddAntiScrapingDetection, FetchRedirection } from '../platform/AntiScrapingDetection';
+
+AddAntiScrapingDetection(async (render) => {
+    const dom = await render();
+    return dom.documentElement.innerHTML.includes(`ct_anti_ddos_key`) ? FetchRedirection.Automatic : undefined;
+});
 
 @Common.MangaCSS(/^{origin}\/[^/]+\/$/, FlatManga.queryMangaTitle, FlatManga.MangaLabelExtractor)
 @Common.MangasMultiPageCSS(FlatManga.pathMultiPageManga, FlatManga.queryMangas, 1, 1, 0, FlatManga.MangaExtractor)
@@ -16,5 +23,9 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async Initialize(): Promise<void> {
+        return await FetchWindowScript(new Request(new URL('/manga-list.html', this.URI)), 'true', 3000, 30000);//trigger antiDDOSS
     }
 }
