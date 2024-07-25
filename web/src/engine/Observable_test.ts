6b139e96862e4f3dc13ce93d9eb1a04537c1aaea
Ronny <wegener.ronny@gmail.com>
2024-07-25 16:53:43 +0200

    introduce array observer

--- web/src/engine/Observable_test.ts
+++ web/src/engine/Observable_test.ts
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect } from 'vitest';
-import { Observable } from './Observable';
+import { Observable, ObservableArray } from './Observable';
 
 describe('Observable', () => {
 
@@ -62,5 +62,91 @@ describe('Observable', () => {
         testee.Value++;
 
         expect(callback).toHaveBeenCalledTimes(0);
+    });
+});
+
+describe('ObservableArray', () => {
+
+    it('Should extend Observable', () => {
+        const testee = new ObservableArray([]);
+        expect(testee).toBeInstanceOf(Observable);
+    });
+
+    it('Should notify each subscriber when pop is invoked', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 1, 2, 3, 4, 5 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Pop();
+
+        expect(actual).toBe(5);
+        expect(testee.Value).toStrictEqual([ 1, 2, 3, 4 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should notify each subscriber when push is invoked ', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 1, 2, 3 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Push(4, 5);
+
+        expect(actual).toBe(5);
+        expect(testee.Value).toStrictEqual([ 1, 2, 3, 4, 5 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should notify each subscriber when shift is invoked ', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 1, 2, 3, 4, 5 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Shift();
+
+        expect(actual).toBe(1);
+        expect(testee.Value).toStrictEqual([ 2, 3, 4, 5 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should notify each subscriber when unshift is invoked ', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 3, 4, 5 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Unshift(1, 2);
+
+        expect(actual).toBe(5);
+        expect(testee.Value).toStrictEqual([ 1, 2, 3, 4, 5 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should notify each subscriber when splice is invoked ', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 1, 2, 3, 4, 5 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Splice(1, 3, 999, 777);
+
+        expect(actual).toStrictEqual([ 2, 3, 4 ]);
+        expect(testee.Value).toStrictEqual([ 1, 999, 777, 5 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should notify each subscriber when sort is invoked ', () => {
+        const callback = vi.fn();
+        const testee = new ObservableArray([ 1, 3, 5, 2, 4 ]);
+
+        testee.Subscribe(callback);
+        const actual = testee.Sort((a, b) => a - b);
+
+        expect(actual).toBe(testee.Value);
+        expect(testee.Value).toStrictEqual([ 1, 2, 3, 4, 5 ]);
+        expect(callback).toHaveBeenCalledWith(testee.Value, undefined);
+        expect(callback).toHaveBeenCalledTimes(1);
     });
 });
