bbbf22e2d1ec9249ef413a25f6f8d03ec6079025
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-26 14:03:56 +0200

    custom reject message

--- web/src/engine/websites/ReadComicOnline.ts
+++ web/src/engine/websites/ReadComicOnline.ts
@@ -31,7 +31,7 @@ const pageScript = `
                 tries++;
                 if (tries > 10) {
                     clearInterval(interval);
-                    reject(error);
+                    reject('Unable to get pictures after more than 10 tries !');
                 }
             }
         }, 1000);
