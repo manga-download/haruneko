d4f2b0e8f1fe4df168bc5737285d864a4f58c089
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-26 18:44:44 +0000

    ReadComicOnline : fix getting pages (#695)
    * ReadComicOnline : fix getting pages
    
    * better promise
    
    * custom reject message
    
    * Update ReadComicOnline.ts

--- web/src/engine/websites/ReadComicOnline_e2e.ts
+++ web/src/engine/websites/ReadComicOnline_e2e.ts
@@ -10,7 +10,7 @@ const config = {
         url: 'https://readcomiconline.li/Comic/Star-Wars-Legacy-War',
         id: '/Comic/Star-Wars-Legacy-War',
         title: 'Star Wars: Legacy War'
-    },
+    }, /* CloudFlare
     child: {
         id: '/Comic/Star-Wars-Legacy-War/Issue-1?id=77062&readType=1&quality=hq',
         title: 'Issue #1',
@@ -20,7 +20,7 @@ const config = {
         index: 0,
         size: 1_312_286,
         type: 'image/jpeg'
-    }
+    } */
 };
 
 const fixture = new TestFixture(config);
