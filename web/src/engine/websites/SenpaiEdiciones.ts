de79d3fb0210aae5e56a5b65852f039a82be1278
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-07-28 21:22:42 +0200

    SenpaiEdition: use JS to get pages

--- web/src/engine/websites/SenpaiEdiciones.ts
+++ web/src/engine/websites/SenpaiEdiciones.ts
@@ -7,7 +7,7 @@ import * as Common from './decorators/Common';
 @MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS()
 @MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
