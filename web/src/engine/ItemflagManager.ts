8bd2884530263a2d69f2c9f05eef30e568bd42cd
Ronny <wegener.ronny@gmail.com>
2024-08-02 01:27:09 +0200

    provisional reactivity for flag manager

--- web/src/engine/ItemflagManager.ts
+++ web/src/engine/ItemflagManager.ts
@@ -1,19 +1,45 @@
+import { Observable } from './Observable';
 import type { MediaChild, MediaContainer } from './providers/MediaPlugin';
 import { type StorageController, Store } from './StorageController';
 
+export type ItemFlag = {
+    IdentifierHash: string,
+    TitleHash: string,
+    kind: FlagType,
+}
+
+export const enum FlagType {
+    Current,
+    Viewed,
+}
+
+export type EntryFlagEventData = {
+    readonly Entry: MediaContainer<MediaChild>;
+    readonly Kind?: FlagType;
+};
+
+// TODO: Revise how flags can be handled in a more effcient/optimized and reactive way
+// See: Proposalfor managing generic annotation data => https://github.com/manga-download/haruneko/tree/decouple-bookmarks-itemflags-dependency
 export class ItemflagManager {
 
-    //public readonly FlagChanged: Event<MediaContainer<MediaChild>, FlagType> = new Event<MediaContainer<MediaChild>, FlagType>();
-    //public readonly MediaFlagsChanged: Event<ItemflagManager, MediaContainer<MediaChild>> = new Event<this, MediaContainer<MediaChild>>();
+    /**
+     * Occures when the flag for a single entry is changed
+     */
+    public readonly EntryFlagEventChannel = new Observable<EntryFlagEventData, this>(null, this);
 
-    private items:Map<string, ItemFlag[]> = new Map();
+    /**
+     * Occurs when one or more flags of entries in a container are changed
+     */
+    public readonly ContainerFlagsEventChannel = new Observable<MediaContainer<MediaChild>, this>(null, this);
+
+    private items = new Map<string, ItemFlag[]>();
     constructor(private readonly storage: StorageController) {}
 
     public async LoadContainerFlags(media: MediaContainer<MediaChild>) {
         if(media.Parent) {
             const mediaflags = await this.storage.LoadPersistent<ItemFlag[]>(Store.Itemflags, this.StorageKey(media));
             this.items.set(this.StorageKey(media), mediaflags);
-            this.MediaFlagsChanged.Dispatch(this, media);
+            this.ContainerFlagsEventChannel.Value = media;
         }
     }
 
@@ -72,8 +98,11 @@ export class ItemflagManager {
         flags.push(newflag);
         this.items.set(this.StorageKey(entry.Parent), flags);
         await this.SaveContainerFlags(entry.Parent, flags);
-        this.FlagChanged.Dispatch(entry, kind);
-        this.MediaFlagsChanged.Dispatch(this, entry.Parent);
+        this.EntryFlagEventChannel.Value = {
+            Entry: entry,
+            Kind: kind,
+        };
+        this.ContainerFlagsEventChannel.Value = entry.Parent;
     }
 
     /* Remove the flag of an item */
@@ -85,8 +114,11 @@ export class ItemflagManager {
         });
         this.items.set(this.StorageKey(itemToRemove.Parent), flags);
         await this.SaveContainerFlags(itemToRemove.Parent, flags);
-        this.FlagChanged.Dispatch(itemToRemove, undefined);
-        this.MediaFlagsChanged.Dispatch(this, itemToRemove.Parent);
+        this.EntryFlagEventChannel.Value = {
+            Entry: itemToRemove,
+            Kind: undefined,
+        };
+        this.ContainerFlagsEventChannel.Value = itemToRemove.Parent;
     }
 
     /* Get the flagType of an item */
@@ -108,14 +140,4 @@ export class ItemflagManager {
             return mark === undefined;
         });
     }
-}
-
-export type ItemFlag = {
-    IdentifierHash: string,
-    TitleHash: string,
-    kind:FlagType,
-}
-export const enum FlagType {
-    Current,
-    Viewed,
 }
