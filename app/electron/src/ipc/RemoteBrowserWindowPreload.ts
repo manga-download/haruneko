8449d61d9d4071a39f0e5bfab43a09325e15f194
Ronny <wegener.ronny@gmail.com>
2024-08-09 15:40:39 +0200

    address electron codacy issues

--- app/electron/src/ipc/RemoteBrowserWindowPreload.ts
+++ app/electron/src/ipc/RemoteBrowserWindowPreload.ts
@@ -1,8 +1,9 @@
 export const argvPreloadScript = '--preload-script=';
+const invoke: (script: string) => unknown = window['eval'];
 
 const preload = process.argv.find(arg => arg.startsWith(argvPreloadScript))?.split(argvPreloadScript).pop() ?? '';
 if(preload) {
-    eval(atob(preload));
+    invoke(atob(preload));
     console.log('======== Preload Script ========');
     console.log(atob(preload));
     console.log('================================');
