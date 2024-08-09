7281a6bab201ffb1ad1e6a9a94730ccde562e38b
Ronny <wegener.ronny@gmail.com>
2024-08-09 07:54:44 +0200

    use template for tightly coupled websites

--- web/src/engine/websites/LELScanVF.ts
+++ web/src/engine/websites/LELScanVF.ts
@@ -1,15 +1,8 @@
 import { Tags } from '../Tags';
 import icon from './LELScanVF.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Common from './decorators/Common';
-import * as FuzzyDoodle from './decorators/FuzzyDoodle';
+import { FuzzyDoodle } from './templates/FuzzyDoodle';
 
-@Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, FuzzyDoodle.queryMangatitle, FuzzyDoodle.MangaLabelExtractor)
-@Common.MangasMultiPageCSS(FuzzyDoodle.mangaPath, FuzzyDoodle.queryMangas, 1, 1, 0, FuzzyDoodle.MangaInfoExtractor)
-@FuzzyDoodle.ChaptersMultiPageCSS()
-@Common.PagesSinglePageCSS(FuzzyDoodle.queryPages)
-@Common.ImageAjax()
-export default class extends DecoratableMangaScraper {
+export default class extends FuzzyDoodle {
 
     public constructor() {
         super('lelscanvf', `LELSCAN-VF`, 'https://lelscanfr.com', Tags.Language.French, Tags.Media.Manga, Tags.Source.Aggregator);
@@ -18,5 +11,4 @@ export default class extends DecoratableMangaScraper {
     public override get Icon() {
         return icon;
     }
-
 }
