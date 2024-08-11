3d508f2b492ecd320ec3f6b275a1f3150e13b2a1
Ronny <wegener.ronny@gmail.com>
2024-08-11 19:16:46 +0200

    enable github-actions reporter

--- web/scripts/website-metrics.vitest.ts
+++ web/scripts/website-metrics.vitest.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
     test: {
         include: [ '**/website-metrics.ts' ],
+        reporters: process.env.GITHUB_ACTIONS ? [ 'basic', 'github-actions' ] : [ 'default' ],
     },
 });
