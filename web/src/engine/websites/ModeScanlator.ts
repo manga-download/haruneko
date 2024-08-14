e2effeb58ee80152df9b5f50744bd79a66a23b7a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-08-14 15:05:16 +0200

    Update ModeScanlator.ts

--- web/src/engine/websites/ModeScanlator.ts
+++ web/src/engine/websites/ModeScanlator.ts
@@ -14,7 +14,7 @@ const apiUrl = 'https://api.modescanlator.com';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('modescanlator', `Mode Scanlator`, 'https://modescanlator.com', Tags.Language.Portuguese, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Scanlator, Tags.Accessibility.RegionLocked);
+        super('modescanlator', `Mode Scanlator`, 'https://site.modescanlator.com', Tags.Language.Portuguese, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Scanlator);
     }
     public override get Icon() {
         return icon;
