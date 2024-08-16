bcaa00b41eb7b339af7919ac7d17cd9fa9e6d12c
Manga Download (Owner) <147331778+manga-download-owner@users.noreply.github.com>
2024-08-16 16:05:35 +0200

    New Crowdin updates (#749)
    * New translations en_us.ts (French)
    
    * New translations en_us.ts (Spanish)
    
    * New translations en_us.ts (Arabic)
    
    * New translations en_us.ts (German)
    
    * New translations en_us.ts (Portuguese)
    
    * New translations en_us.ts (Turkish)
    
    * New translations en_us.ts (Chinese Simplified)
    
    * New translations en_us.ts (Indonesian)
    
    * New translations en_us.ts (Thai)
    
    * New translations en_us.ts (Hindi)
    
    * New translations en_us.ts (Filipino)
    
    * New translations en_us.ts (Zulu)

--- web/src/i18n/locales/fr_FR.ts
+++ web/src/i18n/locales/fr_FR.ts
@@ -224,6 +224,8 @@ const translations: VariantResource = { // NOTE: Use defaults for missing transl
   Tags_Source_Scanlator: "Traducteur",
   Tags_Source_Aggregator: "Agrégateur",
   Tags_Accessibility: "Accessibilité",
+  Tags_Accessibility_DomainRotation: 'Domain Rotation',
+  Tags_Accessibility_DomainRotationDescription: 'Regular changing its (sub-)domain, probably to avoid scraping or DMCA',
   Tags_Accessibility_RegionLock: "Verrouillage régional",
   Tags_Accessibility_RegionLockDescription: "Accès uniquement pour les pays sans blocage géographique (IP)",
   Tags_Accessibility_RateLimit: "Limite de débit",
