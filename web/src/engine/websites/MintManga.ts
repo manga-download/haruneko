5c677a22a0dd17d76ce85941647978f035f9299a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-08-16 20:09:50 +0200

    Update MintManga.ts

--- web/src/engine/websites/MintManga.ts
+++ web/src/engine/websites/MintManga.ts
@@ -3,6 +3,7 @@ import icon from './MintManga.webp';
 import { DecoratableMangaScraper} from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as ReadM from './decorators/ReadMangaLive';
+import { FetchWindowScript } from '../platform/FetchProvider';
 
 @Common.MangaCSS(/^{origin}\/[^/]+$/, ReadM.queryMangaTitle)
 @Common.MangasMultiPageCSS(ReadM.pathMangas, ReadM.queryMangas, 0, ReadM.pageMangaOffset, 0, Common.AnchorInfoExtractor(true))
@@ -11,9 +12,16 @@ import * as ReadM from './decorators/ReadMangaLive';
 @ReadM.ImageAjax()
 export default class extends DecoratableMangaScraper {
     public constructor() {
-        super('mintmanga', `MintManga`, 'https://24.mintmanga.one', Tags.Language.Russian, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator);
+        super('mintmanga', `MintManga`, 'https://24.mintmanga.one', Tags.Language.Russian, Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Aggregator, Tags.Accessibility.DomainRotation );
     }
+
     public override get Icon() {
         return icon;
     }
+
+    public override async Initialize(): Promise<void> {
+        this.URI.href = await FetchWindowScript(new Request(this.URI), 'window.location.origin');
+        console.log(`Assigned URL '${this.URI}' to ${this.Title}`);
+    }
+
 }
