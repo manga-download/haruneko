581a4d8d9d2ecbbd64e63c9bea0fa4edd58ed5b5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-08-16 13:13:53 +0200

    Add Qiman5

---
+++ web/src/engine/websites/Qiman5_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'qiman5',
+        title: '奇漫屋 (Qiman5)'
+    },
+    container: {
+        url: 'https://qiman2.com/16257.html',
+        id: '/16257.html',
+        title: '斗破苍穹之大主宰'
+    },
+    child: {
+        id: '/16257/1274202.html',
+        title: '1话 激战过后'
+    },
+    entry: {
+        index: 3,
+        size: 278_279,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
