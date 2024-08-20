68c68bee03bb9a4c8c0b7dea4035fbf0fb8bc971
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-08-20 20:45:33 +0200

    Mangatown: handle both chapters types

--- web/src/engine/websites/MangaTown.ts
+++ web/src/engine/websites/MangaTown.ts
@@ -1,13 +1,34 @@
 import { Tags } from '../Tags';
 import icon from './MangaTown.webp';
 import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
-import { FetchCSS } from '../platform/FetchProvider';
 import * as Common from './decorators/Common';
+
+//Modded DM5 script
+const pageScript = `
+    new Promise(async (resolve, reject) => {
+        try {
+            const pagecount = window.total_pages;
+            const images = await Promise.all([...new Array(pagecount).keys()].map(async p => {
+                eval(await $.ajax({
+                    url: 'chapterfun.ashx',
+                    data: {
+                        cid: window.chapter_id,
+                        page: p + 1,
+                    }
+                }));
+                return d[0];
+            }));
+            resolve(images.map(link => new URL(link, window.location.href).href));
+        } catch(error) {
+            reject(error);
+        }
+    });
+`;
 
 @Common.MangaCSS(/^{origin}\/manga\//, 'div.article_content h1.title-top')
 @Common.MangasMultiPageCSS('/directory/0-0-0-0-0-0/{page}.htm', 'ul.manga_pic_list li p.title a', 1, 1, 0, Common.AnchorInfoExtractor(true))
 @Common.ChaptersSinglePageCSS('ul.chapter_list li a')
-@Common.ImageAjaxFromHTML('img#image')
+@Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -19,7 +40,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const data = await FetchCSS<HTMLOptionElement>(new Request(new URL(chapter.Identifier, this.URI)), 'div.manga_read_footer div.page_select select option');
-        return data.filter(option => !/featured\.html$/.test(option.value)).map(option => new Page(this, chapter, new URL(option.value, this.URI), { Referer: 'mangahere.com' }));
+        const pages = await Common.FetchPagesSinglePageJS.call(this, chapter, pageScript);
+        return pages.map(page => new Page(this, chapter, page.Link, { Referer: 'https://mangahere.com' }));
     }
 }
