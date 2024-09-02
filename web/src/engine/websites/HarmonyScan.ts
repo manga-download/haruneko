f3c51680be642de8743e0be84234b12a675bfeb8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-02 09:34:14 +0000

    Maintenance : domain changes & dead websites (#760)
    * Remove CloverManga
    
    Redirect to Webtoonhatti
    
    * DemonSect : change domain
    
    * DuaLeoTruyen: change domain
    
    * Firecomics : change domain
    
    * Remove FuryoSquad
    
    "Site not installed". Also, 6 years without release
    
    * HarmonyScan : change domaine & use madara
    
    * HentaiVN : change domain
    
    * HunterScan : change domain
    
    * lianscans : change domain
    
    * LuaScans : change domain
    
    * lunascans : change domain
    
    * MagusManga : change domain
    
    websites needs a rewrite too
    
    * MangaSwat : change domain
    
    * ManhwaList : change domain
    
    * ManjaNoon : change domain
    
    * NightScans: change domain
    
    * Qimeitj : change domain
    
    * Remove TempestFansub
    
    redirect to TempestScans
    
    * YanpFansub : change domain

--- web/src/engine/websites/HarmonyScan.ts
+++ web/src/engine/websites/HarmonyScan.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
 import icon from './HarmonyScan.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as MangaStream from './decorators/WordPressMangaStream';
+import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https?:\/\/banana-scan\.com\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageJS()
+@Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'div.post-title h1')
+@Madara.MangasMultiPageAJAX()
+@Madara.ChaptersSinglePageAJAXv2()
+@Madara.PagesSinglePageCSS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('harmonyscan', 'Harmony Scan', 'https://banana-scan.com', Tags.Media.Manga, Tags.Language.French);
+        super('harmonyscan', 'Harmony Scan', 'https://harmony-scan.fr', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Language.French, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
