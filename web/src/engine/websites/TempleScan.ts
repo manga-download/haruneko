e7cebc340bf9f929f919ca5891257f65f9fec470
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-04 10:12:46 +0200

    requested changes

--- web/src/engine/websites/TempleScan.ts
+++ web/src/engine/websites/TempleScan.ts
@@ -26,7 +26,7 @@ type APIChapter = {
 export default class extends DecoratableMangaScraper {
 
     private readonly apiUrl = 'https://api.templescan.net/api/';
-    private readonly mangaRegexp = new RegExp(`^${this.URI.origin}/(comic/[^/]+)$`);
+    private readonly mangaRegexp = new RegExpSafe(`^${this.URI.origin}/(comic/[^/]+)$`);
 
     public constructor() {
         super('templescan', 'TempleScan', 'https://templescan.net', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
@@ -42,23 +42,23 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const { series_slug, title } = await FetchJSON<APIManga>(new Request(new URL(url.match(this.mangaRegexp)[1], this.apiUrl)));
-        return new Manga(this, provider, series_slug, title.trim());
+        return new Manga(this, provider, series_slug, title);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         const data = await FetchJSON<APIManga[]>(new Request(new URL('allComics', this.apiUrl)));
-        return data.map(manga => new Manga(this, provider, manga.series_slug, manga.title.trim()));
+        return data.map(manga => new Manga(this, provider, manga.series_slug, manga.title));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
         const { Season } = await FetchJSON<APIManga>(new Request(new URL(`comic/${manga.Identifier}`, this.apiUrl)));
+        if (!Season) return [];
         const results = Season.reduce((accumulator: Chapter[], element) => {
             const chapters = element.Chapter.map(chapter => {
                 const title = chapter.chapter_title ? [chapter.chapter_name, chapter.chapter_title].join(' : ') : chapter.chapter_name;
-                return new Chapter(this, manga, chapter.chapter_slug, title.trim());
+                return new Chapter(this, manga, chapter.chapter_slug, title);
             });
-            accumulator.push(...chapters);
-            return accumulator;
+            return [...accumulator, ...chapters];
         }, []);
         return results;
     }
