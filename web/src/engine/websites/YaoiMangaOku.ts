e13d0cda2f95d549f596a51313a6d3a73e11ce18
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-05 11:28:42 +0200

    yaoimangaoku : fix typo

--- web/src/engine/websites/YaoiMangaOku.ts
+++ web/src/engine/websites/YaoiMangaOku.ts
@@ -1,5 +1,5 @@
 import { Tags } from '../Tags';
-import icon from './YaoiManngaOku.webp';
+import icon from './YaoiMangaOku.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
