ad85979cb418f1294a2241ed324cb29ef823560c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-05 10:37:58 +0000

    add requested websites (#762)
    * Add HariManga
    
    https://github.com/manga-download/hakuneko/issues/7379
    
    * Add yaoimangaoku
    
    https://github.com/manga-download/hakuneko/issues/7260
    
    * yaoimangaoku : fix typo
    
    * Add LectorManga (.Lat)
    
    https://github.com/manga-download/hakuneko/issues/7247
    
    * Add HorrorFC
    
    https://github.com/manga-download/hakuneko/issues/7248
    
    * Add KabusManga
    
    https://github.com/manga-download/hakuneko/issues/7228
    
    * Add Stray Fansub
    
    https://github.com/manga-download/hakuneko/issues/7227

---
+++ web/src/engine/websites/HorrorFC_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'horrorfc',
+        title: 'HorrorFC'
+    },
+    container: {
+        url: 'https://horrorfc.com/genkai-chitai/',
+        id: '/genkai-chitai/',
+        title: 'Genkai Chitai'
+    },
+    child: {
+        id: '/genkai-chitai-phantom-zone-ii-2-aether-forest/',
+        title: 'Phantom Zone II 2 – Aether Forest'
+    },
+    entry: {
+        index: 1,
+        size: 1_659_687,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
