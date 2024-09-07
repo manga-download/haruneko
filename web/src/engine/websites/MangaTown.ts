df8ae9a3ae6b10785fabf3e5ea586cdab39c6881
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-07 10:09:15 +0200

    get images urls synchroneously

--- web/src/engine/websites/MangaTown.ts
+++ web/src/engine/websites/MangaTown.ts
@@ -2,23 +2,22 @@ import { Tags } from '../Tags';
 import icon from './MangaTown.webp';
 import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
+import { FetchWindowScript } from '../platform/FetchProvider';
 
 //Modded DM5 script
 const pageScript = `
     new Promise(async (resolve, reject) => {
         try {
             const pagecount = window.total_pages;
-            const images = await Promise.all([...new Array(pagecount).keys()].map(async p => {
-                eval(await $.ajax({
-                    url: 'chapterfun.ashx',
-                    data: {
-                        cid: window.chapter_id,
-                        page: p + 1,
-                    }
-                }));
-                return d[0];
-            }));
-            resolve(images.map(link => new URL(link, window.location.href).href));
+            const pages = [];
+            for (let page = 1; page <= pagecount; page++) {
+                const response = await fetch('chapterfun.ashx?cid='+ window.chapter_id + '&page=' + page);
+                const script = await response.text();
+                eval(script);
+                pages.push(d[0]);
+            }
+            resolve(pages.map(link => new URL(link, window.location.href).href));
+
         } catch(error) {
             reject(error);
         }
@@ -40,7 +39,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const pages = await Common.FetchPagesSinglePageJS.call(this, chapter, pageScript);
-        return pages.map(page => new Page(this, chapter, page.Link, { Referer: 'https://mangahere.com' }));
+        const data = await FetchWindowScript<string[]>(new Request(new URL(chapter.Identifier, this.URI)), pageScript);
+        return data.map(link => new Page(this, chapter, new URL(link), { Referer: 'https://mangahere.com' }));
     }
 }
