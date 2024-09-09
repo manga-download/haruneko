6cc18a42c12af0b1ebb228d702d95e8b87e51132
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-09 23:09:19 +0200

    Update _index.ts

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -90,6 +90,7 @@ export { default as CulturedWorks } from './CulturedWorks';
 export { default as CyComi } from './CyComi';
 export { default as CypherScans } from './CypherScans';
 export { default as DankeFursLesen } from './DankeFursLesen';
+export { default as DayComics } from './DayComics';
 export { default as Daysneo } from './Daysneo';
 export { default as DeathTollScans } from './DeathTollScans';
 export { default as DecadenceScans } from './DecadenceScans';
