ac6af54db65e7d56eae1a55dc40ec45df0800f9d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-13 10:15:42 +0200

    ResetScans : extends FuzzyDoodle template

--- web/src/engine/websites/ResetScans.ts
+++ web/src/engine/websites/ResetScans.ts
@@ -1,15 +1,8 @@
 import { Tags } from '../Tags';
 import icon from './ResetScans.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Madara from './decorators/WordPressMadara';
-import * as Common from './decorators/Common';
+import { FuzzyDoodle } from './templates/FuzzyDoodle';
 
-@Madara.MangaCSS(/^{origin}\/manga\/[^/]+$/, 'div.post-title h1')
-@Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv2()
-@Madara.PagesSinglePageCSS()
-@Common.ImageAjax()
-export default class extends DecoratableMangaScraper {
+export default class extends FuzzyDoodle {
 
     public constructor() {
         super('resetscans', 'Reset Scans', 'https://reset-scans.xyz', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Language.English, Tags.Source.Scanlator);
