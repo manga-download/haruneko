1fa09353eb55831180730ae907c00ad0846c948d
ronny <ronny@github.com>
2024-09-26 20:51:56 +0200

    update ts-doc linting

--- web/src/engine/transformers/BookmarkConverter.ts
+++ web/src/engine/transformers/BookmarkConverter.ts
@@ -3,8 +3,8 @@ import { Exception } from '../Error';
 import type { BookmarkSerialized } from '../providers/Bookmark';
 
 /**
- * DO NOT USE!
- * @remark - Only exported for testing
+ * __DO NOT USE!__
+ * @remarks Only exported for testing
  */
 export const legacyWebsiteIdentifierMap = new Map([
     [ 'aresnov', 'scarmanga' ],
