72d3f5cec524046ca88cb12a22940aade3e76896
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-09-27 14:19:05 +0200

    use +18 comic for tests

--- web/src/engine/websites/DayComics_e2e.ts
+++ web/src/engine/websites/DayComics_e2e.ts
@@ -7,17 +7,17 @@ const config = {
         title: 'DayComics'
     },
     container: {
-        url: 'https://daycomics.com/content/102562',
-        id: '102562',
-        title: 'Draco Run'
+        url: 'https://daycomics.com/content/101023',
+        id: '101023',
+        title: 'Sex Study Group'
     },
     child: {
-        id: '/content/102562/134459',
+        id: '/content/101023/113522',
         title: 'Episode 1'
     },
     entry: {
         index: 0,
-        size: 162_394,
+        size: 356_854,
         type: 'image/webp'
     }
 };
