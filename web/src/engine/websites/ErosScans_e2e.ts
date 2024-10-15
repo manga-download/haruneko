229730c2777f95d49f0d3190ee142519b0dd2fc0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-10-15 10:11:25 +0200

    Add back TecnoScan
    and add related website ErosScans

---
+++ web/src/engine/websites/ErosScans_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'erosscans',
+        title: 'Eros Scans'
+    },
+    container: {
+        url: 'https://helorscans.xyz/manga/absolute-sword-sense/',
+        id: '/manga/absolute-sword-sense/',
+        title: 'Absolute Sword Sense'
+    },
+    child: {
+        id: '/absolute-sword-sense-chapter-100/',
+        title: 'Chapter 100'
+    },
+    entry: {
+        index: 1,
+        size: 880_280,
+        type: 'image/webp'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
