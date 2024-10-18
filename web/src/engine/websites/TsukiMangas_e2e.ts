9beb71ca335fedd3abc4c7df06bf348761bf89ac
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-10-18 12:52:19 +0000

    Add Tsukimangas (#793)
    * first draft
    
    * fix 403 errors
    
    * better mirrors use for pages
    
    * requested changes

---
+++ web/src/engine/websites/TsukiMangas_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'tsukimangas',
+        title: 'Tsuki-Mangas'
+    }, /* CloudFlare
+    container: {
+        url: 'https://tsuki-mangas.com/obra/105/tower-of-god-3-season',
+        id: '105',
+        title: 'Tower of God 3 season'
+    },
+    child: {
+        id: '/read/207989/34655/1',
+        title: '217'
+    },
+    entry: {
+        index: 0,
+        size: 632_904,
+        type: 'image/png'
+    }*/
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
