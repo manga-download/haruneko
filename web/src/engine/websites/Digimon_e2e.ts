d13f4aeec27f5fdcf56d538fc38c7ed3befdab35
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-10-20 08:48:57 +0000

    Add Digimon (#834)
    * Add Digimon
    
    Mangas and chapters , everything is on same page.
    We rely on link anchors & div identifiers .
    
    * requested changes

---
+++ web/src/engine/websites/Digimon_e2e.ts
@@ -0,0 +1,26 @@
+﻿import { describe } from 'vitest';
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+const config = {
+    plugin: {
+        id: 'digimon',
+        title: 'Digimon'
+    },
+    container: {
+        url: 'https://digimon.net/digimoncomic/en/#seekers',
+        id: '/digimoncomic/en/#seekers',
+        title: 'DIGIMON SEEKERS ～The Crossroad Witch～'
+    },
+    child: {
+        id: '/digimoncomic/en/comic/seekers/ep_001/',
+        title: 'ep.1'
+    },
+    entry: {
+        index: 0,
+        size: 335_303,
+        type: 'image/jpeg'
+    }
+};
+
+const fixture = new TestFixture(config);
+describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
