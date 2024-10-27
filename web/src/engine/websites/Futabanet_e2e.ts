0e913f6c12be163f4f1d6371d46d3b0a21afe5c6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-10-27 10:31:34 +0100

    update tests

--- web/src/engine/websites/Futabanet_e2e.ts
+++ web/src/engine/websites/Futabanet_e2e.ts
@@ -1,4 +1,3 @@
-import { describe } from 'vitest';
 import { TestFixture, type Config } from '../../../test/WebsitesFixture';
 
 const config: Config = {
@@ -23,8 +22,7 @@ const config: Config = {
     }
 };
 
-const fixture = new TestFixture(config);
-describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
+new TestFixture(config).AssertWebsite();
 
 const NoSpeedBinbConfig: Config = {
     plugin: {
