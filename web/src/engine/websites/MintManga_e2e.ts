ccfcf9369fca605bff4c3f53f3b22334d37a9a65
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-10-27 10:06:23 +0100

    update tests

--- web/src/engine/websites/MintManga_e2e.ts
+++ web/src/engine/websites/MintManga_e2e.ts
@@ -1,4 +1,3 @@
-import { describe } from 'vitest';
 import { TestFixture, type Config } from '../../../test/WebsitesFixture';
 
 const config: Config = {
@@ -24,5 +23,4 @@ const config: Config = {
     }*/
 };
 
-const fixture = new TestFixture(config);
-describe(fixture.Name, async () => (await fixture.Connect()).AssertWebsite());
+new TestFixture(config).AssertWebsite();
