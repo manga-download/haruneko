87d149ca41a15c215e912c372fb6323358e6369b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-02 12:11:24 +0100

    make it only one website

--- web/src/engine/websites/TappyToon.ts
+++ web/src/engine/websites/TappyToon.ts
@@ -4,6 +4,11 @@ import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from 
 import * as Common from './decorators/Common';
 import { FetchJSON, FetchWindowScript } from '../platform/FetchProvider';
 
+type MangaID = {
+    id: string,
+    language: string
+}
+
 type NextData = {
     props: {
         initialProps: {
@@ -25,7 +30,8 @@ type NextData = {
 type APIManga = {
     id: number,
     title: string,
-    isSuperHighQualitySupported: boolean
+    isSuperHighQualitySupported: boolean,
+    locale: string
 }
 
 type APIChapter = {
@@ -43,15 +49,20 @@ type APIPages = {
     }[]
 }
 
+const mangaLanguageMap = new Map([
+    ['en', Tags.Language.English],
+    ['fr', Tags.Language.French],
+    ['de', Tags.Language.German],
+]);
+
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
-    protected language = 'en';
     private bearer: string = undefined;
-    private deviceid = undefined;
+    private deviceId: string = undefined;
     private readonly apiUrl = 'https://api-global.tappytoon.com';
 
-    public constructor(id = 'tappytoon', label = 'TappyToon', url = 'https://www.tappytoon.com', tags = [Tags.Media.Manhwa, Tags.Language.English, Tags.Source.Official]) {
-        super(id, label, url, ...tags);
+    public constructor() {
+        super('tappytoon', 'TappyToon', 'https://www.tappytoon.com', Tags.Media.Manhwa, Tags.Language.Multilingual, Tags.Source.Official);
     }
 
     public override get Icon() {
@@ -59,49 +70,63 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/${this.language}/book/[^/]+$`).test(url);
+        return new RegExpSafe(`^${this.URI.origin}/[a-z]{2}/book/[^/]+$`).test(url);
     }
 
     public override async Initialize(): Promise<void> {
         const { props: { initialState: { axios: { headers } } } } = await this.GetNextData(new URL(this.URI));
         this.bearer = headers.Authorization;
-        this.deviceid = headers['X-Device-Uuid'];
+        this.deviceId = headers['X-Device-Uuid'];
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const { props: { initialProps: { pageProps: { comic } } } } = await this.GetNextData(new URL(url));
-        return new Manga(this, provider, comic.id.toString(), comic.title.replace(/(\[(FR|DE)\])$/, '').trim());
+        const { props: { initialProps: { pageProps: { comic: { id, title, locale } } } } } = await this.GetNextData(new URL(url));
+        const mangaId = JSON.stringify({ id: id.toString(), language: locale });
+        return new Manga(this, provider, mangaId, title.trim(), mangaLanguageMap.get(locale) ?? Tags.Language.English);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const comics = await FetchJSON<APIManga[]>(this.CreateRequest(new URL(`/comics?locale=${this.language}`, this.apiUrl)));
-        return comics.map(comic => new Manga(this, provider, comic.id.toString(), comic.title.replace(/(\[(FR|DE)\])$/, '').trim()));
+        const mangaList: Manga[] = [];
+        for (const [language] of mangaLanguageMap) {
+            const mangas = await FetchJSON<APIManga[]>(this.CreateRequest(new URL(`/comics?locale=${language}`, this.apiUrl), language));
+            mangaList.push(...mangas.map(manga => {
+                const id = JSON.stringify({ id: manga.id.toString(), language });
+                return new Manga(this, provider, id, manga.title.trim(), mangaLanguageMap.get(manga.locale) ?? Tags.Language.English);
+            }));
+        }
+        return mangaList;
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters = await FetchJSON<APIChapter[]>(this.CreateRequest(new URL(`/comics/${manga.Identifier}/chapters?limit=9999&skipAgeRestriction=true&locale=${this.language}`, this.apiUrl)));
+        const { language, id } = JSON.parse(manga.Identifier) as MangaID;
+        const chapters = await FetchJSON<APIChapter[]>(this.CreateRequest(new URL(`/comics/${id}/chapters?limit=9999&skipAgeRestriction=true&locale=${language}`, this.apiUrl), language));
         return chapters.filter(chapter => chapter.isAccessible && (chapter.isFree || chapter.isUserUnlocked || chapter.isUserRented))
             .map(chapter => new Chapter(this, manga, chapter.id.toString(), chapter.title));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const { isSuperHighQualitySupported } = await FetchJSON<APIManga>(this.CreateRequest(new URL(`/comics/${chapter.Parent.Identifier}&locale=${this.language}`, this.apiUrl)));
-        const { files } = await FetchJSON<APIPages>(this.CreateRequest(new URL(`/content-delivery/contents/manifest?chapterId=${chapter.Identifier}&variant=${isSuperHighQualitySupported ? 'super_high' : 'high'}&locale=${this.language}&canAcceptSignedCookie=true`, this.apiUrl)));
+        const { language, id } = JSON.parse(chapter.Parent.Identifier) as MangaID;
+        const { isSuperHighQualitySupported } = await FetchJSON<APIManga>(this.CreateRequest(new URL(`/comics/${id}&locale=${language}`, this.apiUrl), language));
+        const url = new URL(`/content-delivery/contents/manifest?chapterId=${chapter.Identifier}`, this.apiUrl);
+        url.searchParams.set('variant', isSuperHighQualitySupported ? 'super_high' : 'high');
+        url.searchParams.set('canAcceptSignedCookie', 'true');
+        url.searchParams.set('locale', language);
+        const { files } = await FetchJSON<APIPages>(this.CreateRequest(url, language));
         return files.map(page => new Page(this, chapter, new URL(page.url)));
     }
 
-    private async GetNextData(url: URL): Promise<NextData> {
-        return await FetchWindowScript<NextData>(this.CreateRequest(url), '__NEXT_DATA__', 2000);
+    private async GetNextData(url: URL, language: string = 'en'): Promise<NextData> {
+        return await FetchWindowScript<NextData>(this.CreateRequest(url, language), '__NEXT_DATA__', 2000);
     }
 
-    private CreateRequest(url: URL): Request {
+    private CreateRequest(url: URL, language: string): Request {
         return new Request(url, {
             headers: {
                 Referer: this.URI.href,
                 Origin: this.URI.origin,
-                'Accept-Language': this.language,
+                'Accept-Language': language,
                 Authorization: this.bearer,
-                'X-Device-Uuid': this.deviceid
+                'X-Device-Uuid': this.deviceId
             }
         });
     }
--- web/src/engine/websites/TappyToon_e2e.ts
+++ web/src/engine/websites/TappyToon_e2e.ts
@@ -1,13 +1,13 @@
-import { TestFixture } from '../../../test/WebsitesFixture';
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+const configEN = {
     plugin: {
         id: 'tappytoon',
         title: 'TappyToon'
     },
     container: {
         url: 'https://www.tappytoon.com/en/book/the-duchess-who-sees-ghosts',
-        id: '345',
+        id: JSON.stringify({ id: '345', language: 'en' }),
         title: 'The Duchess Who Sees Ghosts'
     },
     child: {
@@ -21,4 +21,50 @@ const config = {
     }
 };
 
-new TestFixture(config).AssertWebsite();
+new TestFixture(configEN).AssertWebsite();
+
+const configFR = {
+    plugin: {
+        id: 'tappytoon',
+        title: 'TappyToon'
+    },
+    container: {
+        url: 'https://www.tappytoon.com/fr/book/the-duchess-who-sees-ghosts-fr',
+        id: JSON.stringify({ id: '10345', language: 'fr' }),
+        title: 'The Duchess Who Sees Ghosts [FR]'
+    },
+    child: {
+        id: '957430020',
+        title: 'Épisode 1'
+    },
+    entry: {
+        index: 0,
+        size: 529_830,
+        type: 'image/jpeg'
+    }
+};
+
+new TestFixture(configFR).AssertWebsite();
+
+const configDE = {
+    plugin: {
+        id: 'tappytoon',
+        title: 'TappyToon'
+    },
+    container: {
+        url: 'https://www.tappytoon.com/de/book/the-duchess-who-sees-ghosts-de',
+        id: JSON.stringify({ id: '20345', language: 'de' }),
+        title: 'The Duchess Who Sees Ghosts [DE]'
+    },
+    child: {
+        id: '232906583',
+        title: 'Kapitel 1'
+    },
+    entry: {
+        index: 0,
+        size: 523_689,
+        type: 'image/jpeg'
+    }
+};
+
+new TestFixture(configDE).AssertWebsite();
