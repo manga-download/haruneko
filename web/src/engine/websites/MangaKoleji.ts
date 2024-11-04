7ddbd5fd9f4276edde84d27a23130df2c33d81e9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-04 12:48:03 +0000

    Add requested websites (#862)
    * Add SushiScans (com & fr)
    
    https://github.com/manga-download/hakuneko/issues/7538
    
    * Add dtupscan
    
    https://github.com/manga-download/hakuneko/issues/7539
    
    * Add MangaKoleji
    
    https://github.com/manga-download/hakuneko/issues/7536

---
+++ web/src/engine/websites/MangaKoleji.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './MangaKoleji.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as MangaStream from './decorators/WordPressMangaStream';
+import * as Common from './decorators/Common';
+
+@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
+@MangaStream.MangasSinglePageCSS()
+@MangaStream.ChaptersSinglePageCSS()
+@MangaStream.PagesSinglePageJS()
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('mangakoleji', 'Manga Koleji', 'https://mangakoleji.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
