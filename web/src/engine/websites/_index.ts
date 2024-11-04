00149270b318325304ac45d8e45e35e9e92aef64
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-04 13:15:21 +0100

    Add dtupscan
    https://github.com/manga-download/hakuneko/issues/7539

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -123,6 +123,7 @@ export { default as DoujinZa } from './DoujinZa';
 export { default as DragonTea } from './DragonTea';
 export { default as DragonTranslation } from './DragonTranslation';
 export { default as DrakeScans } from './DrakeScans';
+export { default as DtupScan } from './DtupScan';
 export { default as DuaLeoTruyen } from './DuaLeoTruyen';
 export { default as DynastyScans } from './DynastyScans';
 export { default as EarlyManga } from './EarlyManga';
