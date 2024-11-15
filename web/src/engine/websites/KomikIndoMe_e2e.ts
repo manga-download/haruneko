15a4e8e7b64a3c6c900801dff5de04d2553294fe
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-15 10:12:32 +0100

    update domains

--- web/src/engine/websites/KomikIndoMe_e2e.ts
+++ web/src/engine/websites/KomikIndoMe_e2e.ts
@@ -6,7 +6,7 @@ const config: Config = {
         title: 'KomikIndoMe'
     },
     container: {
-        url: 'https://komikindo.fyi/manga/secret-class/',
+        url: 'https://komikindo.nl/manga/secret-class/',
         id: '/manga/secret-class/',
         title: 'Secret Class'
     },
