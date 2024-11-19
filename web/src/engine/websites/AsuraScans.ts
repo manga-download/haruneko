30c3fcf22c7fece1abb894b4570824366a6f3f61
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-19 14:29:55 +0100

    AsuraScans: fix getting chapters & pages

--- web/src/engine/websites/AsuraScans.ts
+++ web/src/engine/websites/AsuraScans.ts
@@ -1,6 +1,6 @@
 import { Tags } from '../Tags';
 import icon from './AsuraScans.webp';
-import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 import { FetchCSS, FetchWindowScript } from '../platform/FetchProvider';
@@ -20,9 +20,20 @@ function MangaInfoExtractor(anchor: HTMLAnchorElement) {
     };
 }
 
+type JSONChapters = {
+    chapters: {
+        name: number
+    }[]
+}
+
+type JSONPages = {
+    pages: {
+        url: string
+    }[]
+}
+
 @Common.MangasMultiPageCSS('/series?page={page}', 'div.grid a', 1, 1, 0, MangaInfoExtractor)
-@MangaStream.PagesSinglePageCSS(excludes, 'img[alt*="chapter"]')
-@Common.ImageAjax()
+@Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -47,8 +58,40 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters = await FetchCSS<HTMLAnchorElement>(new Request(new URL(manga.Identifier, this.URI)), 'div.scrollbar-thumb-themecolor a.block');
-        return chapters.map(chapter => new Chapter(this, manga, [manga.Identifier, 'chapter', chapter.pathname.match(/(\d+)+$/)[1]].join('/'), chapter.textContent.trim()));
+        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(`${manga.Identifier}`, this.URI)), 'script:not([src])');
+        const { chapters } = this.FindJSONObject<JSONChapters>(scripts, /is_early_access/, 'chapters');
+        return chapters.map(chapter => new Chapter(this, manga, [manga.Identifier, 'chapter', chapter.name.toString()].join('/'), `Chapter ${chapter.name}`));
     }
 
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(`${chapter.Identifier}`, this.URI)), 'script:not([src])');
+        const { pages } = this.FindJSONObject<JSONPages>(scripts, /chapterName/, 'pages');
+        return pages
+            .filter(page => excludes.none(pattern => pattern.test(page.url)))
+            .map(page => new Page(this, chapter, new URL(page.url)));
+    }
+
+    private FindJSONObject<T>(scripts: HTMLScriptElement[], scriptRegex: RegExp, keyName: string, currentElement = undefined): T {
+
+        if (scripts && scriptRegex) {
+            const script = scripts.find(script => scriptRegex.test(script.text))?.text;
+            if (!script) return undefined;
+            //script are like self.__next_f.push([1,"
+            const json = JSON.parse(script.substring(script.indexOf(',"') + 1, script.length - 2));// to remove trailing )]
+            currentElement = JSON.parse(json.substring(json.indexOf(':') + 1));
+            return this.FindJSONObject<T>(undefined, undefined, keyName, currentElement);
+        }
+
+        if (!currentElement) return undefined;
+        if (currentElement[keyName]) {
+            return currentElement;
+        }
+        let result = undefined;
+        for (let i in currentElement) {
+            if (result) break;
+            if (typeof currentElement[i] === 'object')
+                result = result ?? this.FindJSONObject<T>(undefined, undefined, keyName, currentElement[i]);
+        }
+        return result as T;
+    }
 }
