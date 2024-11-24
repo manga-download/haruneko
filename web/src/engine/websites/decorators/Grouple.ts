2127d77d66c82c4229e84ba5e0e8cc29df381e0a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-11-24 12:22:13 +0100

    requested changes

--- web/src/engine/websites/decorators/Grouple.ts
+++ web/src/engine/websites/decorators/Grouple.ts
@@ -1,3 +1,4 @@
+//Glouple.co russian website family : AllHentai, SelfManga, Seimanga, Usagi, ReadManga
 import { Fetch, FetchWindowScript } from '../../platform/FetchProvider';
 import { type MangaScraper, type Chapter, Page } from '../../providers/MangaPlugin';
 import { type Priority } from '../../taskpool/DeferredTask';
@@ -5,25 +6,28 @@ import * as Common from './Common';
 
 const pagesWithServersScript = `
     new Promise(resolve => {
-        const pics = rm_h.pics.map(pic => {
-            const picUrl = new URL(rm_h.reader.preparePicUrl(pic.url.replace(/^\\/\\//, 'https://')));
-            return picUrl.pathname + picUrl.search;
-        });
         const servers =  rm_h.servers.map(server => server.path.replace(/^\\/\\//, 'https://'));
         const mainserver = servers.shift();
-        resolve( pics.map( picture => {
-            const mirrors = servers.map(server => new URL(picture, server).href);
-            return { url : new URL(picture, mainserver).href, mirrors };
+        resolve( rm_h.pics.map(pic => {
+            const picUrl = new URL(rm_h.reader.preparePicUrl(pic.url).replace(/^\\/\\//, 'https://'));
+            const mirrors = servers.map(server => new URL(picUrl.pathname + picUrl.search, server).href);
+            return { url : new URL(picUrl.pathname + picUrl.search, mainserver).href, mirrors };
         }));
-       resolve(pics);
     });
 `;
 
 export const chapterScript = `
     new Promise(resolve => {
         resolve([...document.querySelectorAll('a.chapter-link.cp-l')].map(chapter => {
+            const chapterUrl = new URL(chapter.pathname + chapter.search, window.location.origin);
+            chapterUrl.searchParams.set('mtr', '1');
+
+            for (const bloat of chapter.querySelectorAll('sup')) {
+                if (bloat.parentElement) bloat.parentElement.removeChild(bloat);
+            };
+
             return {
-                id: chapter.pathname + chapter.search,
+                id: chapterUrl.pathname + chapterUrl.search,
                 title: chapter.text.trim(),
             };
         }));
@@ -36,16 +40,16 @@ export const pathMangas = '/list?offset={page}';
 export const pageMangaOffset = 70;
 export const queryPages = [
     'div#all img.img-responsive',
-    'div.text-center img[loading="lazy"]',
+    'div.text-center img[loading="lazy"]'
 ].join(',');
 
 type ImagesData = {
     url: string,
-    mirrors: string[];
+    mirrors: string[]
 }
 
 type PageParameters = {
-    mirrors: string[],
+    mirrors: string[]
 };
 
 /*************************************************
@@ -53,30 +57,18 @@ type PageParameters = {
  *************************************************/
 
 /**
- * Extracts the pages from the HTML page of the given {@link chapter}.
- * @param this - A reference to the {@link MangaScraper}
- * @param chapter - A reference to the {@link Chapter} which contains the pages
- * @param script - A script to extract the pages as \{ url : string, mirrors: string[] \}
- */
-async function FetchPagesSinglePageJS(this: MangaScraper, chapter: Chapter, script: string = pagesWithServersScript, delay: number = 1500): Promise<Page[]> {
-    const uri = new URL(chapter.Identifier, this.URI);
-    uri.searchParams.set('mtr', '1');
-    const images = await FetchWindowScript<ImagesData[]>(new Request(uri), script, delay);
-    return images.map(image => new Page<PageParameters>(this, chapter, new URL(image.url), { mirrors: image.mirrors }));
-}
-/**
  * A class decorator that adds the ability to extract all pages for a given chapter using the given JS {@link script}.
  * The pages are extracted from the composed url based on the `Identifier` of the chapter and the `URI` of the website.
- * @param script - A script to extract the pages as \{ url : string, mirrors: string[] \}
+ * @param script - A script to extract the pages as `{`url: string, mirrors: string[] `}`[]
  * @param delay - An initial delay [ms] before the {@link script} is executed
  */
 export function PagesSinglePageJS(script = pagesWithServersScript, delay = 0) {
     return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
         Common.ThrowOnUnsupportedDecoratorContext(context);
-
         return class extends ctor {
             public async FetchPages(this: MangaScraper, chapter: Chapter): Promise<Page[]> {
-                return FetchPagesSinglePageJS.call(this, chapter, script, delay);
+                const images = await FetchWindowScript<ImagesData[]>(new Request(new URL(chapter.Identifier, this.URI)), script, delay);
+                return images.map(image => new Page<PageParameters>(this, chapter, new URL(image.url), { mirrors: image.mirrors }));
             }
         };
     };
@@ -84,7 +76,7 @@ export function PagesSinglePageJS(script = pagesWithServersScript, delay = 0) {
 
 /**
  * A class decorator that adds the ability to get the image data for a given page by loading the source asynchronous with the `Fetch API`.
- * This method fetch pages by trying different urls. Page must have "mirrors: url,url, url, etc..." in Parameters.
+ * This method fetch pages by trying different urls. Parameters must have `{`mirrors: string[]`}`
  */
 export function ImageAjaxWithMirrors() {
     return function DecorateClass<T extends Common.Constructor>(ctor: T, context?: ClassDecoratorContext): T {
@@ -92,7 +84,7 @@ export function ImageAjaxWithMirrors() {
         return class extends ctor {
             public async FetchImage(this: MangaScraper, page: Page, priority: Priority, signal: AbortSignal): Promise<Blob> {
                 return FetchMirroredImage.call(this, page, priority, signal);
-            }
+            };
         };
     };
 }
