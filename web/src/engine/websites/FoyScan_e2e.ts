4824d4f86020a11fdfafa1d638c2155fe7dee5cc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-12-01 16:17:10 +0000

    Requested websites (#911)
    * Add Grabber
    
    https://github.com/manga-download/hakuneko/issues/7567
    
    * Add DEXHentai
    
    https://github.com/manga-download/hakuneko/issues/7568
    
    * Add FoyScan
    
    https://github.com/manga-download/hakuneko/issues/7578

---
+++ web/src/engine/websites/FoyScan_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'foyscan',
+        title: 'FoyScan'
+    },
+    container: {
+        url: 'https://foyscan.xyz/serie/juego-del-rey',
+        id: '/serie/juego-del-rey',
+        title: 'Juego del Rey'
+    },
+    child: {
+        id: '/serie/juego-del-rey/capitulo-43',
+        title: 'Capítulo 43'
+    },
+    entry: {
+        index: 0,
+        size: 590_342,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
---
+++ web/src/engine/websites/Grabber.ts
@@ -0,0 +1,21 @@
+﻿import { Tags } from '../Tags';
+import icon from './Grabber.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Madara from './decorators/WordPressMadara';
+import * as Common from './decorators/Common';
+
+@Madara.MangaCSS(/^{origin}\/comics\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
+@Madara.MangasMultiPageAJAX()
+@Madara.ChaptersSinglePageAJAXv2('ul li.wp-manga-chapter > a:nth-child(3)')
+@Madara.PagesSinglePageCSS()
+@Common.ImageAjax()
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('grabber', 'Grabber', 'https://grabber.zone', Tags.Media.Comic, Tags.Language.Multilingual, Tags.Source.Aggregator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
