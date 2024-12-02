213533cf61733962182d57f844cead3a84a15ef1
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-12-02 08:53:43 +0000

    Monday maintenance (#912)
    * remove dead websites
    
    wuqimh ; parked domain
    xinaman123 : dead for a while
    
    * websites updates

--- web/src/engine/websites/WebtoonHatti_e2e.ts
+++ web/src/engine/websites/WebtoonHatti_e2e.ts
@@ -6,17 +6,17 @@ const config = {
         title: 'Webtoon Hatti'
     },
     container: {
-        url: 'https://webtoonhatti.com/webtoon/bokutachi-wa-benkyou-ga-dekinai/',
-        id: JSON.stringify({ post: '15001', slug: '/webtoon/bokutachi-wa-benkyou-ga-dekinai/' }),
-        title: 'Bokutachi wa Benkyou ga Dekinai'
+        url: 'https://webtoonhatti.dev/webtoon/suikascinin-kanunlari/',
+        id: JSON.stringify({ post: '22986', slug: '/webtoon/suikascinin-kanunlari/' }),
+        title: 'Suikasçının Kanunları'
     },
     child: {
-        id: '/webtoon/bokutachi-wa-benkyou-ga-dekinai/bolum-109/',
-        title: 'Bölüm 109'
+        id: '/webtoon/suikascinin-kanunlari/bolum-28/',
+        title: 'Bölüm 28'
     },
     entry: {
         index: 0,
-        size: 556_719,
+        size: 1_082_948,
         type: 'image/jpeg'
     }
 };
