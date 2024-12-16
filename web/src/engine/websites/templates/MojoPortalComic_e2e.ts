2f52fce1eabfd6c8fb43ec0cfad560adfce431c1
Ronny <ronny@local.host>
2024-12-16 01:16:20 +0100

    [truyentranhaudioonline] adapt to nettruyen template (mojo portal)

--- web/src/engine/websites/templates/MojoPortalComic_e2e.ts
+++ web/src/engine/websites/templates/MojoPortalComic_e2e.ts
@@ -1,4 +1,6 @@
+import '../MangaBTT_e2e';
 import '../NetTruyenOK_e2e';
 import '../NetTruyenPlus_e2e';
 import '../NetTruyenViet_e2e';
 import '../NetTruyenZZZ_e2e';
+import '../TruyenTranhOnline_e2e';
