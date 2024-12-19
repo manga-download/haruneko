cd19a11102211596fa50ed2ffce193472b8e0497
MikeZeDev <MikeZeDev@users.noreply.github.com>
2024-12-19 17:26:47 +0100

    Add BakaMH
    https://github.com/manga-download/hakuneko/issues/7599

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -38,6 +38,7 @@ export { default as AyaToon } from './AyaToon';
 export { default as AzoraWorld } from './AzoraWorld';
 export { default as BacaKomik } from './BacaKomik';
 export { default as BacaManga } from './BacaManga';
+export { default as BakaMH } from './BakaMH';
 export { default as Baozimh } from './Baozimh';
 export { default as BarManga } from './BarManga';
 export { default as Batoto } from './Batoto';
