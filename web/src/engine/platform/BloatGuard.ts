eb7cb7751844038bac476f7370f37d01ad3a5248
Sheep <sheepux@live.com>
2025-01-02 14:22:27 +0100

    limit facebook to comments plugin

--- web/src/engine/platform/BloatGuard.ts
+++ web/src/engine/platform/BloatGuard.ts
@@ -57,6 +57,6 @@ const patterns = [
     '*://pickupfaxmultitude.com/*',
     '*://t7cp4fldl.com/*',
     '*://tumultmarten.com/*',
-    '*://*.facebook.com/*',
-    '*://*.facebook.net/*',
+    '*://www.facebook.com/*/plugins/comments.php*',
+    '*://www.facebook.net/*/plugins/comments.php*',
 ];
