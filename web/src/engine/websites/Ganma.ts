9aa073c439c7a9415bfdc91bdc7e1f203cc256b0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-03 18:16:36 +0100

    Ganma : use new api (GraphQL)

--- web/src/engine/websites/Ganma.ts
+++ web/src/engine/websites/Ganma.ts
@@ -2,45 +2,137 @@
 import icon from './Ganma.webp';
 import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchJSON } from '../platform/FetchProvider';
+import { FetchGraphQL } from '../platform/FetchProvider';
+import { WebsiteResourceKey as R } from '../../i18n/ILocale';
+import { Exception } from '../Error';
 
-type APIRequest<T> = {
-    success: boolean;
-    root: T
-}
-
-type APIManga = {
-    id: string,
-    title: string,
-    class : string
-}
-
-type APIBox = {
-    boxes: {
-        class: string,
-        id: string,
-        panels: APIManga[]
-    }[]
-}
-
-type APIChapters = {
-    items: {
-        id: string,
+type APIMagazine = {
+    magazine: {
+        magazineId: string,
         title: string,
-        number: number,
-        subtitle: string
-        page: APIPages
-    }[]
+        storyInfos: {
+            edges: {
+                node: APIStory
+            }[]
+        }
+        storyContents: APIPages
+    }
+}
+
+type APIStory = {
+    storyId: string,
+    title: string,
+    subtitle: string | null,
+    isPurchased: boolean
 }
 
 type APIPages = {
-    baseUrl: string,
-    token : string,
-    files: string[]
+    pageImages: {
+        pageCount: number,
+        pageImageBaseURL: string,
+        pageImageSign: string
+    } | undefined
 }
 
-@Common.ImageAjax()
+type APIRanking = {
+    ranking: {
+        totalRanking: {
+            magazineId: string,
+            title: string
+        }[]
+    }
+}
+
+type APIMagazineCompleted = {
+    magazinesByCategory: {
+        magazines: {
+            edges: {
+                node: {
+                    magazineId: string,
+                    title: string
+                }
+            }[]
+        }
+    }
+}
+
+const queryMagazine = `
+    query MagazineDetail($magazineIdOrAlias: String!) {
+      magazine(magazineIdOrAlias: $magazineIdOrAlias) {
+        magazineId
+        title
+        storyInfos(first: 9999) {
+          edges {
+            node {
+              ...MagazineDetailStoryInfo
+            }
+          }
+        }
+      }
+    }
+    fragment MagazineDetailStoryInfo on StoryInfo {
+      storyId
+      title
+      subtitle
+      isPurchased
+    }
+`;
+
+const queryPages = `
+    query magazineStoryForReader($magazineIdOrAlias: String!, $storyId: String!) {
+      magazine(magazineIdOrAlias: $magazineIdOrAlias) {
+        storyContents(storyId: $storyId) {
+          ... on StoryContents {
+            pageImages {
+              pageCount
+              pageImageBaseURL
+              pageImageSign
+            }
+          }
+        }
+      }
+    }
+`;
+
+const queryRanking = `
+    query MagazineRankingTotal {
+      ranking {
+        totalRanking {
+          ...rankingMagazineFragment
+        }
+      }
+    }
+    fragment rankingMagazineFragment on Magazine {
+      magazineId
+      title
+    }
+`;
+
+const queryFinishedMagazines = `
+    query finishedMagazines($after: String) {
+      magazinesByCategory(category: FINISHED) {
+        magazines(after: $after, first: 99999) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          edges {
+            node {
+              magazineId
+              alias
+              title
+            }
+          }
+        }
+      }
+    }
+
+`;
+
+@Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
+
+    private readonly apiUrl = 'https://ganma.jp/api/graphql';
 
     public constructor() {
         super('ganma', `GANMA!`, 'https://ganma.jp', Tags.Language.Japanese, Tags.Media.Manga, Tags.Source.Official);
@@ -51,71 +143,48 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/[^/]+$`).test(url);
+        return new RegExpSafe(`^${this.URI.origin}/web/magazine/[^/]+$`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const request = new Request(new URL('/api/1.0/magazines/web/' + new URL(url).pathname.split('/').at(-1), this.URI), {
-            headers: {
-                'X-From': this.URI.href
-            }
-        });
-        const { root: { id, title } } = await FetchJSON<APIRequest<APIManga>>(request);
-        return new Manga(this, provider, id, title.trim());
+        const mangaId = new URL(url).pathname.split('/').at(-1);
+        const { magazine: { magazineId, title } } = await FetchGraphQL<APIMagazine>(this.CreateRequest(), 'MagazineDetail', queryMagazine, { magazineIdOrAlias: mangaId });
+        return new Manga(this, provider, magazineId, title.trim());
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         return [
             ...await this.GetMangasTop(provider),
             ...await this.GetMangasCompleted(provider)
-        ];
+        ].distinct();
     }
 
     private async GetMangasCompleted(provider: MangaPlugin): Promise<Manga[]> {
-        const request = new Request(new URL('/api/1.1/ranking?flag=Finish', this.URI), {
-            headers: {
-                'X-From': this.URI.href
-            }
-        });
-        const { root } = await FetchJSON<APIRequest<APIManga[]>>(request);
-        return root.map(manga => new Manga(this, provider, manga.id, manga.title.trim()));
+        const { magazinesByCategory: { magazines: { edges } } } = await FetchGraphQL<APIMagazineCompleted>(this.CreateRequest(), 'finishedMagazines', queryFinishedMagazines, {});
+        return edges.map(manga => new Manga(this, provider, manga.node.magazineId, manga.node.title));
     }
 
     private async GetMangasTop(provider: MangaPlugin): Promise<Manga[]> {
-        const request = new Request(new URL('/api/2.2/top', this.URI), {
-            headers: {
-                'X-From': this.URI.href
-            }
-        });
-        const { root: { boxes } } = await FetchJSON<APIRequest<APIBox>>(request);
-        return boxes.reduce((accumulator, box) => {
-            const mangas = box.panels.filter(panel => panel.class === 'CustomTopMagazinePanel')
-                .map(panel => new Manga(this, provider, panel.id, panel.title.trim()));
-            return accumulator.concat(mangas);
-        }, []);
+        const { ranking: { totalRanking } } = await FetchGraphQL<APIRanking>(this.CreateRequest(), 'MagazineRankingTotal', queryRanking, {});
+        return totalRanking.map(manga => new Manga(this, provider, manga.magazineId, manga.title));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const request = new Request(new URL(`/api/1.0/magazines/web/${manga.Identifier}`, this.URI), {
-            headers: {
-                'X-From': this.URI.href
-            }
-        });
-        const { root: { items } } = await FetchJSON<APIRequest<APIChapters>>(request);
-        return items.map((chapter, index) => {
-            const title = ((chapter.number || index + 1) + ': 【' + chapter.title + '】 ' + (chapter.subtitle || '')).trim();
-            return new Chapter(this, manga, chapter.id, title);
-        });
+        const { magazine: { storyInfos: { edges } } } = await FetchGraphQL<APIMagazine>(this.CreateRequest(), 'MagazineDetail', queryMagazine, { magazineIdOrAlias: manga.Identifier });
+        return edges.map(chapter => new Chapter(this, manga, chapter.node.storyId, [chapter.node.title, chapter.node.subtitle || ''].join(' ').trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const request = new Request(new URL(`/api/1.0/magazines/web/${chapter.Parent.Identifier}`, this.URI), {
+        const { magazine: { storyContents: { pageImages } } } = await FetchGraphQL<APIMagazine>(this.CreateRequest(), 'magazineStoryForReader', queryPages, { magazineIdOrAlias: chapter.Parent.Identifier, storyId: chapter.Identifier });
+        if (!pageImages) throw new Exception(R.Plugin_Common_Chapter_UnavailableError);
+        return new Array<Page>(pageImages.pageCount).fill(null).map((_, index) => new Page(this, chapter, new URL(`${pageImages.pageImageBaseURL}${index + 1}.jpg?${pageImages.pageImageSign}`)));
+    }
+
+    private CreateRequest(): Request {
+        return new Request(new URL(this.apiUrl), {
             headers: {
                 'X-From': this.URI.href
             }
         });
-        const { root: { items } } = await FetchJSON<APIRequest<APIChapters>>(request);
-        const pages = items.find(item => item.id === chapter.Identifier).page;
-        return pages.files.map(image => new Page(this, chapter, new URL(`${image}?${pages.token}`, pages.baseUrl)));
     }
 }
--- web/src/engine/websites/Ganma_e2e.ts
+++ web/src/engine/websites/Ganma_e2e.ts
@@ -6,13 +6,13 @@ const config = {
         title: 'GANMA!'
     },
     container: {
-        url: 'https://ganma.jp/kyatapi',
+        url: 'https://ganma.jp/web/magazine/kyatapi',
         id: '5111fc10-5046-11ee-8042-c6601c61f7a6',
         title: 'きゃたぴランド'
     },
     child: {
         id: '8c1ab720-60cc-11ee-a11e-86ad1c7f6bc1',
-        title: '1: 【第1話】 しんそつ！！'
+        title: '第1話 しんそつ！！'
     },
     entry: {
         index: 0,
