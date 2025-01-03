a0518dc4b2b32bdf40e8bf16ce8d43c9103caab3
Ronny <ronny@local.host>
2025-01-03 15:41:54 +0100

    add end-to-end tests set

---
+++ web/src/engine/websites/decorators/MangaReaderCMS_e2e.ts
@@ -0,0 +1,8 @@
+import '../AnzManga_e2e';
+import '../Hanman18_e2e';
+import '../MangaEighteenClub_e2e';
+import '../MangaRaw1001_e2e';
+import '../MangAs_e2e';
+import '../ReadComicOnline_e2e';
+import '../ScanVF_e2e';
+import '../MangaZuki_e2e';
