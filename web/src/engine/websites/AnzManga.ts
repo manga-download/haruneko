55ba8f3e5a7071898204415f1f66c559cdcbf643
Ronny <ronny@local.host>
2025-01-03 17:55:05 +0100

    [anzmanga] revise website

--- web/src/engine/websites/AnzManga.ts
+++ web/src/engine/websites/AnzManga.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
 import icon from './AnzManga.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Common from './decorators/Common';
 import * as MangaReader from './decorators/MangaReaderCMS';
+import * as Common from './decorators/Common';
 
-@Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, MangaReader.queryMangaTitle)
-@MangaReader.MangasSinglePageCSS()
-@Common.ChaptersSinglePageCSS(MangaReader.queryChapters, MangaReader.ChapterInfoExtractor)
-@Common.PagesSinglePageCSS(MangaReader.queryPages, MangaReader.ChapterPageExtractor)
+@Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, MangaReader.queryManga)
+@Common.MangasSinglePagesCSS([ MangaReader.patternMangas ], MangaReader.queryMangas)
+@Common.ChaptersSinglePageCSS('ul.chapters li a')
+@Common.PagesSinglePageCSS('div#all img.img-responsive')
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('anzmanga', `AnzManga`, 'https://www.anzmangashd.com', Tags.Media.Manga, Tags.Language.Spanish, Tags.Source.Aggregator);
+        super('anzmanga', 'AnzManga', 'https://www.anzmangashd.com', Tags.Media.Manga, Tags.Language.Spanish, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
