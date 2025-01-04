84f6cb222d6e193c43d71969e6fbd3d45f378629
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-04 21:37:53 +0100

    Vortex Scans : fix getting images
    and update Hivescans tests
    
    Closes https://github.com/manga-download/haruneko/issues/964

--- web/src/engine/websites/VortexScans.ts
+++ web/src/engine/websites/VortexScans.ts
@@ -1,6 +1,12 @@
 import { Tags } from '../Tags';
+import { FetchCSS } from '../platform/FetchProvider';
+import { Page, type Chapter } from '../providers/MangaPlugin';
 import icon from './VortexScans.webp';
 import { VTheme } from './templates/VTheme';
+
+type JSONImage = {
+    url: string
+}
 
 export default class extends VTheme {
 
@@ -10,5 +16,13 @@ export default class extends VTheme {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(chapter.Identifier, this.URI)), 'script:not([src])');
+        const imagesRegexp = /\\"images\\":(\[.*]),\\"nextChapter/;
+        const goodscript = scripts.find(script => imagesRegexp.test(script.text));
+        const images: JSONImage[] = JSON.parse(goodscript.text.match(imagesRegexp)[1].replaceAll('\\', ''));
+        return images.map(image => new Page(this, chapter, new URL(image.url), { Referer: this.URI.href }));
     }
 }
