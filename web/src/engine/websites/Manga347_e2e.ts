74a831c821cf93932db211c402988819f7e9036d
Ronny <ronny@local.host>
2025-01-06 01:25:16 +0100

    revision of mangareader/mojoportal/liliana templates

--- web/src/engine/websites/Manga347_e2e.ts
+++ web/src/engine/websites/Manga347_e2e.ts
@@ -1,14 +1,14 @@
 import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'manga347',
-        title: 'Manga 347'
+        title: 'Manga 347',
     },
     container: {
         url: 'https://manga347.com/manga/100-000-levels-of-body-refining-all-the-dogs-i-raise-are-the-emperor',
         id: '/manga/100-000-levels-of-body-refining-all-the-dogs-i-raise-are-the-emperor',
-        title: '100,000 Levels of Body Refining : All the dogs I raise are the Emperor'
+        title: '100,000 Levels of Body Refining : All the dogs I raise are the Emperor',
     },
     child: {
         id: '/manga/100-000-levels-of-body-refining-all-the-dogs-i-raise-are-the-emperor/chapter-306',
@@ -17,8 +17,6 @@ const config = {
     entry: {
         index: 0,
         size: 752_237,
-        type: 'image/jpeg'
+        type: 'image/jpeg',
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
