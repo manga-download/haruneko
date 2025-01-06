90e1d1383cb3839eb27a96e6c709d2499aea6fad
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-06 21:41:25 +0100

    requested changes

--- web/src/engine/websites/VortexScans.ts
+++ web/src/engine/websites/VortexScans.ts
@@ -1,5 +1,5 @@
 import { Tags } from '../Tags';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchHTML } from '../platform/FetchProvider';
 import { Page, type Chapter } from '../providers/MangaPlugin';
 import icon from './VortexScans.webp';
 import { VTheme } from './templates/VTheme';
@@ -19,10 +19,8 @@ export default class extends VTheme {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(chapter.Identifier, this.URI)), 'script:not([src])');
-        const imagesRegexp = /\\"images\\":(\[.*]),\\"nextChapter/;
-        const goodscript = scripts.find(script => imagesRegexp.test(script.text));
-        const images: JSONImage[] = JSON.parse(goodscript.text.match(imagesRegexp)[1].replaceAll('\\', ''));
+        const doc = await FetchHTML(new Request(new URL(chapter.Identifier, this.URI)));
+        const images: JSONImage[] = JSON.parse(doc.documentElement.innerHTML.match(/\\"images\\":(\[.*]),\\"nextChapter/)[1].replaceAll('\\', ''));
         return images.map(image => new Page(this, chapter, new URL(image.url), { Referer: this.URI.href }));
     }
 }
