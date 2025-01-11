58f68adf20e9caa3cd7a320f3bb8f0b1352ee5c0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-11 15:50:37 +0000

    ManjaNoon : no more MangaStream (#970)

--- web/src/engine/websites/Manjanoon.ts
+++ web/src/engine/websites/Manjanoon.ts
@@ -1,20 +1,27 @@
 import { Tags } from '../Tags';
 import icon from './Manjanoon.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageJS()
+const pageScripts = `
+    new Promise ( resolve => {
+        const realUrl = document.documentElement.innerHTML.match(/realUrl\\s*=\\s*\`([^$]+)/)[1];
+        resolve( [...document.querySelectorAll('.myImage')].map(image => new URL(image.getAttribute('uid'), realUrl).href));
+    });
+`;
+
+@Common.MangaCSS(/^{origin}\/series\/[^/]+$/, 'div.grid div.grid h1')
+@Common.MangasSinglePagesCSS(['/latest'], 'div.grid a.grid', Common.AnchorInfoExtractor(true))
+@Common.ChaptersSinglePageCSS('div#chapters a', Common.AnchorInfoExtractor(true))
+@Common.PagesSinglePageJS(pageScripts, 500)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('manjanoon', 'Manjanoon', 'https://vrnoin.site', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Arabic, Tags.Source.Scanlator);
+        super('manjanoon', 'Manjanoon', 'https://manjanoon.art', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Arabic, Tags.Source.Scanlator);
     }
     public override get Icon() {
         return icon;
     }
+
 }
