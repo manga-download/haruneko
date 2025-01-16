5c51ee095c7ca2fed448c062d4c5870835460788
Ronny <ronny@local.host>
2025-01-16 07:42:38 +0100

    update website domains and tests

--- web/src/engine/websites/Toonkor.ts
+++ web/src/engine/websites/Toonkor.ts
@@ -25,7 +25,7 @@ const pageScript = `
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('toonkor', `Toonkor`, 'https://toonkor472.com', Tags.Media.Manhwa, Tags.Language.Korean, Tags.Source.Aggregator, Tags.Accessibility.DomainRotation);
+        super('toonkor', `Toonkor`, 'https://toonkor473.com', Tags.Media.Manhwa, Tags.Language.Korean, Tags.Source.Aggregator, Tags.Accessibility.DomainRotation);
     }
 
     public override get Icon() {
