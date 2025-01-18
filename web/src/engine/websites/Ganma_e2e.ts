3d8a97970286a17b396602dc72f6aa80ee177e26
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-18 10:54:02 +0100

    update test

--- web/src/engine/websites/Ganma_e2e.ts
+++ web/src/engine/websites/Ganma_e2e.ts
@@ -1,6 +1,6 @@
 ﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'ganma',
         title: 'GANMA!'
@@ -19,6 +19,4 @@ const config = {
         size: 910_108,
         type: 'image/jpeg'
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
