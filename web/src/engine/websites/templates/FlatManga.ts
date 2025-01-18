6f9132d0bfee44e7a9d4ddf48cb23017784f8bf8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-18 18:40:20 +0100

    requested changes

--- web/src/engine/websites/templates/FlatManga.ts
+++ web/src/engine/websites/templates/FlatManga.ts
@@ -1,18 +1,7 @@
-﻿import { AddAntiScrapingDetection, FetchRedirection } from "../../platform/AntiScrapingDetection";
-import { FetchCSS } from "../../platform/FetchProvider";
+﻿import { FetchCSS } from "../../platform/FetchProvider";
 import { DecoratableMangaScraper, type Chapter, type MangaScraper } from "../../providers/MangaPlugin";
 import { Page } from "../../providers/MangaPlugin";
 import * as Common from '../decorators/Common';
-
-AddAntiScrapingDetection(async (invoke) => {
-    const result = await invoke<boolean>(`document.documentElement.innerHTML.includes('ct_anti_ddos_key')`);// Sample => Mangagun, NicoManga, Rawinu, Weloma, WeloveManga
-    if (result) {
-        await new Promise(resolve => setTimeout(resolve, 3000));
-        return FetchRedirection.Automatic;
-    } else return undefined;
-
-});
-
 export function MangaLabelExtractor(element: HTMLElement) {
     return CleanTitle(element.getAttribute('text') ? element.getAttribute('text') : element.textContent);
 }
@@ -32,65 +21,52 @@ export function CleanTitle(title: string): string {
 }
 
 export function PageLinkExtractor<E extends HTMLImageElement>(this: MangaScraper, element: E): string {
-    let page = element.dataset.aload || element.dataset.src || element.dataset.srcset || element.dataset.original || element.dataset.pagespeedLazySrc || element.src;
+    let page = element.dataset.src || element.dataset.srcset || element.dataset.original || element.src;
     try {
         page = window.atob(page);
     } catch { }
     return page.replace(/\n/g, '');
-
 }
 
 export const pathSinglePageManga = '/manga-list.html?listType=allABC';
 const pathMultiPageManga = '/manga-list.html?page={page}';
 
 export const queryMangaTitle = [
-    'li:last-of-type span[itemprop="name"]',
     'ul.manga-info h3',
     'ul.manga-info h1',
 ].join(',');
 
-export const queryMangas = [
-    'span[data-toggle="mangapop"] a',
-    'div.media h3.media-heading a',
-    'div.container a[data-toggle="mangapop"]',
-    'div.card-body div.series-title a'
-].join(',');
+export const queryMangas = 'div.series-title a';
+const queryChapters = 'ul.list-chapters > a';
+export const queryPages = 'img.chapter-img';
 
-const queryChapters = [
-    'div#tab-chapper table tr td a.chapter',
-    'ul.list-chapters > a',
-    'div#tab-chapper div#list-chapters span.title a.chapter'
-].join(',');
-
-export const queryPages = [
-    'img.chapter-img',
-    'div#chapter-content img',
-    'div.chapter-content img'
-].join(',');
-
-export const chapterScript = `
-    [...document.querySelectorAll('ul.list-chapters > a')].map(chapter => {
-        return {
-            id: chapter.pathname,
-            title : chapter.title.trim()
-        };
-    });
+export function ChapterScript(query: string = queryChapters): string {
+    return `
+        [...document.querySelectorAll('${query}')].map(chapter => {
+            return {
+                id: chapter.pathname,
+                title : chapter.title.trim()
+            };
+        });
 `;
+}
 
-export const pageScript = `
-    new Promise(resolve => {
-        const images = [...document.querySelectorAll('${queryPages}')]
-            .filter(element => element.alt != 'olimposcans.com' && !element.alt.includes('nicoscan'))
-            .map(element => {
-                const page = element.dataset.aload || element.dataset.src || element.dataset.srcset || element.dataset.original || element.dataset.pagespeedLazySrc || element.src;
-                try {
-                    page = window.atob(page);
-                } catch {}
-                return page.replace(/\\n/g, '');
-            });
-        resolve(images);
-    });
-`;
+export function PageScript(query: string = queryPages): string {
+    return `
+        new Promise(resolve => {
+            const images = [...document.querySelectorAll('${query}')]
+                .filter(element => element.alt != 'olimposcans.com' && !element.alt.includes('nicoscan'))
+                .map(element => {
+                    const page = element.dataset.src || element.dataset.srcset || element.dataset.original || element.src;
+                    try {
+                        page = window.atob(page);
+                    } catch {}
+                    return page.replace(/\\n/g, '');
+                });
+            resolve(images);
+        });
+    `;
+}
 
 @Common.MangaCSS(/^{origin}\/[^/]+\.html$/, queryMangaTitle, MangaLabelExtractor)
 @Common.MangasMultiPageCSS(pathMultiPageManga, queryMangas, 1, 1, 0, MangaExtractor)
