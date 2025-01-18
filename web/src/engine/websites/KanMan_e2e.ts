58ff560863e23cbe5d9e84949909c0c6ed0e7dba
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-01-18 12:55:50 +0100

    Update KanMan_e2e.ts

--- web/src/engine/websites/KanMan_e2e.ts
+++ web/src/engine/websites/KanMan_e2e.ts
@@ -1,6 +1,6 @@
 import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'kanman',
         title: '看漫画 (KanMan)'
@@ -19,6 +19,4 @@ const config = {
         size: 246_994,
         type: 'image/webp'
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
