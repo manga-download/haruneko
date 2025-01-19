4b84b2a3944d5517f3b502379bf89691f32f2e6e
Ronny <ronny@local.host>
2025-01-19 15:01:16 +0100

    [flatmanga] add test suite

---
+++ web/src/engine/websites/templates/FlatManga_e2e.ts
@@ -0,0 +1,7 @@
+import '../HolyManga_e2e';
+import '../KLManga_e2e';
+import '../MangaGun_e2e';
+import '../ManhwaFullNet_e2e';
+import '../RawInu_e2e';
+import '../WeLoMa_e2e';
+import '../WeLoveManga_e2e';
