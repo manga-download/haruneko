139eef7602cb31bca8954d67ada55ca524ca1a06
Ronny <ronny@local.host>
2025-01-20 00:18:15 +0100

    [flatmanga] update test suite

--- web/src/engine/websites/templates/FlatManga_e2e.ts
+++ web/src/engine/websites/templates/FlatManga_e2e.ts
@@ -2,6 +2,7 @@ import '../HolyManga_e2e';
 import '../KLManga_e2e';
 import '../MangaGun_e2e';
 import '../ManhwaFullNet_e2e';
+import '../NicoManga_e2e';
 import '../RawInu_e2e';
 import '../WeLoMa_e2e';
 import '../WeLoveManga_e2e';
