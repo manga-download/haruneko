453902c4791ebe01daac9d1fa3b8004d5c7de7f4
Ronny <ronny@local.host>
2025-01-26 21:15:45 +0100

    [nyxscans] use array type declaration

--- web/src/engine/websites/NyxScans.ts
+++ web/src/engine/websites/NyxScans.ts
@@ -4,9 +4,9 @@ import { FetchRegex } from '../platform/FetchProvider';
 import { Page, type Chapter } from '../providers/MangaPlugin';
 import { VTheme } from './templates/VTheme';
 
-type JSONImage = {
+type JSONImages = {
     url: string
-}
+}[]
 
 export default class extends VTheme {
 
@@ -19,8 +19,8 @@ export default class extends VTheme {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const [data] = await FetchRegex(new Request(new URL(chapter.Identifier, this.URI)), /\\"images\\":(\[.*]),\\"nextChapter/g);
-        const images: JSONImage[] = JSON.parse(data.replaceAll('\\', ''));
+        const [ data ] = await FetchRegex(new Request(new URL(chapter.Identifier, this.URI)), /\\"images\\":(\[.*]),\\"nextChapter/g);
+        const images: JSONImages = JSON.parse(data.replaceAll('\\', ''));
         return images.map(image => new Page(this, chapter, new URL(image.url), { Referer: this.URI.href }));
     }
 }
