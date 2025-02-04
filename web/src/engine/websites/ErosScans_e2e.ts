fa6c3e068a48b5a9c21d4a62b77f265e7a79301d
Ronny <ronny@local.host>
2025-02-04 19:18:45 +0100

    Revert "Remove ErosScans, add DoomicScans (#1003)"
    This reverts commit 4bd5f7582ca7b11f36e47817ff25926e3ccc13d9.

--- web/src/engine/websites/ErosScans_e2e.ts
+++ web/src/engine/websites/ErosScans_e2e.ts
@@ -2,11 +2,11 @@
 
 new TestFixture({
     plugin: {
-        id: 'doomicscans',
-        title: 'Doomic Scans'
+        id: 'erosscans',
+        title: 'Eros Scans'
     },
     container: {
-        url: 'https://doomcomic.xyz/manga/absolute-sword-sense/',
+        url: 'https://erosmanga.xyz/manga/absolute-sword-sense/',
         id: '/manga/absolute-sword-sense/',
         title: 'Absolute Sword Sense'
     },
@@ -16,7 +16,7 @@ new TestFixture({
     },
     entry: {
         index: 1,
-        size: 1_395_498,
-        type: 'image/jpeg'
+        size: 994_822,
+        type: 'image/webp'
     }
 }).AssertWebsite();
