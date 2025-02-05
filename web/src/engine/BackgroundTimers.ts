b00620a4cb3f1f2a3280e06acf4bf34de167d172
Ronny <ronny@local.host>
2025-02-05 02:21:25 +0100

    prepared background timers

--- web/src/engine/BackgroundTimers.ts
+++ web/src/engine/BackgroundTimers.ts
@@ -1,19 +1,74 @@
-//const worker = new Worker(new URL('./BackgroundTimersWorker.ts', import.meta.url));
+type Action = () => void;
+type Payload = { action: string } & Record<string, any>;
 
-export function SetTimeout(callback: () => void, ms: number): number {
-    //worker.onmessage = event => console.log(event);
-    //worker.postMessage('...', );
-    return window.setTimeout(callback, ms);
+const worker = new Worker(new URL('./BackgroundTimersWorker.ts', import.meta.url), { type: 'module' });
+const timeoutCallbacks = new Map<number, Action>();
+const intervalCallbacks = new Map<number, Action>();
+
+function GenerateUID() {
+    return `${Date.now()}${Math.random()}`;
 }
 
-export function ClearTimeout(id: number): void {
-    window.clearTimeout(id);
+worker.addEventListener('message', event => {
+    switch(event.data.action) {
+        case 'BackgroundTimers::TickTimeout': return TickTimeout(event.data.timerID);
+        case 'BackgroundTimers::TickInterval': return TickInterval(event.data.timerID);
+    }
+});
+
+function TickTimeout(timerID: number): void {
+    console.log(new Date().toISOString(), 'TickTimeout', timerID, timeoutCallbacks.get(timerID));
+    if(timeoutCallbacks.has(timerID)) {
+        timeoutCallbacks.get(timerID).call(undefined);
+        timeoutCallbacks.delete(timerID);
+    }
 }
 
-export function SetInterval(callback: () => void, ms: number): number {
-    return window.setInterval(callback, ms);
+function TickInterval(timerID: number): void {
+    console.log(new Date().toISOString(), 'TickInterval', timerID, intervalCallbacks.get(timerID));
+    if(intervalCallbacks.has(timerID)) {
+        intervalCallbacks.get(timerID).call(undefined);
+    }
 }
 
-export function ClearInterval(id: number): void {
-    window.clearInterval(id);
+export function SetTimeout(callback: Action, ms: number): Promise<number> {
+    return new Promise<number>(resolve => {
+        const _uid = GenerateUID();
+        function responseListener(event: MessageEvent<Payload>) {
+            console.log(new Date().toISOString(), event.data);
+            if(event.data.action === _uid) {
+                worker.removeEventListener('message', responseListener);
+                timeoutCallbacks.set(event.data.timerID, callback);
+                resolve(event.data.timerID);
+            }
+        }
+        worker.addEventListener('message', responseListener);
+        worker.postMessage({ action: 'Worker::SetTimeout', ms, _uid });
+    });
+}
+
+export function ClearTimeout(timerID: number): void {
+    timeoutCallbacks.delete(timerID);
+    worker.postMessage({ action: 'Worker::ClearTimeout', timerID });
+}
+
+export function SetInterval(callback: Action, ms: number): Promise<number> {
+    return new Promise<number>(resolve => {
+        const _uid = GenerateUID();
+        function responseListener(event: MessageEvent<Payload>) {
+            console.log(new Date().toISOString(), event.data);
+            if(event.data.action === _uid) {
+                worker.removeEventListener('message', responseListener);
+                intervalCallbacks.set(event.data.timerID, callback);
+                resolve(event.data.timerID);
+            }
+        }
+        worker.addEventListener('message', responseListener);
+        worker.postMessage({ action: 'Worker::SetInterval', ms, _uid });
+    });
+}
+
+export function ClearInterval(timerID: number): void {
+    intervalCallbacks.delete(timerID);
+    worker.postMessage({ action: 'Worker::ClearInterval', timerID });
 }
--- web/src/engine/BackgroundTimersWorker.ts
+++ web/src/engine/BackgroundTimersWorker.ts
@@ -1,7 +1,35 @@
-/*
-self.onmessage = (event: MessageEvent) => {
-    setTimeout(() => {
-        self.postMessage('Timeout completed');
-    }, event.data);
-};
-*/
+/// <reference lib="webworker" />
+
+type Payload = { action: string } & Record<string, any>;
+
+addEventListener('message', (event: MessageEvent<Payload>) => {
+    const payload = event.data;
+    switch(payload.action) {
+        case 'Worker::SetTimeout': return SetTimeout(payload._uid, payload.ms);
+        case 'Worker::ClearTimeout': return ClearTimeout(payload.timerID);
+        case 'Worker::SetInterval': return SetInterval(payload._uid, payload.ms);
+        case 'Worker::ClearInterval': return ClearInterval(payload.timerID);
+    }
+});
+
+function SetTimeout(uid: string, ms: number): void {
+    console.log(new Date().toISOString(), 'SetTimeout', uid, ms);
+    const timerID = setTimeout(() => postMessage({ action: 'BackgroundTimers::TickTimeout', timerID }), ms);
+    postMessage({ action: uid, timerID });
+}
+
+function ClearTimeout(timerID: number): void {
+    console.log(new Date().toISOString(), 'ClearTimeout', timerID);
+    clearTimeout(timerID);
+}
+
+function SetInterval(uid: string, ms: number): void {
+    console.log(new Date().toISOString(), 'SetInterval', uid, ms);
+    const timerID = setInterval(() => postMessage({ action: 'BackgroundTimers::TickInterval', timerID }), ms);
+    postMessage({ action: uid, timerID });
+}
+
+function ClearInterval(timerID: number): void {
+    console.log(new Date().toISOString(), 'ClearInterval', timerID);
+    clearInterval(timerID);
+}
