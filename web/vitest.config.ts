80c1c359ed6fa6873fc23a4d964c3c5eabd5d2ab
Ronny <ronny@local.host>
2025-02-07 01:15:59 +0100

    introduce vitest web-worker mock

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
     test: {
         include: [ '**/*[_.](test|spec).[jt]s(x)?' ],
         reporters: [ process.env.GITHUB_ACTIONS ? 'github-actions' : 'default' ],
+        setupFiles: [ '@vitest/web-worker' ],
     },
 });
