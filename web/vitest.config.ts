a3e8e6d23ae559579aa2d7748936594fcc486964
Ronny <ronny@local.host>
2025-02-08 01:40:17 +0100

    add mock via setup

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+        setupFiles: './vitest.setup.ts',
         include: [ '**/*[_.](test|spec).[jt]s(x)?' ],
         reporters: [ process.env.GITHUB_ACTIONS ? 'github-actions' : 'default' ],
-        setupFiles: [ '@vitest/web-worker' ],
     },
 });
