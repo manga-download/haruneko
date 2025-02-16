666a3c7e73d9a7488bc6ee3f9d365dbc07f6e9d5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-02-16 15:58:43 +0100

    default to name in case rus_name is null

--- web/src/engine/websites/MangaLib.ts
+++ web/src/engine/websites/MangaLib.ts
@@ -24,7 +24,8 @@ type APIResult<T> = {
 
 type APIManga = {
     id: number,
-    rus_name: string,
+    name: string,
+    rus_name: string | null,
     slug_url: string
 }
 
@@ -73,8 +74,8 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const mangaSlug = new URL(url).pathname.split('/').at(-1);
-        const { data: { slug_url, rus_name } } = await FetchJSON<APIResult<APIManga>>(new Request(new URL(`./manga/${mangaSlug}`, this.apiUrl)));
-        return new Manga(this, provider, slug_url, rus_name);
+        const { data: { slug_url, rus_name, name } } = await FetchJSON<APIResult<APIManga>>(new Request(new URL(`./manga/${mangaSlug}`, this.apiUrl)));
+        return new Manga(this, provider, slug_url, rus_name || name);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
@@ -83,7 +84,7 @@ export default class extends DecoratableMangaScraper {
             const url = new URL(`./manga?page=${page}&site_id[]=${this.siteId}`, this.apiUrl);
             await Delay(500);
             const { data, meta: { has_next_page } } = await FetchJSON<APIResult<APIManga[]>>(new Request(url));
-            mangaList.push(...data.map(manga => new Manga(this, provider, manga.slug_url, manga.rus_name.trim())));
+            mangaList.push(...data.map(manga => new Manga(this, provider, manga.slug_url, manga.rus_name || manga.name)));
             if (!has_next_page) break;
         }
         return mangaList;
