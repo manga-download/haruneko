2679ef88f30307ccbab5d2a39f44bfc84fc6848e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-02-17 11:02:44 +0100

    monday maintenance
    * Remove Mangayaro (now unrelated website)
    * Remove LynxScans (dead for a while)
    * Rebrand KingOfManga to Shojoverse

---
+++ web/src/engine/websites/Shojoverse_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'shojoverse',
+        title: 'Shojoverse'
+    },
+    container: {
+        url: 'https://shojoverse.com/manga/reality-quest/',
+        id: '/manga/reality-quest/',
+        title: 'The Tale of the Tiger Girl',
+    },
+    child: {
+        id: '/reality-quest-chapter-152/',
+        title: 'Chapter 152'
+    },
+    entry: {
+        index: 0,
+        size: 311_196,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
