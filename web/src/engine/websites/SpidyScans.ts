d8a4e19b2f12dbbb65a205d76d542d4af749512c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-02-18 14:24:07 +0100

    Fix Spider/Spidy scans mess

--- web/src/engine/websites/SpidyScans.ts
+++ web/src/engine/websites/SpidyScans.ts
@@ -1,9 +1,8 @@
 import { Tags } from '../Tags';
-import icon from './SpiderScans.webp';
+import icon from './SpidyScans.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
-import { FetchWindowScript } from '../platform/FetchProvider';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
 @Madara.MangasMultiPageAJAX()
@@ -13,15 +12,10 @@ import { FetchWindowScript } from '../platform/FetchProvider';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('spiderscans', 'Spider Scans', 'https://spiderscans.xyz', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+        super('spidyscans', 'Spidy Scans', 'https://spidyscans.xyz', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
         return icon;
     }
-
-    public override async Initialize(): Promise<void> {
-        return FetchWindowScript(new Request(new URL(this.URI)), `window.cookieStore.set('wpmanga-adult', '1')`);
-    }
-
 }
