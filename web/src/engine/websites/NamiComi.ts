afa79a43e0c9deeb943a56e2d4f056e885e02d46
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-02-22 14:59:15 +0100

    Namicomi : add language tag to chapters
    * fix chapter naming scheme
    * fix getting manga list with missing titles

--- web/src/engine/websites/NamiComi.ts
+++ web/src/engine/websites/NamiComi.ts
@@ -38,6 +38,26 @@ type APIPages = {
     }[]
 }
 
+const chapterLanguageMap = new Map([
+    ['ar', Tags.Language.Arabic],
+    ['de', Tags.Language.German],
+    ['en', Tags.Language.English],
+    ['es-419', Tags.Language.Spanish],
+    ['es-es', Tags.Language.Spanish],
+    ['fr', Tags.Language.French],
+    ['id', Tags.Language.Indonesian],
+    ['it', Tags.Language.Italian],
+    ['ja', Tags.Language.Japanese],
+    ['ko', Tags.Language.Korean],
+    ['pl', Tags.Language.Polish],
+    ['pt-br', Tags.Language.Portuguese],
+    ['pt-t', Tags.Language.Portuguese],
+    ['ru', Tags.Language.Russian],
+    ['tr', Tags.Language.Turkish],
+    ['zh-hans', Tags.Language.Chinese],
+    ['zh-hant', Tags.Language.Chinese]
+]);
+
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -57,34 +77,34 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const id = url.split('/').at(-2);
-        const request = new Request(new URL(`/title/${id}`, this.apiUrl));
-        const { data } = await FetchJSON<APIResult<APIManga>>(request);
-        const title = (data.attributes.title[data.attributes.originalLanguage] || data.attributes.title.en).trim();
+        const { data } = await FetchJSON<APIResult<APIManga>>(new Request(new URL(`/title/${id}`, this.apiUrl)));
+        const title = (data.attributes.title[data.attributes.originalLanguage] || data.attributes.title.en || Object.values(data.attributes.title).shift()).trim();
         return new Manga(this, provider, data.id, title);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const request = new Request(new URL(`/title/search?limit=9999&offset=0`, this.apiUrl));
-        const { data } = await FetchJSON<APIResult<APIManga[]>>(request);
+        const { data } = await FetchJSON<APIResult<APIManga[]>>(new Request(new URL(`/title/search?limit=9999&offset=0`, this.apiUrl)));
         return data.map(item => {
-            const title = (item.attributes.title[item.attributes.originalLanguage] || item.attributes.title.en).trim();
+            const title = (item.attributes.title[item.attributes.originalLanguage] || item.attributes.title.en || Object.values(item.attributes.title).shift()).trim();
             return new Manga(this, provider, item.id, title);
         });
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const request = new Request(new URL(`/chapter?titleId=${manga.Identifier}&limit=9999&offset=0`, this.apiUrl));
-        const { data } = await FetchJSON<APIResult<APIChapter[]>>(request);
+        const { data } = await FetchJSON<APIResult<APIChapter[]>>(new Request(new URL(`/chapter?titleId=${manga.Identifier}&limit=9999&offset=0`, this.apiUrl)));
         return data.map(item => {
-            let title = item.attributes.name ? `${item.attributes.chapter} ${item.attributes.name}` : `Volume ${item.attributes.volume} Episode ${item.attributes.chapter}`;
+            let title = item.attributes.volume ? `Volume ${item.attributes.volume} ` : '';
+            title += item.attributes.chapter ? `Chapter ${item.attributes.chapter} ` : '';
+            title += item.attributes.name ? `${item.attributes.name} ` : '';
+            title = title.trim() || 'Oneshot';
             title = [title, `[${item.attributes.translatedLanguage}]`].join(' ').trim();
-            return new Chapter(this, manga, item.id, title);
+            return new Chapter(this, manga, item.id, title,
+                ...chapterLanguageMap.has(item.attributes.translatedLanguage) ? [chapterLanguageMap.get(item.attributes.translatedLanguage)] : []);
         });
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const request = new Request(new URL(`/images/chapter/${chapter.Identifier}?newQualities=true`, this.apiUrl));
-        const { data } = await FetchJSON<APIResult<APIPages>>(request);
+        const { data } = await FetchJSON<APIResult<APIPages>>(new Request(new URL(`/images/chapter/${chapter.Identifier}?newQualities=true`, this.apiUrl)));
         return data.high.map(item => {
             const url = new URL(`/chapter/${chapter.Identifier}/${data.hash}/high/${item.filename}`, data.baseUrl);
             return new Page(this, chapter, url);
