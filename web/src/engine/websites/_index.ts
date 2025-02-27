f2cd03a1d1568f48801cd8cb0c435eb842a16b23
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-02-27 19:20:57 +0000

    Remove HikariScans (#1044)
    shutdown

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -196,7 +196,6 @@ export { default as HentaiWebtoon } from './HentaiWebtoon';
 export { default as HentaiZone } from './HentaiZone';
 export { default as HerosWeb } from './HerosWeb';
 export { default as HeroTruyen } from './HeroTruyen';
-export { default as HikariScan } from './HikariScan';
 export { default as Hinapyon } from './Hinapyon';
 export { default as HiperCool } from './HiperCool';
 export { default as Hiperdex } from './Hiperdex';
