a53523184131a039d71bd8167af0c8d7b3bf0c16
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-03-01 17:33:03 +0100

    use last fix

--- web/src/engine/websites/MangaGo.d.ts
+++ web/src/engine/websites/MangaGo.d.ts
@@ -1,10 +1,8 @@
-import type { Page} from '../providers/MangaPlugin';
-import { DecoratableMangaScraper, type Chapter } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
 import type { Priority } from '../taskpool/DeferredTask';
 export default class extends DecoratableMangaScraper {
     constructor();
     get Icon(): any;
-    FetchPages(chapter: Chapter): Promise<Page[]>;
-    private FetchPagesData;
+    Initialize(): Promise<void>;
     FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob>;
 }
