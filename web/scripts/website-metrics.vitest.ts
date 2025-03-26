4f4c6d2ff7985f4de634e55b6994c6a0d9a7b524
Ronny <ronny@local.host>
2025-03-26 21:02:28 +0100

    fix pathes for website status/metric test

--- web/scripts/website-metrics.vitest.ts
+++ web/scripts/website-metrics.vitest.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        setupFiles: '/web/vitest.setup.ts',
-        include: [ '/web/website-metrics.ts' ],
+        setupFiles: 'web/vitest.setup.ts',
+        include: [ 'web/scripts/website-metrics.ts' ],
         reporters: [ process.env.GITHUB_ACTIONS ? 'github-actions' : 'dot' ],
     },
 });
