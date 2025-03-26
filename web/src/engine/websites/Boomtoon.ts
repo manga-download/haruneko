3bd4d08e72115be962bcee7ced74a9f280a1713d
Ronny <ronny@local.host>
2025-03-26 08:07:03 +0100

    [delitoon] revision of balcony requests and decryption

--- web/src/engine/websites/Boomtoon.ts
+++ web/src/engine/websites/Boomtoon.ts
@@ -5,7 +5,7 @@ import { DelitoonBase } from './templates/DelitoonBase';
 export default class extends DelitoonBase {
     public constructor() {
         super('boomtoon', `Boomtoon`, 'https://www.boomtoon.com', Tags.Language.Thai, Tags.Media.Manhwa, Tags.Source.Official);
-        this.BalconyID = 'BOOMTOON_COM';
+        this.balconyID = 'BOOMTOON_COM';
     }
 
     public override get Icon() {
