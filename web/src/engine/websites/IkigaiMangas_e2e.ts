2dc1e3cdb18100d3f373c9dc9934b4c1b6a10650
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-03-31 11:56:41 +0200

    update more domains

--- web/src/engine/websites/IkigaiMangas_e2e.ts
+++ web/src/engine/websites/IkigaiMangas_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Ikigai Mangas'
     },
     container: {
-        url: 'https://visualikigai.colombiasinpalabras.com/series/el-sacerdote-sanador-del-sol/',
+        url: 'https://visualikigai.eltanews.com/series/el-sacerdote-sanador-del-sol/',
         id: '/series/el-sacerdote-sanador-del-sol/',
         title: 'El Sacerdote Sanador del Sol'
     },
