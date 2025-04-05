c388fe8e684ea656379e82a20f40b84749b17e71
Ronny <ronny@local.host>
2025-04-05 13:01:14 +0200

    [japscan] update image acquisation

--- web/src/engine/websites/JapScan.d.ts
+++ web/src/engine/websites/JapScan.d.ts
@@ -1,10 +1,8 @@
 import type { Chapter, Page } from '../providers/MangaPlugin';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import type { Priority } from '../taskpool/TaskPool';
 export default class extends DecoratableMangaScraper {
     constructor();
     get Icon(): string;
     Initialize(): Promise<void>;
     FetchPages(chapter: Chapter): Promise<Page[]>;
-    FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob>;
 }
