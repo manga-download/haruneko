80a9c03a48f785de47922b9c6a9336945e44dbb8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-05 13:29:29 +0200

    Update ReadComicOnline.ts

--- web/src/engine/websites/ReadComicOnline.ts
+++ web/src/engine/websites/ReadComicOnline.ts
@@ -4,44 +4,20 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchWindowScript } from '../platform/FetchProvider';
 
-function ChapterExtractor(anchor: HTMLAnchorElement) {
-    const link = new URL(anchor.href);
-    link.searchParams.set('readType', '1');
-    link.searchParams.set('quality', 'hq');
-    return {
-        id: link.pathname + link.search,
-        title: anchor.text.trim()
-    };
-}
-
 const pageScript = `
-    new Promise( (resolve, reject) => {
-        const images = document.querySelectorAll('#divImage img');
-        LoadNextPages(images.length);
-        const start = Date.now();
-        const interval = setInterval(function () {
-            try {
-                if ([...images].at(-1).style.backgroundImage != '') {
-                    clearInterval(interval);
-                    resolve([ ...images ].map(img => img.style.backgroundImage.slice(4, -1).replace(/["']/g, '').replace('=s1600?', '=s0?')));
-                }
-            } catch (error) {
-                clearInterval(interval);
-                reject(error);
-            } finally {
-                if(Date.now() - start > 10_000) {
-                    clearInterval(interval);
-                    reject(new Error('Unable to get pictures after more than 10 seconds !'));
-                }
-            }
-        }, 1000);
+    new Promise( resolve => {
+        const regexp = /\\.attr\\('src',\\s*([^\\(]+)\\(([^\\[]+)\\[currImage\\]\\)/;
+        const matches = document.documentElement.innerHTML.match(regexp);
+        const imageArray = eval(matches[2]);
+        const decodingFunc = eval(matches[1]);
+        resolve( imageArray.map(image => decodingFunc(image) ));
     });
 `;
 
 @Common.MangaCSS(/^{origin}\/Comic\/[^/]+$/, 'div.barContent a.bigChar')
 @Common.MangasMultiPageCSS('/ComicList?page={page}', '.list-comic .item > a')
-@Common.ChaptersSinglePageCSS('div.episodeList table.listing tr td:first-of-type a, div.section ul.list li a', ChapterExtractor)
-@Common.PagesSinglePageJS(pageScript, 2500)
+@Common.ChaptersSinglePageCSS('div.episodeList table.listing tr td:first-of-type a, div.section ul.list li a')
+@Common.PagesSinglePageJS(pageScript, 1000)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
