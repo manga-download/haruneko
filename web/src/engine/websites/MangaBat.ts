b9ce445b9a10b1ab155c538a8cd1691877968acd
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-12 15:16:09 +0200

    Update MangaBat.ts

--- web/src/engine/websites/MangaBat.ts
+++ web/src/engine/websites/MangaBat.ts
@@ -5,7 +5,7 @@ import { MangaNel } from './templates/MangaNel';
 export default class extends MangaNel {
 
     public constructor() {
-        super('mangabat', `MangaBat`, 'https://www.mangabats.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator, Tags.Media.Manga);
+        super('mangabat', `MangaBat`, 'https://www.mangabats.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
