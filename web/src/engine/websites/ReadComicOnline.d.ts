337944df1572ce46ed0d905d25c9f16307a0dd20
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-14 19:03:42 +0200

    ReadComicOnline : fix getting pages (again)

---
+++ web/src/engine/websites/ReadComicOnline.d.ts
@@ -0,0 +1,6 @@
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+export default class extends DecoratableMangaScraper {
+    constructor();
+    get Icon(): any;
+    Initialize(): Promise<void>;
+}
