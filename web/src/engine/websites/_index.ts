fda95248942d23f252ce7221abead2f974bf8d5a
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-15 12:01:27 +0200

    Remove Yurineko
    Website closed https://github.com/keiyoushi/extensions-source/issues/8481

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -753,7 +753,6 @@ export { default as Ynjn } from './Ynjn';
 export { default as YomuComics } from './YomuComics';
 export { default as YumeKomik } from './YumeKomik';
 export { default as YuraManga } from './YuraManga';
-export { default as Yurineko } from './Yurineko';
 export { default as YuriVerso } from './YuriVerso';
 export { default as Zebrack } from './Zebrack';
 export { default as ZenithScans } from './ZenithScans';
