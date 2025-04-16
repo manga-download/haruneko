a17d36efcb0df26ebbfacec667618d1f8553ffc7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-16 13:13:47 +0200

    fix of the fix
    i know you are watching, RCO.  xoxo

--- web/src/engine/websites/ReadComicOnline.d.ts
+++
@@ -1,6 +0,0 @@
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-export default class extends DecoratableMangaScraper {
-    constructor();
-    get Icon(): any;
-    Initialize(): Promise<void>;
-}
