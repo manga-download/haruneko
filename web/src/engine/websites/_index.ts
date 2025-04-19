1f7050d8f0eef6fa19a92878fcbaf4d0c22b9063
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-04-19 11:13:58 +0200

    Add SüratManga
    https://github.com/manga-download/hakuneko/issues/7881

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -647,6 +647,7 @@ export { default as Sukima } from './Sukima';
 export { default as SumManga } from './SumManga';
 export { default as Summertoon } from './Summertoon';
 export { default as SundayWebry } from './SundayWebry';
+export { default as SuratManga } from './SuratManga';
 export { default as SuryaScans } from './SuryaScans';
 export { default as SushiScans } from './SushiScans';
 export { default as SushiScansFR } from './SushiScansFR';
