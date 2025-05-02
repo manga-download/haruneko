6676cd4e2d3dc135eaf59071085ddaab19b07910
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-02 11:59:37 +0200

    friday maintenance
    * update domains
    * Remove MangaRomance
    * Shojoverse > KingOfManhwa

--- web/src/engine/websites/KingOfManhwa.ts
+++ web/src/engine/websites/KingOfManhwa.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
-import icon from './Shojoverse.webp';
+import icon from './KingOfManhwa.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^{origin}\/series\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS(undefined, '/series/list-mode/')
+@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
+@MangaStream.MangasSinglePageCSS()
 @MangaStream.ChaptersSinglePageCSS()
 @MangaStream.PagesSinglePageJS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('shojoverse', 'Shojoverse', 'https://mythicscans.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
+        super('kingofmanhwa', 'KingOfManhwa', 'https://kingofmanhwa.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
