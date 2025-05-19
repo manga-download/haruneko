da6dc5ca3cfa6b9caf4169d837da6a69c4483f5d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-19 08:53:45 +0000

    domains updates (#1166)
    * change domains
    
    * update manga168

--- web/src/engine/websites/IkigaiMangas_e2e.ts
+++ web/src/engine/websites/IkigaiMangas_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Ikigai Mangas'
     },
     container: {
-        url: 'https://visualikigai.eltanews.com/series/el-sacerdote-sanador-del-sol/',
+        url: 'https://visualikigai.udonun.net/series/el-sacerdote-sanador-del-sol/',
         id: '/series/el-sacerdote-sanador-del-sol/',
         title: 'El Sacerdote Sanador del Sol'
     },
