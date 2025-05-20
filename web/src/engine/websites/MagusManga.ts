a63bd59f7075fc3e81efb9357e282cb5c15e50ab
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-20 14:19:35 +0200

    MagusManga : change theme

--- web/src/engine/websites/MagusManga.ts
+++ web/src/engine/websites/MagusManga.ts
@@ -1,18 +1,18 @@
 import { Tags } from '../Tags';
 import icon from './MagusManga.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Common from './decorators/Common';
-import * as KeyoApp from './templates/KeyoApp';
+import { VTheme } from './templates/VTheme';
 
+/*
 @Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, KeyoApp.queryMangaTitle)
 @Common.MangasSinglePagesCSS([ KeyoApp.queryMangaPath ], KeyoApp.queryManga, Common.AnchorInfoExtractor(true))
 @Common.ChaptersSinglePageCSS(KeyoApp.queryChapters, Common.AnchorInfoExtractor(true))
 @Common.PagesSinglePageJS(KeyoApp.pagesScript, 500)
-@Common.ImageAjax(true)
-export default class extends DecoratableMangaScraper {
+@Common.ImageAjax(true)*/
+export default class extends VTheme {
 
     public constructor() {
-        super('magusmanga', 'MagusManga', 'https://magustoon.net', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.English, Tags.Source.Aggregator);
+        super('magusmanga', 'MagusManga', 'https://magustoon.org', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.English, Tags.Source.Aggregator);
+        this.apiUrl = new URL('https://api.magustoon.org/api/');
     }
 
     public override get Icon() {
