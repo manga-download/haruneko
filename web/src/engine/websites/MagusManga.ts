17019822252c4adf5e8372ff8eff95fba69faf3f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-20 16:00:02 +0200

    cleanup code

--- web/src/engine/websites/MagusManga.ts
+++ web/src/engine/websites/MagusManga.ts
@@ -2,12 +2,6 @@ import { Tags } from '../Tags';
 import icon from './MagusManga.webp';
 import { VTheme } from './templates/VTheme';
 
-/*
-@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, KeyoApp.queryMangaTitle)
-@Common.MangasSinglePagesCSS([ KeyoApp.queryMangaPath ], KeyoApp.queryManga, Common.AnchorInfoExtractor(true))
-@Common.ChaptersSinglePageCSS(KeyoApp.queryChapters, Common.AnchorInfoExtractor(true))
-@Common.PagesSinglePageJS(KeyoApp.pagesScript, 500)
-@Common.ImageAjax(true)*/
 export default class extends VTheme {
 
     public constructor() {
