be9b0ae6a3144fc9d4c6393bed984dc9d48ce3e5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-27 14:17:07 +0200

    ColaManga: fix for unencrypted pictures

--- web/src/engine/websites/ColaManga.d.ts
+++ web/src/engine/websites/ColaManga.d.ts
@@ -1,11 +1,14 @@
 import type { Page } from '../providers/MangaPlugin';
 import { type Chapter, DecoratableMangaScraper } from '../providers/MangaPlugin';
 import type { Priority } from '../taskpool/DeferredTask';
+type PageKey = {
+    key: Uint8Array | null;
+};
 export default class extends DecoratableMangaScraper {
     constructor();
     get Icon(): string;
-    FetchPages(chapter: Chapter): Promise<Page[]>;
-    private ConvertWordArrayToUint8Array;
-    FetchImage(page: Page, priority: Priority, signal: AbortSignal): Promise<Blob>;
+    FetchPages(chapter: Chapter): Promise<Page<PageKey>[]>;
+    FetchImage(page: Page<PageKey>, priority: Priority, signal: AbortSignal): Promise<Blob>;
     private DecryptPicture;
 }
+export {};
--- web/src/engine/websites/ColaManga_e2e.ts
+++ web/src/engine/websites/ColaManga_e2e.ts
@@ -1,6 +1,7 @@
 import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+//Case : crypted pictures
+new TestFixture({
     plugin: {
         id: 'colamanga',
         title: 'Coco漫画'
@@ -19,6 +20,26 @@ const config = {
         size: 39_019,
         type: 'image/jpeg'
     }
-};
+}).AssertWebsite();
 
-new TestFixture(config).AssertWebsite();
+// Case: not crypted pictures
+new TestFixture({
+    plugin: {
+        id: 'colamanga',
+        title: 'Coco漫画'
+    },
+    container: {
+        url: 'https://www.colamanga.com/manga-rk63083/',
+        id: '/manga-rk63083/',
+        title: `王者大师兄(我就是不按套路出牌`
+    },
+    child: {
+        id: '/manga-rk63083/1/54.html',
+        title: '第54话 顾清欢'
+    },
+    entry: {
+        index: 0,
+        size: 236_995,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
