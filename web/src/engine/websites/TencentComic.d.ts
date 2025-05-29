5e7a16086938c5e32b56652ff5e153215914b302
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-05-29 14:25:48 +0200

    sync with other repo

--- web/src/engine/websites/TencentComic.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Chapter, Page } from '../providers/MangaPlugin';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-export default class extends DecoratableMangaScraper {
-    constructor();
-    get Icon(): string;
-    FetchPages(chapter: Chapter): Promise<Page[]>;
-}
