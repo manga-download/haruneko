d4f15d1bffb2dc8fddcbda97b68a882ea20ac426
Ronny <ronny@local.host>
2025-06-01 16:53:24 +0200

    [gorakuweb] revision

--- web/src/engine/websites/GorakuWeb.ts
+++ web/src/engine/websites/GorakuWeb.ts
@@ -1,20 +1,22 @@
 import { Tags } from '../Tags';
 import icon from './GorakuWeb.webp';
-import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, type Manga, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { FetchCSS } from '../platform/FetchProvider';
 import { GetBytesFromHex } from '../BufferEncoder';
 import type { Priority } from '../taskpool/DeferredTask';
 
-type JSONMedia = Array<{
-    href: string,
-    title: string
-}>;
+type HydratedEpisodeData = {
+    episodeList: {
+        href: string,
+        title: string,
+    }[]
+};
 
-type JSONPages = {
+type HydratedPageData = {
     accessKey: string,
-    ivBytes: string,
-    keyBytes: string,
+    keyBytes?: string,
+    ivBytes?: string,
     base: string,
     metadata: {
         pages: {
@@ -24,11 +26,12 @@ type JSONPages = {
 }
 
 type PageParams = {
-    keyBytes: string,
-    ivBytes: string
+    keyData?: string,
+    iv?: string
 }
 
 @Common.MangaCSS(/^{origin}\/episode\/\d+$/, 'meta[name="twitter:title"]')
+@Common.MangasSinglePagesCSS([ '/series' ], '#main div.group a[href*="episode"]', (a: HTMLAnchorElement) => ({ id: a.pathname, title: a.querySelector('h3').innerText.trim() }))
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -39,68 +42,42 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL('/series', this.URI)), 'script:not([src])');
-        const data = this.ExtractData<JSONMedia[]>(scripts, 'cardList', 'cardList');
-        return data.reduce((accumulator: Manga[], array) => {
-            const mangas = array.map(manga => new Manga(this, provider, new URL(manga.href, this.URI).pathname, manga.title.trim()));
-            return accumulator.concat(mangas);
-        }, []);
-    }
-
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(manga.Identifier, this.URI)), 'script:not([src])');
-        const data = this.ExtractData<JSONMedia>(scripts, 'episodeList', 'episodeList');
-        return data.map(chapter => new Chapter(this, manga, new URL(chapter.href, this.URI).pathname, chapter.title.trim()));
+        const { episodeList } = await this.FetchFlightDataNextJS<HydratedEpisodeData>(manga.Identifier, data => data['episodeList']);
+        return episodeList.map(chapter => new Chapter(this, manga, new URL(chapter.href, this.URI).pathname, chapter.title.trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page<PageParams>[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(chapter.Identifier, this.URI)), 'script:not([src])');
-        const { metadata: { pages }, accessKey, keyBytes, ivBytes, base } = this.ExtractData<JSONPages>(scripts, 'accessKey', 'metadata', true);
-        return pages.map(page => new Page<PageParams>(this, chapter, new URL(`${base}/${page.filename}?__token__=${accessKey}`), {
-            keyBytes, ivBytes
-        }));
+        const { metadata: { pages }, accessKey, keyBytes, ivBytes, base } = await this.FetchFlightDataNextJS<HydratedPageData>(chapter.Identifier, data => data['metadata'] && data['accessKey']);
+        return pages.map(page => new Page<PageParams>(this, chapter, new URL(`${base}/${page.filename}?__token__=${accessKey}`), { keyData: keyBytes, iv: ivBytes }));
     }
 
     public override async FetchImage(page: Page<PageParams>, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const data = await Common.FetchImageAjax.call(this, page, priority, signal, true);
-        if (!page.Parameters.keyBytes) return data;
-        const decrypted = await this.DecryptPicture(new Uint8Array(await data.arrayBuffer()), page.Parameters);
+        return page.Parameters.keyData ? this.DecryptImage(data, page.Parameters) : data;
+    }
+
+    private async DecryptImage(encrypted: Blob, page: PageParams): Promise<Blob> {
+        const algorithm = { name: 'AES-CBC', iv: GetBytesFromHex(page.iv) }; // NOTE: The default for the `length` is `128`
+        const key = await crypto.subtle.importKey('raw', GetBytesFromHex(page.keyData), algorithm, true, [ 'decrypt' ]);
+        const decrypted = await crypto.subtle.decrypt(algorithm, key, await encrypted.arrayBuffer());
         return Common.GetTypedData(decrypted);
     }
 
-    private async DecryptPicture(encrypted: Uint8Array, page: PageParams): Promise<ArrayBuffer> {
-        const secretKey = await crypto.subtle.importKey(
-            'raw',
-            GetBytesFromHex(page.keyBytes),
-            {
-                name: 'AES-CBC',
-                length: 128
-            }, true, [ 'decrypt']);
-
-        return crypto.subtle.decrypt({
-            name: 'AES-CBC',
-            iv: GetBytesFromHex(page.ivBytes)
-        }, secretKey, encrypted);
-    }
-
-    private ExtractData<T>(scripts: HTMLScriptElement[], scriptMatcher: string, keyName: string, asObject: boolean = false): T {
-        const script = scripts.map(script => script.text).find(text => text.includes(scriptMatcher) && text.includes(keyName));
-        const content = JSON.parse(script.substring(script.indexOf(',"') + 1, script.length - 2)) as string;
-        const record = JSON.parse(content.substring(content.indexOf(':') + 1)) as JSONObject;
-
-        return (function FindValueForKeyName(parent: JSONElement): JSONElement {
-            if (parent[keyName]) {
-                return asObject ? parent : parent[keyName];
-            }
-            for (const child of (Object.values(parent) as JSONElement[]).filter(value => value && typeof value === 'object')) {
-                const result = FindValueForKeyName(child);
-                if (result) {
-                    return result;
+    private async FetchFlightDataNextJS<T extends JSONElement>(endpoint: string, predicate: (data: JSONElement) => unknown): Promise<T> {
+        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(endpoint, this.URI)), 'script:not([src])');
+        return scripts
+            .map(script => script.text)
+            .filter(script => script.includes('self.__next_f.push'))
+            .map(script => {
+                // TODO: This is an extremely flakey extractor for NextJS flight data which needs much improvement for generic use
+                try {
+                    const content = JSON.parse(script.slice(script.indexOf(',"') + 1, -2)) as string;
+                    return JSON.parse(content.slice(content.indexOf(':') + 1)).at(-1) as T;
+                } catch {
+                    return {} as T;
                 }
-            }
-            return undefined;
-        })(record) as T;
+            })
+            .find(predicate);
     }
-
 }
