8c2d7458432d50ca8a89c2a82b3036041dcbb707
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-01 11:00:10 +0200

    CosmicScansID : change domain

--- web/src/engine/websites/CosmicScansID.ts
+++ web/src/engine/websites/CosmicScansID.ts
@@ -12,7 +12,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('cosmicscansid', 'Cosmic Scans ID', 'https://cosmic02.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Scanlator);
+        super('cosmicscansid', 'Cosmic Scans ID', 'https://kmvs03.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
