9947845fd4bffb3babc59eef7b111d7d5e4add83
Ronny <ronny@local.host>
2025-06-01 18:03:22 +0200

    [tencent] unflag revision

--- web/src/engine/websites/TencentComic.ts
+++ web/src/engine/websites/TencentComic.ts
@@ -4,13 +4,12 @@ import { DecoratableMangaScraper, type Chapter, Page } from '../providers/MangaP
 import * as Common from './decorators/Common';
 import { DRMProvider } from './TencentComic.DRM.js';
 
-// TODO: Revision
-
 @Common.MangaCSS(/^{origin}\/Comic\/ComicInfo\/id\/\d+$/i, 'h2.works-intro-title')
 @Common.MangasMultiPageCSS('/Comic/all/search/time/vip/1/page/{page}', 'div.ret-main ul.ret-search-list li.ret-search-item div.ret-works-info h3.ret-works-title a')
 @Common.ChaptersSinglePageCSS('.works-chapter-list.chapter-page-all li span.works-chapter-item a')
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
+
     readonly #drm: DRMProvider = new DRMProvider();
 
     public constructor() {
