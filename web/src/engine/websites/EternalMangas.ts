53374e69a4da1cbc1fcbc055efe9011bac3e088c
Ronny <ronny@local.host>
2025-06-03 00:14:19 +0200

    [eternalmanga] migrate to fetch nextjs

--- web/src/engine/websites/EternalMangas.ts
+++ web/src/engine/websites/EternalMangas.ts
@@ -2,13 +2,15 @@ import { Tags } from '../Tags';
 import icon from './EternalMangas.webp';
 import { DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchNextJS } from '../platform/FetchProvider';
 
-type JSONManga = {
-    id: number,
-    name: string,
-    slug: string
-}
+type HydratedMangaData = {
+    aea: {
+        id: number,
+        name: string,
+        slug: string,
+    }[]
+};
 
 @Common.MangaCSS(/^{origin}\/ver\/[^/]+$/, 'meta[property="og:title"]')
 @Common.ChaptersSinglePageCSS('div[class*="infoProject_divListChapter"] a[class*="infoProject_divChapter"]', Common.AnchorInfoExtractor(false, 'span:not([class*="infoProject_numChapter"])'))
@@ -25,27 +27,8 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL('./comics', this.URI)), 'script:not([src])');
-        const mangas = this.ExtractData<JSONManga[]>(scripts, 'project_id', 'aea');
-        return mangas.map(manga => new Manga(this, provider, `/ver/${manga.slug}`, manga.name));
-    }
-
-    private ExtractData<T>(scripts: HTMLScriptElement[], scriptMatcher: string, keyName: string): T {
-        const script = scripts.map(script => script.text).find(text => text.includes(scriptMatcher) && text.includes(keyName));
-        const content = JSON.parse(script.substring(script.indexOf(',"') + 1, script.length - 2)) as string;
-        const record = JSON.parse(content.substring(content.indexOf(':') + 1)) as JSONObject;
-
-        return (function FindValueForKeyName(parent: JSONElement): JSONElement {
-            if (parent[keyName]) {
-                return parent[keyName];
-            }
-            for (const child of (Object.values(parent) as JSONElement[]).filter(value => value && typeof value === 'object')) {
-                const result = FindValueForKeyName(child);
-                if (result) {
-                    return result;
-                }
-            }
-            return undefined;
-        })(record) as T;
+        const request = new Request(new URL('./comics', this.URI));
+        const { aea: entries } = await FetchNextJS<HydratedMangaData>(request, data => data['aea']);
+        return entries.map(entry => new Manga(this, provider, `/ver/${entry.slug}`, entry.name));
     }
 }
