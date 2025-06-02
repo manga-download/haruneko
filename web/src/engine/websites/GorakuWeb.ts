2bc3bf1da5632e76672d4552ce12c1770d3b493e
Ronny <ronny@local.host>
2025-06-03 00:14:04 +0200

    [gorakumanga] migrate to fetch nextjs

--- web/src/engine/websites/GorakuWeb.ts
+++ web/src/engine/websites/GorakuWeb.ts
@@ -2,7 +2,7 @@ import { Tags } from '../Tags';
 import icon from './GorakuWeb.webp';
 import { Chapter, DecoratableMangaScraper, type Manga, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchNextJS } from '../platform/FetchProvider';
 import { GetBytesFromHex } from '../BufferEncoder';
 import type { Priority } from '../taskpool/DeferredTask';
 
@@ -23,12 +23,12 @@ type HydratedPageData = {
             filename: string
         }[]
     }
-}
+};
 
 type PageParams = {
     keyData?: string,
     iv?: string
-}
+};
 
 @Common.MangaCSS(/^{origin}\/episode\/\d+$/, 'meta[name="twitter:title"]')
 @Common.MangasSinglePagesCSS([ '/series' ], '#main div.group a[href*="episode"]', (a: HTMLAnchorElement) => ({ id: a.pathname, title: a.querySelector('h3').innerText.trim() }))
@@ -43,12 +43,14 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { episodeList } = await this.FetchFlightDataNextJS<HydratedEpisodeData>(manga.Identifier, data => data['episodeList']);
+        const request = new Request(new URL(manga.Identifier, this.URI));
+        const { episodeList } = await FetchNextJS<HydratedEpisodeData>(request, data => data['episodeList']);
         return episodeList.map(chapter => new Chapter(this, manga, new URL(chapter.href, this.URI).pathname, chapter.title.trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page<PageParams>[]> {
-        const { metadata: { pages }, accessKey, keyBytes, ivBytes, base } = await this.FetchFlightDataNextJS<HydratedPageData>(chapter.Identifier, data => data['metadata'] && data['accessKey']);
+        const request = new Request(new URL(chapter.Identifier, this.URI));
+        const { metadata: { pages }, accessKey, keyBytes, ivBytes, base } = await FetchNextJS<HydratedPageData>(request, data => data['metadata'] && data['accessKey']);
         return pages.map(page => new Page<PageParams>(this, chapter, new URL(`${base}/${page.filename}?__token__=${accessKey}`), { keyData: keyBytes, iv: ivBytes }));
     }
 
@@ -62,22 +64,5 @@ export default class extends DecoratableMangaScraper {
         const key = await crypto.subtle.importKey('raw', GetBytesFromHex(page.keyData), algorithm, false, [ 'decrypt' ]);
         const decrypted = await crypto.subtle.decrypt(algorithm, key, await encrypted.arrayBuffer());
         return Common.GetTypedData(decrypted);
-    }
-
-    private async FetchFlightDataNextJS<T extends JSONElement>(endpoint: string, predicate: (data: JSONElement) => unknown): Promise<T> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(endpoint, this.URI)), 'script:not([src])');
-        return scripts
-            .map(script => script.text)
-            .filter(script => script.includes('self.__next_f.push'))
-            .map(script => {
-                // TODO: This is an extremely flakey extractor for NextJS flight data which needs much improvement for generic use
-                try {
-                    const content = JSON.parse(script.slice(script.indexOf(',"') + 1, -2)) as string;
-                    return JSON.parse(content.slice(content.indexOf(':') + 1)).at(-1) as T;
-                } catch {
-                    return {} as T;
-                }
-            })
-            .find(predicate);
     }
 }
