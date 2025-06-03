9f1293534538b96a11c1cfeeea2c2bc9adbc09d3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-03 15:05:48 +0200

    update websites and tests

--- web/src/engine/websites/MangaOwlio_e2e.ts
+++ web/src/engine/websites/MangaOwlio_e2e.ts
@@ -1,24 +1,22 @@
 ﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'mangaowlio',
         title: 'MangaOwl.io'
     },
     container: {
-        url: 'https://mangaowl.io/manga/terminally-ill-genius-dark-knight/',
-        id: JSON.stringify({ post: '4698', slug: '/manga/terminally-ill-genius-dark-knight/' }),
-        title: 'Terminally-Ill Genius Dark Knight'
+        url: 'https://mangaowl.io/read-1/the-newbie-is-too-strong/',
+        id: JSON.stringify({ post: '7645', slug: '/read-1/the-newbie-is-too-strong/' }),
+        title: 'The Newbie is Too Strong'
     },
     child: {
-        id: '/manga/terminally-ill-genius-dark-knight/chapter-1/',
-        title: 'Chapter 1'
+        id: '/read-1/the-newbie-is-too-strong/chapter-120/',
+        title: 'Chapter 120'
     },
     entry: {
-        index: 2,
-        size: 1_085_668,
+        index: 0,
+        size: 1_711_912,
         type: 'image/jpeg'
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
