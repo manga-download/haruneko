256fe311428776b1f14fffbced1ff3fb601033fe
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-08 08:10:07 +0000

    MangaHubBase : port all websites (#1191)

---
+++ web/src/engine/websites/MangaKakalotFun_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'mangakakalotfun',
+        title: 'MangaKakalotFun',
+    },
+    container: {
+        url: 'https://mangakakalot.fun/manga/enkan-no-lapin',
+        id: 'enkan-no-lapin',
+        title: 'Enkan No Lapin'
+    },
+    child: {
+        id: '1',
+        title: 'Ch.1 - Vol.1 - Chapter 1'
+    },
+    entry: {
+        index: 0,
+        size: 26_317,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
