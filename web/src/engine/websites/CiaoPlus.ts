af19564583e71effd385622479f8ca4a243281b0
Ronny <ronny@local.host>
2025-06-09 21:40:10 +0200

    [ciaoplus + shonenmagazine] revision

--- web/src/engine/websites/CiaoPlus.ts
+++ web/src/engine/websites/CiaoPlus.ts
@@ -8,80 +8,29 @@ import type { Priority } from '../taskpool/DeferredTask';
 import DeScramble from '../transformers/ImageDescrambler';
 import { GetHexFromBytes, GetBytesFromUTF8 } from '../BufferEncoder';
 
-// TODO: Revision
-
-type APIMangas = {
-    title_list: {
-        title_id: number,
-        title_name: string
-        episode_id_list: number[]
-    }[]
-}
-
-export type MangaData = {
-    id: string,
-    title: string,
-    episode_list: number[]
-}
+type APIManga = {
+    title_list: [{
+        title_id: number;
+        title_name: string;
+        episode_id_list: number[];
+    }];
+};
 
 type APIChapters = {
     episode_list: {
-        episode_id: number,
-        episode_name: string
-    }[]
-}
+        episode_id: number;
+        episode_name: string;
+    }[];
+};
 
 type APIPages = {
-    scramble_seed: number,
-    page_list: string[]
-}
-
-type TDimension = {
-    width: number,
-    height: number
-}
+    scramble_seed?: number;
+    page_list: string[];
+};
 
 type PageSeed = {
-    seed: number
-}
-
-export class DRMProvider {
-
-    constructor(private readonly apiURL: string, readonly requestHeaderHash: { name: string, seed: string }) {}
-
-    public async FetchAPI<T extends JSONElement>(endpoint: string, parameters: Record<string, string>, init: RequestInit = { method: 'GET' }): Promise<T> {
-        const request = await this.CreateRequest(endpoint, init, parameters);
-        return FetchJSON<T>(request);
-    }
-
-    private async CreateRequest(endpoint: string, init: RequestInit, parameters: Record<string, string>) {
-        const payload = new URLSearchParams(parameters);
-        const uri = new URL(endpoint, this.apiURL);
-        if(/^POST$/i.test(init.method)) {
-            init.body = payload;
-        } else {
-            uri.search = payload.toString();
-        }
-        const request = new Request(uri, init);
-        request.headers.set(this.requestHeaderHash.name, await this.ComputeHash(payload, this.requestHeaderHash.seed));
-        return request;
-    }
-
-    private async ComputeHash(parameters: URLSearchParams, seed: string): Promise<string> {
-        parameters.sort();
-        const parameterHashes = await Promise.all([ ...parameters.entries() ].map(async ([ key, value ]) => [
-            await this.SHA(key, 'SHA-256'),
-            await this.SHA(value, 'SHA-512'),
-        ].join('_')));
-        const aggreagteHash = await this.SHA(parameterHashes.join(','), 'SHA-256');
-        return this.SHA(aggreagteHash + seed, 'SHA-512');
-    }
-
-    private async SHA(text: string, algorithm: 'SHA-256' | 'SHA-512'): Promise<string> {
-        const hash = await crypto.subtle.digest(algorithm, GetBytesFromUTF8(text));
-        return GetHexFromBytes(new Uint8Array(hash));
-    }
-}
+    seed: number;
+};
 
 @Common.MangasNotSupported()
 export default class extends DecoratableMangaScraper {
@@ -91,61 +40,41 @@ export default class extends DecoratableMangaScraper {
         seed: '',
     });
 
-    public constructor(id = 'ciaoplus', label = 'Ciao Plus', url = 'https://ciao.shogakukan.co.jp', tags = [Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator]) {
-        super(id, label, url, ...tags );
+    public constructor(id = 'ciaoplus', label = 'Ciao Plus', url = 'https://ciao.shogakukan.co.jp', tags = [ Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official ]) {
+        super(id, label, url, ...tags);
     }
 
     public override get Icon() {
         return icon;
     }
 
-    public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/comics/title/\\d+/episode/\\d+$`).test(url);
+    async #FetchMangaInfo(mangaID: string): Promise<APIManga> {
+        return this.drm.FetchAPI<APIManga>('./title/list', {
+            platform: '3',
+            title_id_list: mangaID,
+        });
     }
 
-    protected async GetMangaDatas(mangaId: string): Promise<MangaData> {
-        const { title_list: [ manga ] } = await this.drm.FetchAPI<APIMangas>('./title/list', {
-            platform: '3',
-            title_id_list: mangaId
-        });
-        return {
-            id: manga.title_id.toString(),
-            title: manga.title_name.trim(),
-            episode_list: manga.episode_id_list,
-        };
+    public override ValidateMangaURL(url: string): boolean {
+        return new RegExpSafe(`^${this.URI.origin}/comics/title/\\d+/.*`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const mangaid = parseInt(url.split('/').at(-3));
-        const manga = await this.GetMangaDatas(mangaid.toString());
-        return new Manga(this, provider, manga.id, manga.title);
-
+        const { title_list: [ { title_id, title_name } ] } = await this.#FetchMangaInfo(new URL(url).pathname.split('/').at(3));
+        return new Manga(this, provider, title_id.toString(), title_name.trim());
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        return this.FetchChapterList(manga);
+        const { title_list: [ { episode_id_list } ] } = await this.#FetchMangaInfo(manga.Identifier);
+        return this.FetchChapterList(manga, episode_id_list);
     }
 
-    protected async FetchChapterList(manga: Manga): Promise<Chapter[]> {
-        const { episode_list } = await this.GetMangaDatas(manga.Identifier);
+    protected async FetchChapterList(manga: Manga, chapterIDs: number[]): Promise<Chapter[]> {
         const chapters: Chapter[] = [];
-
-        const chunkSize = 50;
-        const chaptersChunks: Array<Array<number>> = [];
-        for (let i = 0; i < episode_list.length; i += chunkSize) {
-            const chunk = episode_list.slice(i, i + chunkSize);
-            chaptersChunks.push(chunk);
-        }
-
-        for (const chapterChunk of chaptersChunks) {
+        while(chapterIDs.length > 0) {
             const { episode_list } = await this.drm.FetchAPI<APIChapters>(`./episode/list`, {
                 platform: '3',
-                episode_id_list: chapterChunk.toString(),
-            }, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                }
+                episode_id_list: chapterIDs.splice(0, 50).join(','),
             });
             chapters.push(...episode_list.map(chapter => new Chapter(this, manga, chapter.episode_id.toString(), chapter.episode_name.trim())));
         };
@@ -154,45 +83,85 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchPages(chapter: Chapter): Promise<Page<PageSeed>[]> {
         const { page_list, scramble_seed } = await this.drm.FetchAPI<APIPages>('./web/episode/viewer', {
-            0: '',
             platform: '3',
             episode_id: chapter.Identifier,
         });
-        return page_list.map(page => new Page<PageSeed>(this, chapter, new URL(page), { seed: scramble_seed }));
+        return page_list.map(page => new Page<PageSeed>(this, chapter, new URL(page), { seed: scramble_seed ?? 1 }));
     }
 
     public override async FetchImage(page: Page<PageSeed>, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const blob = await Common.FetchImageAjax.call(this, page, priority, signal);
-
         return DeScramble(blob, async (image, ctx) => {
-
             ctx.drawImage(image, 0, 0);
-            const o = getPieceDimension(image.width, image.height, COL_NUM);
-            ctx.clearRect(0, 0, o.width * COL_NUM, o.height * COL_NUM);
-            for (const piece of xs(COL_NUM, page.Parameters.seed ?? 1)) {
+            const tileInfo = GetTileInfo(image.width, image.height);
+            for (const { source, dest } of GetTileMap(page.Parameters.seed)) {
                 ctx.drawImage(
                     image,
-                    piece.source.x * o.width,
-                    piece.source.y * o.height,
-                    o.width,
-                    o.height,
-                    piece.dest.x * o.width,
-                    piece.dest.y * o.height,
-                    o.width,
-                    o.height
+                    source.x * tileInfo.width,
+                    source.y * tileInfo.height,
+                    tileInfo.width,
+                    tileInfo.height,
+                    dest.x * tileInfo.width,
+                    dest.y * tileInfo.height,
+                    tileInfo.width,
+                    tileInfo.height
                 );
             }
-
         });
     }
 }
 
-// Website Script
+export class DRMProvider {
+
+    constructor(private readonly apiURL: string, readonly requestHeaderHash: { name: string, seed: string }) {}
+
+    public async FetchAPI<T extends JSONElement>(endpoint: string, parameters: Record<string, string>, init: RequestInit = { method: 'GET' }): Promise<T> {
+        const request = await this.#CreateRequest(endpoint, init, parameters);
+        return FetchJSON<T>(request);
+    }
+
+    async #CreateRequest(endpoint: string, init: RequestInit, parameters: Record<string, string>) {
+        const payload = new URLSearchParams(parameters);
+        const uri = new URL(endpoint, this.apiURL);
+        if(/^POST$/i.test(init.method)) {
+            init.body = payload;
+        } else {
+            uri.search = payload.toString();
+        }
+        const request = new Request(uri, init);
+        request.headers.set(this.requestHeaderHash.name, await this.#ComputeHash(payload, this.requestHeaderHash.seed));
+        return request;
+    }
+
+    async #ComputeHash(parameters: URLSearchParams, seed: string): Promise<string> {
+        parameters.sort();
+        const parameterHashes = await Promise.all([ ...parameters.entries() ].map(async ([ key, value ]) => [
+            await this.#ComputeSHA(key, 'SHA-256'),
+            await this.#ComputeSHA(value, 'SHA-512'),
+        ].join('_')));
+        const aggreagteHash = await this.#ComputeSHA(parameterHashes.join(','), 'SHA-256');
+        return this.#ComputeSHA(aggreagteHash + seed, 'SHA-512');
+    }
+
+    async #ComputeSHA(text: string, algorithm: 'SHA-256' | 'SHA-512'): Promise<string> {
+        const hash = await crypto.subtle.digest(algorithm, GetBytesFromUTF8(text));
+        return GetHexFromBytes(new Uint8Array(hash));
+    }
+}
+
+// Copy & Paste from Website
 
 const COL_NUM = 4;
 
-function getPieceDimension(width: number, height: number, t: number): TDimension {
+function GetTileInfo(imageWidth: number, imageHeight: number) {
+    return getPieceDimension(imageWidth, imageHeight, COL_NUM);
+}
 
+function GetTileMap(seed: number) {
+    return xs(COL_NUM, seed);
+}
+
+function getPieceDimension(width: number, height: number, t: number) {
     if (width < t || height < t) return null;
     const s = Cs(t, 8);
     return width > s && height > s && (width = Math.floor(width / s) * s, height = Math.floor(height / s) * s),
--- web/src/engine/websites/ShonenMagazine.ts
+++ web/src/engine/websites/ShonenMagazine.ts
@@ -1,48 +1,52 @@
 import { Tags } from '../Tags';
 import icon from './ShonenMagazine.webp';
-import CiaoPlus, { DRMProvider, type MangaData } from './CiaoPlus';
-
-// TODO: Revision
+import { type MangaPlugin, Manga, type Chapter } from '../providers/MangaPlugin';
+import CiaoPlus, { DRMProvider } from './CiaoPlus';
 
 type APIManga = {
     web_title: {
-        title_id: number,
-        episode_id_list: number[],
-        title_name: string
-    }
-}
+        title_id: number;
+        title_name: string;
+        episode_id_list: number[];
+    };
+};
 
 export default class extends CiaoPlus {
 
     protected override readonly drm = new DRMProvider('https://api.pocket.shonenmagazine.com/', {
         name: 'X-Manga-Hash',
-        seed: [ // SHA256('') + '_' + SHA512('')
-            'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
-            'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e',
+        seed: [
+            'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855', // SHA256('')
+            'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e', // SHA512('')
         ].join('_')
     });
 
     public constructor() {
-        super('shonenmagazine', '週刊少年マガジ (Weekly Shonen Magazine & Pocket Magazine)', 'https://pocket.shonenmagazine.com', [Tags.Language.Japanese, Tags.Source.Official, Tags.Media.Manga]);
+        super('shonenmagazine', '週刊少年マガジ (Weekly Shonen Magazine & Pocket Magazine)', 'https://pocket.shonenmagazine.com', [ Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official ]);
     }
 
     public override get Icon() {
         return icon;
     }
 
-    public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/title/\\d+/episode/\\d+$`).test(url);
+    async #FetchMangaInfo(mangaID: string): Promise<APIManga> {
+        return this.drm.FetchAPI<APIManga>('./web/title/detail', {
+            platform: '3',
+            title_id: mangaID,
+        });
     }
 
-    protected override async GetMangaDatas(mangaId: string): Promise<MangaData> {
-        const { web_title: { episode_id_list, title_id, title_name } } = await this.drm.FetchAPI<APIManga>('./web/title/detail', {
-            platform: '3',
-            title_id: mangaId,
-        });
-        return {
-            id: title_id.toString(),
-            title: title_name.trim(),
-            episode_list: episode_id_list
-        };
+    public override ValidateMangaURL(url: string): boolean {
+        return new RegExpSafe(`^${this.URI.origin}/title/\\d+/.*`).test(url);
+    }
+
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        const { web_title: { title_id, title_name } } = await this.#FetchMangaInfo(new URL(url).pathname.split('/').at(2));
+        return new Manga(this, provider, title_id.toString(), title_name.trim());
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const { web_title: { episode_id_list } } = await this.#FetchMangaInfo(manga.Identifier);
+        return this.FetchChapterList(manga, episode_id_list);
     }
 }
