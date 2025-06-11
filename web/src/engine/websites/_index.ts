b8c3a22a0e909fcbb9d147b14dac4aebf5169f46
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-11 15:01:32 +0200

    Add LevaScans
    https://github.com/manga-download/hakuneko/issues/7979

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -288,6 +288,7 @@ export { default as LegacyScans } from './LegacyScans';
 export { default as LELManga } from './LELManga';
 export { default as LELScan } from './LELScan';
 export { default as LELScanVF } from './LELScanVF';
+export { default as LevaScans } from './LevaScans';
 export { default as LewdManhwa } from './LewdManhwa';
 export { default as LezhinES } from './LezhinES';
 export { default as LHTranslation } from './LHTranslation';
