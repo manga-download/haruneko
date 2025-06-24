1966b5780f24f9c72acd1a74665942ba560a68d6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-24 11:00:02 +0000

    Add Tibiu (#1211)
    * Add Tibiu
    
    https://github.com/manga-download/hakuneko/issues/7961

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -680,6 +680,7 @@ export { default as TenshiID } from './TenshiID';
 export { default as TheBlank } from './TheBlank';
 export { default as ThreeHentai } from './ThreeHentai';
 export { default as ThunderScans } from './ThunderScans';
+export { default as Tibiu } from './Tibiu';
 export { default as TitanManga } from './TitanManga';
 export { default as TmoManga } from './TmoManga';
 export { default as ToCoronaEx } from './ToCoronaEx';
