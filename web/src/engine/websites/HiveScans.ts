3fc15150b1cb424df2ca69b9ea76af2a727f4b34
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-28 19:20:30 +0200

    HiveScans : change api url

--- web/src/engine/websites/HiveScans.ts
+++ web/src/engine/websites/HiveScans.ts
@@ -6,6 +6,7 @@ export default class extends VTheme {
 
     public constructor() {
         super('hivescans', 'Hive Scans', 'https://hivetoons.org', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+        this.apiUrl = new URL('https://api.hivetoons.org/api/');
     }
 
     public override get Icon() {
