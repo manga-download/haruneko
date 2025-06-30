b9b617fa5fa7d7b4bd20128baf0a13dc68f42cd6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-06-30 07:45:14 +0000

    update domains & websites (#1219)
    * Remove GuncelManga
    
    redirect to WebtoonHatti
    
    * Truyengg -> FoxTruyen
    
    * update domain
    
    * update domains

--- web/src/engine/websites/IkigaiMangas_e2e.ts
+++ web/src/engine/websites/IkigaiMangas_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Ikigai Mangas'
     },
     container: {
-        url: 'https://lectorikigai.vrinari.org/series/el-sacerdote-sanador-del-sol/',
+        url: 'https://visualikigai.deliciousdelight.online/series/el-sacerdote-sanador-del-sol/',
         id: '/series/el-sacerdote-sanador-del-sol/',
         title: 'El Sacerdote Sanador del Sol'
     },
