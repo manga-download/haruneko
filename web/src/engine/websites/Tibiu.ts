d4890b6f0d5b7b3c8877b98d4a8aabe48bbef542
Ronny <ronny@local.host>
2025-07-06 16:43:29 +0200

    [tibiu] use larger manga list page

--- web/src/engine/websites/Tibiu.ts
+++ web/src/engine/websites/Tibiu.ts
@@ -1,26 +1,23 @@
 import { Tags } from '../Tags';
 import icon from './Tibiu.webp';
 import { FetchJSON } from '../platform/FetchProvider';
-import { type MangaPlugin, Manga, Chapter, DecoratableMangaScraper, type Page } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper, type Manga, Chapter } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { Delay } from '../BackgroundTimers';
 
-type APIMediaItems = {
+type APIChapters = {
     data: {
-        id: string,
+        link: string;
         name: string;
     }[];
 };
 
-function PageExtractor(element: HTMLImageElement) {
-    return element.dataset.original || element.getAttribute('src');
-}
-
 @Common.MangaCSS(/^{origin}\/comic\/\d+$/, 'h2.comicInfo__title')
-//@Common.PagesSinglePageCSS('div.rd-article__pic img[data-original]', PageExtractor)
+@Common.MangasMultiPageCSS('https://comic.tibiu.net/search/_/{page}', 'div.cate-comic-list p.comic__title > a')
+@Common.PagesSinglePageCSS('div[data-pid] > img[data-original]:not([data-original$="boylove.cc"])', img => img.dataset.original)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-    private readonly apiUrl = 'https://comic.tibiu.net/index.php/api/';
+
+    private readonly apiURL = 'https://comic.tibiu.net/index.php/api/';
 
     public constructor () {
         super('tibiu', 'Tibiu', 'https://comic.tibiu.net', Tags.Media.Manga, Tags.Media.Manhua, Tags.Language.Chinese, Tags.Source.Official, Tags.Rating.Pornographic);
@@ -30,29 +27,8 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const mangaList: Manga[] = [];
-        for (let page = 1, run = true; run; page++) {
-            await Delay(150);
-            const mangas = await this.GetMangasFromPage(page, provider);
-            mangas.length > 0 ? mangaList.push(...mangas) : run = false;
-        }
-        return mangaList;
-    }
-
-    private async GetMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
-        const { data } = await FetchJSON<APIMediaItems>(new Request(new URL(`./data/comic?page=${page}`, this.apiUrl)));
-        return data.map(manga => new Manga(this, provider, `/comic/${manga.id}`, new DOMParser().parseFromString(manga.name, 'text/html').body.innerText.trim()));
-    }
-
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const mangaId = manga.Identifier.match(/\d+$/).at(0);
-        const { data } = await FetchJSON<APIMediaItems>(new Request(new URL(`./data/chapter?mid=${mangaId}`, this.apiUrl)));
-        return data.map(chapter => new Chapter(this, manga, `/chapter/${mangaId}/${chapter.id}`, chapter.name.trim()));
-    }
-
-    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const pages = await Common.FetchPagesSinglePageCSS.call(this, chapter, 'div.rd-article__pic img[data-original]', PageExtractor);
-        return pages.filter(page => [ /boylove\.cc$/ ].none(pattern => pattern.test(page.Link.pathname)));
+        const { data } = await FetchJSON<APIChapters>(new Request(new URL('./comic/chapter?mid=' + manga.Identifier.split('/').at(-1), this.apiURL)));
+        return data.map(chapter => new Chapter(this, manga, new URL(chapter.link, this.URI).pathname, chapter.name.trim()));
     }
 }
