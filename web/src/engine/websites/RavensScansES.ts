e6784ab0f539e4d0ca2f621612e4ac7647c75607
Ronny <ronny@local.host>
2025-07-06 15:04:45 +0200

    format lines

--- web/src/engine/websites/RavensScansES.ts
+++ web/src/engine/websites/RavensScansES.ts
@@ -8,12 +8,13 @@ const apiUrl = 'https://api.ravens-scans.com';
 const cdnUrl = 'https://img-cdn1.ravens-scans.com';
 
 @ReaderFront.MangaAJAX(/^{origin}\/work\/es\/[^/]+/, apiUrl)
-@ReaderFront.MangasSinglePageAJAX(apiUrl, ['es'])
+@ReaderFront.MangasSinglePageAJAX(apiUrl, [ 'es' ])
 @ReaderFront.ChaptersSinglePageAJAX(apiUrl)
 @ReaderFront.PagesSinglePageAJAX(apiUrl, cdnUrl)
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
-    public constructor() {
+
+    public constructor () {
         super('ravensscans-es', `RavensScans (Spanish)`, 'https://ravens-scans.com', Tags.Language.Spanish, Tags.Media.Manga, Tags.Source.Scanlator);
     }
 
