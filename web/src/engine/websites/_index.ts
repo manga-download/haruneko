134d8bbfa9f2bd06b7bc8b4f478e903d8fee7e31
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-07-08 12:20:24 +0200

    Add TenshiManga
    https://github.com/manga-download/hakuneko/issues/8032

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -679,6 +679,7 @@ export { default as TempleScan } from './TempleScan';
 export { default as TempleScanEsp } from './TempleScanEsp';
 export { default as TencentComic } from './TencentComic';
 export { default as TenshiID } from './TenshiID';
+export { default as TenshiManga } from './TenshiManga';
 export { default as TheBlank } from './TheBlank';
 export { default as ThreeHentai } from './ThreeHentai';
 export { default as ThunderScans } from './ThunderScans';
