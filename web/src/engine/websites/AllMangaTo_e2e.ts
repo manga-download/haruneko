4908bb5f23a0830b2289460ade14ae09a9e888bc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-07-10 13:57:32 +0200

    use regular Graphql requests

--- web/src/engine/websites/AllMangaTo_e2e.ts
+++ web/src/engine/websites/AllMangaTo_e2e.ts
@@ -11,12 +11,33 @@ new TestFixture({
         title: 'Yuan Zun'
     },
     child: {
-        id: JSON.stringify({id: '643.5', translation: 'sub'}),
-        title: 'Chapter 643.5 [sub]'
+        id: JSON.stringify({ id: '652.5', translationType: 'sub' }),
+        title: 'Chapter 652.5'
     },
     entry: {
         index: 0,
-        size: 714_974, // 715_757
+        size: 124_050,
+        type: 'image/webp'
+    }
+}).AssertWebsite();
+
+new TestFixture({
+    plugin: {
+        id: 'allmangato',
+        title: 'AllManga.to'
+    },
+    container: {
+        url: 'https://allmanga.to/manga/kFvrdRcbubPjrhr63',
+        id: 'kFvrdRcbubPjrhr63',
+        title: 'Yuan Zun'
+    },
+    child: {
+        id: JSON.stringify({ id: '261', translationType: 'raw' }),
+        title: 'Chapter 261 Appeal for Aid [raw]'
+    },
+    entry: {
+        index: 0,
+        size: 381_378,
         type: 'image/jpeg'
     }
 }).AssertWebsite();
