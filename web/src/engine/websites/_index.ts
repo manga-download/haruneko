3cf67a44bbc839f500194b4dea3f5534967f3b6d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-07-17 13:00:31 +0200

    Add  FateScans
    https://github.com/manga-download/hakuneko/issues/8060

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -153,6 +153,7 @@ export { default as EternalMangas } from './EternalMangas';
 export { default as EuphoriaScan } from './EuphoriaScan';
 export { default as EvilFlowers } from './EvilFlowers';
 export { default as FalcoScan } from './FalcoScan';
+export { default as FateScans } from './FateScans';
 export { default as FbSquads } from './FbSquads';
 export { default as FeelWeb } from './FeelWeb';
 export { default as FireComics } from './FireComics';
