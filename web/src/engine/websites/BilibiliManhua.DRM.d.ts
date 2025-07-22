408fe45105dece0f683b0e7527796870c0aaf3e3
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-07-22 13:04:57 +0200

    use their WASM to decrypt pictures

--- web/src/engine/websites/BilibiliManhua.DRM.d.ts
+++ web/src/engine/websites/BilibiliManhua.DRM.d.ts
@@ -5,6 +5,6 @@ export declare class DRMProvider {
         x: number;
         path: string;
     }[]): string;
-    ExtractImageData(pageUrl: string, pageIndex: number, chapterUrl: string): Promise<ArrayBuffer>;
+    ExtractImageData(pageUrl: string, pageIndex: number, chapterUrl: string): Promise<Uint8Array>;
     FetchTwirp<T extends JSONObject>(uri: URL, path: string, payload: JSONObject, payloadWithFingerprint?: boolean): Promise<T>;
 }
