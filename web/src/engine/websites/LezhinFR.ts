be12e4bb12b5c979e05b9608d5e133f46ca2608d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-07-24 15:42:07 +0200

    fix and improve delitoon search

--- web/src/engine/websites/LezhinFR.ts
+++ web/src/engine/websites/LezhinFR.ts
@@ -1,15 +1,14 @@
 import { Tags } from '../Tags';
 import icon from './LezhinFR.webp';
 import { BalconyDRM, DelitoonBase } from './templates/DelitoonBase';
-import * as Common from './decorators/Common';
 
 // TODO: Check for possible revision
-@Common.MangasNotSupported()
 export default class extends DelitoonBase {
 
     public constructor () {
         super('delitoon', 'Lezhin (French)', 'https://www.lezhinfr.com', Tags.Media.Manhwa, Tags.Language.French, Tags.Source.Official);
         this.drm = new BalconyDRM(this.URI, 'LEZHINFR_COM');
+        this.mangaSearchVersion = 2;
     }
 
     public override get Icon() {
