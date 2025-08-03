a3d211430cfe7c8b0a300cfc8bb5a732f4a6ef3b
Ronny <ronny@local.host>
2025-08-03 12:45:14 +0200

    [manhastro] do not use random chapter links

--- web/src/engine/websites/ManHastro.ts
+++ web/src/engine/websites/ManHastro.ts
@@ -7,7 +7,7 @@ import { FetchCSS } from '../platform/FetchProvider';
 
 @Madara.MangaCSS(/^{origin}\/lermanga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
 @Madara.MangasMultiPageAJAX()
-@Common.PagesSinglePageJS('imageLinks.map( image => atob(image));')
+@Common.PagesSinglePageJS('imageLinks.map(image => atob(image));')
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
 
@@ -20,9 +20,8 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        //manga page only got 6 chapters but each chapter page got the full chapter list
         const chapters = await Madara.FetchChaptersSinglePageCSS.call(this, manga);
-        const elements = await FetchCSS<HTMLOptionElement>(new Request(new URL(chapters[Math.floor(Math.random() * chapters.length)].Identifier, this.URI)), 'select.single-chapter-select option');
-        return elements.map(element => new Chapter(this, manga, new URL(element.dataset.redirect).pathname, element.textContent.trim()));
+        const options = await FetchCSS<HTMLOptionElement>(new Request(new URL(chapters.at(0).Identifier, this.URI)), 'select.single-chapter-select option');
+        return options.map(option => new Chapter(this, manga, new URL(option.dataset.redirect).pathname, option.textContent.trim()));
     }
 }
