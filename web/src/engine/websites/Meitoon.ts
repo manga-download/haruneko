e2818a1c657c97bdaa9936d079eb9446f5291297
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-03 11:34:06 +0200

    Add Meitoon
    https://github.com/manga-download/hakuneko/issues/8090

---
+++ web/src/engine/websites/Meitoon.ts
@@ -0,0 +1,21 @@
+import { Tags } from '../Tags';
+import icon from './Meitoon.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as KeyoApp from './templates/KeyoApp';
+
+@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, KeyoApp.queryMangaTitle)
+@Common.MangasSinglePagesCSS([ KeyoApp.queryMangaPath ], KeyoApp.queryManga, Common.AnchorInfoExtractor(true))
+@Common.ChaptersSinglePageCSS(KeyoApp.queryChapters, Common.AnchorInfoExtractor(true))
+@Common.PagesSinglePageJS(KeyoApp.pagesScript, 500)
+@Common.ImageAjax(true)
+export default class extends DecoratableMangaScraper {
+
+    public constructor() {
+        super('meitoon', 'MeiToon', 'https://meitoon.org', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
