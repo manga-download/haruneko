2ee9573fb0131c7cace16ce1eca4060b5b8e75f7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-04 10:11:26 +0000

    update domains + Rebrand CosmicScansID (#1278)
    * CosmicScansID : rebrand to NovaScans
    
    * update domains

--- web/src/engine/websites/NovaScans.ts
+++ web/src/engine/websites/NovaScans.ts
@@ -1,10 +1,10 @@
 import { Tags } from '../Tags';
-import icon from './CosmicScansID.webp';
+import icon from './NovaScans.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 
-@MangaStream.MangaCSS(/^https:\/\/lc\d\.cosmicscans\.asia\/manga\/[^/]+\/$/)
+@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @Common.MangasMultiPageCSS('/page/{page}/', 'div.bixbox div.listupd div.utao div.imgu a', 1, 1, 0, Common.AnchorInfoExtractor(true))
 @MangaStream.ChaptersSinglePageCSS()
 @MangaStream.PagesSinglePageJS([/800X250\.webp$/, /\.gif$/], 'ts_reader.params.sources.shift().images;')
@@ -12,7 +12,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('cosmicscansid', 'Cosmic Scans ID', 'https://lc3.cosmicscans.asia', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Scanlator);
+        super('cosmicscansid', 'Nova Scans', 'https://novascans.id', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
