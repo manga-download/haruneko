3574339cfa4769dcdefa12944556bb92cd61c06f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-05 16:57:48 +0200

    Webnovel : fix getting pages
    and code refactoring

--- web/src/engine/websites/WebNovel.ts
+++ web/src/engine/websites/WebNovel.ts
@@ -7,61 +7,58 @@ import * as Common from './decorators/Common';
 type APIComic = {
     comicId: number,
     comicName: string
-}
+};
 
 type APIResult<T> = {
     code: number,
     data: T,
-    msg: string
-}
+};
 
-type APIBooklist = {
+type APIBooklist = APIResult<{
     items: {
         bookId: string,
         bookName: string
     }[]
-}
+}>;
 
-type APIChapterList = {
+type APIChapterList = APIResult<{
     comicChapters: {
         chapterId: string,
         chapterName: string,
         chapterIndex: number
     }[]
-}
+}>;
 
-type APIPageList = {
+type APIPageList = APIResult<{
     chapterInfo: {
         chapterPage: {
-            url : string
+            url: string
         }[]
     }
-}
+}>;
 
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
     private token = '';
 
     public constructor() {
-        super('webnovel', `Webnovel Comics`, 'https://www.webnovel.com', Tags.Language.English, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Official);
+        super('webnovel', 'Webnovel Comics', 'https://www.webnovel.com', Tags.Language.English, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Source.Official);
     }
     public override get Icon() {
         return icon;
     }
     public override async Initialize(): Promise<void> {
-        const request = new Request(this.URI.href);
-        const data = await FetchWindowScript<string>(request, `new Promise( resolve => resolve( decodeURIComponent( document.cookie ).match( /_csrfToken=([^;]+);/ )[1] ) )`);
+        const data = await FetchWindowScript<string>(new Request(this.URI), `new Promise( resolve => resolve( decodeURIComponent( document.cookie ).match( /_csrfToken=([^;]+);/ )[1] ) )`);
         this.token = data;
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/comic/\\S+_\\d+$`).test(url);
+        return new RegExpSafe(`^${this.URI.origin}/comic/([^/]+_)?\\d+$`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const request = new Request(url);
-        const data = await FetchWindowScript<APIComic>(request, `new Promise( resolve => resolve( window.g_data.book.comicInfo));`);
-        return new Manga(this, provider, data.comicId.toString(), data.comicName.trim());
+        const { comicId, comicName } = await FetchWindowScript<APIComic>(new Request(new URL(url)), 'window.g_data.book.comicInfo');
+        return new Manga(this, provider, comicId.toString(), comicName);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
@@ -73,44 +70,20 @@ export default class extends DecoratableMangaScraper {
         return mangaList;
     }
     private async GetMangasFromPage(page: number, provider: MangaPlugin): Promise<Manga[]> {
-        page = page || 1;
-        const uri = new URL('/go/pcm/category/categoryAjax', this.URI);
-        const params = new URLSearchParams({
-            pageIndex: String(page),
-            _csrfToken: this.token,
-            categoryId: '0',
-            categoryType: '2',
-        });
-        uri.search = params.toString();
-        const request = new Request(uri.href);
-        const data = await FetchJSON<APIResult<APIBooklist>>(request);
-        return data.code == 0 ? data.data.items.map(manga => new Manga(this, provider, manga.bookId, manga.bookName.trim())) : [];
+        const uri = new URL(`./go/pcm/category/categoryAjax?pageIndex=${page}&_csrfToken=${this.token}&categoryId=0&categoryType=2`, this.URI);
+        const data = await FetchJSON<APIBooklist>(new Request(uri));
+        return data.code == 0 ? data.data.items.map(({ bookId, bookName }) => new Manga(this, provider, bookId, bookName)) : [];
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const uri = new URL('/go/pcm/comic/getChapterList', this.URI);
-        const params = new URLSearchParams({
-            _csrfToken: this.token,
-            comicId: manga.Identifier
-        });
-        uri.search = params.toString();
-        const request = new Request(uri.href);
-        const data = await FetchJSON<APIResult<APIChapterList>>(request);
-        return data.code == 0 ? data.data.comicChapters.map(chapter => new Chapter(this, manga, chapter.chapterId, chapter.chapterIndex + ' : ' + chapter.chapterName)) : [];
+        const uri = new URL(`/go/pcm/comic/getChapterList?_csrfToken=${this.token}&comicId=${manga.Identifier}`, this.URI);
+        const data = await FetchJSON<APIChapterList>(new Request(uri));
+        return data.code == 0 ? data.data.comicChapters.map(({ chapterId, chapterIndex, chapterName }) => new Chapter(this, manga, chapterId, chapterIndex + ' : ' + chapterName)) : [];
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const uri = new URL('/go/pcm/comic/getContent', this.URI);
-        const params = new URLSearchParams({
-            width: '1920',
-            _csrfToken: this.token,
-            comicId: chapter.Parent.Identifier,
-            chapterid: chapter.Identifier
-
-        });
-        uri.search = params.toString();
-        const request = new Request(uri.href);
-        const data = await FetchJSON<APIResult<APIPageList>>(request);
-        return data.code == 0 ? data.data.chapterInfo.chapterPage.map(page => new Page(this, chapter, new URL(page.url))) : [];
+        const uri = new URL(`/go/pcm/comic/getContent?_csrfToken=${this.token}&comicId=${chapter.Parent.Identifier}&chapterId=${chapter.Identifier}&width=1920`, this.URI);
+        const data = await FetchJSON<APIPageList>(new Request(uri));
+        return data.code == 0 ? data.data.chapterInfo.chapterPage.map(({ url }) => new Page(this, chapter, new URL(url))) : [];
     }
 }
