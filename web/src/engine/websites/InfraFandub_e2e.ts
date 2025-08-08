4f714db6a761159582680681ee380afdab54b856
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-08 21:25:21 +0200

    Infrafandub : fix getting chapters

--- web/src/engine/websites/InfraFandub_e2e.ts
+++ web/src/engine/websites/InfraFandub_e2e.ts
@@ -1,6 +1,6 @@
 ﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'infrafandub',
         title: 'Infra Fandub'
@@ -19,6 +19,4 @@ const config = {
         size: 1_195_138,
         type: 'image/jpeg'
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
