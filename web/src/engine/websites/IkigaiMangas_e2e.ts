f1bfb3427409958cb78df821a7182e5cd76ed623
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-08 09:32:53 +0000

    update domains & remove dead websites (#1286)
    * Remove DianxiaTrad NetTruyenPLus, PatyScans, PCNet

--- web/src/engine/websites/IkigaiMangas_e2e.ts
+++ web/src/engine/websites/IkigaiMangas_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Ikigai Mangas'
     },
     container: {
-        url: 'https://visualikigai.kennyhowes.com/series/el-sacerdote-sanador-del-sol/',
+        url: 'https://viralikigai.vteam.online/series/el-sacerdote-sanador-del-sol/',
         id: '/series/el-sacerdote-sanador-del-sol/',
         title: 'El Sacerdote Sanador del Sol'
     },
