d9be9d1a67fd7930ebdd2536d30fad91d0dec53e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-08 19:51:52 +0200

    Update ManhwaLatino.ts

--- web/src/engine/websites/ManhwaLatino.ts
+++ web/src/engine/websites/ManhwaLatino.ts
@@ -1,6 +1,6 @@
 import { Tags } from '../Tags';
 import icon from './ManhwaLatino.webp';
-import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
@@ -10,6 +10,7 @@ function PageExtractor(img: HTMLImageElement) {
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @Madara.MangasMultiPageCSS()
+@Madara.ChaptersSinglePageCSS('ul li.wp-manga-chapter div.mini-letters a')
 @Common.PagesSinglePageCSS('div.page-break img.img-responsive', PageExtractor)
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
@@ -20,10 +21,5 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
-    }
-
-    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters = await Madara.FetchChaptersSinglePageCSS.call(this, manga, 'ul li.wp-manga-chapter div.mini-letters a');
-        return chapters.map(chapter => new Chapter(this, manga, chapter.Identifier, chapter.Title.replace(manga.Title, '').trim()));
     }
 }
