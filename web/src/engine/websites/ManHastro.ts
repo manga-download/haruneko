bb3604951f256e86cdc576976bd5fe6fbb77e782
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-16 16:50:42 +0200

    Manhastro : website redesign
    * No more Madara
    * Mandatory login
    * Login token need for api access

--- web/src/engine/websites/ManHastro.ts
+++ web/src/engine/websites/ManHastro.ts
@@ -1,27 +1,120 @@
 import { Tags } from '../Tags';
 import icon from './ManHastro.webp';
-import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
-import * as Madara from './decorators/WordPressMadara';
+import { Chapter, DecoratableMangaScraper, type MangaPlugin, Manga, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchJSON, FetchWindowScript } from '../platform/FetchProvider';
 
-@Madara.MangaCSS(/^{origin}\/lermanga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
-@Madara.MangasMultiPageAJAX()
-@Common.PagesSinglePageJS('imageLinks.map(image => atob(image));')
-@Common.ImageAjax(true)
+type APIResult<T> = {
+    success: string,
+    data: T
+};
+
+type APIManga = {
+    manga_id: number,
+    titulo: string
+};
+
+type APIChapter = {
+    capitulo_id: number,
+    capitulo_nome: string
+};
+
+type APIPages = APIResult<{
+    chapter: {
+        baseUrl: string,
+        hash: string,
+        data: string[]
+    }
+}>;
+
+type APIChapters = APIResult<APIChapter[]>;
+type APIMangas = APIResult<APIManga[]>;
+
+/**
+ * A basic oAuth token manager with ManHastro specific business logic
+ */
+class TokenProvider {
+
+    #token: string = null;
+
+    constructor(private readonly clientURI: URL) { }
+
+    /**
+     * Extract the token directly from the website (e.g., after login/logout through manual website interaction)
+     */
+    public async UpdateToken() {
+        try {
+            this.#token = await FetchWindowScript<string>(new Request(this.clientURI), `localStorage.getItem('auth') || null);`) ?? null;
+        } catch (error) {
+            console.warn('UpdateToken()', error);
+            this.#token = null;
+        }
+    }
+
+    /**
+     * Determine the _Bearer_ extracted from the current token and add it as authorization header to the given {@link init} headers (replacing any existing authorization header).
+     * In case the _Bearer_ could not be extracted from the current token the authorization header will not be added/replaced.
+     */
+    public async ApplyAuthorizationHeader(init: HeadersInit): Promise<HeadersInit> {
+        const headers = new Headers(init);
+        if (this.#token) {
+            headers.set('Authorization', 'Bearer ' + this.#token);
+        }
+        return headers;
+    }
+}
+
+@Common.ImageElement(true)
 export default class extends DecoratableMangaScraper {
+    private readonly tokenProvider: TokenProvider;
+    private readonly apiUrl = 'https://api2.manhastro.net/';
 
     public constructor() {
         super('manhastro', 'ManHastro', 'https://manhastro.net', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Portuguese, Tags.Source.Aggregator);
+        this.tokenProvider = new TokenProvider(this.URI);
     }
 
     public override get Icon() {
         return icon;
     }
 
+    public override async Initialize(): Promise<void> {
+        await this.tokenProvider.UpdateToken();
+    }
+
+    public override ValidateMangaURL(url: string): boolean {
+        return new RegExpSafe(`^${this.URI.origin}/manga/[^/]+$`).test(url);
+    }
+
+    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
+        return (await this.GetAllMangas(provider)).find(manga => manga.Identifier === url.split('/').at(-1));
+    }
+
+    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
+        return await this.GetAllMangas(provider);
+    }
+
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters = await Madara.FetchChaptersSinglePageCSS.call(this, manga);
-        const options = await FetchCSS<HTMLOptionElement>(new Request(new URL(chapters.at(0).Identifier, this.URI)), 'select.single-chapter-select option');
-        return options.map(option => new Chapter(this, manga, new URL(option.dataset.redirect, this.URI).pathname, option.textContent.trim()));
+        const { data } = await this.FetchAPI<APIChapters>(`./dados/${manga.Identifier}`);
+        return data
+            .sort((self, other) => other.capitulo_id - self.capitulo_id)
+            .map(chapter => new Chapter(this, manga, chapter.capitulo_id.toString(), chapter.capitulo_nome));
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const { data: { chapter: { baseUrl, data, hash } } } = await this.FetchAPI<APIPages>(`./paginas/${chapter.Identifier}`);
+        return data.map(page => new Page(this, chapter, new URL(`${baseUrl}/${hash}/${page}`)));
+    }
+
+    private async GetAllMangas(provider: MangaPlugin): Promise<Manga[]> {
+        const { data } = await this.FetchAPI<APIMangas>('./dados');
+        return data.map(manga => new Manga(this, provider, manga.manga_id.toString(), manga.titulo));
+    }
+
+    private async FetchAPI<T extends JSONElement>(endpoint: string) {
+        const request = new Request(new URL(endpoint, this.apiUrl), {
+            headers: await this.tokenProvider.ApplyAuthorizationHeader({}),
+        });
+        return FetchJSON<T>(request);
     }
 }
