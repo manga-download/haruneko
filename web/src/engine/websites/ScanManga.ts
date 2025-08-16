de3464548043015e62a0fc95e7eb683e9a191cfc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-16 11:58:08 +0200

    ScanMangas : fix getting chapters

--- web/src/engine/websites/ScanManga.ts
+++ web/src/engine/websites/ScanManga.ts
@@ -5,16 +5,40 @@ import * as Common from './decorators/Common';
 import type { Priority } from '../taskpool/DeferredTask';
 import { Fetch } from '../platform/FetchProvider';
 
-const pagescript = `
+// TODO : Revise this website once 'credential: omit' is fixed
+
+const chapterScript = `
     new Promise( async (resolve, reject) => {
         try {
-            const response = await fetch("/api/lel/" + idc + '.json', {
+            const response = await fetch('/api/chapter/' + n + '.json', {
+                method: 'GET',
+                credentials: 'omit',
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    'Token': 'yf'
+                }
+            });
+            const data = await response.json();
+            resolve( data.c.map( chapter =>  {
+                return { id: '/lecture-en-ligne/'+ chapter.u,title: chapter.nt };
+            }));
+
+        } catch (error) {
+            reject(error);
+        }
+    });
+`;
+
+const pageScript = `
+    new Promise( async (resolve, reject) => {
+        try {
+            const response = await fetch('/api/lel/' + idc + '.json', {
                 method: 'POST',
                 credentials: 'omit',
                 headers: {
                     'Content-type': 'application/json; charset=UTF-8',
                     'source': window.location.href,
-                    'Token': 'sm'
+                    'Token': 'yf'
                 },
                 body: JSON.stringify({
                     a: sme,
@@ -23,15 +47,15 @@ const pagescript = `
             });
 
             const result = await response.text();
-            const _CHAPTERDATA = JSON.parse(atob(result.replace(new RegExp(idc.toString(16) + '$'), '').split('').reverse().join('')));
+            const _CHAPTERDATA = 
             const _HOST = _CHAPTERDATA.dN;
-            const _FOLDER = "/" + _CHAPTERDATA.s + '/' + _CHAPTERDATA.v + '/' + _CHAPTERDATA.c + '';
-            const _BASEURL = "https://" + _HOST + '' + _FOLDER + '';
+            const _FOLDER = '/' + _CHAPTERDATA.s + '/' + _CHAPTERDATA.v + '/' + _CHAPTERDATA.c + '';
+            const _BASEURL = 'https://' + _HOST + '' + _FOLDER + '';
             const _IMGLIST = Object.keys(_CHAPTERDATA.p).map(_ITEM => {
                 const _page = _CHAPTERDATA.p[_ITEM];
                 let _path_ = _BASEURL;
                 const _extension = _page.e;
-                const finalpath = "/" + encodeURIComponent(_page.f) + '.' + _extension + '';
+                const finalpath = '/' + encodeURIComponent(_page.f) + '.' + _extension + '';
                 _path_ += finalpath;
                 return _path_;
             });
@@ -42,10 +66,10 @@ const pagescript = `
     });
 `;
 
-@Common.MangaCSS(/^{origin}\/\d+\/[^.]+\.html$/, 'div.h2_titre h2')
+@Common.MangaCSS(/^{origin}\/\d+(-\d+)?\/[^.]+\.html$/, 'div.h2_titre h2')
 @Common.MangasNotSupported()
-@Common.ChaptersSinglePageCSS('div.contenu_volume_manga ul li.chapitre div.chapitre_nom a')
-@Common.PagesSinglePageJS(pagescript, 500)
+@Common.ChaptersSinglePageJS(chapterScript, 200)
+@Common.PagesSinglePageJS(pageScript, 500)
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
