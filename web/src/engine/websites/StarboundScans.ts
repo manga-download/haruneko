ca23846bf5f22e27e2543d0a73d0dc8ed9c30e7e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-08-26 09:53:53 +0200

    StarboundScans : website change

--- web/src/engine/websites/StarboundScans.ts
+++ web/src/engine/websites/StarboundScans.ts
@@ -1,42 +1,19 @@
 import { Tags } from '../Tags';
 import icon from './StarboundScans.webp';
-import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchJSON } from '../platform/FetchProvider';
+import { FetchWindowScript } from '../platform/FetchProvider';
 
-type APIMangas = Array<{
-    id: string,
-    title: string,
-}>
+type JSONMangas = Array<{
+    title: string;
+    permalink: string;
+}>;
 
-type APIChapters = Array<{
-    chapters: {
-        id: string,
-        number: number
-    }[]
-}>
-
-type APIPages = Array<{
-    pages: {
-        id: string,
-        image_url: string
-    }[]
-}>
-
-const ApiToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5b214c3Bvb3NicnNsZGJicHRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMDUwOTYsImV4cCI6MjA2MTc4MTA5Nn0.eHEJnJgzeZkxzCAfIHXg1tYUYk8spWiMamybyVk6FfQ';
-
+@Common.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'h1.project-title')
+@Common.ChaptersSinglePageCSS('div.chapter-item a')
+@Common.PagesSinglePageCSS('div.page-container img.wp-manga-chapter-img')
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-
-    private readonly cdnUrl = 'https://ayomxspoosbrsldbbptc.supabase.co/storage/v1/object/public/webtoon-images/';
-    private readonly apiUrl = 'https://ayomxspoosbrsldbbptc.supabase.co/rest/v1/';
-    private readonly apiRequestInit = {
-        headers: {
-            'Authorization': 'Bearer ' + ApiToken,
-            'ApiKey': ApiToken,
-            'X-Client-Info': 'supabase-js-web/2.49.4',
-        }
-    };
 
     public constructor() {
         super('starboundscans', 'Starbound Scans', 'https://starboundscans.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.French, Tags.Source.Scanlator);
@@ -46,34 +23,8 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/webtoon/[^/]+$`).test(url);
-    }
-
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const mangas = await this.FetchAPI<APIMangas>('./webtoons?select=id,title');
-        return mangas.map(manga => new Manga(this, provider, manga.id, manga.title));
-    }
-
-    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const mangaID = url.split('/').at(-1);
-        const [{ id, title }] = await this.FetchAPI<APIMangas>(`./webtoons?select=id,title&id=eq.${mangaID}`);
-        return new Manga(this, provider, id, title);
-    }
-
-    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const [{ chapters }] = await this.FetchAPI<APIChapters>(`./webtoons?select=chapters(id,number)&id=eq.${manga.Identifier}`);
-        return chapters.sort((a, b) => a.number - b.number)
-            .map(chapter => new Chapter(this, manga, chapter.id.toString(), `Chapitre ${chapter.number}`));
-    }
-
-    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const [{ pages }] = await this.FetchAPI<APIPages>(`./chapters?select=pages(page_number,image_url)&id=eq.${chapter.Identifier}`);
-        return pages.map(page => new Page(this, chapter, new URL(page.image_url, this.cdnUrl)));
-    }
-
-    private async FetchAPI<T extends JSONElement>(endpoint: string): Promise<T> {
-        const request = new Request(new URL(endpoint, this.apiUrl), this.apiRequestInit);
-        return await FetchJSON<T>(request);
+        const mangas = await FetchWindowScript<JSONMangas>(new Request(new URL('/projets', this.URI)), 'mangaData.mangas');
+        return mangas.map(({ permalink, title }) => new Manga(this, provider, new URL(permalink, this.URI).pathname, new DOMParser().parseFromString(title, 'text/html').body.innerText.trim()));
     }
 }
--- web/src/engine/websites/StarboundScans_e2e.ts
+++ web/src/engine/websites/StarboundScans_e2e.ts
@@ -1,5 +1,4 @@
-﻿/* NW.js crash on website initialize => CloudFlare
-import { TestFixture } from '../../../test/WebsitesFixture';
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
 new TestFixture({
     plugin: {
@@ -7,18 +6,17 @@ new TestFixture({
         title: 'Starbound Scans'
     },
     container: {
-        url: 'https://starboundscans.com/webtoon/e7e2c66d-190d-4bbb-b3d0-cabd17522ace',
-        id: 'e7e2c66d-190d-4bbb-b3d0-cabd17522ace',
-        title: `Academy's Genius Swordmaster`
+        url: 'https://starboundscans.com/manga/academy-s-genius-swordmaster/',
+        id: '/manga/academy-s-genius-swordmaster/',
+        title: `Academy’s Genius Swordmaster`
     },
     child: {
-        id: '46bf48aa-f4f8-4832-b804-521232d37c99',
-        title: 'Chapitre 45'
+        id: '/manga/academy-s-genius-swordmaster/chapitre-67/',
+        title: 'Chapitre 67'
     },
     entry: {
-        index: 1,
-        size: 651_094,
-        type: 'image/webp'
+        index: 11,
+        size: 853_304,
+        type: 'image/avif'
     }
 }).AssertWebsite();
-*/
