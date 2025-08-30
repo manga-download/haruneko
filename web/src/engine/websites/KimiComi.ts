ca49d754eb004f3c27f130483f425d2017c33289
Ronny <ronny@local.host>
2025-08-31 00:25:48 +0200

    [kimicomi] adjust website title and logo

--- web/src/engine/websites/KimiComi.ts
+++ web/src/engine/websites/KimiComi.ts
@@ -3,8 +3,9 @@ import icon from './KimiComi.webp';
 import { ComiciViewer } from'./templates/ComiciViewer';
 
 export default class extends ComiciViewer {
+
     public constructor() {
-        super('kimicomi', 'KimiComi', 'https://kimicomi.com', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official);
+        super('kimicomi', 'キミコミ (KimiComi)', 'https://kimicomi.com', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Official);
     }
 
     public override get Icon() {
--- web/src/engine/websites/KimiComi_e2e.ts
+++ web/src/engine/websites/KimiComi_e2e.ts
@@ -3,20 +3,20 @@ import { TestFixture } from '../../../test/WebsitesFixture';
 new TestFixture({
     plugin: {
         id: 'kimicomi',
-        title: 'KimiComi'
+        title: 'キミコミ (KimiComi)',
     },
     container: {
         url: 'https://kimicomi.com/series/8d465f5d76117',
         id: '/series/8d465f5d76117',
-        title: 'Ziggurat'
+        title: 'Ziggurat',
     },
     child: {
         id: '/episodes/6a80a48fefa39/',
-        title: 'Relic:001'
+        title: 'Relic:001',
     },
     entry: {
         index: 0,
         size: 1_469_910,
-        type: 'image/png'
+        type: 'image/png',
     }
 }).AssertWebsite();
