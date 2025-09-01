035ced20f1423a9cc8bb4f71d29f285154a81912
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-01 18:26:05 +0200

    restore QuantumScans from master

---
+++ web/src/engine/websites/QuantumScans.ts
@@ -0,0 +1,23 @@
+import { Tags } from '../Tags';
+import icon from './QuantumScans.webp';
+import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import * as Common from './decorators/Common';
+import * as KeyoApp from './templates/KeyoApp';
+
+// TODO : Change to HeanCMS template
+
+@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, KeyoApp.queryMangaTitle)
+@Common.MangasSinglePagesCSS([ KeyoApp.queryMangaPath ], KeyoApp.queryManga, Common.AnchorInfoExtractor(true))
+@Common.ChaptersSinglePageCSS(KeyoApp.queryChapters, Common.AnchorInfoExtractor(true))
+@Common.PagesSinglePageJS(KeyoApp.PagesScript(), 500)
+@Common.ImageAjax(true)
+export default class extends DecoratableMangaScraper {
+
+    public constructor () {
+        super('quantumscans', 'Quantum Scans', 'https://quantumtoon.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
