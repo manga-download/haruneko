b21dbc223c55d649bf2cbc4197507f3fc0dce9b8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-01 17:10:19 +0200

    Revert "move quantum scans to VTHEME"
    This reverts commit d417d5e7689327ab8dc941bd35d00c6e426fcbaa.

---
+++ web/src/engine/websites/QuantumScans.ts
@@ -0,0 +1,14 @@
+import { Tags } from '../Tags';
+import icon from './QuantumScans.webp';
+import { HeanCMS } from './templates/HeanCMS';
+
+export default class extends HeanCMS {
+    public constructor() {
+        super('quantumscans', 'Quantum Scans', 'https://quantumtoon.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+        this.mediaUrl = 'https://cdn.meowing.org';
+    }
+
+    public override get Icon() {
+        return icon;
+    }
+}
