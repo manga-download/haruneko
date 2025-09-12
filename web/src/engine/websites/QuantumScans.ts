4bf14c3b43e533b10cd63d9add46fd7a966bb8b0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-12 13:07:50 +0000

    Vtheme : improve pages sorting (#1328)
    * Vtheme : improve pages sorting
    
    Depending on a server flag, website could use another sorting method. Was found on at least 2 websites (albeit not always activated)
    
    * requested changes
    
    * fix Vortex & Nyxscans missing pictures
    
    * Move QuantumScans to VTHEME

--- web/src/engine/websites/QuantumScans.ts
+++ web/src/engine/websites/QuantumScans.ts
@@ -1,20 +1,14 @@
 import { Tags } from '../Tags';
-import icon from './QuantumScans.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import * as KeyoApp from './templates/KeyoApp';
+import icon from './QuantumScans.webp';
+import { VTheme } from './templates/VTheme';
 
-// TODO : Change to HeanCMS template
+@Common.PagesSinglePageJS(`[...document.querySelectorAll('div.container div.items-center div.w-full img[loading]')].map(image => image.src);`, 1500)
+export default class extends VTheme {
 
-@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, KeyoApp.queryMangaTitle)
-@Common.MangasSinglePagesCSS([ KeyoApp.queryMangaPath ], KeyoApp.queryManga, Common.AnchorInfoExtractor(true))
-@Common.ChaptersSinglePageCSS(KeyoApp.queryChapters, Common.AnchorInfoExtractor(true))
-@Common.PagesSinglePageJS(KeyoApp.PagesScript(), 500)
-@Common.ImageAjax(true)
-export default class extends DecoratableMangaScraper {
-
-    public constructor () {
+    public constructor() {
         super('quantumscans', 'Quantum Scans', 'https://quantumtoon.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+        this.apiUrl = new URL('https://vapi.quantumtoon.com/api/');
     }
 
     public override get Icon() {
