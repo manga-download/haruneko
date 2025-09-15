6c27f5911083863c718a8ce30bf189e9d0b69ef4
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-15 11:19:53 +0200

    Add QiScans
    fusion of NightScans & QuantumScans

--- web/src/engine/websites/QiScans.ts
+++ web/src/engine/websites/QiScans.ts
@@ -1,14 +1,14 @@
 import { Tags } from '../Tags';
 import * as Common from './decorators/Common';
-import icon from './QuantumScans.webp';
+import icon from './QiScans.webp';
 import { VTheme } from './templates/VTheme';
 
 @Common.PagesSinglePageJS(`[...document.querySelectorAll('div.container div.items-center div.w-full img[loading]')].map(image => image.src);`, 1500)
 export default class extends VTheme {
 
     public constructor() {
-        super('quantumscans', 'Quantum Scans', 'https://quantumtoon.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
-        this.apiUrl = new URL('https://vapi.quantumtoon.com/api/');
+        super('quantumscans', 'Qi Scans', 'https://qiscans.org', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Scanlator);
+        this.apiUrl = new URL('https://api.qiscans.org/api/');
     }
 
     public override get Icon() {
--- web/src/engine/websites/QiScans_e2e.ts
+++ web/src/engine/websites/QiScans_e2e.ts
@@ -3,20 +3,20 @@
 new TestFixture({
     plugin: {
         id: 'quantumscans',
-        title: 'Quantum Scans'
+        title: 'Qi Scans'
     },
     container: {
-        url: 'https://quantumtoon.com/series/celestial-phenomenon',
+        url: 'https://qiscans.org/series/celestial-phenomenon',
         id: '5',
         title: 'Celestial Phenomenon'
     },
     child: {
-        id: '/series/celestial-phenomenon/chapter-47',
-        title: 'Chapter 47'
+        id: '/series/celestial-phenomenon/chapter-76',
+        title: 'Chapter 76'
     },
     entry: {
         index: 1,
-        size: 4_544_610,
+        size: 1_915_066,
         type: 'image/jpeg'
     }
 }).AssertWebsite();
