970e84cddd1c9402ceb2b839faf1c0b34c91e935
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-18 23:11:10 +0200

    requested changes

--- web/src/engine/websites/MangaTek.ts
+++ web/src/engine/websites/MangaTek.ts
@@ -2,16 +2,20 @@ import { Tags } from '../Tags';
 import icon from './MangaTek.webp';
 import { type Manga, Chapter, DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchWindowScript } from '../platform/FetchProvider';
 
-type HydratedManga = {
-    manga: {
-        MangaChapters: {
-            chapter_number: string;
-            title: string;
-        }[]
-    }
+type HydratedChapter = {
+    chapter_number: string;
+    title: string;
 };
+
+const chapterScript = `
+    new Promise(resolve => {
+        const element = document.querySelector('astro-island[component-url*="MangaChaptersLoader"]');
+        element.hydrator = () => (_, props) => resolve(props.manga.MangaChapters);
+        element.hydrate();
+    });
+`;
 
 @Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, 'img#mangaCover', (element: HTMLImageElement) => element.alt.trim())
 @Common.MangasMultiPageCSS('/manga-list?page={page}', 'div.manga-card a[dir]')
@@ -28,39 +32,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { manga: { MangaChapters } } = await this.FetchAstro<HydratedManga>(new URL(manga.Identifier, this.URI), data => 'manga' in data);
-        return MangaChapters.map(({ title, chapter_number: number }) => new Chapter(this, manga, `/reader/${manga.Identifier.split('/').at(-1)}/${number}`, title));
-    }
-
-    private async FetchAstro<T extends JSONElement>(url: URL, predicate: (data: JSONObject<JSONElement> | JSONArray<JSONElement>) => unknown): Promise<T> {
-        const payloads = await FetchCSS<HTMLElement>(new Request(url), 'astro-island');
-        const goodElement = payloads.find(el => {
-            const jsonProps = JSON.parse(el.getAttribute('props') ?? '');
-            return predicate(jsonProps);
-        });
-        return !goodElement ? {} as T : this.Deserialize(JSON.parse(goodElement.getAttribute('props'))) as T;
-    }
-
-    private Deserialize(obj: JSONElement) {
-        const revivers = {
-            0: t => this.Deserialize(t),
-            1: t => hydrateArray(t),
-            /* 2: t => new RegExp(t),
-            3: t => new Date(t),
-            4: t => new Map(hydrateArray(t)),
-            5: t => new Set(hydrateArray(t)),
-            6: t => BigInt(t),
-            7: t => new URL(t),
-            8: t => new Uint8Array(t),
-            9: t => new Uint16Array(t),
-            10: t => new Uint32Array(t),
-            11: t => 1 / 0 * t*/
-        };
-        const hydrate = t => {
-            let [reviverType, value] = t;
-            return reviverType in revivers ? revivers[reviverType](value) : void 0;
-        };
-        const hydrateArray = t => t.map(hydrate);
-        return typeof obj != 'object' || obj === null ? obj : Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, hydrate(value)]));
+        const chapters = await FetchWindowScript<HydratedChapter[]>(new Request(new URL(manga.Identifier, this.URI)), chapterScript, 1500);
+        return chapters.map(({ title, chapter_number: number }) => new Chapter(this, manga, `/reader/${manga.Identifier.split('/').at(-1)}/${number}`, title));
     }
 }
