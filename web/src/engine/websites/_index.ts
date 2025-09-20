8ed50d05cd148dc6f57928729c5d3db471066098
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-20 14:01:20 +0200

    Add MangaTaro
    https://github.com/manga-download/hakuneko/issues/8193

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -428,6 +428,7 @@ export { default as MangaSushi } from './MangaSushi';
 export { default as MangaSusuku } from './MangaSusuku';
 export { default as MangaSwat } from './MangaSwat';
 export { default as MangaTales } from './MangaTales';
+export { default as MangaTaro } from './MangaTaro';
 export { default as Mangatellers } from './Mangatellers';
 export { default as MangaTepesi } from './MangaTepesi';
 export { default as MangaTilkisi } from './MangaTilkisi';
