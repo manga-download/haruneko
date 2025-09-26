e2f577799f1001fca0f22205a19788771639e96f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-09-26 10:50:03 +0000

    update domains  & remove dead websites (#1363)
    * Remove CypherScans
    
    redirect to ErosScans
    
    * update domains

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -99,7 +99,6 @@ export { default as CrunchyScan } from './CrunchyScan';
 export { default as CulturedWorks } from './CulturedWorks';
 export { default as CuuTruyen } from './CuuTruyen';
 export { default as CyComi } from './CyComi';
-export { default as CypherScans } from './CypherScans';
 export { default as DankeFursLesen } from './DankeFursLesen';
 export { default as DatgarScan } from './DatgarScan';
 export { default as DayComics } from './DayComics';
