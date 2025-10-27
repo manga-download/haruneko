21441d0aa8d80ba04698725085cbb510d3d31c11
Ronny <ronny@local.host>
2025-10-27 07:42:58 +0100

    [cmangax] updated domain

--- web/src/engine/websites/CMangax.ts
+++ web/src/engine/websites/CMangax.ts
@@ -37,7 +37,7 @@ export default class extends DecoratableMangaScraper {
     }
 
     public constructor() {
-        super('cmangax', 'CMangax', 'https://cmangax6.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Vietnamese, Tags.Source.Aggregator);
+        super('cmangax', 'CMangax', 'https://cmangax7.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Vietnamese, Tags.Source.Aggregator, Tags.Accessibility.DomainRotation);
     }
 
     public override get Icon() {
