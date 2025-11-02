b32bdc43265b2c0dcd1cacdf30642662308fdfa9
Zephyris <200662396+Zephyris-Pro@users.noreply.github.com>
2025-11-02 19:07:32 +0100

    Fix NicoManga not working

--- web/src/engine/websites/NicoManga.ts
+++ web/src/engine/websites/NicoManga.ts
@@ -1,24 +1,33 @@
 import { Tags } from '../Tags';
 import icon from './NicoManga.webp';
-import { FetchJSON } from '../platform/FetchProvider';
-import { DecoratableMangaScraper, type MangaPlugin, Manga, Chapter, type Page } from '../providers/MangaPlugin';
+import { FetchCSS, FetchJSON } from '../platform/FetchProvider';
+import { DecoratableMangaScraper, type MangaPlugin, Manga, Chapter, Page } from '../providers/MangaPlugin';
 import * as FlatManga from './templates/FlatManga';
 import * as Common from './decorators/Common';
 
-type APIMangas = {
-    manga_list: {
-        name: string,
-        slug: string
-    }[],
-    lang: {
-        manga_slug: string
-    }
+type ApiManga = {
+    name: string,
+    slug: string,
+    url: string,
 }
 
-type APIChapters = {
+type APIMangas = {
+    data: ApiManga[]
+}
+
+type APIChapter = {
     chapter: string,
     name: string,
-}[];
+}
+
+type APIChapters = APIChapter[];
+
+type APIImage = {
+    url: string,
+}
+type APIImages = {
+    images: APIImage[],
+}
 
 @Common.MangaCSS<HTMLHeadingElement>(FlatManga.pathManga, FlatManga.queryMangaTitle, (head, uri) => ({ id: uri.pathname, title: head.innerText.replace(/\(MANGA\)$/i, '').trim() }))
 @Common.ImageAjax()
@@ -35,38 +44,32 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const mangaList: Manga[] = [];
-        for (let page = 1, run = true; run; page++) {
-            const mangas = await this.GetMangasFromPageAJAX(provider, page);
-            mangaList.isMissingLastItemFrom(mangas) ? mangaList.push(...mangas) : run = false;
-        }
-        return mangaList.distinct();
-    }
-
-    private async GetMangasFromPageAJAX(provider: MangaPlugin, page: number): Promise<Manga[]> {
-        const request = new Request(new URL(`/app/manga/controllers/cont.display.homeTopday.php?page=${page}`, this.URI), {
-            headers: {
-                Referer: new URL('/manga-list.html', this.URI.origin).href,
-            }
-        });
-        const { manga_list, lang: { manga_slug } } = await FetchJSON<APIMangas>(request);
-        return manga_list.map(manga => new Manga(this, provider, `/${manga_slug}-${manga.slug}.html`, manga.name.trim()));
+        const request = new Request(new URL("/app/manga/controllers/cont.allmanga.php", this.URI));
+        const { data } = await FetchJSON<APIMangas>(request);
+        return data.map(manga => new Manga(this, provider, `/${manga.slug}.html`, manga.name.trim()));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const slug = FlatManga.ExtractSlug(manga);
+        const slug = manga.Identifier.replace(".html", "").replace("/", "");
         const uri = new URL(this.pathChapters.replace('{manga}', slug), this.URI);
-        const chapters = await FetchJSON<APIChapters>(new Request(uri, { headers: { Referer: new URL(manga.Identifier, this.URI).href } }));
+        const chapters = await FetchJSON<APIChapters>(new Request(uri));
         return chapters.map(chapter => new Chapter(this, manga, `/read-${slug}-chapter-${chapter.chapter}.html`, chapter.name));
     }
 
+    public async ExtractImages(chapter: Chapter): Promise<APIImage[]> {
+        const uri = new URL(chapter.Identifier, this.URI);
+        const scripts = await FetchCSS<HTMLScriptElement>(new Request(uri, {headers: {"Cookie": "smartlink_shown_guest=1"}}), "script");
+        const cid = scripts.map(script => {
+            const match = script.textContent.match(/loadImagesChapter\s*\(\s*(\d+)\s*,\s*'listImgs'\s*\)/);
+            return match ? match[1] : null;
+        }).filter(cid => cid !== null);
+        const imageuri = new URL(`/caches/chapterboth/${cid}.json`, this.URI);
+        const images = await FetchJSON<APIImages>(new Request(imageuri));
+        return images.images;
+    }
+
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        return FlatManga.FetchPagesAJAX.call(
-            this,
-            chapter,
-            /loadImagesChapter\s*\(\s*(\d+)\s*,\s*'listImgs'\s*\)/g,
-            '/app/manga/controllers/cont.imgsList.php?cid={chapter}',
-            FlatManga.queryPages,
-            img => img.dataset.srcset);
+        const images = await this.ExtractImages(chapter);
+        return images.map(img => new Page(this, chapter, new URL(img.url)));
     }
 }
