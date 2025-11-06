5460d31f6d2b8ed42e3f3e303c860380200d66ae
Ronny <27136773+ronny-x@users.noreply.github.com>
2025-11-06 02:10:52 +0100

    [weebdex]: add rate limit and improve manga list

--- web/src/engine/websites/WeebDex.ts
+++ web/src/engine/websites/WeebDex.ts
@@ -1,6 +1,8 @@
 import { Tags } from '../Tags';
 import icon from './WeebDex.webp';
 import { FetchJSON } from '../platform/FetchProvider';
+import { TaskPool, Priority } from '../taskpool/TaskPool';
+import { RateLimit } from '../taskpool/RateLimit';
 import { DecoratableMangaScraper, type MangaPlugin, Manga, Chapter, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 
@@ -36,10 +38,11 @@ type APIPages = {
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
-    readonly #apiURL = 'https://api.weebdex.org';
+    readonly #apiTaskPool = new TaskPool(1, new RateLimit(4, 1));
 
     public constructor() {
         super('weebdex', 'WeebDex', 'https://weebdex.org', Tags.Media.Manga, Tags.Language.Multilingual, Tags.Source.Aggregator);
+        this.imageTaskPool
     }
 
     public override get Icon() {
@@ -47,23 +50,34 @@ export default class extends DecoratableMangaScraper {
     }
 
     public async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        return Array.fromAsync(async function* () {
-            for (let page = 1, run = true; run; page++) {
-                const uri = new URL(`/manga?sort=title&order=asc&hasChapters=true&limit=98&page=${page}`, this.#apiURL);
-                const { data } = await FetchJSON<APIMangas>(new Request(uri));
-                const mangas = !data ? [] : data.map(({ id, title }) => {
-                    return new Manga(this, provider, id, title);
-                });
-                mangas.length > 0 ? yield* mangas : run = false;
+        type This = typeof this;
+        return Array.fromAsync(async function* (this: This) {
+            for (let year = 1900; year <= new Date().getFullYear(); year++) {
+                for (let page = 1, run = true; run; page++) {
+                    const { data } = await this.#FetchAPI<APIMangas>('/manga', [
+                        ['contentRating', 'safe'],
+                        ['contentRating', 'suggestive'],
+                        ['contentRating', 'erotica'],
+                        ['contentRating', 'pornographic'],
+                        ['sort', 'views'],
+                        ['hasChapters', 'true'],
+                        ['yearFrom', `${year === 1900 ? 0 : year}`],
+                        ['yearTo', `${year}`],
+                        ['page', `${page}`],
+                        ['limit', '100'],
+                    ]);
+                    const mangas = !data ? [] : data.map(({ id, title }) => new Manga(this, provider, id, title));
+                    mangas.length > 0 ? yield* mangas : run = false;
+                }
             }
         }.call(this));
     }
 
     public async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        return Array.fromAsync(async function* () {
+        type This = typeof this;
+        return Array.fromAsync(async function* (this: This) {
             for (let page = 1, run = true; run; page++) {
-                const uri = new URL(`/manga/${manga.Identifier}/chapters?limit=250&page=${page}`, this.#apiURL);
-                const { data } = await FetchJSON<APIChapters>(new Request(uri));
+                const { data } = await this.#FetchAPI<APIChapters>(`/manga/${manga.Identifier}/chapters`, { limit: '500', page: `${page}`});
                 const chapters = !data ? [] : data.map(({ id, volume, chapter, title, language }) => {
                     title = [
                         volume ? 'Vol. ' + volume : null,
@@ -79,8 +93,13 @@ export default class extends DecoratableMangaScraper {
     }
 
     public async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const uri = new URL('/chapter/' + chapter.Identifier, this.#apiURL);
-        const { node, data } = await FetchJSON<APIPages>(new Request(uri));
+        const { node, data } = await this.#FetchAPI<APIPages>('/chapter/' + chapter.Identifier);
         return data.map(({ name }) => new Page(this, chapter, new URL(`/data/${chapter.Identifier}/${name}`, node)));
+    }
+
+    async #FetchAPI<T extends JSONElement>(endpoint: string, search: string[][] | Record<string, string> = {}): Promise<T> {
+        const uri = new URL(endpoint, 'https://api.weebdex.org');
+        uri.search = new URLSearchParams(search).toString();
+        return this.#apiTaskPool.Add<T>(() => FetchJSON<T>(new Request(uri)), Priority.Normal);
     }
 }
