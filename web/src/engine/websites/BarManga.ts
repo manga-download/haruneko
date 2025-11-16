f7c777c8fd5dcad6132ba07b039193dee1a1485a
Ronny <27136773+ronny-x@users.noreply.github.com>
2025-11-16 04:20:43 +0100

    [barmanga] evaluate instead of regex parsing

--- web/src/engine/websites/BarManga.ts
+++ web/src/engine/websites/BarManga.ts
@@ -1,13 +1,13 @@
 import { Tags } from '../Tags';
 import icon from './BarManga.webp';
-import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import { FetchWindowPreloadScript } from '../platform/FetchProvider';
+import { DecoratableMangaScraper, type Chapter, Page } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
-import { FetchCSS } from '../platform/FetchProvider';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type a')
 @Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv2('span.chapter-link-inner', (element) => ({ id: new URL(element.dataset.href).pathname, title: element.textContent.trim() }))
+@Madara.ChaptersSinglePageAJAXv2('span.chapter-link-inner', element => ({ id: new URL(element.dataset.href).pathname, title: element.textContent.trim() }))
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -20,18 +20,14 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const regex = /var\s+([a-zA-Z_$][a-zA-Z0-9_$]*)\s*=\s*'([0-9a-fA-F]+)'/g;
-        const scripts = (await FetchCSS<HTMLScriptElement>(new Request(new URL(chapter.Identifier, this.URI)), 'div.reading-content script:not([src])'))
-            .filter(script => script.text.indexOf('hexToString') > 0);
-        return scripts.map(script => {
-            let match = undefined;
-            let finalUrl = '';
-
-            while ((match = regex.exec(script.text)) !== null) {
-                finalUrl += match[2];
-            }
-            return new Page(this, chapter, new URL(String.fromCharCode(...finalUrl.match(/.{1,2}/g).map(e => parseInt(e, 16)))));
-        });
-
+        const data = await FetchWindowPreloadScript<string[]>(new Request(new URL(chapter.Identifier, this.URI)), () => {
+            URL.createObjectURL = obj => obj.toString();
+            XMLHttpRequest.prototype.open = function (_: string, url: string) {
+                Object.defineProperty(this, 'status', { value: 200 });
+                Object.defineProperty(this, 'response', { value: url });
+                this.onload();
+            };
+        }, () => [...document.querySelectorAll<HTMLImageElement>('.reading-content .page-break img[id^="preload-"]')].map(({ src }) => src), 2500);
+        return data.map(link => new Page(this, chapter, new URL(link)));
     }
 }
