e16c3274876ec29629c5268a269bf929f3a7aa2c
MikeZeDev <244155418+MikeZeDev2@users.noreply.github.com>
2025-11-21 10:20:07 +0100

    fix merge conflict

--- web/src/engine/websites/KLMangash.ts
+++ web/src/engine/websites/KLMangash.ts
@@ -5,7 +5,7 @@ import { Zing92Base } from './templates/Zing92Base';
 export default class extends Zing92Base {
 
     public constructor () {
-        super('klmangash', 'KLManga(.sh)', 'https://klmanga.nu', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
+        super('klmangash', 'KLManga(.sh)', 'https://klmanga.pl', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
         this.decodeImageAjaxAction = 'decode_images_g';
         this.zingParamsScript = `new Promise(resolve => resolve({ nonce: zing.nonce_a, apiURL: zing.ajax_url }));`;
         this.nonceParameterName = 'nonce_a';
