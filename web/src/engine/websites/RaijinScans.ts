39cc99a77a3ebb56df486d50ae05b131b6a8e8ce
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-08 21:02:16 +0100

    fix: RaijinScans : getting pictures urls

--- web/src/engine/websites/RaijinScans.ts
+++ web/src/engine/websites/RaijinScans.ts
@@ -1,26 +1,28 @@
 import { Tags } from '../Tags';
 import icon from './RaijinScans.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
-
-function PageExtractor(element: HTMLElement) {
-    const xored = atob(element.dataset.v.split('').reverse().join(''));
-    return atob(xored.split('').map(value => String.fromCharCode(value.charCodeAt(0) ^ 93)).join(''));
-}
+import { DRMProvider } from './RaijinScans.DRM';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'div.serie-info h1.serie-title')
 @Madara.MangasMultiPageAJAX()
 @Madara.ChaptersSinglePageAJAXv2()
-@Common.PagesSinglePageCSS('div.protected-image-data', PageExtractor)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
+    readonly #drm: DRMProvider;
 
     public constructor() {
         super('raijinscans', 'RaijinScans', 'https://raijin-scans.fr', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.French, Tags.Source.Scanlator);
+        this.#drm = new DRMProvider();
     }
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const data = await this.#drm.CreatePageLinks(new URL(chapter.Identifier, this.URI));
+        return data.map(link => new Page(this, chapter, new URL(link, this.URI)));
     }
 }
