8812181eb07c89f6473781174a78f8578b1bf174
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-11 14:13:05 +0100

    fix: Nicomanga : website changes
    Closes https://github.com/manga-download/haruneko/issues/1468

--- web/src/engine/websites/NicoManga.ts
+++ web/src/engine/websites/NicoManga.ts
@@ -1,33 +1,17 @@
 import { Tags } from '../Tags';
 import icon from './NicoManga.webp';
-import { FetchCSS, FetchJSON, FetchWindowScript } from '../platform/FetchProvider';
-import { DecoratableMangaScraper, type MangaPlugin, Manga, Chapter, Page } from '../providers/MangaPlugin';
+import { FetchWindowScript } from '../platform/FetchProvider';
+import { DecoratableMangaScraper, type Manga, type Chapter } from '../providers/MangaPlugin';
 import * as FlatManga from './templates/FlatManga';
 import * as Common from './decorators/Common';
-
-type APIMangas = {
-    data: {
-        url: string;
-        name: string;
-    }[];
-};
-
-type APIChapters = {
-    chapter: string;
-    name: string;
-}[];
-
-type APIPages = {
-    images: {
-        url: string;
-    }[];
-}
 
 function CleanMangaTitle(title: string): string {
     return title.replace(/\(Manga\)/i, '').replace(/- RAW/i, '').trim();
 }
 
-@Common.MangaCSS<HTMLSpanElement>(/^{origin}\/manga-[^/]+\.html$/, 'main nav ul span:last-of-type', (span, uri) => ({ id: uri.pathname, title: CleanMangaTitle(span.innerText) }))
+@Common.MangaCSS<HTMLSpanElement>(/^{origin}\/manga-[^/]+\.html$/, 'div.breadcrumb-navigation span:last-of-type', (span, uri) => ({ id: uri.pathname, title: CleanMangaTitle(span.innerText) }))
+@Common.MangasMultiPageCSS<HTMLAnchorElement>('a.manga-title', Common.PatternLinkGenerator('/manga-post.html?page={page}'), 0, anchor => ({ id: anchor.pathname, title: CleanMangaTitle(anchor.text) }))
+@Common.PagesSinglePageJS(`[...document.querySelectorAll('div.chapter-image-wrapper img')].map(image => image.dataset.src?.trim() ?? image.src);`)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -45,21 +29,9 @@ export default class extends DecoratableMangaScraper {
         });
     }
 
-    public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const { data } = await FetchJSON<APIMangas>(new Request(new URL('/app/manga/controllers/cont.allmanga.php', this.URI)));
-        return data.map(({ url, name }) => new Manga(this, provider, url, CleanMangaTitle(name)));
-    }
-
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
         const slug = FlatManga.ExtractSlug(manga);
-        const uri = new URL(`/app/manga/controllers/cont.Listchapter.php?slug=${slug}`, this.URI);
-        const chapters = await FetchJSON<APIChapters>(new Request(uri));
-        return chapters.map(({ chapter, name }) => new Chapter(this, manga, `/read-${slug}-chapter-${chapter}.html`, name));
+        return Common.FetchChaptersSinglePageCSS.call(this, manga, 'ul.list-chapters a', `/app/manga/controllers/cont.Listchapter.php?slug=${slug}`, Common.AnchorInfoExtractor(true));
     }
 
-    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const [cid] = await FetchCSS<HTMLInputElement>(new Request(new URL(chapter.Identifier, this.URI)), 'input#chapter');
-        const { images } = await FetchJSON<APIPages>(new Request(new URL(`/caches/chapterboth/${cid.value}.json`, this.URI)));
-        return images.map(({ url }) => new Page(this, chapter, new URL(url)));
-    }
 }
