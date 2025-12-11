f07ac9c52e50ef26dab99611cc1af6eab6560de3
Ronny <27136773+ronny-x@users.noreply.github.com>
2025-12-11 22:40:37 +0100

    website(nicomanga): use decorator

--- web/src/engine/websites/NicoManga.ts
+++ web/src/engine/websites/NicoManga.ts
@@ -1,7 +1,7 @@
 import { Tags } from '../Tags';
 import icon from './NicoManga.webp';
 import { FetchWindowScript } from '../platform/FetchProvider';
-import { DecoratableMangaScraper, type Manga, type Chapter } from '../providers/MangaPlugin';
+import { DecoratableMangaScraper, type Manga} from '../providers/MangaPlugin';
 import * as FlatManga from './templates/FlatManga';
 import * as Common from './decorators/Common';
 
@@ -9,8 +9,13 @@ function CleanMangaTitle(title: string): string {
     return title.replace(/\(Manga\)/i, '').replace(/- RAW/i, '').trim();
 }
 
+function ChapterLinkResolver(this: DecoratableMangaScraper, manga: Manga) {
+    return new URL(`/app/manga/controllers/cont.Listchapter.php?slug=${FlatManga.ExtractSlug(manga)}`, this.URI);
+}
+
 @Common.MangaCSS<HTMLSpanElement>(/^{origin}\/manga-[^/]+\.html$/, 'div.breadcrumb-navigation span:last-of-type', (span, uri) => ({ id: uri.pathname, title: CleanMangaTitle(span.innerText) }))
 @Common.MangasMultiPageCSS<HTMLAnchorElement>('a.manga-title', Common.PatternLinkGenerator('/manga-post.html?page={page}'), 0, anchor => ({ id: anchor.pathname, title: CleanMangaTitle(anchor.text) }))
+@Common.ChaptersSinglePageCSS('ul.list-chapters a', ChapterLinkResolver, Common.AnchorInfoExtractor(true))
 @Common.PagesSinglePageJS(`[...document.querySelectorAll('div.chapter-image-wrapper img')].map(image => image.dataset.src?.trim() ?? image.src);`)
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
@@ -24,14 +29,6 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override Initialize(): Promise<void> {
-        return FetchWindowScript(new Request(this.URI), () => {
-            window.cookieStore.set('unlock_chapter_guest', '1');
-        });
+        return FetchWindowScript(new Request(this.URI), () => window.cookieStore.set('unlock_chapter_guest', '1'));
     }
-
-    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const slug = FlatManga.ExtractSlug(manga);
-        return Common.FetchChaptersSinglePageCSS.call(this, manga, 'ul.list-chapters a', `/app/manga/controllers/cont.Listchapter.php?slug=${slug}`, Common.AnchorInfoExtractor(true));
-    }
-
 }
