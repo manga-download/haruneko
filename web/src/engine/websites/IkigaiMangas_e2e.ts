bd303b46cb3c370730ab47bf9b30f0173f0c9072
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-12 09:55:59 +0100

    fix: update domains

--- web/src/engine/websites/IkigaiMangas_e2e.ts
+++ web/src/engine/websites/IkigaiMangas_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Ikigai Mangas'
     },
     container: {
-        url: 'https://viralikigai.melauroral.com/series/jinx-manhwa/',
+        url: 'https://viralikigai.cookingrecipes500.store/series/jinx-manhwa/',
         id: '/series/jinx-manhwa/',
         title: 'Jinx'
     },
