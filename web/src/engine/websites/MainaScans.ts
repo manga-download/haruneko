7fc92ab5bd06a7a404d1c8074c4d083c0ea050f7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-13 14:59:07 +0100

    Update MainaScans.ts

--- web/src/engine/websites/MainaScans.ts
+++ web/src/engine/websites/MainaScans.ts
@@ -1,8 +1,4 @@
-import { Tags } from '../Tags';
 import icon from './MainaScans.webp';
-import { FetchGraphQL, FetchNextJS } from '../platform/FetchProvider';
-import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../providers/MangaPlugin';
-import * as Common from './decorators/Common';
 import LetonaScans from './LetonaScans';
 
 @Common.ImageAjax()
