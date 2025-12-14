14a9961398e724bce0002373cd41b959a16f72b8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-14 10:28:09 +0100

    fix: RaijinsScans : getting pages (again)

--- web/src/engine/websites/RaijinScans.DRM.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class DRMProvider {
-    CreatePageLinks(chapterURL: URL): Promise<string[]>;
-}
--- web/src/engine/websites/RaijinScans.ts
+++ web/src/engine/websites/RaijinScans.ts
@@ -3,17 +3,13 @@ import icon from './RaijinScans.webp';
 import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
-
-import { DRMProvider } from './RaijinScans.DRM';
-import { FetchWindowScript } from '../platform/FetchProvider';
+import { FetchHTML, FetchWindowScript } from '../platform/FetchProvider';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'div.serie-info h1.serie-title')
 @Madara.MangasMultiPageAJAX()
 @Madara.ChaptersSinglePageAJAXv2()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-
-    readonly #drm = new DRMProvider();
 
     public constructor() {
         super('raijinscans', 'RaijinScans', 'https://raijin-scans.fr', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.French, Tags.Source.Scanlator);
@@ -28,7 +24,19 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const data = await this.#drm.CreatePageLinks(new URL(chapter.Identifier, this.URI));
-        return data.map(link => new Page(this, chapter, new URL(link, this.URI)));
+        const doc = await FetchHTML(new Request(new URL(chapter.Identifier, this.URI)));
+        const rmd = doc.body.innerHTML.match(/window\._rmd\s*=\s*["']([^"']+)["']/).at(1).replaceAll('-', '+').replaceAll('_', '/');
+        const rmk = doc.body.innerHTML.match(/window\._rmk\s*=\s*["']([^"']+)["']/).at(1);
+
+        // XOR(rmd) with XORED key rmk => JSON array of url strings
+        const pages = JSON.parse(new TextDecoder().decode(this.XOR(rmd, this.XOR(rmk, new Uint8Array([90, 60, 126, 29, 159, 178, 78, 106]))))) as string[];
+        return pages.map(page => new Page(this, chapter, new URL(page)));
     }
+
+    private XOR(data: string, key: Uint8Array): Uint8Array {
+        //Don't use GetBytesFromB64 because their B64 is sometimes unpadded and fails our integrity checks
+        const decoded = Uint8Array.from(atob(data.replace(/\s+/g, '')), c => c.charCodeAt(0));
+        return decoded.map((value, index) => value ^ key[index % key.length]);
+    }
+
 }
