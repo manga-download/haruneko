bf4f4575aea0c310145f08713740e4e0f890257c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2025-12-19 09:37:41 +0000

    fix: domains update and fixes (#1482)

--- web/src/engine/websites/Raw18.ts
+++ web/src/engine/websites/Raw18.ts
@@ -11,7 +11,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('haremu18', 'Raw18', 'https://raw18.buzz', Tags.Media.Manhwa, Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator, Tags.Rating.Pornographic);
+        super('haremu18', 'Raw18', 'https://raw18.fit', Tags.Media.Manhwa, Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator, Tags.Rating.Pornographic);
     }
 
     public override get Icon() {
