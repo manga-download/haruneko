fc75d2eea3f48e3d09d3bc8f4bb2456282571ee2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-02 10:51:27 +0000

    friday maintenance : remove dead websites & update domains (#1500)
    * fix : cmangax fix api
    
    * remove dead websites
    
    MangaEfendisi, MangaSefiri, TitanManga, VortexScanFree
    
    * update domains
    
    * domain update
    
    * provisional update for hunterscans

--- web/src/engine/websites/HunterScan.ts
+++ web/src/engine/websites/HunterScan.ts
@@ -4,7 +4,7 @@ import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
 
-@Madara.MangaCSS(/^{origin}\/series\/[^/]+\/$/, 'div.post-title h1')
+@Madara.MangaCSS(/^{origin}\/read\/[^/]+\/$/, 'div.post-title h1')
 @Madara.MangasMultiPageAJAX()
 @Madara.ChaptersSinglePageAJAXv2()
 @Madara.PagesSinglePageCSS()
