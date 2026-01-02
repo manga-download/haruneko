04a6a804eb53185eb8892ccb1d6bc3f3c3a61eec
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-02 13:27:00 +0100

    Update KLMangash.ts

--- web/src/engine/websites/KLMangash.ts
+++ web/src/engine/websites/KLMangash.ts
@@ -5,7 +5,7 @@ import { Zing92Base } from './templates/Zing92Base';
 export default class extends Zing92Base {
 
     public constructor () {
-        super('klmangash', 'KLManga(.sh)', 'https://klmanga.skin', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
+        super('klmangash', 'KLManga(.sh)', 'https://klmanga.sale', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
         this.decodeImageAjaxAction = 'decode_images_g';
         this.zingParamsScript = `new Promise(resolve => resolve({ nonce: zing.nonce_a, apiURL: zing.ajax_url }));`;
         this.nonceParameterName = 'nonce_a';
