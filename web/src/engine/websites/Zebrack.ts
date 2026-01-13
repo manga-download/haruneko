2af61a7f1ff51dd6d77c80ff16db919c480b4328
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-13 19:33:16 +0100

    Update Zebrack.ts

--- web/src/engine/websites/Zebrack.ts
+++ web/src/engine/websites/Zebrack.ts
@@ -7,6 +7,8 @@ import { FetchProto, FetchWindowScript } from '../platform/FetchProvider';
 import type { Priority } from '../taskpool/TaskPool';
 import { Exception } from '../Error';
 import { WebsiteResourceKey as R } from '../../i18n/ILocale';
+import { GetTypedData } from './decorators/Common';
+import { GetBytesFromHex } from '../BufferEncoder';
 
 type ZebrackResponse = {
     titleDetailView: TitleDetailView;
@@ -139,7 +141,7 @@ export default class extends DecoratableMangaScraper {
         if (/^\/magazine\//.test(uri.pathname)) {
             const magazineId = uri.pathname.match(/\/magazine\/(\d+)/).at(1);
             const { magazine: { name } } = await FetchProto<MagazineDetailViewV3>(new Request(new URL(`./api/v3/magazine_detail?os=browser&magazine_id=${magazineId}`, this.apiURL)), protoTypes, 'Zebrack.MagazineDetailViewV3');
-            return new Manga(this, provider, uri.pathname.replace(/detail$/, ''), name);
+            return new Manga(this, provider, uri.pathname.replace(/\/detail$/, ''), name);
 
         } else if (/^\/gravure\//.test(uri.pathname)) {
             const gravureId = uri.pathname.match(/\/gravure\/(\d+)/).at(-1);
@@ -197,41 +199,44 @@ export default class extends DecoratableMangaScraper {
 
         switch (type) {
             case 'chapter': {
-                const { pages } = await this.FetchViewer<ChapterViewerViewV3>(type, mangaId, chapterId, secretKey);
+                const { pages } = await this.FetchViewer<ChapterViewerViewV3>(type, mangaId, chapterId, secretKey, 'Zebrack.ChapterViewerViewV3');
                 return pages?.filter(page => page.image?.imageUrl)
                     .map(({ image: { imageUrl, encryptionKey } }) => new Page<PageParam>(this, chapter, new URL(imageUrl), { encryptionKey }));
             };
 
             case 'gravure': {
-                let data = await this.FetchViewer<GravureViewerViewV3>(type, mangaId, chapterId, secretKey);
-                if (!data.images) data = await this.FetchViewer<GravureViewerViewV3>(type, mangaId, chapterId, secretKey, true);
+                let data = await this.FetchViewer<GravureViewerViewV3>(type, mangaId, chapterId, secretKey, 'Zebrack.GravureViewerViewV3');
+                if (!data.images) data = await this.FetchViewer<GravureViewerViewV3>(type, mangaId, chapterId, secretKey, 'Zebrack.GravureViewerViewV3', true);
                 if (data.images) {
                     return data.images.map(({ imageUrl, encryptionKey }) => new Page<PageParam>(this, chapter, new URL(imageUrl), { encryptionKey }));
-                }
+                };
+                break;
             };
 
             case 'magazine': {
-                let data = await this.FetchViewer<ZebrackResponse>(type, mangaId, chapterId, secretKey);
-                if (!data.magazineViewerView) data = await this.FetchViewer<ZebrackResponse>(type, mangaId, chapterId, secretKey, true);
+                let data = await this.FetchViewer<ZebrackResponse>(type, mangaId, chapterId, secretKey, this.responseRootType);
+                if (!data.magazineViewerView) data = await this.FetchViewer<ZebrackResponse>(type, mangaId, chapterId, secretKey, this.responseRootType, true);
                 if (data.magazineViewerView?.images) {
                     return data.magazineViewerView.images?.filter(page => page.imageUrl)
                         .map(({ imageUrl, encryptionKey }) => new Page<PageParam>(this, chapter, new URL(imageUrl), { encryptionKey }));
                 }
+                break;
             };
             case 'volume': {
-                let data = await this.FetchViewer<VolumeViewerViewV3>(type, mangaId, chapterId, secretKey);
-                if (!data.pages) data = await this.FetchViewer<VolumeViewerViewV3>(type, mangaId, chapterId, secretKey, true);
+                let data = await this.FetchViewer<VolumeViewerViewV3>(type, mangaId, chapterId, secretKey, 'Zebrack.VolumeViewerViewV3');
+                if (!data.pages) data = await this.FetchViewer<VolumeViewerViewV3>(type, mangaId, chapterId, secretKey, 'Zebrack.VolumeViewerViewV3', true);
                 if (data.pages) {
                     return data.pages.filter(page => page.image?.imageUrl)
                         .map(({ image: { imageUrl, encryptionKey } }) => new Page<PageParam>(this, chapter, new URL(imageUrl), { encryptionKey }));
                 }
+                break;
             };
         }
 
         throw new Exception(R.Plugin_Common_Chapter_InvalidError);
     }
 
-    private async FetchViewer<T extends JSONElement>(type: string, mangaId: string, issueId: string, secretKey: string, isTrial: boolean = false): Promise<T> {
+    private async FetchViewer<T extends JSONElement>(type: string, mangaId: string, issueId: string, secretKey: string, message: string, isTrial: boolean = false): Promise<T> {
         const searchParams = new URLSearchParams({
             secret: secretKey,
             'is_trial': isTrial ? '1' : '0',
@@ -253,52 +258,44 @@ export default class extends DecoratableMangaScraper {
                     headers: {
                         'content-type': 'application/x-www-form-urlencoded'
                     }
-                }), protoTypes, 'Zebrack.ChapterViewerViewV3');
+                }), protoTypes, message);
             };
 
             case 'volume': {
                 searchParams.set('title_id', mangaId);
                 searchParams.set('volume_id', issueId);
                 url = new URL('./api/v3/manga_volume_viewer', this.apiURL);
-                url.search = searchParams.toString();
-                return await FetchProto<T>(new Request(url), protoTypes, 'Zebrack.VolumeViewerViewV3');
+                break;
             };
 
             case 'magazine': {
                 searchParams.set('magazine_id', mangaId);
                 searchParams.set('magazine_issue_id', issueId);
                 url = new URL('./api/browser/magazine_viewer', this.oldApiUrl);
-                url.search = searchParams.toString();
-                return await FetchProto<T>(new Request(url), protoTypes, this.responseRootType);
+                break;
             };
 
             case 'gravure': {
                 searchParams.set('gravure_id', mangaId);
                 url = new URL('./api/v3/gravure_viewer', this.apiURL);
-                url.search = searchParams.toString();
-                return await FetchProto<T>(new Request(url), protoTypes, 'Zebrack.GravureViewerViewV3');
+                break;
             };
-        }
+        };
+
+        url.search = searchParams.toString();
+        return await FetchProto<T>(new Request(url), protoTypes, message);
     }
 
     public override async FetchImage(page: Page<PageParam>, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const blob = await Common.FetchImageAjax.call(this, page, priority, signal);
-        if (!page.Parameters.encryptionKey) return blob;
-        const encrypted = await new Response(blob).arrayBuffer();
-        const decrypted = XORDecrypt(new Uint8Array(encrypted), page.Parameters.encryptionKey);
-        return new Blob([decrypted], { type: blob.type });
+        return !page.Parameters.encryptionKey ? blob : this.DecryptImage(blob, page.Parameters.encryptionKey);
     }
-}
 
-function XORDecrypt(encrypted: Uint8Array<ArrayBuffer>, key: string): Uint8Array {
-    if (key) {
-        const t = new Uint8Array(key.match(/.{1,2}/g).map(e => parseInt(e, 16)));
-        const s = new Uint8Array(encrypted);
-        for (let n = 0; n < s.length; n++) {
-            s[n] ^= t[n % t.length];
-        }
-        return s;
-    } else {
-        return encrypted;
+    private async DecryptImage(blob: Blob, key: string): Promise<Blob> {
+        const bytes = new Uint8Array(await blob.arrayBuffer());
+        const xorkey = new Uint8Array(GetBytesFromHex(key));
+        for (let n = 0; n < bytes.length; n++)
+            bytes[n] = bytes[n] ^ xorkey[n % xorkey.length];
+        return GetTypedData(bytes.buffer);
     }
 }
