d4b8100c40b188e37a3f9fe884b3846706ac9bd9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-19 13:15:44 +0100

    update domains

--- web/src/engine/websites/Toonkor_e2e.ts
+++ web/src/engine/websites/Toonkor_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'Toonkor'
     },
     container: {
-        url: 'https://tkor086.com/허니-트러블',
+        url: 'https://tkor088.com/허니-트러블',
         id: encodeURI('/허니-트러블'),
         title: '허니 트러블'
     },
