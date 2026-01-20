23a3dc65fa0197503a7b73295922e7ceb7b1c311
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-20 18:47:42 +0100

    remove daycomics

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -103,7 +103,6 @@ export { default as CyComi } from './CyComi';
 export { default as DankeFursLesen } from './DankeFursLesen';
 export { default as DatgarScan } from './DatgarScan';
 export { default as DayComics } from './DayComics';
-export { default as DayComicsME } from './DayComicsME';
 export { default as Daysneo } from './Daysneo';
 export { default as DeathTollScans } from './DeathTollScans';
 export { default as DecadenceScans } from './DecadenceScans';
