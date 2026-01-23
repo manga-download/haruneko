556a36d10921f9032832f190a6b66a3a940567d6
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-23 09:49:36 +0000

    update domains, remove Batoto & LevaScans (#1527)

--- web/src/engine/websites/HentaiVN_e2e.ts
+++ web/src/engine/websites/HentaiVN_e2e.ts
@@ -6,7 +6,7 @@ new TestFixture({
         title: 'HentaiVN'
     },
     container: {
-        url: 'https://hentaivn.taxi/truyen-hentai/dong-ho-ngung-dong-thoi-gian/',
+        url: 'https://hentaivn.show/truyen-hentai/dong-ho-ngung-dong-thoi-gian/',
         id: JSON.stringify({ post: '10351', slug: '/truyen-hentai/dong-ho-ngung-dong-thoi-gian/' }),
         title: 'Đồng Hồ Ngưng Đọng Thời Gian'
     },
