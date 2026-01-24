6a1f4b70fa76ff1e472c0b4b1a1b29e587bd8595
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-24 14:18:50 +0100

    Update MangaOneJp.ts

--- web/src/engine/websites/MangaOneJp.ts
+++ web/src/engine/websites/MangaOneJp.ts
@@ -28,11 +28,11 @@ type WebChapterListForViewerResponse = {
 };
 
 type WebChapterListUsedByPaging = {
-    chapterList: ChapterProto[]
+    chapterList: ChapterProto[];
 };
 
 type WebVolumeListUsedByPaging = {
-    volumeList: Volume[]
+    volumeList: Volume[];
 };
 
 type ChapterProto = {
@@ -95,7 +95,7 @@ export default class extends DecoratableMangaScraper {
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         const { dayOfWeekTitleLists } = await FetchProto<WebRensaiListResponse>(new Request(new URL('?rq=rensai', this.apiUrl)), protoTypes, 'MangaOneJp.WebRensaiListResponse');
         return dayOfWeekTitleLists.reduce((accumulator: Manga[], day) => {
-            accumulator.push(...day.titles.map(({ title: { titleId, titleName } }) => new Manga(this, provider, titleId.toString(), titleName)));
+            accumulator.push(...day.titles.map(({ title: { titleId, titleName } }) => new Manga(this, provider, `${titleId}`, titleName)));
             return accumulator;
         }, []);
     }
