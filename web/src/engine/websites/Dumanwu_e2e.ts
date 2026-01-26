ef6a6db1e0ad948864768be898710614fbeca6ed
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-01-26 12:52:08 +0100

    feat: Add Dumanwu back

---
+++ web/src/engine/websites/Dumanwu_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'dumanwu',
+        title: 'Dumanwu'
+    },/* Geoblocked : China
+    container: {
+        url: 'https://dumanwu1.com/IVppVyi/',
+        id: '/IVppVyi/',
+        title: '斗罗大陆2绝世唐门'
+    },
+    child: {
+        id: '/IVppVyi/DhNhNZq.html',
+        title: '321 奇异的震荡弹'
+    },
+    entry: {
+        index: 0,
+        size: 168_972,
+        type: 'image/jpeg'
+    }*/
+}).AssertWebsite();
