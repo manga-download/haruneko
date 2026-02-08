7bf1c2ccf484a440510f52eaffb1fc385eefd4a7
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-02-08 15:35:51 +0100

    bookhodai : multipaged chapters

--- web/src/engine/websites/BookHodai.ts
+++ web/src/engine/websites/BookHodai.ts
@@ -3,17 +3,25 @@ import icon from './BookHodai.webp';
 import { Chapter, DecoratableMangaScraper, type Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as SpeedBinb from './decorators/SpeedBinb';
-import { FetchHTML } from '../platform/FetchProvider';
+import { FetchHTML, FetchJSON } from '../platform/FetchProvider';
 import { SpeedBindVersion } from './decorators/SpeedBinb';
 
-function MangaInfoExtractor(element: HTMLElement, uri: URL) {
-    return {
-        id: uri.pathname,
-        title: element.textContent.split('＞').pop().trim() || element.textContent.trim()
-    };
-}
+type APIChapters = {
+    backnumber: {
+        book_id: string;
+        branch_no: string;
+        book_type: string;
+        book_title: string;
+        flg_vol: '0' | '1';
+        vol: string;
+    }[],
+    max_backnumber_page: number;
+};
 
-@Common.MangaCSS(/^{origin}\/[^/]+\/backnumber\/\d+$/, 'ol.c-breadcrumb li:last-of-type a, div.p-book-overview__detail h2.p-book-overview__detail-bookname', MangaInfoExtractor)
+@Common.MangaCSS(/^{origin}\/[^/]+\/backnumber\/\d+$/, 'ol.c-breadcrumb li:last-of-type a, div.p-book-overview__detail h2.p-book-overview__detail-bookname', (element, uri) => ({
+    id: uri.pathname,
+    title: element.textContent.split('＞').pop().trim() || element.textContent.trim()
+}))
 @SpeedBinb.PagesSinglePageAjax(SpeedBindVersion.v016130)
 @SpeedBinb.ImageAjax()
 export default class extends DecoratableMangaScraper {
@@ -37,22 +45,31 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const chapters: Chapter[] = [];
         const dom = await FetchHTML(new Request(new URL(manga.Identifier, this.URI)));
+        let firstChapter: Chapter = undefined;
 
         //get first "chapter" (book) details
         const bookdetails = dom.querySelector<HTMLDivElement>('section.p-book-overview');
+        const bookInfosElement = dom.querySelector<HTMLElement>('[data-book-id]');
+
         let title = (bookdetails.querySelector('span.p-book-overview__detail-volnumber') ?? bookdetails.querySelector('h2.p-book-overview__detail-vol')).textContent.replaceAll('\n', '').trim();
         title = title.replace(manga.Title, '').trim() != '' ? title.replace(manga.Title, '').trim() : title;
-        const chapterlinkNode = bookdetails.querySelector<HTMLAnchorElement>('a[href*="viewer"]');
-        if (chapterlinkNode) chapters.push(new Chapter(this, manga, chapterlinkNode.pathname + chapterlinkNode.search, title));
+        const chapterlinkNode = bookdetails.querySelector<HTMLAnchorElement>('a.p-book-button[href*="viewer"]');
+        if (chapterlinkNode) firstChapter = new Chapter(this, manga, chapterlinkNode.pathname + chapterlinkNode.search, title);
 
-        const chaptersNodes = [...dom.querySelectorAll<HTMLElement>('div.p-book-backnumber-series__item')];
-        for (const chapter of chaptersNodes) {
-            const title = (chapter.querySelector('.p-book-backnumber-series__volnumber') ?? chapter.querySelector('.p-book-backnumber-series__book-nm')).textContent.trim();
-            const link = chapter.querySelector<HTMLAnchorElement>('a[href*="viewer"]');
-            chapters.push(new Chapter(this, manga, link.pathname + link.search, title.replace(manga.Title, '').trim()));
-        }
-        return chapters.distinct();
+        type This = typeof this;
+        const moreChapters: Chapter[] = await Array.fromAsync(async function* (this: This) {
+            for (let page = 1, run = true; run; page++) {
+                const { backnumber, max_backnumber_page } = await FetchJSON<APIChapters>(new Request(new URL(`./book/ajax_backnumber_series_page?book_id=${bookInfosElement.dataset.bookId}&member_id=0&book_type=${bookInfosElement.dataset.bookType}&page=${page}&matome=1`, this.URI), {
+                    headers: {
+                        'X-Requested-With': 'XMLHttpRequest'
+                    }
+                }));
+                const chapters = backnumber.map(({ book_id: id, book_title: title, book_type: type, branch_no: number, flg_vol: flagVolume, vol }) => new Chapter(this, manga, `/viewer?book_id=${id}&branch_no=${number}&book_type=${type}`, flagVolume === '1' ? `${vol} 巻` : title));
+                yield* chapters;
+                run = max_backnumber_page > page;
+            }
+        }.call(this));
+        return [firstChapter, ...moreChapters,].filter(el => el);
     }
 }
--- web/src/engine/websites/BookHodai_e2e.ts
+++ web/src/engine/websites/BookHodai_e2e.ts
@@ -9,20 +9,16 @@ new TestFixture({
         url: 'https://bookhodai.jp/magazine/backnumber/1029551',
         id: '/magazine/backnumber/1029551',
         title: 'comicグラスト',
-        timeout: 15000
-
     },
     child: {
-        id: '/viewer?book_id=2000035407&branch_no=01&book_type=3',
-        title: 'vol.85',
-        timeout: 20000
+        id: '/viewer?book_id=2000035407&branch_no=02&book_type=3',
+        title: '85号',
+        timeout: 15000
     },
     entry: {
         index: 0,
-        size: 3_984_399,
+        size: 4_122_795,
         type: 'image/png',
-        timeout: 30000
-
     }
 }).AssertWebsite();
 
@@ -35,18 +31,15 @@ new TestFixture({
         url: 'https://bookhodai.jp/manga/backnumber/62789',
         id: '/manga/backnumber/62789',
         title: '風光る',
-        timeout: 15000
-
     },
     child: {
         id: '/viewer?book_id=3000099022&branch_no=01&book_type=4',
         title: '1 ～ 44',
-        timeout: 20000
+        timeout: 15000
     },
     entry: {
         index: 0,
-        size: 4_011_228,
+        size: 4_319_976,
         type: 'image/png',
-        timeout: 30000
     }
 }).AssertWebsite();
