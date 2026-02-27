1497afd4335fb42022fffc60836c1b923ec530e2
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-02-27 10:47:49 +0100

    fix: rewrite MangaCrab

--- web/src/engine/websites/MangaCrab.ts
+++ web/src/engine/websites/MangaCrab.ts
@@ -1,24 +1,23 @@
 import { Tags } from '../Tags';
 import icon from './MangaCrab.webp';
-import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import { Chapter, DecoratableMangaScraper, type Manga, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
-import { Fetch, FetchCSS } from '../platform/FetchProvider';
+import { Fetch, FetchCSS, FetchJSON, FetchWindowScript } from '../platform/FetchProvider';
 import type { Priority } from '../taskpool/DeferredTask';
 
-const chapterScript = `
-    [ ...document.querySelectorAll('li.wp-manga-chapter div.parm-extras > a') ].map(element => ({
-        id: element.pathname,
-        title : element.text.trim(),
-    }));
-`;
+type APIChapters = {
+    data: {
+        list: string;
+        total_pages: number;
+    }
+};
 
 type PageKey = {
     imgKey: string;
 }
 
-@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, 'h1.post-title')
-@Common.MangasMultiPageCSS('div.post-title h2 > a', Common.PatternLinkGenerator('/page/{page}/?s&post_type=wp-manga'))
-@Common.ChaptersSinglePageJS(chapterScript, 750)
+@Common.MangaCSS(/^{origin}\/series\/[^/]+\/$/, 'title')
+@Common.MangasMultiPageCSS('article.catalog-card > a', Common.PatternLinkGenerator('/series/page/{page}/'))
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
@@ -29,13 +28,50 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const { manga_id, nonce } = await FetchWindowScript<{ manga_id: string, nonce: string }>(new Request(new URL(manga.Identifier, this.URI)), `
+            new Promise( resolve => {
+                resolve ({
+                    manga_id : document.getElementById('mv-chapter-list').dataset.mangaId,
+                    nonce : mvTheme.nonce
+                });
+            });
+        `, 1500);
+
+        type This = typeof this;
+        return Array.fromAsync(async function* (this: This) {
+            for (let page = 1, run = true; run; page++) {
+                const { data: { list, total_pages } } = await FetchJSON<APIChapters>(new Request(new URL('./wp-admin/admin-ajax.php', this.URI), {
+                    method: 'POST',
+                    body: new URLSearchParams({
+                        action: 'mv_get_chapters',
+                        nonce,
+                        manga_id,
+                        page: `${page}`,
+                        search: '',
+                        _ts: `${Date.now()}`
+                    }).toString(),
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        Referer: this.URI.href
+                    }
+                }));
+                const chapters = [...new DOMParser().parseFromString(list, 'text/html').querySelectorAll('article.chapter-item div a')].map((chapter: HTMLAnchorElement) => {
+                    return new Chapter(this, manga, chapter.pathname + chapter.search, chapter.text.trim());
+                });
+                yield* chapters;
+                run = total_pages > page;
+            }
+        }.call(this));
+    }
+
     public override async FetchPages(chapter: Chapter): Promise<Page<PageKey>[]> {
         //NOT using script because of ad wall intermediate page interfering
         const [body] = await FetchCSS(new Request(new URL(chapter.Identifier, this.URI)), 'body');
-        const images = [...body.querySelectorAll('div.page-break img.wp-manga-chapter-img[id^="image-"]')]
+        const images = [...body.querySelectorAll('div.reader-body img.mv-secure-img')]
             .map(img => [...img.attributes].find(attribute => attribute.value.startsWith('/encript.php'))?.value)
             .filter(img => img);
-        const imgKey = body.innerHTML.match(/Img-X'\s*:\s*'(.*)'/).at(1);
+        const imgKey = body.innerHTML.match(/['"]imgHeader['"]\s*:\s*['"]([^'"]+)['"]/).at(1);
         return images.map(page => new Page<PageKey>(this, chapter, new URL(page, this.URI), { imgKey }));
     }
 
@@ -44,7 +80,7 @@ export default class extends DecoratableMangaScraper {
             return (await Fetch(new Request(page.Link, {
                 headers: {
                     Referer: new URL(page.Parent.Identifier, this.URI).href,
-                    'Img-X': page.Parameters.imgKey
+                    'Node': page.Parameters.imgKey
                 },
             }))).blob();
         }, priority, signal);
