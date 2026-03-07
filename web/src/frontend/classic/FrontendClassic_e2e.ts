8b58cfc1254b61b3f1211d46dc0d6ca89fe6b6ad
Ronny <ronny@local.host>
2026-03-07 03:51:36 +0100

    chore: update end-to-end test for classic frontend

--- web/src/frontend/classic/FrontendClassic_e2e.ts
+++ web/src/frontend/classic/FrontendClassic_e2e.ts
@@ -1,27 +1,46 @@
-import { describe, it/*, expect*/ } from 'vitest';
+import { describe, beforeEach, it, expect } from 'vitest';
 import { TestFixture } from './FrontendClassic_fixture';
 
-describe('Front-End (Classic)', { concurrent: false, timeout: 60_000 }, () => {
+describe.sequential('Front-End (Classic)', { timeout: 60_000 }, () => {
+
+    const fixture = new TestFixture();
+
+    beforeEach(() => fixture.Reset('classic'));
+
+    describe('Startup', async () => {
+
+        it('Should render basic layout', async () => {
+            await fixture.WaitForSelectors(5000,
+                'div#app > header > a.bx--header__name',
+                'div#app > nav.bx--side-nav__navigation',
+                'div#app > main#hakunekoapp > div#Media > div#MediaList',
+                'div#app > main#hakunekoapp > div#Item > div#ItemList',
+                'div#app > main#hakunekoapp > div#Content > main > div#Home',
+                'div#app > main#hakunekoapp > div#Bottom div.downloads',
+            );
+        });
+    });
 
     describe('Bookmarks', async () => {
 
-        const fixture = new TestFixture();
-
-        it('Should bookmark a media title that is not yet bookmarked', async () => {
-
-            // TODO: Ensure bookmark list is empty
-
+        it('Should bookmark a manga that is not yet bookmarked', async () => {
             await fixture.SetWebsiteFilter('Dex');
             await fixture.SelectWebsite('mangadex');
-            await fixture.UpdateWebsiteMediaTitles(7500);
-            await fixture.SetMediaTitleFilter('Apple');
-            await fixture.Delay(250);
-            await fixture.BookmarkMediaTitle('Apple Collection');
-
+            await fixture.UpdateWebsiteMangaList(7500);
+            await fixture.SetMangaFilter('Apple');
+            await fixture.BookmarkManga('Apple Collection');
             await fixture.SetWebsiteFilter('Bookmarks');
             await fixture.SelectWebsite('bookmarks');
-            await fixture.SetMediaTitleFilter('Apple');
-            await fixture.SelectMediaTitle('Apple Collection');
+            await fixture.SetMangaFilter('Apple');
+            await fixture.SelectManga('Apple Collection');
+        });
+
+        it('Should load preview images of a chapter', async () => {
+            await fixture.PasteMangaFromClipboard('https://mangadex.org/title/a037e450-6537-4725-aa7b-189fb2586e09/');
+            await fixture.ClickChapterPreview('Vol.01 Ch.0004 - Thirsty (en) [The Company]');
+            for (const link of await fixture.GetPreviewImageLinks(14)) {
+                expect(link?.startsWith('blob:https://localhost:5000/')).toBeTruthy();
+            }
         });
     });
 });
