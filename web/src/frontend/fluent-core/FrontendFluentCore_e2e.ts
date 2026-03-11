a36ea8b8832735dc4774efe801c3382c884af080
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-03-11 08:56:51 +0100

    Update FrontendFluentCore_e2e.ts

--- web/src/frontend/fluent-core/FrontendFluentCore_e2e.ts
+++ web/src/frontend/fluent-core/FrontendFluentCore_e2e.ts
@@ -10,7 +10,7 @@ describe.sequential('Front-End (Fluent-Core)', { timeout: 60_000 }, () => {
     describe('SmokeTests', async () => {
 
         it('Should render basic layout', async () => {
-            await fixture.WaitForSelectors(10_000,
+            await fixture.WaitForSelectors(30_000,
                 'div#app > fluent-app',
             );
         });
