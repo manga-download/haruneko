0ef3d9b69d3ffb1ce9782670b16e441cf2680b37
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-03-12 18:55:11 +0100

    universal script in boost

--- web/src/engine/websites/legacy/ComicBoost.ts
+++ web/src/engine/websites/legacy/ComicBoost.ts
@@ -8,33 +8,55 @@ import { DecoratableMangaScraper } from '../../providers/MangaPlugin';
 const pagesScript = `
     new Promise(async (resolve, reject) => {
         try {
-            const a2f = NFBR.a2F ? new NFBR.a2F() : new NFBR.a2f();//StaticServer
-            const params = new URL(window.location).searchParams;
-            const parameters = await a2f.a5W({
-                contentId: params.get(NFBR.a5q.Key.CONTENT_ID), // Content ID => 'cid'
-                a6m: params.get(NFBR.a5q.Key.LICENSE_TOKEN), // License Token => 'lit'
-                preview: params.get(NFBR.a5q.Key.LOOK_INSIDE) === '1', // Look Inside => 'lin'
-                contentType: params.get(NFBR.a5q.Key.CONTENT_TYPE) || 1, // Content Type => 'cty'
-                title: params.get(NFBR.a5q.Key.CONTENT_TITLE), // Content Title => 'cti'
-                winWidth: 3840,
-                winHeight: 2160
-            });
 
-            //Create a model
+        const urlParams = new URL(window.location).searchParams;
+
+          //Create a model
             const model = new NFBR.a6G.Model({
                 'settings': new self.NFBR.Settings('NFBR.SettingData'),
-                'viewerFontSize': self.NFBR.a0X.a3k,
+                'viewerFontSize': self.NFBR.a0X.a3K,
                 'viewerFontFace': self.NFBR.a0X.a3k,
                 'viewerSpreadDouble': true,
                 'viewerb5c': null,
                 'viewerSpread': {},
-                'queryParamForContentUrl' : parameters.contentAppendParam,
             });
 
+        const v_a6L = new NFBR.a6G.a6L(model); //a6G.a6L : class with 'POPUP_NAME_URL';
+        const contentId = urlParams.get(NFBR.a5q.Key.CONTENT_ID);
+        let contentType = parseInt(urlParams.get(NFBR.a5q.Key.CONTENT_TYPE)) ?? undefined;
+
+        v_a6L.model.set('contentId', contentId);
+        v_a6L.model.set('contentType', contentType);
+
+        const a5wBody = {
+                contentId: urlParams.get(NFBR.a5q.Key.CONTENT_ID), // Content ID => 'cid'
+                a6m: urlParams.get(NFBR.a5q.Key.LICENSE_TOKEN), // License Token => 'lit'
+                preview: urlParams.get(NFBR.a5q.Key.LOOK_INSIDE) === '1', // Look Inside => 'lin'
+                contentType,
+                title: true,
+                winWidth: 3840,
+                winHeight: 2160
+
+                //contentType: urlParams.get(NFBR.a5q.Key.CONTENT_TYPE), // Content Type => 'cty'
+                //title: urlParams.get(NFBR.a5q.Key.CONTENT_TITLE), // Content Title => 'cti'
+                //winWidth: 3840,
+                //winHeight: 2160
+        };
+
+            // Create Server
+            const a2f = NFBR.StaticServer ?  new NFBR.StaticServer() : new NFBR.a2F ? new NFBR.a2F() : new NFBR.a2f();//StaticServer
+            const parameters = await a2f.a5W(a5wBody);
+
+            //fix parameters
+            v_a6L.model.set('contentType', parameters[NFBR.a5q.Key.CONTENT_TYPE]);
+            v_a6L.model.set('queryParamForContentUrl', parameters.contentAppendParam)
+
             //Create a "Content" that will be filled using the bookloader as5 async function
-            const content = new NFBR.a6i.Content(parameters.url);
-            const v_a6L = new NFBR.a6G.a6L(model); //a6G.a6L : class with 'POPUP_NAME_URL'
+            const label = [self.NFBR.a0X.a3K, self.NFBR.a0X.a3k].join('_');
+            const content = parameters[NFBR.a5q.Key.CONTENT_TYPE] === 1 || parameters[NFBR.a5q.Key.CONTENT_TYPE]  === 2 ? new NFBR.a6i.Content(parameters.url) : new NFBR.a6i.Content(parameters.url + label + "/");
             await v_a6L.bookLoader_.a5s(content, 'configuration', v_a6L);
+
+            //ABOVE PART SEEM GOOD FOR ALL WEBSITES, NOW WE MUST FIX PAGE AND BLOCK COMPUTING 
 
             const pages = content.configuration.contents.map((page, index) => {
 
