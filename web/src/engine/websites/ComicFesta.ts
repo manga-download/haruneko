32afa64fec6e82d4964eab98373b2a64e2a9fbec
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-03-30 10:16:21 +0200

    use fixed FetchNextJS from master

--- web/src/engine/websites/ComicFesta.ts
+++ web/src/engine/websites/ComicFesta.ts
@@ -3,24 +3,24 @@ import icon from './ComicFesta.webp';
 import { Chapter, DecoratableMangaScraper, type Manga } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import * as ClipStudioReader from './decorators/ClipStudioReader';
-import { FetchCSS } from '../platform/FetchProvider';
+import { FetchNextJS } from '../platform/FetchProvider';
 
 type JSONChapters = {
     packages: {
-        id: number,
-        number: number,
+        id: number;
+        number: number;
         fairInfo: {
             free: {
-                endAt: string,
-                startAt: string
+                endAt: string;
+                startAt: string;
             },
             trial: {
-                endAt: string,
-                startAt: string
+                endAt: string;
+                startAt: string;
             }
         }
     }[]
-}
+};
 
 @Common.MangaCSS(/^{origin}\/titles\/\d+/, 'section[class*="title-name-section"] h2', Common.WebsiteInfoExtractor({ queryBloat: 'span' }))
 @Common.MangasNotSupported()
@@ -37,36 +37,10 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const scripts = await FetchCSS<HTMLScriptElement>(new Request(new URL(manga.Identifier, this.URI)), 'script:not([src])');
-        const { packages } = this.FindJSONObject<JSONChapters>(scripts, /isDisplayVolumeNumber/, 'isDisplayVolumeNumber');
+        const { packages } = await FetchNextJS<JSONChapters>(new Request(new URL(manga.Identifier, this.URI)), data => 'isDisplayVolumeNumber' in data);
         return packages
             .map(({ fairInfo: { trial }, id, number }) => {
                 return new Chapter(this, manga, `/volumes/${id}${trial ? '/trial_download' : '/free_download'}`, `${number}`);
             });
-    }
-
-    // NOTE: current FetchNEXTJS implementation can't find what we need
-    private FindJSONObject<T>(scripts: HTMLScriptElement[], scriptRegex: RegExp, keyName: string, currentElement = undefined): T {
-
-        if (scripts && scriptRegex) {
-            const script = scripts.find(script => scriptRegex.test(script.text))?.text;
-            if (!script) return undefined;
-            //script are like self.__next_f.push([1,"
-            const json = JSON.parse(script.substring(script.indexOf(',"') + 1, script.length - 2));// to remove trailing )]
-            currentElement = JSON.parse(json.substring(json.indexOf(':') + 1));
-            return this.FindJSONObject<T>(undefined, undefined, keyName, currentElement);
-        }
-
-        if (!currentElement) return undefined;
-        if (currentElement[keyName]) {
-            return currentElement;
-        }
-        let result = undefined;
-        for (let i in currentElement) {
-            if (result) break;
-            if (typeof currentElement[i] === 'object')
-                result = this.FindJSONObject<T>(undefined, undefined, keyName, currentElement[i]);
-        }
-        return result as T;
     }
 }
