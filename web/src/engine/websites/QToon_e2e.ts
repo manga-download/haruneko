343bf6316a3303eabbcf9db1039e6730346b6f84
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-04-09 11:42:10 +0000

    feat: Add QToon (#1607)
    * feat: Add QToon
    
    Closes https://github.com/manga-download/hakuneko/issues/8451
    
    * refactor

---
+++ web/src/engine/websites/QToon_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'qtoon',
+        title: 'QToon'
+    },
+    container: {
+        url: 'https://qqtoon.com/pt/detail/c_9h7GLWdlXrsrkT1XO7Eq',
+        id: 'c_9h7GLWdlXrsrkT1XO7Eq',
+        title: 'A Imperatriz Se Casou Novamente [pt]'
+    },
+    child: {
+        id: 'e_1d2zPI0VKA5yf9KXWspt',
+        title: 'Capítulo 178'
+    },
+    entry: {
+        index: 0,
+        size: 280_257,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
