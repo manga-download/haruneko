fe9778a9987fac68608edb315ec1bb66f99d45a9
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-04-15 16:41:30 +0200

    fix: MangaBrasuka : bypass ads gate
    Closes https://github.com/manga-download/haruneko/issues/1648

--- web/src/engine/websites/MangasBrasuka.ts
+++ web/src/engine/websites/MangasBrasuka.ts
@@ -1,13 +1,14 @@
 ﻿import { Tags } from '../Tags';
 import icon from './MangasBrasuka.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
+import { Fetch, FetchCSS, FetchWindowScript } from '../platform/FetchProvider';
+import { GetBytesFromBase64 } from '../BufferEncoder';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type')
 @Madara.MangasMultiPageAJAX()
 @Madara.ChaptersSinglePageAJAXv2()
-@Madara.PagesSinglePageCSS()
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
 
@@ -17,5 +18,17 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        //get adgate link
+        const [crapLink] = await FetchCSS<HTMLAnchorElement>(new Request(new URL(chapter.Identifier, this.URI)), 'div.page-break a');
+        const authParam: string = JSON.parse(new TextDecoder().decode(GetBytesFromBase64(new URL(crapLink.href).searchParams.get('o').split('/').at(-1)))).url;
+        //get real url (redirected)
+        const response = await Fetch(new Request(new URL(crapLink.href)));
+        const newURL = response.url.replace('/ref/', '/finish/cpurl/');
+        await FetchWindowScript<string[]>(new Request(new URL(newURL)), '', 5000);
+        const pages = await FetchCSS<HTMLImageElement>(new Request(new URL(`/campanha.php?auth=${encodeURIComponent(authParam)}`, this.URI)), 'div.manga-content img');
+        return pages.map(page => new Page(this, chapter, new URL(page.src)));
     }
 }
