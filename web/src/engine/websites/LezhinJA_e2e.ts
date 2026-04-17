16663b534da5bc373a9e004fa47dfe20a11068ec
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-04-17 13:59:15 +0000

    feat:Add LezhinJA (#1269)

---
+++ web/src/engine/websites/LezhinJA_e2e.ts
@@ -0,0 +1,45 @@
+import { TestFixture } from '../../../test/WebsitesFixture';
+
+// CASE : Chapter
+new TestFixture({
+    plugin: {
+        id: 'lezhin-ja',
+        title: 'Lezhin (Japanese)'
+    },
+    container: {
+        url: 'https://lezhin.jp/comic/hikarikage',
+        id: '/comic/hikarikage',
+        title: '光と影【連載】'
+    },
+    child: {
+        id: JSON.stringify({ id: '01k0r83mds2fk2y2q73e0kxp2w', type: 'chapter' }),
+        title: '第1話'
+    },
+    entry: {
+        index: 0,
+        size: 85_182,
+        type: 'image/webp'
+    }
+}).AssertWebsite();
+
+// CASE : Volume
+new TestFixture({
+    plugin: {
+        id: 'lezhin-ja',
+        title: 'Lezhin (Japanese)'
+    },
+    container: {
+        url: 'https://lezhin.jp/comic/t5_470281',
+        id: '/comic/t5_470281',
+        title: '五等分の花嫁'
+    },
+    child: {
+        id: JSON.stringify({ id: '01jqf186spk0b7ce0vwm0804s6', type: 'volume' }),
+        title: '（１）'
+    },
+    entry: {
+        index: 0,
+        size: 75_322,
+        type: 'image/webp'
+    }
+}).AssertWebsite();
