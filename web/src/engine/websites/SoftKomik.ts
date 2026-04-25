aaa7ee1923379afaf5fe655d776e07808cb2e16b
Ronny <ronny@local.host>
2026-04-25 03:56:49 +0200

    chore: extract drm from softkomik

--- web/src/engine/websites/SoftKomik.ts
+++ web/src/engine/websites/SoftKomik.ts
@@ -4,14 +4,13 @@ import * as Common from './decorators/Common';
 import { DecoratableMangaScraper, Manga, type MangaPlugin } from '../providers/MangaPlugin';
 import { FetchJSON, FetchWindowPreloadScript } from '../platform/FetchProvider';
 import { RandomText } from '../Random';
-
-type APIManga = {
-    title: string;
-    title_slug: string;
-};
+import { Delay } from '../BackgroundTimers';
 
 type APIMangas = {
-    data: APIManga[];
+    data: {
+        title: string;
+        title_slug: string;
+    }[];
 };
 
 type TokenData = {
@@ -20,26 +19,45 @@ type TokenData = {
     ex: number;
 };
 
-function tokenScript(eventName: string): string {
-    return `
-        JSON.parse = new Proxy(JSON.parse, {
-            apply: function(target, thisArg, argumentsList) {
-                const result = Reflect.apply(target, thisArg, argumentsList);
-                if( result?.token && result?.ex && result?.sign)
-                    setInterval(() => window.dispatchEvent(new CustomEvent('${eventName}', { detail: result })), 250);
-                return result;
-            }
-        });
-    `;
-};
+class DRMProvider {
 
-function payloadScript(eventName: string): string {
-    return `
-        new Promise( resolve => {
-            window.addEventListener('${eventName}', event => resolve(event.detail), { once: true });
-        });
-    `;
-};
+    #auth: TokenData = undefined;
+
+    constructor(private readonly uri: URL, private readonly api: URL) { }
+
+    private async RefreshToken(): Promise<void> {
+
+        if (this.#auth?.ex > Date.now()) return;
+
+        const eventName = RandomText(Math.random() * 8 + 8);
+        this.#auth = await FetchWindowPreloadScript<TokenData>(new Request(new URL('/komik/list', this.uri)), `
+            JSON.parse = new Proxy(JSON.parse, {
+                apply: function(funcNative, funcThis, funcArgs) {
+                    const result = Reflect.apply(funcNative, funcThis, funcArgs);
+                    if (result?.token && result?.sign && result?.ex) {
+                        setInterval(() => window.dispatchEvent(new CustomEvent('${eventName}', { detail: result })), 250);
+                    }
+                    return result;
+                }
+            });
+        `, `
+            new Promise(resolve => {
+                window.addEventListener('${eventName}', event => resolve(event.detail), { once: true });
+            });
+        `);
+    }
+
+    public async FetchSigned<T extends JSONElement>(endpoint: string): Promise<T> {
+        await this.RefreshToken();
+        return FetchJSON<T>(new Request(new URL(endpoint, this.api), {
+            headers: {
+                'Referer': this.uri.href,
+                'X-Token': this.#auth.token,
+                'X-Sign': this.#auth.sign,
+            }
+        }));
+    }
+}
 
 @Common.MangaCSS(/^{origin}\/[^/]+$/, 'div.bg-content.title h1', (element, uri) => ({ id: uri.pathname.split('/').at(-1), title: element.textContent.trim() }))
 @Common.ChaptersSinglePageJS(`[...document.querySelectorAll('div.chapter-list a')].map(el => ({ id: el.pathname, title:el.textContent.trim() }))`, 1500)
@@ -47,8 +65,7 @@ function payloadScript(eventName: string): string {
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
 
-    private readonly apiUrl = 'https://v2.softdevices.my.id/';
-    private websiteToken: TokenData = undefined;
+    #drm: DRMProvider = new DRMProvider(this.URI, new URL('https://v2.softdevices.my.id/'));
 
     public constructor() {
         super('softkomik', 'Softkomik', 'https://softkomik.co', Tags.Media.Manga, Tags.Media.Manhua, Tags.Media.Manhwa, Tags.Language.Indonesian, Tags.Source.Aggregator, Tags.Accessibility.RegionLocked);
@@ -58,37 +75,15 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override async Initialize(): Promise<void> {
-        await this.RefreshToken();
-    }
-
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         type This = typeof this;
         return Array.fromAsync(async function* (this: This) {
             for (let page = 1, run = true; run && page; page++) {
-                const { data } = await this.FetchSigned<APIMangas>(`./komik?page=${page}&limit=50&sortBy=newKomik`);
+                const { data } = await this.#drm.FetchSigned<APIMangas>(`./komik?sortBy=newKomik&limit=50&page=${page}`);
                 const mangas = data.map(({ title, title_slug: slug }) => new Manga(this, provider, slug, title.trim()));
                 mangas.length > 0 ? yield* mangas : run = false;
+                await Delay(1000);
             }
         }.call(this));
-    }
-
-    private async RefreshToken(): Promise<void> {
-        if (this.websiteToken?.ex === undefined || Date.now() > this.websiteToken?.ex) {
-            const eventName = RandomText(32);
-            this.websiteToken = await FetchWindowPreloadScript<TokenData>(new Request(new URL('/komik/list', this.URI)), tokenScript(eventName), payloadScript(eventName));
-        }
-    }
-
-    private async FetchSigned<T extends JSONElement>(endpoint: string): Promise<T> {
-        await this.RefreshToken();
-        return FetchJSON<T>(new Request(new URL(endpoint, this.apiUrl), {
-            headers: {
-                Origin: this.URI.origin,
-                Referer: this.URI.href,
-                'X-Token': this.websiteToken.token,
-                'X-Sign': this.websiteToken.sign,
-            }
-        }));
     }
 }
