c433db9838aa90bfd1e15c8eff347618da4aa449
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-05-02 15:04:12 +0200

    feat: Add NexusScans
    Closes https://github.com/manga-download/hakuneko/issues/8526

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -500,6 +500,7 @@ export { default as NetTruyenOK } from './NetTruyenOK';
 export { default as NetTruyenViet } from './NetTruyenViet';
 export { default as NeuManga } from './NeuManga';
 export { default as NewToki } from './NewToki';
+export { default as NexusScans } from './NexusScans';
 export { default as NexusToons } from './NexusToons';
 export { default as Ngomik } from './Ngomik';
 export { default as NhatTruyen } from './NhatTruyen';
