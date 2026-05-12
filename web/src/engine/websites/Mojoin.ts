1d7df373343b97167907749fe12b8466ada20935
Ronny <ronny@local.host>
2026-05-13 01:20:21 +0200

    website (mojoin): align with creativecomic

--- web/src/engine/websites/Mojoin.ts
+++ web/src/engine/websites/Mojoin.ts
@@ -1,29 +1,21 @@
 ﻿import { Tags } from '../Tags';
 import icon from './Mojoin.webp';
-import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
-import * as Common from './decorators/Common';
-import { GetBytesFromBase64, GetBytesFromHex, GetBytesFromUTF8, GetHexFromBytes } from '../BufferEncoder';
+import type { Priority } from '../taskpool/TaskPool';
 import { Fetch, FetchJSON, FetchWindowScript } from '../platform/FetchProvider';
-import type { Priority } from '../taskpool/DeferredTask';
+import { type MangaPlugin, Manga, Chapter, Page, DecoratableMangaScraper } from '../providers/MangaPlugin';
+import { GetBytesFromBase64, GetBytesFromHex, GetBytesFromUTF8, GetUTF8FromBytes } from '../BufferEncoder';
+import * as Common from './decorators/Common';
 
-// TODO: Update token and key after manual user interaction
-
-type AESParams = {
-    key: string;
-    iv: string;
-};
-
-type APIResult<T> = {
-    data: T;
+type TokenData = {
+    uuid: string;
+    token?: string;
 };
 
 type APIMangas = {
-    data: APIManga[];
-};
-
-type APIManga = {
-    id: number;
-    name: string;
+    data: {
+        id: number;
+        name: string;
+    }[];
 };
 
 type APIChapters = {
@@ -40,22 +32,48 @@ type APIPages = {
     }[]
 };
 
-type SessionData = {
-    uuid: string;
-    access_token: string;
+type PageParameters = {
+    EncryptionKeyData: string;
 }
 
-type PageParameters = {
-    keyData: string;
-};
+class DRMProvider {
 
+    #uuid = '';
+    public get UUID() { return this.#uuid; }
+
+    #bearer = '';
+    public get Bearer() { return this.#bearer; }
+
+    #iv: Uint8Array<ArrayBuffer>;
+    //public get IV() { return this.#iv; }
+
+    #keyData: Uint8Array<ArrayBuffer>;
+    //public get KeyData() { return this.#keyData; }
+
+    public async Update(uuid: string, token: string) {
+        const hash = await crypto.subtle.digest({ name: 'SHA-512' }, GetBytesFromUTF8(token));
+        this.#uuid = uuid;
+        this.#bearer = `Bearer ${token}`;
+        this.#iv = new Uint8Array(hash, 15, 16);
+        this.#keyData = new Uint8Array(hash, 0, 32);
+    }
+
+    public async Decrypt(encrypted: BufferSource, iv = this.#iv, keyData = this.#keyData): Promise<string> {
+        const algorithm = { name: 'AES-CBC', iv };
+        const key = await crypto.subtle.importKey('raw', keyData, algorithm, false, ['decrypt']);
+        const decrypted = await crypto.subtle.decrypt(algorithm, key, encrypted);
+        return GetUTF8FromBytes(decrypted);
+    }
+}
+
+@Common.MangaCSS(/^{origin}\/comics\/\d+#?/, 'head > title', (t, uri) => ({ id: uri.pathname.split('/').at(-1), title: t.innerText.split('｜').at(0) }))
 export default class extends DecoratableMangaScraper {
-    private readonly drmProvider: DRMProvider;
-    private readonly apiUrl = 'https://mojoin-api.com/';
+
+    readonly #drm = new DRMProvider();
+    readonly #apiURL = 'https://mojoin-api.com';
 
     public constructor() {
         super('mojoin', 'Mojoin', 'https://mojoin.com', Tags.Media.Manga, Tags.Language.Chinese, Tags.Source.Official);
-        this.drmProvider = new DRMProvider(this.URI);
     }
 
     public override get Icon() {
@@ -63,112 +81,48 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async Initialize(): Promise<void> {
-        await this.drmProvider.UpdateToken();
-    }
-
-    public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/comics/\\d+`).test(url);
-    }
-
-    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const mangaId = new URL(url).pathname.match(/\/comics\/(\d+)/).at(1);
-        const { id, name } = await this.FetchAPI<APIManga>(`./comics/book/info/${mangaId}`);
-        return new Manga(this, provider, `${id}`, name);
+        // TODO: update token and uuid after manual website interaction (i.e login)
+        const { uuid, token } = await FetchWindowScript<TokenData>(new Request(this.URI), `({
+            uuid: localStorage.getItem('uuid'),
+            token: localStorage.getItem('access_token'),
+        });`, 2500);
+        await this.#drm.Update(uuid, token ?? 'freereadingcomicstar');
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
-        const { data } = await this.FetchAPI<APIMangas>('./comics/book?rows_per_page=9999');
+        const { data } = await this.#FetchAPI<APIMangas>('/comics/book?rows_per_page=9999');
         return data.map(({ id, name }) => new Manga(this, provider, `${id}`, name));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { data } = await this.FetchAPI<APIChapters>(`./comics/book/${manga.Identifier}/chapter?page=1&rows_per_page=9999`);
+        const { data } = await this.#FetchAPI<APIChapters>(`/comics/book/${manga.Identifier}/chapter?page=1&rows_per_page=9999`);
         return data.map(({ customized_number: number, name }) => new Chapter(this, manga, number, name));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page<PageParameters>[]> {
-        const { content } = await this.FetchAPI<APIPages>(`./comics/book/${chapter.Parent.Identifier}/chapter/${chapter.Identifier}`);
-        return content.map(({ url, key: keyData }) => new Page<PageParameters>(this, chapter, new URL(url, this.URI), { keyData }));
-    }
-
-    private async FetchAPI<T extends JSONElement>(endpoint: string): Promise<T> {
-        return (await FetchJSON<APIResult<T>>(new Request(new URL(endpoint, this.apiUrl), {
-            headers: await this.drmProvider.ApplyNeededHeaders({
-                device: 'web'
-            }),
-        }))).data as T;
+        const { content } = await this.#FetchAPI<APIPages>(`/comics/book/${chapter.Parent.Identifier}/chapter/${chapter.Identifier}`);
+        return content.map(({ url, key }) => new Page<PageParameters>(this, chapter, new URL(url, this.URI), { EncryptionKeyData: key }));
     }
 
     public override async FetchImage(page: Page<PageParameters>, priority: Priority, signal: AbortSignal): Promise<Blob> {
-        const blob = await Common.FetchImageAjax.call(this, page, priority, signal);
-        return this.drmProvider.DecryptImage(blob, page.Parameters.keyData);
-    }
-}
+        const encrypted = await this.imageTaskPool.Add(async () => {
+            const response = await Fetch(new Request(page.Link.href));
+            return response.arrayBuffer();
+        }, priority, signal);
 
-/**
- * A basic DRM manager with Mojoin specific business logic
- */
-class DRMProvider {
-    #defaultKey: string = 'freereadingcomicstar';
-    #defaultSession = { uuid: '', access_token: '' };
-
-    #session: SessionData = this.#defaultSession;
-    #aesParams: AESParams = { iv: '', key: '' };
-
-    constructor(private readonly clientURI: URL) { }
-
-    /**
-     * Extract the token and uuid from website and compute the decryption key from token in case it exists
-     */
-    public async UpdateToken() {
-        try {
-            this.#session = await FetchWindowScript<SessionData>(new Request(this.clientURI), `new Promise(resolve=> resolve({ uuid: localStorage.getItem('uuid'), access_token : localStorage.getItem('access_token')}))`, 1500) ?? null;
-            this.#aesParams = await this.ComputeAESParams(this.#session.access_token ?? this.#defaultKey);
-        } catch (error) {
-            console.warn('UpdateToken()', error);
-            this.#session = this.#defaultSession;
-            this.#aesParams = await this.ComputeAESParams(this.#defaultKey);
-        }
+        const [keyData, iv] = (await this.#drm.Decrypt(GetBytesFromBase64(page.Parameters.EncryptionKeyData))).split(':');
+        const decrypted = await this.#drm.Decrypt(encrypted, GetBytesFromHex(iv), GetBytesFromHex(keyData));
+        return Fetch(new Request(decrypted)).then(response => response.blob());
     }
 
-    /**
-     * Determine the _Bearer_ and uuid extracted from the current token and add it as authorization header to the given {@link init} headers (replacing any existing authorization header).
-     * In case the _Bearer_ could not be extracted from the current token the authorization header will not be added/replaced.
-     */
-    public async ApplyNeededHeaders(init: HeadersInit): Promise<HeadersInit> {
-        const headers = new Headers(init);
-        headers.set('uuid', this.#session.uuid);
-        if (this.#session.access_token) {
-            headers.set('Authorization', 'Bearer ' + this.#session.access_token);
-        }
-        return headers;
-    }
-
-    /**
-     * Decrypt Image data from image blob using provider key (in base64)
-     */
-    public async DecryptImage(blob: Blob, base64KeyData: string): Promise<Blob> {
-        const aesParamsData = await this.Decrypt(GetBytesFromBase64(base64KeyData).buffer, this.#aesParams.key, this.#aesParams.iv);
-        const [imageKey, imageIv] = aesParamsData.split(':');
-        const decryptedBase64ImageData = await this.Decrypt(await blob.arrayBuffer(), imageKey, imageIv);
-        if (!/^data:image\/(jpe?g|png|webp|avif|bmp|gif);base64,[A-Za-z0-9+/]+=*$/.test(decryptedBase64ImageData)) {
-            throw new Error(`Decrypted image data is not a valid base64 data URI`);
-        }
-        return (await Fetch(new Request(decryptedBase64ImageData))).blob();
-    }
-
-    private async ComputeAESParams(text: string): Promise<AESParams> {
-        const hash = GetHexFromBytes(new Uint8Array(await crypto.subtle.digest('SHA-512', GetBytesFromUTF8(text))));
-        return {
-            key: hash.slice(0, 64),
-            iv: hash.slice(30, 62)
-        };
-    }
-
-    private async Decrypt(encrypted: ArrayBuffer, keyData: string, iv: string): Promise<string> {
-        const algorithm = { name: 'AES-CBC', iv: GetBytesFromHex(iv) };
-        const key = await crypto.subtle.importKey('raw', GetBytesFromHex(keyData), algorithm, false, ['decrypt']);
-        const decrypted = await crypto.subtle.decrypt(algorithm, key, encrypted);
-        return new TextDecoder('utf-8').decode(decrypted);
+    async #FetchAPI<T extends JSONElement>(endpoint: string): Promise<T> {
+        const { data } = await FetchJSON<{ data: T }>(new Request(new URL(endpoint, this.#apiURL), {
+            headers: {
+                Device: 'web',
+                UUID: this.#drm.UUID,
+                Authorization: this.#drm.Bearer,
+            }
+        }));
+        return data;
     }
 }
