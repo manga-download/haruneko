0bf611cd37a4107c706db5e4522c292ccd86c68d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-05-15 17:02:27 +0200

    MadTheme : Fuse MangaMonk/MangaForest => MangaK

--- web/src/engine/websites/MangaKio.ts
+++ web/src/engine/websites/MangaKio.ts
@@ -1,11 +1,11 @@
 import { Tags } from '../Tags';
-import icon from './MangaMonk.webp';
+import icon from './MangaKio.webp';
 import { MadTheme } from './templates/MadTheme';
 
 export default class extends MadTheme {
 
     public constructor() {
-        super('mangamonk', 'MangaMonk', 'https://mangamonk.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
+        super('mangakio', 'MangaKio', 'https://mangak.io', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
