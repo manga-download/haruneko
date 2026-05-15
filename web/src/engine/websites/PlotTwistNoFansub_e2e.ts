bd017bf3ae69a56dded38b5defe6eb2d5fae843e
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-05-15 18:27:08 +0200

    fix: PlotTwistNofansub - website changes
    + remove links with MangasNoSekai

--- web/src/engine/websites/PlotTwistNoFansub_e2e.ts
+++ web/src/engine/websites/PlotTwistNoFansub_e2e.ts
@@ -6,17 +6,18 @@ new TestFixture({
         title: 'Plot Twist No Fansub'
     },
     container: {
-        url: 'https://plotnofansub.com/manga/guarda-fantasmas/',
-        id: '/manga/guarda-fantasmas/',
-        title: 'Guarda-Fantasmas'
+        url: 'https://plotnofansub.com/manga/seitokai-ni-mo-ana-wa-aru/',
+        id: '/manga/seitokai-ni-mo-ana-wa-aru/',
+        title: 'Seitokai ni mo Ana wa Aru!',
+        timeout: 10_000
     },
     child: {
-        id: '/manga/guarda-fantasmas/14/',
-        title: '14 Amamiya'
+        id: '/manga/seitokai-ni-mo-ana-wa-aru/121/',
+        title: '121 Dímelo a mí también',
     },
     entry: {
         index: 0,
-        size: 152_680,
+        size: 137_890,
         type: 'image/webp'
     }
 }).AssertWebsite();
