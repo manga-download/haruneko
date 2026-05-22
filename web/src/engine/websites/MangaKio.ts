917c6cc4c59ebb4dfbba095b6dffc85e08d196c1
Ronny <ronny@local.host>
2026-05-22 08:39:25 +0200

    website (mangakio): change types

--- web/src/engine/websites/MangaKio.ts
+++ web/src/engine/websites/MangaKio.ts
@@ -9,47 +9,43 @@ type APIResult<T> = {
     data: T;
 };
 
-type APIMangas = APIResult<{
-    items: APIManga[];
-}>;
-
-type JSONMangaDetails = {
-    initialManga: {
-        id: string;
-        name: string;
-    };
-};
-
-type APIChapters = APIResult<{
-    chapters: APIChapter[];
-}>;
-
-type APIChapter = {
-    id: string;
-    name: string;
-    images: string[];
-};
-
-type APIManga = {
-    id: string;
-    url: string;
-    name: string;
-    slug: string;
-};
-
-type APIPages = APIResult<{
-    chapter: APIChapter;
-}>;
-
-type NEXTDATA<T> = {
+type NEXTManga = {
     props: {
-        pageProps: T;
+        pageProps: {
+            initialManga: {
+                id: string;
+                name: string;
+            };
+        }
     }
 };
 
+type APIMangas = APIResult<{
+    items: {
+        id: string;
+        url: string;
+        name: string;
+        slug: string;
+    }[];
+}>;
+
+type APIChapters = APIResult<{
+    chapters: {
+        id: string;
+        name: string;
+    }[];
+}>;
+
+type APIPages = APIResult<{
+    chapter: {
+        images: string[];
+    };
+}>;
+
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
-    private readonly apiUrl = 'https://api.mangak.io/';
+
+    private readonly apiURL = 'https://api.mangak.io/';
 
     public constructor() {
         super('mangakio', 'MangaKio', 'https://mangak.io', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
@@ -64,7 +60,8 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const { initialManga: { id, name } } = await this.GetEmbeddedJSON<JSONMangaDetails>(new URL(url));
+        const [{ text }] = await FetchCSS<HTMLScriptElement>(new Request(new URL(url)), 'script#__NEXT_DATA__');
+        const { props: { pageProps: { initialManga: { id, name } } } } = <NEXTManga>JSON.parse(text);
         return new Manga(this, provider, id, name);
     }
 
@@ -73,7 +70,7 @@ export default class extends DecoratableMangaScraper {
         return Array.fromAsync(async function* (this: This) {
             for (let page = 1, run = true; run; page++) {
                 await Delay(500);
-                const { data: { items } } = await FetchJSON<APIMangas>(new Request(new URL(`./titles/search?page=${page}&limit=50`, this.apiUrl)));
+                const { data: { items } } = await FetchJSON<APIMangas>(new Request(new URL(`./titles/search?limit=50&page=${page}`, this.apiURL)));
                 const mangas = items.map(({ id, name }) => new Manga(this, provider, id, name));
                 mangas.length > 0 ? yield* mangas : run = false;
             }
@@ -81,18 +78,12 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { data: { chapters } } = await FetchJSON<APIChapters>(new Request(new URL(`./titles/${manga.Identifier}/chapters`, this.apiUrl)));
+        const { data: { chapters } } = await FetchJSON<APIChapters>(new Request(new URL(`./titles/${manga.Identifier}/chapters`, this.apiURL)));
         return chapters.map(({ id, name }) => new Chapter(this, manga, id, name));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const { data: { chapter: { images } } } = await FetchJSON<APIPages>(new Request(new URL(`./titles/${chapter.Parent.Identifier}/chapters/${chapter.Identifier}`, this.apiUrl)));
+        const { data: { chapter: { images } } } = await FetchJSON<APIPages>(new Request(new URL(`./titles/${chapter.Parent.Identifier}/chapters/${chapter.Identifier}`, this.apiURL)));
         return images.map(image => new Page(this, chapter, new URL(image), { Referer: this.URI.href }));
     }
-
-    private async GetEmbeddedJSON<T>(uri: URL): Promise<T> {
-        const [script] = await FetchCSS<HTMLScriptElement>(new Request(uri), 'script#__NEXT_DATA__');
-        return (JSON.parse(script.text) as NEXTDATA<T>).props.pageProps;
-    }
-
 }
