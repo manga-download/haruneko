459f64952394683dc17ebd70353587f1f1fc1647
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-05-26 10:20:31 +0200

    fix: FlowerMangas : trigger Cloudflare at start

--- web/src/engine/websites/FlowerMangas.ts
+++ web/src/engine/websites/FlowerMangas.ts
@@ -3,6 +3,7 @@ import icon from './FlowerMangas.webp';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as Madara from './decorators/WordPressMadara';
 import * as Common from './decorators/Common';
+import { FetchWindowScript } from '../platform/FetchProvider';
 
 @Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type')
 @Madara.MangasMultiPageAJAX()
@@ -17,5 +18,10 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async Initialize(): Promise<void> {
+        //trigger Cloudflare at initialization
+        return await FetchWindowScript(new Request(new URL('/manga/-/', this.URI)), '');
     }
 }
