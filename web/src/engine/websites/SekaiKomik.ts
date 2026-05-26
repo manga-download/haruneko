7f2b9fa9da298451272fccbb94b000a3e3690294
Ronny <ronny@local.host>
2026-05-26 07:39:53 +0200

    website (sekaikomik): minor revision

--- web/src/engine/websites/SekaiKomik.ts
+++ web/src/engine/websites/SekaiKomik.ts
@@ -8,26 +8,29 @@ type APIResult<T> = {
     data: T;
 };
 
-type APIManga = {
+type APIMangas = APIResult<{
     title: string;
     slug: string;
-    chapters: APIChapter[];
-};
+}[]>;
 
-type APIChapter = {
-    chapterNumber: number;
-    title: string;
-};
+type APIChapters = APIResult<{
+    chapters: {
+        chapterNumber: number;
+        title: string;
+    }[];
+}>;
 
-type APIPages = {
+type APIPages = APIResult<{
     pages: {
         url: string;
     }[];
-};
+}>;
 
+@Common.MangaCSS(/^{origin}\/manga\/[^/]+$/, 'div.container h1', (h1, uri) => ({ id: uri.pathname.split('/').at(-1), title: h1.innerText.trim() }))
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
-    private readonly apiUrl = `${this.URI.origin}/api/proxy/`;
+
+    private readonly apiURL = `${this.URI.origin}/api/proxy/`;
 
     public constructor() {
         super('sekaikomik', 'ManhwaLand (SekaiKomik)', 'https://05c.manhwaland.land', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Language.Indonesian, Tags.Rating.Pornographic, Tags.Source.Aggregator);
@@ -37,37 +40,24 @@ export default class extends DecoratableMangaScraper {
         return icon;
     }
 
-    public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/manga/[^/]+$`).test(url);
-    }
-
-    public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const { slug, title } = await this.FetchAPI<APIManga>(`./manga/${url.split('/').at(-1)}`);
-        return new Manga(this, provider, slug, title);
-    }
-
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
         type This = typeof this;
         return Array.fromAsync(async function* (this: This) {
             for (let page = 1, run = true; run; page++) {
-                const datas = await this.FetchAPI<APIManga[]>(`./manga?limit=50&page=${page}`);
-                const mangas = datas.map(({ slug, title }) => new Manga(this, provider, slug, title));
+                const { data } = await FetchJSON<APIMangas>(new Request(new URL(`./manga?limit=50&page=${page}`, this.apiURL)));
+                const mangas = data.map(({ slug, title }) => new Manga(this, provider, slug, title));
                 mangas.length > 0 ? yield* mangas : run = false;
             }
         }.call(this));
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { chapters } = await this.FetchAPI<APIManga>(`./manga/${manga.Identifier}`);
+        const { data: { chapters } } = await FetchJSON<APIChapters>(new Request(new URL(`./manga/${manga.Identifier}`, this.apiURL)));
         return chapters.map(({ chapterNumber, title }) => new Chapter(this, manga, `${chapterNumber}`, [`Ch. ${chapterNumber}`, title].filter(Boolean).join(' ').trim()));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const { pages } = await this.FetchAPI<APIPages>(`./manga/${chapter.Parent.Identifier}/chapters/${chapter.Identifier}`);
+        const { data: { pages } } = await FetchJSON<APIPages>(new Request(new URL(`./manga/${chapter.Parent.Identifier}/chapters/${chapter.Identifier}`, this.apiURL)));
         return pages.map(({ url }) => new Page(this, chapter, new URL(url, this.URI)));
-    }
-
-    public async FetchAPI<T extends JSONElement>(endpoint: string): Promise<T> {
-        return (await FetchJSON<APIResult<T>>(new Request(new URL(endpoint, this.apiUrl)))).data;
     }
 }
