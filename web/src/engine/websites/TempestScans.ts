d32b26312824249da6380751b18e5691a04fe28f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-06-05 18:40:56 +0200

    fix: TempestScans - rewrite website
    without manga list for now (they dont have one).
    
    Closes https://github.com/manga-download/hakuneko/issues/8572

--- web/src/engine/websites/TempestScans.ts
+++ web/src/engine/websites/TempestScans.ts
@@ -1,21 +1,60 @@
 import { Tags } from '../Tags';
 import icon from './TempestScans.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as MangaStream from './decorators/WordPressMangaStream';
+import { Chapter, DecoratableMangaScraper, type Manga, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
+import { FetchJSON } from '../platform/FetchProvider';
 
-@MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
-@MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
-@MangaStream.PagesSinglePageJS()
+type APIResult<T> = {
+    json: T;
+};
+
+type APIChapter = {
+    id: string;
+    slug: string;
+    number: number;
+    pages: {
+        imageUrl: string;
+    }[];
+};
+
+@Common.MangaCSS(/^{origin}\/explore\/[^/]+$/, 'ol[data-slot="breadcrumb-list"] li:last-of-type', (el, uri) => ({
+    id: uri.pathname.split('/').at(-1),
+    title: el.textContent.trim()
+}))
+@Common.MangasNotSupported()
 @Common.ImageAjax(true)
 export default class extends DecoratableMangaScraper {
 
+    private readonly apiURL = 'https://juratempe.st/api/rpc/';
+
     public constructor() {
-        super('tempestscans', 'Tempest Scans', 'https://tempestmangas.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish, Tags.Source.Scanlator);
+        super('tempestscans', 'Tempest Scans', 'https://juratempe.st', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Turkish, Tags.Source.Scanlator);
     }
 
     public override get Icon() {
         return icon;
     }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        const chapters = await this.FetchAPI<APIChapter[]>('./chapter/byMangaSlug', { slug: manga.Identifier });
+        return chapters.map(({ slug, number }) => new Chapter(this, manga, slug, `Bölüm ${number}`));
+    }
+
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const [{ pages } ]= await this.FetchAPI<APIChapter[]>('./release/byChapterSlug', { mangaSlug: chapter.Parent.Identifier, chapterSlug: chapter.Identifier });
+        return pages.map(({ imageUrl }) => new Page(this, chapter, new URL(imageUrl)));
+    }
+
+    private async FetchAPI<T extends JSONElement>(endpoint: string, payload: JSONElement): Promise<T> {
+        return (await FetchJSON<APIResult<T>>(new Request(new URL(endpoint, this.apiURL), {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                json: payload
+            })
+        }))).json;
+    }
+
 }
