8b7794a1db1453497b90faf7ef74a65d8e47364d
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-06-08 12:56:37 +0000

    domains changes (#1713)

--- web/src/engine/websites/FlowerComics_e2e.ts
+++ web/src/engine/websites/FlowerComics_e2e.ts
@@ -8,7 +8,7 @@ new TestFixture({
     container: {
         url: 'https://flowercomics.jp/title/2216',
         id: '/title/2216',
-        title: '覇王様の異世界花嫁'
+        title: '覇王様の異世界花嫁〜虐げられた心癒し令嬢は皇帝の最愛となる〜'
     },
     child: {
         id: '/chapter/79154',
