359e24ae9178011a5026a5615b6268e00713dfb8
Ronny <ronny@local.host>
2026-06-13 00:42:22 +0200

    website (comix): minor revision

--- web/src/engine/websites/Comix.ts
+++ web/src/engine/websites/Comix.ts
@@ -1,12 +1,10 @@
 import { Tags } from '../Tags';
 import icon from './Comix.webp';
 import { Fetch, FetchJSON } from '../platform/FetchProvider';
-import { Page } from '../providers/MangaPlugin';
-import { type MangaPlugin, Manga, Chapter, DecoratableMangaScraper } from '../providers/MangaPlugin';
+import type { Priority } from '../taskpool/DeferredTask';
+import { DecoratableMangaScraper, type MangaPlugin, Manga, Chapter, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 import { DRMProvider } from './Comix.DRM';
-import type { Priority } from '../taskpool/DeferredTask';
-import { GetTypedData } from './decorators/Common';
 
 type APIMangas = {
     result: {
@@ -21,8 +19,8 @@ type APIMangas = {
 @Common.MangaCSS(/^{origin}\/title\/[^/]+$/, 'meta[property="og:title"]')
 export default class extends DecoratableMangaScraper {
 
-    private readonly apiURL = `${this.URI.origin}/api/v1/`;
     readonly #drm = new DRMProvider();
+    private readonly apiURL = `${this.URI.origin}/api/v1/`;
 
     public constructor() {
         super('comix', 'Comix', 'https://comix.to', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
@@ -63,20 +61,17 @@ export default class extends DecoratableMangaScraper {
             }
         })), priority, signal);
 
-        const seed = parseInt(response.headers.get('x-enc-seed'), 10);
-        const encLength = parseInt(response.headers.get('x-enc-len'), 10);
         const buffer = await response.arrayBuffer();
-        return await GetTypedData(!seed || !encLength ? buffer : this.DecryptImage(buffer, seed, encLength));
+        const seed = response.headers.get('X-Enc-Seed');
+        const limit = response.headers.get('X-Enc-Len');
+        return Common.GetTypedData(seed && limit ? this.#DecryptImage(buffer, parseInt(seed, 10), parseInt(limit, 10)) : buffer);
     }
 
-    private DecryptImage(encrypted: ArrayBuffer, seed: number, length: number): ArrayBuffer {
+    #DecryptImage(encrypted: ArrayBuffer, key: number, limit: number): ArrayBuffer {
         const bytes = new Uint8Array(encrypted);
-        let state = seed;
-        const limit = Math.min(bytes.length, length);
-
-        for (let i = 0; i < limit; i++) {
-            state = state * 1000005 + 1234567891 >> 0;
-            bytes[i] = bytes[i] ^ state >>> 24;
+        for (let i = 0; i < Math.min(bytes.length, limit); i++) {
+            key = key * 1000005 + 1234567891 >> 0;
+            bytes[i] = bytes[i] ^ key >>> 24;
         }
         return bytes.buffer;
     }
