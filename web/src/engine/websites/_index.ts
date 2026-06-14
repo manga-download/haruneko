b2266d4b563af825a33756371021d2639a6ade42
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-06-14 10:49:33 +0200

    feat: Add HerosWeb (new one)
    Closes https://github.com/manga-download/haruneko/issues/1718

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -201,6 +201,7 @@ export { default as HentaiTokyo } from './HentaiTokyo';
 export { default as HentaiXYuri } from './HentaiXYuri';
 export { default as HentaiZone } from './HentaiZone';
 export { default as HerosWeb } from './HerosWeb';
+export { default as HerosWebNew } from './HerosWebNew';
 export { default as HijalaScans } from './HijalaScans';
 export { default as HiperCool } from './HiperCool';
 export { default as Hiperdex } from './Hiperdex';
