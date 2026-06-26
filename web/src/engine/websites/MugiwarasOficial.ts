7b61d04df9745df658ff787d03e716448e24ea05
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-06-26 12:11:14 +0200

    fix: MangaBrasuka : fix getting pages

--- web/src/engine/websites/MugiwarasOficial.ts
+++ web/src/engine/websites/MugiwarasOficial.ts
@@ -1,27 +1,14 @@
 import { Tags } from '../Tags';
 import icon from './MugiwarasOficial.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as Madara from './decorators/WordPressMadara';
-import * as Common from './decorators/Common';
-import { pageScript } from './MangasBrasuka';
-import { FetchWindowScript } from '../platform/FetchProvider';
-
-@Madara.MangaCSS(/^{origin}\/manga\/[^/]+\/$/, 'ol.breadcrumb li:last-of-type')
-@Madara.MangasMultiPageAJAX()
-@Madara.ChaptersSinglePageAJAXv2()
-@Common.PagesSinglePageJS(pageScript)
-@Common.ImageAjax(true)
-export default class extends DecoratableMangaScraper {
+import MangasBrasuka from './MangasBrasuka';
+export default class extends MangasBrasuka {
 
     public constructor() {
-        super('mugiwarasoficial', 'Mugiwaras Oficial', 'https://mugiwarasoficial.com', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Portuguese, Tags.Source.Aggregator);
+        super('mugiwarasoficial', 'Mugiwaras Oficial', 'https://mugiwarasoficial.com', [Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Portuguese, Tags.Source.Aggregator, Tags.Accessibility.RegionLocked]);
     }
 
     public override get Icon() {
         return icon;
     }
 
-    public override async Initialize(): Promise<void> {
-        return FetchWindowScript(new Request(this.URI), `window.cookieStore.set('wpmanga-adault', '1')`);
-    }
 }
