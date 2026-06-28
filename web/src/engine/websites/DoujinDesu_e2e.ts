7ceb78462f6fee1ae853b9f90a70fd248a7184c0
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-06-28 15:42:39 +0200

    update doujindesu tests
    add Html entities case

--- web/src/engine/websites/DoujinDesu_e2e.ts
+++ web/src/engine/websites/DoujinDesu_e2e.ts
@@ -1,5 +1,6 @@
 import { TestFixture } from '../../../test/WebsitesFixture';
 
+// CASE : Regular tests
 new TestFixture({
     plugin: {
         id: 'doujindesu',
@@ -17,6 +18,28 @@ new TestFixture({
     entry: {
         index: 1,
         size: 57_002,
+        type: 'image/webp'
+    }
+}).AssertWebsite();
+
+// CASE : HTML entities in pictures URLS (chapters 1 to 62)
+new TestFixture({
+    plugin: {
+        id: 'doujindesu',
+        title: 'DoujinDesu',
+    },
+    container: {
+        url: 'https://doujin.desu.xxx/manga/im-the-only-man-on-the-military-base',
+        id: 'im-the-only-man-on-the-military-base',
+        title: `I'm the Only Man on the Military Base`
+    },
+    child: {
+        id: '5abc79c4-ea0e-4a4d-ad15-765bbc19a665',
+        title: 'Chapter 62'
+    },
+    entry: {
+        index: 0,
+        size: 348_524,
         type: 'image/webp'
     }
 }).AssertWebsite();
