402a552c7339d1546d0a1474fba5ffd3e826ab4a
Ronny <ronny@local.host>
2026-06-29 19:25:42 +0200

    website (reyume): fix regex

--- web/src/engine/websites/Reyume.ts
+++ web/src/engine/websites/Reyume.ts
@@ -10,7 +10,7 @@ export default class extends ZeistManga {
 
     public constructor() {
         super('reyume', 'Reyume', 'https://www.re-yume.my.id', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Scanlator);
-        this.WithMangaSlugScript(`document.querySelector('div#clwd script').text.match(/run\\(['"](.*)['"]/).at(1)`);
+        this.WithMangaSlugScript(`document.querySelector('div#clwd script').text.match(/run\\(['"]([^'"]+)['"]/).at(1)`);
     }
 
     public override get Icon() {
@@ -21,6 +21,5 @@ export default class extends ZeistManga {
         const [html] = await FetchCSS(new Request(new URL(chapter.Identifier, this.URI)), '#zeist-raw-data');
         return [...new DOMParser().parseFromString(html.textContent, 'text/html').querySelectorAll<HTMLImageElement>('div.separator a img')]
             .map(img => new Page(this, chapter, new URL(PageLinkExtractor(img))));
-
     }
 }
