7d56552d37a538fe251cf6c7d78a8790213c5806
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-05 15:12:14 +0200

    Update Wurmz.ts

--- web/src/engine/websites/Wurmz.ts
+++ web/src/engine/websites/Wurmz.ts
@@ -11,7 +11,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('wurmmz', 'Wurmz', 'https://wurmz.net', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Indonesian, Tags.Source.Aggregator);
+        super('wurmz', 'Wurmz', 'https://wurmz.net', Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Media.Manga, Tags.Language.Indonesian, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
