1583dde11cd442f8757c09b463ba93c18652c7cc
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-07 11:23:17 +0000

    fix: LineWebtoon - fix getting chapters & pages (#1753)
    * fix: LineWebtoon - fix getting chapters & pages
    
    * Changed CSS for chapters
    * Use referer for images parts

--- web/src/engine/websites/LineWebtoon.ts
+++ web/src/engine/websites/LineWebtoon.ts
@@ -1,7 +1,9 @@
 import { Tags } from '../Tags';
 import icon from './LineWebtoon.webp';
-import { LineWebtoonBase } from './templates/LineWebtoonBase';
+import * as Common from './decorators/Common';
+import { ChapterExtractor, LineWebtoonBase } from './templates/LineWebtoonBase';
 
+@Common.ChaptersMultiPageCSS('div.detail_list_area ul.detail_list li > a', Common.PatternLinkGenerator('{id}&page={page}'), 0, ChapterExtractor)
 export default class extends LineWebtoonBase {
 
     public constructor() {
