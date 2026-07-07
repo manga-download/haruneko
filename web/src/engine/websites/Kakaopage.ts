ae3b24fdaeb7a0f47d3eeab1fa8be3cac60e5525
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-07 19:21:28 +0200

    fix: Kakaopage - fix clipboard

--- web/src/engine/websites/Kakaopage.ts
+++ web/src/engine/websites/Kakaopage.ts
@@ -1,21 +1,28 @@
 import { Tags } from '../Tags';
 import icon from './Kakaopage.webp';
-import { FetchCSS, FetchJSON } from '../platform/FetchProvider';
-import { Chapter, DecoratableMangaScraper, Manga, Page, type MangaPlugin } from '../providers/MangaPlugin';
+import { FetchJSON } from '../platform/FetchProvider';
+import { Chapter, DecoratableMangaScraper, Manga, type MangaPlugin, Page } from '../providers/MangaPlugin';
 import * as Common from './decorators/Common';
 
-type APIChapters = {
-    result: {
-        has_next: boolean;
-        list: {
-            cursor_index: number;
-            item: {
-                product_id: number;
-                title: string;
-            }
-        }[];
-    };
+type APIResult<T> = {
+    result: T;
 };
+
+type APIManga = APIResult<{
+    content: {
+        title: string;
+    };
+}>;
+type APIChapters = APIResult<{
+    has_next: boolean;
+    list: {
+        cursor_index: number;
+        item: {
+            product_id: number;
+            title: string;
+        };
+    }[];
+}>;
 
 type APIPages = {
     viewer_data: {
@@ -23,15 +30,15 @@ type APIPages = {
             files: {
                 secureUrl: string;
             }[];
-        }
-    }
+        };
+    };
 };
 
 @Common.MangasNotSupported()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
-    private readonly apiUrl = 'https://bff-page.kakao.com/api/gateway/api/';
+    private readonly apiURL = 'https://bff-page.kakao.com/api/gateway/api/';
 
     public constructor() {
         super('kakaopage', 'Page Kakao (카카오페이지)', 'https://page.kakao.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Korean, Tags.Source.Official);
@@ -42,22 +49,22 @@ export default class extends DecoratableMangaScraper {
     }
 
     public override ValidateMangaURL(url: string): boolean {
-        return new RegExpSafe(`^${this.URI.origin}/content/[\\d]+$`).test(url);
+        return new RegExpSafe(`^${this.URI.origin}/content/[\\d]+(/)?$`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
         const id = new URL(url).pathname.match(/content\/(\d+)/).at(1);
-        const [{ text }] = await FetchCSS<HTMLTitleElement>(new Request(url), 'title');
-        return new Manga(this, provider, id, text.split(' - ').at(0).trim());
+        const { result: { content: { title } } } = await FetchJSON<APIManga>(new Request(new URL(`./v1/content/overview?series_id=${id}`, this.apiURL)));
+        return new Manga(this, provider, id, title);
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { result: { list } } = await FetchJSON<APIChapters>(new Request(new URL(`./v2/content/product/list?series_id=${manga.Identifier}&cursor_index=0&window_size=9999`, this.apiUrl)));
+        const { result: { list } } = await FetchJSON<APIChapters>(new Request(new URL(`./v2/content/product/list?series_id=${manga.Identifier}&cursor_index=0&window_size=9999`, this.apiURL)));
         return list.map(({ item: { product_id: productId, title } }) => new Chapter(this, manga, `${productId}`, title.replace(manga.Title, '').trim() || title));
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
-        const { viewer_data: { imageDownloadData: { files } } } = await FetchJSON<APIPages>(new Request(new URL(`./v1/viewer/data?series_id=${chapter.Parent.Identifier}&product_id=${chapter.Identifier}`, this.apiUrl)));
+        const { viewer_data: { imageDownloadData: { files } } } = await FetchJSON<APIPages>(new Request(new URL(`./v1/viewer/data?series_id=${chapter.Parent.Identifier}&product_id=${chapter.Identifier}`, this.apiURL)));
         return files.map(({ secureUrl }) => new Page(this, chapter, new URL(secureUrl)));
     }
 }
