b4eecef7d01bef4a428a3dbd8e1399f264ece65f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-10 12:22:18 +0200

    feat: LineWebToon : use api to get chapters
    * Faster.
    * More reliable.
    * Can prevent rate limit errors.
    * Keep the same chapter identifiers than previous html scraping.

--- web/src/engine/websites/DongManManhua.ts
+++ web/src/engine/websites/DongManManhua.ts
@@ -1,7 +1,15 @@
 import { Tags } from '../Tags';
 import icon from './DongManManhua.webp';
 import * as Common from './decorators/Common';
-import { ChapterExtractor, LineWebtoonBase } from './templates/LineWebtoonBase';
+import { LineWebtoonBase } from './templates/LineWebtoonBase';
+
+function ChapterExtractor(element: HTMLAnchorElement) {
+    const chapter = element.querySelector('span.tx');
+    let title = chapter ? chapter.textContent.trim() + ' - ' : '';
+    title += element.querySelector('span.subj span').textContent.trim();
+    const id = /'/.test(element.href) ? decodeURIComponent(element.href).match(/'([^']+)'/)[1] : element.pathname + element.search;
+    return { id, title };
+}
 
 @Common.MangasSinglePageCSS<HTMLAnchorElement>('/dailySchedule', 'div.daily_lst ul.daily_card li a.daily_card_item',
     anchor => ({ id: anchor.pathname + anchor.search, title: anchor.querySelector('p.subj').textContent.trim() }))
