9c08903dcbc8658dab1dab981fc373a37068fe82
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-11 14:17:18 +0200

    Update Crypto.ts

--- web/src/engine/Crypto.ts
+++ web/src/engine/Crypto.ts
@@ -1,5 +1,7 @@
 import { GetBytesFromUTF8 } from './BufferEncoder';
 
+export const HMAC256Algo = { name: 'HMAC', hash: { name: 'SHA-256' } };
+
 /**
  * Perform a XOR operation
  * @param source - Bytes to be xored
@@ -32,13 +34,21 @@ async function HASH(message: BufferSource, hashType: string): Promise<ArrayBuffe
 }
 
 /**
- * Compute a HMAC (with SHA-256) signature the provided data
+ * Compute a HMAC (with SHA-256) signature for the provided data
  * @param message - Bytes/string to be hashed
+ * @param key - key used to sign.
  */
-export async function HMAC256(message: string | BufferSource, keyData: string | BufferSource): Promise<ArrayBuffer> {
-    const algorithm = { name: 'HMAC', hash: { name: 'SHA-256' } };
+export async function HMAC256(message: string | BufferSource, key: string | BufferSource | CryptoKey): Promise<ArrayBuffer> {
     if (typeof message === 'string') message = GetBytesFromUTF8(message);
-    if (typeof keyData === 'string') keyData = GetBytesFromUTF8(keyData);
-    const key = await crypto.subtle.importKey('raw', keyData, algorithm, false, ['sign']);
-    return crypto.subtle.sign(algorithm, key, message);
+    key = await HMAC256ImportKey(key);
+    return crypto.subtle.sign(HMAC256Algo, key, message);
+}
+
+/**
+ * Compute a HMAC (with SHA-256) Key
+ * @param key - data to use as key
+ */
+export async function HMAC256ImportKey(key: string | BufferSource | CryptoKey): Promise<CryptoKey> {
+    if (key instanceof CryptoKey) return key;
+    return crypto.subtle.importKey('raw', typeof key === 'string' ? GetBytesFromUTF8(key) : key, HMAC256Algo, false, ['sign']);
 }
