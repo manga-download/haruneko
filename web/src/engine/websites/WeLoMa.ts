765b4cd0b920be957da24c5c98f412c3b83a24e8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-07-25 11:05:47 +0200

    fix: Weloma - change domain, fix pasting
    Closes https://github.com/manga-download/haruneko/issues/1773

--- web/src/engine/websites/WeLoMa.ts
+++ web/src/engine/websites/WeLoMa.ts
@@ -4,16 +4,17 @@ import { FetchWindowScript } from '../platform/FetchProvider';
 import { DecoratableMangaScraper } from '../providers/MangaPlugin';
 import * as FlatManga from './templates/FlatManga';
 import * as Common from './decorators/Common';
+import { GetBytesFromBase64, GetUTF8FromBytes } from '../BufferEncoder';
 
-@Common.MangaCSS(/^{origin}\/\d+\/$/, FlatManga.queryMangaTitle)
+@Common.MangaCSS(/^{origin}\/m\/[^/]+$/, 'ol.breadcrumb li:last-of-type')
 @Common.MangasMultiPageCSS(FlatManga.queryMangas, FlatManga.MangasLinkGenerator)
 @Common.ChaptersSinglePageCSS(FlatManga.queryChapters, undefined, Common.AnchorInfoExtractor(true))
-@Common.PagesSinglePageCSS(FlatManga.queryPages, (img: HTMLImageElement) => atob(img.dataset.img))
+@Common.PagesSinglePageCSS(FlatManga.queryPages, (img: HTMLImageElement) => GetUTF8FromBytes(GetBytesFromBase64(img.dataset.img)))
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('weloma', 'WeLoMa', 'https://weloma.art', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
+        super('weloma', 'WeLoMa', 'https://weloma.net', Tags.Media.Manga, Tags.Language.Japanese, Tags.Source.Aggregator);
     }
 
     public override Initialize(): Promise<void> {
