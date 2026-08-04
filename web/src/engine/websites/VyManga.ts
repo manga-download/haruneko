75c6b9d7c51e0efc158686bb793da01af6a84626
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-08-04 23:19:04 +0200

    domains update & remove dead websites

--- web/src/engine/websites/VyManga.ts
+++ web/src/engine/websites/VyManga.ts
@@ -12,7 +12,7 @@ import * as Common from './decorators/Common';
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('vymanga', 'VyManga', 'https://vymanga.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
+        super('vymanga', 'VyManga', 'https://mangavyvy.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.English, Tags.Source.Aggregator);
     }
 
     public override get Icon() {
