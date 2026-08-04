8522bc030d1b4431ebf4c37ae751b699826c4783
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-08-04 10:55:49 +0200

    Update MyAnimeListManga.ts

--- web/src/engine/websites/MyAnimeListManga.ts
+++ web/src/engine/websites/MyAnimeListManga.ts
@@ -5,13 +5,12 @@ import * as Common from './decorators/Common';
 import { FetchCSS } from '../platform/FetchProvider';
 import type { Priority } from '../taskpool/DeferredTask';
 import { GetTypedData } from './decorators/Common';
-import { Exception } from '../Error';
+import { Exception, NotImplementedError } from '../Error';
 import { WebsiteResourceKey as R } from '../../i18n/ILocale';
 
 // TODO: Handle Novels (modified Publus reader?)
 
 type ChaptersJSON = Array<{
-    id: number;
     numbering_text: string;
     viewer_url: string;
     preview_url: string;
@@ -38,7 +37,7 @@ type PageMode = {
 export default class extends DecoratableMangaScraper {
 
     public constructor() {
-        super('myanimelistmanga', `MyAnimeList (Manga)`, 'https://myanimelist.net', Tags.Language.English, Tags.Media.Manga, Tags.Source.Official);
+        super('myanimelistmanga', 'MyAnimeList (Manga)', 'https://myanimelist.net', Tags.Language.English, Tags.Media.Manga, Tags.Source.Official);
     }
 
     public override get Icon() {
@@ -47,41 +46,33 @@ export default class extends DecoratableMangaScraper {
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
         const [button] = await FetchCSS<HTMLDivElement>(new Request(new URL(manga.Identifier, this.URI)), 'div.v-manga-store-purchase-bulk-button');
-        const chaptersData: ChaptersJSON = JSON.parse(button.dataset.items);
+        const chaptersData = <ChaptersJSON>JSON.parse(button.dataset.items);
         return chaptersData.filter(({ is_free: free, is_possessed: owned, is_previewable: preview }) => preview || free || owned)
-            .filter(({ viewer_url: viewerUrl }) => !viewerUrl.includes('novel_viewer'))
-            .map(({ is_free: free, is_possessed: owned, numbering_text: name, preview_url: pUrl, viewer_url: vUrl }) => {
+            .map(({ is_free: free, is_possessed: owned, numbering_text: name, preview_url: previewUrl, viewer_url: viewerUrl }) => {
                 const isPossessed = owned ?? false;
                 const isFull = free || isPossessed;
-                const title = [name.trim(), isFull ? '' : '[Preview]'].join(' ').trim();
-                return new Chapter(this, manga, new URL(isFull ? vUrl : pUrl, this.URI).pathname, title);
+                const title = [name.trim(), isFull ? '' : '[Preview]'].joinTitleSegments();
+                return new Chapter(this, manga, new URL(isFull ? viewerUrl : previewUrl, this.URI).pathname, title);
             });
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page<PageMode>[]> {
+        // Filter Publus for now
+        if (/novel_viewer/.test(chapter.Identifier)) {
+            throw new NotImplementedError('Novels are not supported.');
+        }
+
         const [viewerElement] = await FetchCSS<HTMLDivElement>(new Request(new URL(chapter.Identifier, this.URI)), 'div.v-manga-store-viewer');
         if (!viewerElement) {
             throw new Exception(R.Plugin_Common_Chapter_UnavailableError);
         }
-        const { manuscript: { filenames, image_base_url, query_params_part } }: ViewerJSON = JSON.parse(viewerElement.dataset.state);
-        return filenames.map(file => {
-            const uri = new URL(image_base_url + '/' + file);
-            uri.search = query_params_part;
-            return new Page<PageMode>(this, chapter, uri, { mode: 'xor' });
-        });
+        const { manuscript: { filenames, image_base_url, query_params_part } } = <ViewerJSON>JSON.parse(viewerElement.dataset.state);
+        return filenames.map(file => new Page<PageMode>(this, chapter, new URL(`${image_base_url}/${file}?${query_params_part}`), { mode: 'xor' }));
     }
 
     public override async FetchImage(page: Page<PageMode>, priority: Priority, signal: AbortSignal): Promise<Blob> {
         const blob = await Common.FetchImageAjax.call(this, page, priority, signal);
-        switch (page.Parameters.mode) {
-            case 'xor': {
-                return GetTypedData(this.XOR(await blob.arrayBuffer()));
-            }
-            default: {
-                // Fallback: return the image blob as-is for unknown modes
-                return blob;
-            }
-        }
+        return GetTypedData(this.XOR(await blob.arrayBuffer()));
     }
 
     private XOR(encrypted: ArrayBuffer): ArrayBuffer {
