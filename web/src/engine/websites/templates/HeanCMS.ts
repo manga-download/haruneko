d1a3b52cfac7d085a8fb2c70270bf1283bce2531
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-08-05 15:33:17 +0200

    little changes

--- web/src/engine/websites/templates/HeanCMS.ts
+++ web/src/engine/websites/templates/HeanCMS.ts
@@ -1,7 +1,6 @@
-import { Exception } from '../../Error';
+import { Exception, NotImplementedError } from '../../Error';
 import { FetchJSON } from '../../platform/FetchProvider';
 import { type MangaPlugin, Manga, Chapter, Page, DecoratableMangaScraper } from '../../providers/MangaPlugin';
-import type { Priority } from '../../taskpool/TaskPool';
 import * as Common from '../decorators/Common';
 import { WebsiteResourceKey as R } from '../../../i18n/ILocale';
 
@@ -38,8 +37,8 @@ type APIPages = {
             files: {
                 url: string;
             }[];
-        }
-    }
+        };
+    };
 };
 
 type APIMediaID = {
@@ -47,22 +46,22 @@ type APIMediaID = {
     slug: string;
 };
 
-type PageType = {
-    type: string;
-};
-
+@Common.ImageAjax()
 export class HeanCMS extends DecoratableMangaScraper {
 
-    protected apiUrl = this.URI.origin.replace('://', '://api.');
-    protected mediaUrl = this.apiUrl;
+    private apiURL = this.URI.origin.replace('://', '://api.');
+    private mediaUrl = this.apiURL;
 
     public override ValidateMangaURL(url: string): boolean {
         return new RegExpSafe(`^${this.URI.origin}/series/[^/]+$`).test(url);
     }
 
     public override async FetchManga(provider: MangaPlugin, url: string): Promise<Manga> {
-        const { title, series_slug, id } = await this.FetchAPI<APIManga>(`./series/${url.split('/').at(-1)}`);
-        return new Manga(this, provider, JSON.stringify({ id: id.toString(), slug: series_slug }), title);
+        const { title, series_slug, id, series_type } = await this.FetchAPI<APIManga>(`./series/${url.split('/').at(-1)}`);
+        if (series_type.toLowerCase() === 'novel') {
+            throw new NotImplementedError('Novels are not supported !');
+        }
+        return new Manga(this, provider, JSON.stringify({ id: `${id}`, slug: series_slug }), title);
     }
 
     public override async FetchMangas(provider: MangaPlugin): Promise<Manga[]> {
@@ -71,7 +70,9 @@ export class HeanCMS extends DecoratableMangaScraper {
             for (const adult of [true, false]) { //adult flag mean ONLY adult, false mean ONLY all ages... Talk about stupid. Old api ignore that flag
                 for (let page = 1, run = true; run; page++) {
                     const { data } = await this.FetchAPI<APIResult<APIManga[]>>(`./query?perPage=100&page=${page}&adult=${adult}`);
-                    const mangas = !data.length ? [] : data.map(({ id, series_slug: slug, title }) => new Manga(this, provider, JSON.stringify({ id: `${id}`, slug }), title));
+                    const mangas = !data.length ? [] : data
+                        .filter(({ series_type: type }) => type.toLowerCase() != 'novel')
+                        .map(({ id, series_slug: slug, title }) => new Manga(this, provider, JSON.stringify({ id: `${id}`, slug }), title));
                     mangas.length > 0 ? yield* mangas : run = false;
                 }
             }
@@ -79,29 +80,26 @@ export class HeanCMS extends DecoratableMangaScraper {
     }
 
     public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
-        const { id: mangaId }: APIMediaID = JSON.parse(manga.Identifier);
+        const { id: mangaId } = <APIMediaID>JSON.parse(manga.Identifier);
         const { data } = await this.FetchAPI<APIResult<APIChapter[]>>(`./chapter/query?series_id=${mangaId}&perPage=9999&page=1`);
         return data.map(({ id, chapter_name: name, chapter_slug: slug, chapter_title: title }) => new Chapter(this, manga, JSON.stringify({
             id: `${id}`,
             slug,
-        }), `${name} ${title || ''}`.trim()));
+        }), [name, title].joinTitleSegments()));
     }
 
-    public override async FetchPages(chapter: Chapter): Promise<Page<PageType>[]> {
-        const { slug: chapterSlug }: APIMediaID = JSON.parse(chapter.Identifier);
-        const { slug: mangaSlug }: APIMediaID = JSON.parse(chapter.Parent.Identifier);
+    public override async FetchPages(chapter: Chapter): Promise<Page[]> {
+        const { slug: chapterSlug } = <APIMediaID>JSON.parse(chapter.Identifier);
+        const { slug: mangaSlug } = <APIMediaID>JSON.parse(chapter.Parent.Identifier);
         const { data, paywall, chapter: { chapter_type, chapter_data, storage } } = await this.FetchAPI<APIPages>(`./chapter/${mangaSlug}/${chapterSlug}`);
 
         if (paywall) {
             throw new Exception(R.Plugin_Common_Chapter_UnavailableError);
         }
 
-        if (chapter_type.toLowerCase() === 'novel') {
-            throw new Exception(R.Plugin_HeanCMS_ErrorNovelsNotSupported);
-        }
         //old API vs new
         const listImages = Array.isArray(data) ? data as string[] : chapter_data.images ? chapter_data.images : chapter_data.files.map(file => file.url);
-        return listImages.map(image => new Page<PageType>(this, chapter, this.ComputePageUrl(image, storage), { type: chapter_type }));
+        return listImages.map(image => new Page(this, chapter, this.ComputePageUrl(image, storage), { type: chapter_type }));
     }
 
     protected ComputePageUrl(image: string, storage: string): URL {
@@ -112,14 +110,8 @@ export class HeanCMS extends DecoratableMangaScraper {
         }
     }
 
-    public override async FetchImage(page: Page<PageType>, priority: Priority, signal: AbortSignal, detectMimeType = true, deProxifyLink = true): Promise<Blob> {
-        if (page.Parameters?.type === 'Comic') {
-            return Common.FetchImageAjax.call(this, page, priority, signal, detectMimeType, deProxifyLink);
-        } else throw new Exception(R.Plugin_HeanCMS_ErrorNovelsNotSupported);
-    }
-
     private async FetchAPI<T extends JSONElement>(endpoint: string): Promise<T> {
-        return FetchJSON<T>(new Request(new URL(endpoint, this.apiUrl), {
+        return FetchJSON<T>(new Request(new URL(endpoint, this.apiURL), {
             headers: {
                 Referer: this.URI.href
             }
