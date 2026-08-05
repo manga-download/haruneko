e9a3db3d3ff7722f17c020869fda448d114fcb10
Ronny <ronny@local.host>
2026-08-05 21:45:11 +0200

    introduce crypto helper for shared functionality

---
+++ web/src/engine/Crypto.ts
@@ -0,0 +1,20 @@
+import { GetBytesFromUTF8 } from './BufferEncoder';
+
+type DigestAlgorithm = 'SHA256' | 'SHA-256' | 'SHA512' | 'SHA-512';
+
+// TODO: Improve TS-DOC ...
+export async function HashUTF8(algorithm: DigestAlgorithm, text: string) {
+    return new Uint8Array(await crypto.subtle.digest(algorithm, GetBytesFromUTF8(text)));
+}
+
+// TODO: Improve TS-DOC ...
+export function XOR(encrypted: Uint8Array, key: Uint8Array): Uint8Array<ArrayBuffer> {
+    return encrypted.map((byte, index) => byte ^ key[index % key.length]);
+}
+
+// TODO: Improve TS-DOC ...
+// Decrypt with AES importing the RAW key-data with the exactly same algorithm identifier
+export async function DecryptAES(encrypted: BufferSource, keyData: BufferSource, algorithm: AesCtrParams | AesCbcParams | AesGcmParams): Promise<ArrayBuffer> {
+    const key = await crypto.subtle.importKey('raw', keyData, algorithm.name, false, ['decrypt']);
+    return crypto.subtle.decrypt(algorithm, key, encrypted);
+}
