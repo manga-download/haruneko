0cf92f6d1e494542f5904268e147e58b9da6dac5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-08-19 21:57:46 +0200

    feat: introduce NovelDex template
    add DivaScans, update ValirScans, update WitchScans
    
    Closes https://github.com/manga-download/hakuneko/issues/8678

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -110,6 +110,7 @@ export { default as Digimon } from './Digimon';
 export { default as DigitalTeam } from './DigitalTeam';
 export { default as Dilar } from './Dilar';
 export { default as DingManhua } from './DingManhua';
+export { default as DivaScans } from './DivaScans';
 export { default as Dm5 } from './Dm5';
 export { default as Dmzj } from './Dmzj';
 export { default as DomalFansub } from './DomalFansub';
