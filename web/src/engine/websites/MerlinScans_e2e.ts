7eea404f90910c8fe0511e2c6fb5cefee2fe46bf
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-08-21 17:10:28 +0200

    fix: MerlinScans - getting pages

--- web/src/engine/websites/MerlinScans_e2e.ts
+++ web/src/engine/websites/MerlinScans_e2e.ts
@@ -9,12 +9,12 @@ new TestFixture({
         url: 'https://merlintoon.com/seri/akademinin-dehasi/',
         id: '/seri/akademinin-dehasi/',
         title: 'Akademinin Dehası',
-        timeout: 10_000
+        timeout: 15_000
     },
     child: {
         id: '/seri/akademinin-dehasi/bolum-65/',
         title: 'Bölüm 65',
-        timeout: 10_000
+        timeout: 15_000
     },
     entry: {
         index: 1,
