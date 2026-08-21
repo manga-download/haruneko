3aac307b5ae013a5fb1b629f693d8bab59fcf0e1
Sheep <sheepux@live.com>
2026-08-21 21:56:51 +0200

    feat(UI classic): viewer preload next item (optional)

--- web/src/frontend/classic/components/viewer/ImageViewerWideSettings.svelte
+++ web/src/frontend/classic/components/viewer/ImageViewerWideSettings.svelte
@@ -20,6 +20,7 @@
         Button,
         Tooltip,
         Stack,
+        Toggle,
     } from "carbon-components-svelte";
     import {
         Key,
@@ -133,11 +134,9 @@
                     portalTooltip
                     class="tooltip"
                 >
-                    <p>
                         {GlobalSettings.Locale[
                             Settings.ViewerMode.Setting.Description
                         ]()}
-                    </p>
                 </Tooltip>
                 <ContentSwitcher size="sm">
                         <Switch
@@ -165,11 +164,9 @@
                         portalTooltip
                         class="tooltip"
                     >
-                        <p>
                             {GlobalSettings.Locale[
                                 Settings.ViewerReverseDirection.Setting.Description
                             ]()}
-                        </p>
                     </Tooltip>
                     <ContentSwitcher size="sm">
                         <Switch
@@ -196,11 +193,9 @@
                         portalTooltip
                         class="tooltip"
                     >
-                        <p>
                             {GlobalSettings.Locale[
                                 Settings.ViewerDoublePage.Setting.Description
                             ]()}
-                        </p>
                     </Tooltip>
                     <ContentSwitcher size="sm">
                         <Switch
@@ -220,6 +215,19 @@
                     </ContentSwitcher>
                 </div>
             {/if}
+            <Toggle class="setting block" bind:toggled={Settings.ViewerPreloadNextItem.Value} >
+                <svelte:fragment slot="labelChildren">
+                    <Tooltip
+                        triggerText={GlobalSettings.Locale[Settings.ViewerPreloadNextItem.Setting.Label]()}
+                        portalTooltip
+                        class="tooltip"
+                    >
+                            {GlobalSettings.Locale[
+                                Settings.ViewerPreloadNextItem.Setting.Description
+                            ]()}
+                    </Tooltip>
+                </svelte:fragment>
+            </Toggle>
         </div>
     </div>
 </div>
@@ -262,7 +270,7 @@
         white-space: nowrap;
     }
     #vieweractions .setting.block {
-        margin-bottom: 0.5em;
+        margin-bottom: 1em;
     }
     #vieweractions :global(.tooltip) {
         margin-bottom:0.25em;
