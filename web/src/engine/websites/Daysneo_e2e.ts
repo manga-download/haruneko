e94775ef26e059988fb060417350d32c6b6c40b8
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-01 19:06:45 +0200

    daysneo

--- web/src/engine/websites/Daysneo_e2e.ts
+++ web/src/engine/websites/Daysneo_e2e.ts
@@ -1,6 +1,6 @@
 ﻿import { TestFixture } from '../../../test/WebsitesFixture';
 
-const config = {
+new TestFixture({
     plugin: {
         id: 'daysneo',
         title: 'Daysneo'
@@ -19,6 +19,4 @@ const config = {
         size: 108_761,
         type: 'image/jpeg'
     }
-};
-
-new TestFixture(config).AssertWebsite();
+}).AssertWebsite();
