c3694b9bc8002f97d8a0e72f0b5249208ba0679b
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-02 11:57:26 +0200

    fix: GuruKomik - move to ZeistManga
    website is broken anyway.

--- web/src/engine/websites/GuruKomik.ts
+++ web/src/engine/websites/GuruKomik.ts
@@ -1,27 +1,15 @@
 import { Tags } from '../Tags';
 import icon from './GuruKomik.webp';
-import { DecoratableMangaScraper } from '../providers/MangaPlugin';
-import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
+import { PageLinkExtractor, ZeistManga } from './templates/ZeistManga';
 
-const scriptChapterList = `
-    [...document.querySelectorAll('div#episodeNime ul li div.eph-num a:last-of-type')].map(anchor => {
-        return {
-            id: anchor.pathname,
-            title: anchor.querySelector('span.chapternum').textContent
-        };
-    });
-`;
-
-@MangaStream.MangaCSS(/^{origin}\/[0-9]{4}\/[0-9]{2}\/[^/]+$/, 'meta[property="og:description"]')
-@Common.MangasNotSupported()
-@Common.ChaptersSinglePageJS(scriptChapterList, 2500)
-@MangaStream.PagesSinglePageCSS()
-@Common.ImageAjax()
-export default class extends DecoratableMangaScraper {
+@Common.MangaCSS(/^{origin}\/\d+\/\d+\/[^/]+\.html$/, 'meta[property="og:description"]')
+@Common.PagesSinglePageCSS('div#readarea img', PageLinkExtractor)
+export default class extends ZeistManga {
 
     public constructor() {
         super('gurukomik', 'Guru Komik', 'https://gurukomiklive.blogspot.com', Tags.Media.Manga, Tags.Media.Manhwa, Tags.Media.Manhua, Tags.Language.Indonesian, Tags.Source.Aggregator);
+        this.WithMangaSlugScript(`new URL(document.querySelector('#chaptermanga script').src, location).pathname.split('/').at(-1)`);
     }
 
     public override get Icon() {
