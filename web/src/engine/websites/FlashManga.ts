a719014889ac43d0e38aefcb433e0d71c168b9dd
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-02 10:12:35 +0200

    Update FlashManga.ts

--- web/src/engine/websites/FlashManga.ts
+++ web/src/engine/websites/FlashManga.ts
@@ -1,13 +1,12 @@
 import { Tags } from '../Tags';
 import icon from './FlashManga.webp';
-import { type Chapter, DecoratableMangaScraper, Page } from '../providers/MangaPlugin';
+import { type Chapter, DecoratableMangaScraper, type Manga, Page } from '../providers/MangaPlugin';
 import * as MangaStream from './decorators/WordPressMangaStream';
 import * as Common from './decorators/Common';
 import { FetchRegex } from '../platform/FetchProvider';
 
 @MangaStream.MangaCSS(/^{origin}\/manga\/[^/]+\/$/)
 @MangaStream.MangasSinglePageCSS()
-@MangaStream.ChaptersSinglePageCSS()
 @Common.ImageAjax()
 export default class extends DecoratableMangaScraper {
 
@@ -17,6 +16,11 @@ export default class extends DecoratableMangaScraper {
 
     public override get Icon() {
         return icon;
+    }
+
+    public override async FetchChapters(manga: Manga): Promise<Chapter[]> {
+        // rare MangaStream website using ASC order for chapters
+        return (await MangaStream.FetchChaptersSinglePageCSS.call(this, manga)).reverse();
     }
 
     public override async FetchPages(chapter: Chapter): Promise<Page[]> {
