46b8acbbabe7df8bff21894fd21b106754e40b3c
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-07 17:29:43 +0200

    Port WanpaMan from old hakuneko

---
+++ web/src/engine/websites/WanPaMan_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'wanpaman',
+        title: 'Wan-Pa Man',
+    },
+    container: {
+        url: 'http://galaxyheavyblow.web.fc2.com/',
+        id: '/',
+        title: 'ワンパンマン'
+    },
+    child: {
+        id: '/fc2-imageviewer/?aid=1&iid=162',
+        title: '第１６０話'
+    },
+    entry: {
+        index: 0,
+        size: 136_200,
+        type: 'image/jpeg'
+    }
+}).AssertWebsite();
