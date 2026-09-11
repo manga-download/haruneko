3bc3b6d6f265434501634920d2583abeb6ac60b5
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-11 14:04:32 +0000

    feat: Port MangaToon from Hakuneko (#1825)

---
+++ web/src/engine/websites/MangaToonDE_e2e.ts
@@ -0,0 +1,22 @@
+﻿import { TestFixture } from '../../../test/WebsitesFixture';
+
+new TestFixture({
+    plugin: {
+        id: 'mangatoon-de',
+        title: 'MangaToon (German)',
+    },
+    container: {
+        url: 'https://de.mangatoon.mobi/1725-der-nationale-traummann-umwickelt-mich',
+        id: '1725',
+        title: 'Der nationale Traummann umwickelt mich'
+    },
+    child: {
+        id: '19146',
+        title: 'Ep.1'
+    },
+    entry: {
+        index: 0,
+        size: 22_426,
+        type: 'image/webp'
+    }
+}).AssertWebsite();
