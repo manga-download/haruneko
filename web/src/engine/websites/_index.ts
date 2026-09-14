26ed7c1c322a5e024759edd564c75559a1fa410f
MikeZeDev <MikeZeDev@users.noreply.github.com>
2026-09-14 16:48:44 +0200

    domains updates

--- web/src/engine/websites/_index.ts
+++ web/src/engine/websites/_index.ts
@@ -36,7 +36,6 @@ export { default as AstreaScans } from './AstreaScans';
 export { default as AsuraScans } from './AsuraScans';
 export { default as AsuraScansTR } from './AsuraScansTR';
 export { default as AsuraToons } from './AsuraToons';
-export { default as Atikrost } from './Atikrost';
 export { default as Atsumaru } from './Atsumaru';
 export { default as Aymry } from './Aymry';
 export { default as AzoraWorld } from './AzoraWorld';
